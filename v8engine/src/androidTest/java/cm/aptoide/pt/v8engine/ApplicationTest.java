bf24c13953ee12453e94ee9449a238b07536c7be
Neurophobic Animal <fredericosantinho@gmail.com>
2016-04-12 20:12:06 +0100

    Added Main Modules.

---
+++ v8engine/src/androidTest/java/cm/aptoide/pt/v8engine/ApplicationTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016.
+ * Modified by Neurophobic Animal on 12/04/2016.
+ */
+
+package cm.aptoide.pt.v8engine;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
---
+++ vanilla/src/main/java/cm/aptoide/pt/aptoideclientv8/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016.
+ * Modified by Neurophobic Animal on 12/04/2016.
+ */
+
+package cm.aptoide.pt.aptoideclientv8;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
