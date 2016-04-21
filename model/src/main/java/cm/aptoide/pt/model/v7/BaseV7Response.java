073521d005cec274a8d0d9cab4aaa00536a72114
Neurophobic Animal <FredericoSantinho@gmail.com>
2016-04-21 02:37:42 +0100

    Added GetStoreResponse.java.

---
+++ model/src/main/java/cm/aptoide/pt/model/v7/BaseV7Response.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016.
+ * Modified by Neurophobic Animal on 21/04/2016.
+ */
+
+package cm.aptoide.pt.model.v7;
+
+import lombok.Data;
+
+/**
+ * Created by neuro on 20-04-2016.
+ */
+@Data
+public class BaseV7Response {
+
+	private Info info;
+
+	@Data
+	public static class Info {
+
+		private Status status;
+		private Time time;
+
+		public enum Status {
+			OK, QUEUED, FAIL
+		}
+
+		@Data
+		private static class Time {
+
+			private Number seconds;
+			private String human;
+		}
+	}
+}
