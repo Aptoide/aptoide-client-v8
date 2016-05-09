138d483d3c772af58a88e01cc7043313e202d265
Fábio Dias <fabio.dias@aptoide.com>
2016-05-09 10:13:45 +0100

    added appview activity code to load static app view code

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/activities/AppViewActivity.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/activities/AppViewActivity.java
@@ -9,17 +9,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import cm.aptoide.pt.v8engine.R;
-import cm.aptoide.pt.v8engine.activity.deprecated.AptoideSimpleFragmentActivityDeprecated;
+import cm.aptoide.pt.v8engine.fragment.implementations.AppViewFragment;
 
 /**
  * Created by sithengineer on 04/05/16.
  */
-public class AppViewActivity extends AptoideSimpleFragmentActivityDeprecated {
-
-	@Override
-	protected Fragment createFragment() {
-		return null;
-	}
+public class AppViewActivity extends AptoideSimpleFragmentActivity {
 
 	@Override
 	protected void loadExtras(Bundle extras) {
@@ -27,11 +22,17 @@ public class AppViewActivity extends AptoideSimpleFragmentActivityDeprecated {
 
 	@Override
 	protected int getContentViewId() {
-		return R.layout.activity_app_view;
+		return R.layout.frame_layout;
 	}
 
 	@Override
 	protected String getAnalyticsScreenName() {
 		return null;
+	}
+
+	@Override
+	protected Fragment createFragment() {
+		// FIXME for debug only
+		return AppViewFragment.newInstance(18696152);
 	}
 }
