86a577e69138773cacda5ad5c2a4dbf43e50c817
Fábio Dias <fabio.dias@aptoide.com>
2016-05-10 14:44:27 +0100

    added developer and details widget

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/displayable/implementations/appView/AppViewDeveloperDisplayable.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/displayable/implementations/appView/AppViewDeveloperDisplayable.java
@@ -1,18 +1,28 @@
 /*
  * Copyright (c) 2016.
- * Modified by SithEngineer on 09/05/2016.
+ * Modified by SithEngineer on 10/05/2016.
  */
 
 package cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.appView;
 
+import cm.aptoide.pt.model.v7.GetAppMeta;
 import cm.aptoide.pt.model.v7.Type;
+import cm.aptoide.pt.v8engine.R;
 
 /**
  * Created by sithengineer on 04/05/16.
  */
-public class AppViewDeveloperDisplayable extends AppViewDisplayable<Object> {
+public class AppViewDeveloperDisplayable extends AppViewDisplayable<GetAppMeta.App> {
 
 	public AppViewDeveloperDisplayable() {
+	}
+
+	public AppViewDeveloperDisplayable(GetAppMeta.App app) {
+		super(app);
+	}
+
+	public AppViewDeveloperDisplayable(GetAppMeta.App pojo, boolean fixedPerLineCount) {
+		super(pojo, fixedPerLineCount);
 	}
 
 	@Override
@@ -22,6 +32,6 @@ public class AppViewDeveloperDisplayable extends AppViewDisplayable<Object> {
 
 	@Override
 	public int getViewLayout() {
-		return 0;
+		return R.layout.displayable_app_view_developer;
 	}
 }
