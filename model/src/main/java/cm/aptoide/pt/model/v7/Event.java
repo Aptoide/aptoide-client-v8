104cd6467b66fddd6d37cc8b060cae93fbdc68a7
Neurophobic Animal <fredericosantinho@gmail.com>
2016-06-29 19:10:50 +0100

    Added Top Stores.

--- model/src/main/java/cm/aptoide/pt/model/v7/Event.java
+++ model/src/main/java/cm/aptoide/pt/model/v7/Event.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016.
- * Modified by SithEngineer on 24/06/2016.
+ * Modified by Neurophobic Animal on 29/06/2016.
  */
 
 package cm.aptoide.pt.model.v7;
@@ -27,6 +27,7 @@ public class Event {
 	public enum Name {
 		// Api
 		listApps,
+		listStores,
 		getStore,
 		getStoreWidgets,
 		getReviews,
