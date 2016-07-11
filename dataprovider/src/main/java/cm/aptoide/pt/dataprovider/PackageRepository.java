f70c29450420b078d4e2656fcc68f3de19ef164f
Marcelo Benites <marcelo.benites@aptoide.com>
2016-07-11 11:58:20 +0100

    add memory cache and fallback method to package repository in order to avoid package manager exception

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/PackageRepository.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/PackageRepository.java
@@ -7,29 +7,36 @@ package cm.aptoide.pt.dataprovider;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
 /**
  * Created by marcelobenites on 6/29/16.
  */
-@AllArgsConstructor
 public class PackageRepository {
 
 	private final PackageManager packageManager;
+	private List<PackageInfo> memoryCache;
+
+	public PackageRepository(PackageManager packageManager) {
+		this.packageManager = packageManager;
+	}
 
 	public Observable<String> getLatestInstalledPackages(int count) {
-		return Observable.fromCallable(() -> packageManager.getInstalledPackages(0))
-				.flatMapIterable(items -> items)
-				.toSortedList((packageInfo, packageInfo2) -> (packageInfo.lastUpdateTime < packageInfo2.lastUpdateTime? 1: (packageInfo
-						.lastUpdateTime ==
-						packageInfo2.lastUpdateTime? 0: -1)))
+		return getInstalledPackages().flatMapIterable(items -> items)
+				.toSortedList((packageInfo, packageInfo2) -> (packageInfo.lastUpdateTime < packageInfo2.lastUpdateTime ? 1 : (packageInfo.lastUpdateTime ==
+						packageInfo2.lastUpdateTime ? 0 : -1)))
 				.flatMapIterable(packageInfos -> packageInfos)
 				.take(count)
 				.map(packageInfo -> packageInfo.packageName)
@@ -37,12 +44,62 @@ public class PackageRepository {
 	}
 
 	public Observable<String> getRandomInstalledPackages(int count) {
-		return Observable.fromCallable(() -> packageManager.getInstalledPackages(0))
-				.map(packageInfos -> {Collections.shuffle(packageInfos); return packageInfos;})
-				.flatMapIterable(packageInfos -> packageInfos)
-				.take(count)
-				.map(packageInfo -> packageInfo.packageName)
-				.subscribeOn(Schedulers.io());
+		return getInstalledPackages().map(packageInfos -> {
+			Collections.shuffle(packageInfos);
+			return packageInfos;
+		}).flatMapIterable(packageInfos -> packageInfos).take(count).map(packageInfo -> packageInfo.packageName).subscribeOn(Schedulers.io());
 	}
 
+	@NonNull
+	private Observable<List<PackageInfo>> getInstalledPackages() {
+		return getCachedInstalledPackages()
+				.onErrorResumeNext(getPackageManagerInstalledPackages())
+				.onErrorResumeNext(getAdbInstalledPackages().subscribeOn(Schedulers.io()))
+				.doOnNext(packageInfos -> setCachedInstalledPackages(packageInfos));
+	}
+
+	private void setCachedInstalledPackages(List<PackageInfo> packageInfos) {
+		memoryCache = packageInfos;
+	}
+
+	private Observable<List<PackageInfo>> getCachedInstalledPackages() {
+		return Observable.fromCallable(() -> {
+			if (memoryCache != null) {
+				return memoryCache;
+			}
+			throw new IllegalStateException("No cached packages available!");
+		});
+	}
+
+	private Observable<List<PackageInfo>> getPackageManagerInstalledPackages() {
+		return Observable.fromCallable(() -> packageManager.getInstalledPackages(0));
+	}
+
+	private Observable<List<PackageInfo>> getAdbInstalledPackages() {
+		return Observable.fromCallable(() -> {
+			final List<PackageInfo> result = new ArrayList<>();
+			BufferedReader bufferedReader = null;
+			try {
+				final Process process = Runtime.getRuntime().exec("pm list packages");
+				bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					final String packageName = line.substring(line.indexOf(':') + 1);
+					final PackageInfo packageInfo = packageManager.getPackageInfo(packageName, 0);
+					result.add(packageInfo);
+				}
+				process.waitFor();
+			} catch (PackageManager.NameNotFoundException | InterruptedException | IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				if (bufferedReader != null) {
+					try {
+						bufferedReader.close();
+					} catch (IOException ignored) {
+					}
+				}
+			}
+			return result;
+		});
+	}
 }
