6be15148b157a88bedc8622ff89d78ee7a372281
Trinkes <fabio.costa@aptoide.com>
2016-08-16 15:57:56 +0100

    Notification actions implemented

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/DownloadNotificationActionsActionsInterface.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/DownloadNotificationActionsActionsInterface.java
@@ -1,7 +1,11 @@
 package cm.aptoide.pt.v8engine;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
 import cm.aptoide.pt.downloadmanager.interfaces.DownloadNotificationActionsInterface;
-import cm.aptoide.pt.logger.Logger;
+import cm.aptoide.pt.preferences.Application;
+import cm.aptoide.pt.v8engine.receivers.DeepLinkIntentReceiver;
 
 /**
  * Created by trinkes on 6/28/16.
@@ -12,11 +16,24 @@ public class DownloadNotificationActionsActionsInterface implements DownloadNoti
 
 	@Override
 	public void button1Pressed() {
-		Logger.d(TAG, "button1Pressed() called with: " + "");
+		Intent intent = createDeeplinkingIntent();
+		intent.putExtra(DeepLinkIntentReceiver.DeepLinksTargets.FROM_DOWNLOAD_NOTIFICATION, true);
+		Application.getContext().startActivity(intent);
 	}
 
 	@Override
 	public void notificationPressed(long appId) {
-		Logger.d(TAG, "notificationPressed() called with: " + "appId = [" + appId + "]");
+		Intent intent = createDeeplinkingIntent();
+		intent.putExtra(DeepLinkIntentReceiver.DeepLinksTargets.APP_VIEW_FRAGMENT, true);
+		intent.putExtra(DeepLinkIntentReceiver.DeepLinksKeys.APP_ID_KEY, appId);
+		Application.getContext().startActivity(intent);
+	}
+
+	@NonNull
+	private Intent createDeeplinkingIntent() {
+		Intent intent = new Intent();
+		intent.setClass(Application.getContext(), MainActivityFragment.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+		return intent;
 	}
 }
