2aa78e884831942636d62c59263520e71ae5906c
Fábio Dias <fabio.dias@aptoide.com>
2016-09-28 17:28:11 +0100

    fixed user data showing after login

--- aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/util/UserInfo.java
+++ aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/util/UserInfo.java
@@ -6,8 +6,8 @@ import lombok.Data;
  * Created by trinkes on 5/5/16.
  * <p/>
  * This class is used to send the user information
+ * <li>{@link #userEmail}</li>
  * <li>{@link #userName}</li>
- * <li>{@link #nickName}</li>
  * <li>{@link #queueName}</li>
  * <li>{@link #userAvatar}</li>
  * <li>{@link #userRepo}</li>
@@ -19,11 +19,11 @@ import lombok.Data;
   /**
    * User name it's usually the email address. It has to be unique
    */
-  String userName;
+  String userEmail;
   /**
    * This is the name shown on comments to identify the user.
    */
-  String nickName;
+  String userName;
   /**
    * This identify the client and it's used on web install
    */
