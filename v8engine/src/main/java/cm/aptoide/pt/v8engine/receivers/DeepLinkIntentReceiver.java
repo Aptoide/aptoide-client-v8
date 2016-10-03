351bc4e282cf28feada477bd1c78a363cf58f58d
Trinkes <fabio.costa@aptoide.com>
2016-10-03 16:22:50 +0100

    [DeepLinks/AN-785] - aptoideinstall now split all the "&"

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/receivers/DeepLinkIntentReceiver.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/receivers/DeepLinkIntentReceiver.java
@@ -16,7 +16,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Base64;
-import android.util.Log;
 import cm.aptoide.pt.database.accessors.DeprecatedDatabase;
 import cm.aptoide.pt.dataprovider.model.MinimalAd;
 import cm.aptoide.pt.logger.Logger;
@@ -132,7 +131,7 @@ public class DeepLinkIntentReceiver extends AppCompatActivity {
 
         String json = new String(Base64.decode(param.getBytes(), 0));
 
-        Log.d("AptoideAptWord", json);
+        Logger.d("AptoideAptWord", json);
 
         GetAdsResponse.Ad ad = null;
         try {
@@ -239,7 +238,7 @@ public class DeepLinkIntentReceiver extends AppCompatActivity {
       } else {
         //old version only with app id
         try {
-          long id = Long.parseLong(substring);
+          long id = Long.parseLong(split[0]);
           startFromAppView(id);
           return;
         } catch (NumberFormatException e) {
