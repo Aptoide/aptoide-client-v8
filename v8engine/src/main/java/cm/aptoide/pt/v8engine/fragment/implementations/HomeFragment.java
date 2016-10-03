7997452d3565809743b7f75910db8b4750951851
Trinkes <fabio.costa@aptoide.com>
2016-10-03 17:58:18 +0100

    broadcast receiver is now register on onViewCreated

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/HomeFragment.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/HomeFragment.java
@@ -5,20 +5,22 @@
 
 package cm.aptoide.pt.v8engine.fragment.implementations;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -214,19 +216,9 @@ public class HomeFragment extends StoreFragment implements DrawerFragment {
   //		mNavigationView = null;
   //	}
 
-  @Override public void onAttach(Activity activity) {
-    super.onAttach(activity);
-    receiver = new ChangeTabReceiver();
-    getContext().registerReceiver(receiver, new IntentFilter(ChangeTabReceiver.SET_TAB_EVENT));
-  }
-
   @Override public void onResume() {
     super.onResume();
     setUserDataOnHeader();
-  }
-
-  @Override public void onDetach() {
-    super.onDetach();
   }
 
   @Override public int getContentViewId() {
@@ -296,6 +288,15 @@ public class HomeFragment extends StoreFragment implements DrawerFragment {
         updatesBadge.hide(true);
       }
     }
+  }
+
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+    receiver = new ChangeTabReceiver();
+    getContext().registerReceiver(receiver, new IntentFilter(ChangeTabReceiver.SET_TAB_EVENT));
+    return view;
   }
 
   @Override public void onDestroyView() {
