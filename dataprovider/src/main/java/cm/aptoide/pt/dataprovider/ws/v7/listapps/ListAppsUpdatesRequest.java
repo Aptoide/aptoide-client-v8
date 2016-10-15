1063bf6fcb54c7bef9a01e30c7ae4becf140bf2f
Neurophobic Animal <fredericosantinho@gmail.com>
2016-10-15 03:05:12 +0100

    Removed unused method.

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/listapps/ListAppsUpdatesRequest.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/listapps/ListAppsUpdatesRequest.java
@@ -6,8 +6,6 @@
 package cm.aptoide.pt.dataprovider.ws.v7.listapps;
 
 import android.content.pm.PackageInfo;
-import cm.aptoide.pt.database.accessors.DeprecatedDatabase;
-import cm.aptoide.pt.database.realm.Installed;
 import cm.aptoide.pt.dataprovider.DataProvider;
 import cm.aptoide.pt.dataprovider.repository.IdsRepository;
 import cm.aptoide.pt.dataprovider.ws.BaseBodyDecorator;
@@ -19,13 +17,10 @@ import cm.aptoide.pt.preferences.secure.SecurePreferencesImplementation;
 import cm.aptoide.pt.utils.AptoideUtils;
 import cm.aptoide.pt.utils.CrashReports;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.realm.Realm;
-import io.realm.RealmResults;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import lombok.AllArgsConstructor;
-import lombok.Cleanup;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -71,23 +66,6 @@ import rx.schedulers.Schedulers;
     for (PackageInfo packageInfo : allInstalledApps) {
       apksDatas.add(new ApksData(packageInfo.packageName, packageInfo.versionCode,
           AptoideUtils.AlgorithmU.computeSha1WithColon(packageInfo.signatures[0].toByteArray())));
-    }
-
-    return apksDatas;
-  }
-
-  private static List<ApksData> getInstalledApksDataWithoutExcluded() {
-    LinkedList<ApksData> apksDatas = new LinkedList<>();
-
-    @Cleanup Realm realm = DeprecatedDatabase.get();
-
-    //RealmResults<Update> excludedUpdates = Database.UpdatesQ.getAll(realm, true);
-    RealmResults<Installed> installeds = DeprecatedDatabase.InstalledQ.getAll(realm);
-    for (Installed installed : installeds) {
-      if (!DeprecatedDatabase.UpdatesQ.contains(installed.getPackageName(), true, realm)) {
-        apksDatas.add(new ApksData(installed.getPackageName(), installed.getVersionCode(),
-            installed.getSignature()));
-      }
     }
 
     return apksDatas;
