949a966185fcc4afb7164805daabd6907b266180
Neurophobic Animal <fredericosantinho@gmail.com>
2016-11-02 18:54:02 +0000

    Minor changes.

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v2/aptwords/GetAdsRequest.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v2/aptwords/GetAdsRequest.java
@@ -53,7 +53,7 @@ import rx.Observable;
     this.googlePlayServicesAvailable = googlePlayServicesAvailable;
   }
 
-  private static GetAdsRequest of(Location location, String keyword, Integer limit,
+  public static GetAdsRequest of(Location location, String keyword, Integer limit,
       String aptoideClientUUID, boolean googlePlayServicesAvailable) {
     return new GetAdsRequest(aptoideClientUUID, googlePlayServicesAvailable).setLocation(location)
         .setKeyword(keyword)
