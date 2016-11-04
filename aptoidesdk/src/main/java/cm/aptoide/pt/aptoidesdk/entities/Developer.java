9582a44fa849c625aa8ceba5f869bc92f4ff7e62
Neurophobic Animal <fredericosantinho@gmail.com>
2016-11-04 15:21:30 +0000

    Added Developer.java.

---
+++ aptoidesdk/src/main/java/cm/aptoide/pt/aptoidesdk/entities/Developer.java
@@ -0,0 +1,11 @@
+package cm.aptoide.pt.aptoidesdk.entities;
+
+import lombok.Data;
+
+/**
+ * Created by neuro on 03-11-2016.
+ */
+@Data public class Developer {
+  private String name;
+  private String email;
+}
