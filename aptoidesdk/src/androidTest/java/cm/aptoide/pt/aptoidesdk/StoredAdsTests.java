8caf28ccd756e7df888b86f4b3b7fa2494684a6c
Neurophobic Animal <fredericosantinho@gmail.com>
2016-11-09 17:22:43 +0000

    Added StoredAdsTests.java.

---
+++ aptoidesdk/src/androidTest/java/cm/aptoide/pt/aptoidesdk/StoredAdsTests.java
@@ -0,0 +1,78 @@
+package cm.aptoide.pt.aptoidesdk;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import cm.aptoide.pt.aptoidesdk.ads.Ad;
+import cm.aptoide.pt.aptoidesdk.ads.Aptoide;
+import cm.aptoide.pt.aptoidesdk.ads.StoredAdsManager;
+import cm.aptoide.pt.utils.AptoideUtils;
+import java.util.Collection;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by neuro on 07-11-2016.
+ */
+
+public class StoredAdsTests {
+
+  private Context context;
+  private StoredAdsManager storedAdsManager;
+
+  @Test public void readWriteAds() throws Exception {
+
+    init();
+
+    Aptoide.integrate(context, "dummyoem");
+    List<Ad> ads = Aptoide.getAds(3, false);
+
+    Ad ad1 = ads.get(0);
+    Ad ad2 = ads.get(1);
+
+    Assert.assertEquals(0, storedAdsManager.size());
+    storedAdsManager.addAd(ad1);
+    Assert.assertEquals(1, storedAdsManager.size());
+    storedAdsManager.addAd(ad2);
+    Assert.assertEquals(2, storedAdsManager.size());
+
+    assertAdsPresent(ad1, ad2);
+
+    storedAdsManager.reload();
+    assertAdsPresent(ad1, ad2);
+
+    storedAdsManager.clear();
+    assertAdsCleared();
+  }
+
+  private void init() {
+    context = InstrumentationRegistry.getTargetContext();
+    AptoideUtils.SystemU.clearApplicationData(context);
+
+    storedAdsManager = StoredAdsManager.getInstance(context);
+  }
+
+  private void assertAdsPresent(Ad... ads) {
+    for (Ad ad : ads) {
+      if (!assertAdPresent(ad)) {
+        throw new RuntimeException("Ad " + ad.getName() + " not present in StoredAdsManager!");
+      }
+    }
+  }
+
+  private boolean assertAdPresent(Ad ad) {
+    Collection<Ad> all = storedAdsManager.getAll();
+
+    for (Ad tmp : all) {
+      if (tmp.equals(ad)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private void assertAdsCleared() {
+    Assert.assertEquals(0, storedAdsManager.size());
+  }
+}
