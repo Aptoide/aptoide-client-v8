213f0ee91bb261f15fe083f36c4b109c0f699d5b
Neurophobic Animal <fredericosantinho@gmail.com>
2016-11-17 18:31:31 +0000

    Added file query string.

--- aptoidesdk/src/main/java/cm/aptoide/pt/aptoidesdk/entities/File.java
+++ aptoidesdk/src/main/java/cm/aptoide/pt/aptoidesdk/entities/File.java
@@ -1,5 +1,6 @@
 package cm.aptoide.pt.aptoidesdk.entities;
 
+import cm.aptoide.pt.aptoidesdk.misc.SdkUtils;
 import cm.aptoide.pt.model.v7.GetAppMeta;
 import lombok.Data;
 
@@ -10,13 +11,19 @@ import lombok.Data;
   private final String path;
   private final String alternativePath;
   private final long size;
+  private final String md5sum;
 
   public static File from(GetAppMeta.GetAppMetaFile file) {
 
-    String path = file.getPath();
-    String alternativePath = file.getPathAlt();
+    String path = file.getPath() + getQueryString();
+    String alternativePath = file.getPathAlt() + getQueryString();
     long size = file.getFilesize();
+    String md5sum = file.getMd5sum();
 
-    return new File(path, alternativePath, size);
+    return new File(path, alternativePath, size, md5sum);
+  }
+
+  private static String getQueryString() {
+    return SdkUtils.FileParameters.getDownloadQueryString();
   }
 }
