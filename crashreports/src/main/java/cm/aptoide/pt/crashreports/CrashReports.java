f6d3391f551c80c6bb31c030c3aba8491ab3c9f6
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-09 15:29:24 +0000

    Moved ScreenUtils to V8Engine.

--- crashreports/src/main/java/cm/aptoide/pt/crashreports/CrashReports.java
+++ crashreports/src/main/java/cm/aptoide/pt/crashreports/CrashReports.java
@@ -5,7 +5,6 @@ import cm.aptoide.pt.logger.Logger;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 import io.fabric.sdk.android.Fabric;
-import java.util.ArrayList;
 import lombok.Setter;
 import pt.aptoide.cm.crashreports.BuildConfig;
 
@@ -76,85 +75,5 @@ public class CrashReports {
 
     Crashlytics.log(priority, tag, message);
     Logger.d(TAG, "logPriorityString: " + priority + " , " + tag + " , " + message);
-  }
-
-  /**
-   * this class should have all the utils methods related to crashlytics
-   */
-  public static class ScreenUtils {
-    static final String SCREEN_HISTORY = "SCREEN_HISTORY";
-    static final String NUMBER_OF_SCREENS = "NUMBER_OF_SCREENS";
-    static final String NUMBER_OF_SCREENS_ON_BACK_STACK = "NUMBER_OF_SCREENS_ON_BACK_STACK";
-    static final String LIFE_CYCLE_STATE = "LIFE_CYCLE_STATE";
-
-    private ArrayList<String> history = new ArrayList<>();
-    //arrayList with all screen names history
-    private int MAX_HISTORY = 10;
-    //this var sets the max screens should be added to history
-    private int totalNumberScreens = 0;
-    //This saves the number of screens showed
-    private int numberScreensOnBackStack = 0;
-    //This is the current number of screens on the backstack
-
-    private ScreenUtils() {
-    }
-
-    /**
-     * new instance of ScreenUtils
-     *
-     * @return instance of ScreenUtils
-     */
-    public static ScreenUtils getInstance() {
-      return ScreenUtilsHelper.INSTANCE;
-    }
-
-    /**
-     * this method adds a screen name to the history to be reported to crashReports
-     *
-     * @param screenName screen name that should be reported to crashReports
-     */
-    public void addScreenToHistory(String screenName) {
-      if (history.size() >= MAX_HISTORY) {
-        history.remove(0);
-      }
-      history.add(screenName);
-      CrashReports.logString(SCREEN_HISTORY, history.toString());
-      Logger.d(TAG, "addScreenToHistory: " + history.toString());
-    }
-
-    /**
-     * increment the number of screens, sends the number os total screens and the number of screens
-     * on backstack
-     */
-    public void incrementNumberOfScreens() {
-      totalNumberScreens++;
-      numberScreensOnBackStack++;
-      CrashReports.logString(NUMBER_OF_SCREENS, String.valueOf(totalNumberScreens));
-      CrashReports.logString(NUMBER_OF_SCREENS_ON_BACK_STACK,
-          String.valueOf(numberScreensOnBackStack));
-      CrashReports.logString(LIFE_CYCLE_STATE, LifeCycle.CREATE.toString());
-      Logger.d(TAG, "incrementNumberOfScreens: NOS: "
-          + NUMBER_OF_SCREENS
-          + ", NOSOBS: "
-          + NUMBER_OF_SCREENS_ON_BACK_STACK);
-    }
-
-    /**
-     * decrement the number of screens, sends the number of screens on backstack
-     */
-    public void decrementNumberOfScreens() {
-      numberScreensOnBackStack--;
-      CrashReports.logString(NUMBER_OF_SCREENS_ON_BACK_STACK,
-          String.valueOf(numberScreensOnBackStack));
-      CrashReports.logString(LIFE_CYCLE_STATE, LifeCycle.DESTROY.toString());
-      Logger.d(TAG,
-          "decrementNumberOfScreens: NOSOBS: " + String.valueOf(numberScreensOnBackStack));
-    }
-
-    public enum LifeCycle {CREATE, DESTROY}
-
-    private static class ScreenUtilsHelper {
-      private static final ScreenUtils INSTANCE = new ScreenUtils();
-    }
   }
 }
