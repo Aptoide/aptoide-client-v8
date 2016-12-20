271a213cf4bc5039dd4536b9fc0fbfcdb738338c
trinkes <fabiojscosta1583@gmail.com>
2016-12-20 11:05:06 +0000

    [TimelineStats/AN-933] - Added the follow message at the beginning of the follows fragment

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/GridRecyclerFragmentWithDecorator.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/GridRecyclerFragmentWithDecorator.java
@@ -26,21 +26,42 @@ public abstract class GridRecyclerFragmentWithDecorator<T extends BaseAdapter>
             RecyclerView.State state) {
 
           int offset = 0;
+          int top = 0;
+          int bottom = 0;
+          int left = 0;
+          int right = 0;
           FragmentActivity activity = getActivity();
           if (activity != null) {
             offset = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5,
                 activity.getResources().getDisplayMetrics());
+            top = offset;
+            bottom = offset;
+            left = offset;
+            right = offset;
           }
 
           if (view.getId() == R.id.brick_app_item) {
-            offset /= 2;
+            top = offset / 2;
+            bottom = offset / 2;
+            left = offset / 2;
+            right = offset / 2;
           }
 
           if (view.getId() == R.id.timeline_stats_layout) {
-            offset = 0;
+            top = 0;
+            left = 0;
+            right = 0;
+            bottom = offset;
           }
 
-          outRect.set(offset, offset, offset, offset);
+          if (view.getId() == R.id.message_white_bg) {
+            top = 0;
+            left = offset;
+            right = offset;
+            bottom = offset;
+          }
+
+          outRect.set(left, top, right, bottom);
         }
       };
 
