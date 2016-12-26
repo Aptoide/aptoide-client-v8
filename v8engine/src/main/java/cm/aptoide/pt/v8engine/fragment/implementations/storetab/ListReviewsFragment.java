19ace11fc516002714481af556231b73bbb7a03e
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-26 22:51:53 +0000

    Created ListReviewsFragment.java.

---
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/ListReviewsFragment.java
@@ -0,0 +1,50 @@
+package cm.aptoide.pt.v8engine.fragment.implementations.storetab;
+
+import android.os.Bundle;
+import cm.aptoide.pt.dataprovider.ws.v7.ListFullReviewsRequest;
+import cm.aptoide.pt.model.v7.FullReview;
+import cm.aptoide.pt.model.v7.ListFullReviews;
+import cm.aptoide.pt.v8engine.repository.RepositoryFactory;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.Displayable;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.DisplayableGroup;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.grid.RowReviewDisplayable;
+import cm.aptoide.pt.v8engine.view.recycler.listeners.EndlessRecyclerOnScrollListener;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import rx.functions.Action1;
+
+/**
+ * Created by neuro on 26-12-2016.
+ */
+
+public class ListReviewsFragment extends StoreGridRecyclerFragment {
+
+  @Override public void load(boolean create, boolean refresh, Bundle savedInstanceState) {
+    super.load(create, refresh, savedInstanceState);
+
+    ListFullReviewsRequest listFullReviewsRequest =
+        RepositoryFactory.getRequestRepositoty().getListFullReviews(url, refresh);
+    Action1<ListFullReviews> listFullReviewsAction = (listFullReviews -> {
+      if (listFullReviews != null
+          && listFullReviews.getDatalist() != null
+          && listFullReviews.getDatalist().getList() != null) {
+        List<FullReview> reviews = listFullReviews.getDatalist().getList();
+        LinkedList<Displayable> displayables = new LinkedList<>();
+        for (int i = 0; i < reviews.size(); i++) {
+          FullReview review = reviews.get(i);
+          displayables.add(new RowReviewDisplayable(review));
+        }
+        this.displayables = new ArrayList<>(reviews.size());
+        this.displayables.add(new DisplayableGroup(displayables));
+        addDisplayables(this.displayables);
+      }
+    });
+    recyclerView.clearOnScrollListeners();
+    endlessRecyclerOnScrollListener =
+        new EndlessRecyclerOnScrollListener(this.getAdapter(), listFullReviewsRequest,
+            listFullReviewsAction, errorRequestListener, true);
+    recyclerView.addOnScrollListener(endlessRecyclerOnScrollListener);
+    endlessRecyclerOnScrollListener.onLoadMore(refresh);
+  }
+}
