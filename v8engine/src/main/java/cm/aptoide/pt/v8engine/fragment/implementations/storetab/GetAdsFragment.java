a3113cb962e0a609c6ad5f1cce00a3516ad44def
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-26 22:53:23 +0000

    Created GetAdsFragment.java.

---
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/GetAdsFragment.java
@@ -0,0 +1,29 @@
+package cm.aptoide.pt.v8engine.fragment.implementations.storetab;
+
+import android.os.Bundle;
+import cm.aptoide.pt.database.realm.MinimalAd;
+import cm.aptoide.pt.v8engine.repository.AdsRepository;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.grid.GridAdDisplayable;
+import java.util.LinkedList;
+
+/**
+ * Created by neuro on 26-12-2016.
+ */
+
+public class GetAdsFragment extends StoreGridRecyclerFragment {
+
+  private static final AdsRepository adsRepository = new AdsRepository();
+
+  @Override public void load(boolean create, boolean refresh, Bundle savedInstanceState) {
+    super.load(create, refresh, savedInstanceState);
+
+    adsRepository.getAdsFromHomepageMore().subscribe(minimalAds -> {
+      displayables = new LinkedList<>();
+      for (MinimalAd minimalAd : minimalAds) {
+        displayables.add(new GridAdDisplayable(minimalAd, tag));
+      }
+
+      addDisplayables(displayables);
+    }, e -> finishLoading());
+  }
+}
