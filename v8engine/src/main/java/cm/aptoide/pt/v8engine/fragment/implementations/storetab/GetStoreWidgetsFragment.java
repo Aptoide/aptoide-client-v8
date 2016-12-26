855f5c346f690bbeabdac3d34499413087ec71b2
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-26 22:51:18 +0000

    Created GetStoreWidgetsFragment.java.

---
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/GetStoreWidgetsFragment.java
@@ -0,0 +1,24 @@
+package cm.aptoide.pt.v8engine.fragment.implementations.storetab;
+
+import android.os.Bundle;
+import cm.aptoide.pt.v8engine.repository.RepositoryFactory;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by neuro on 26-12-2016.
+ */
+
+public class GetStoreWidgetsFragment extends StoreGridRecyclerFragment {
+
+  @Override public void load(boolean create, boolean refresh, Bundle savedInstanceState) {
+    super.load(create, refresh, savedInstanceState);
+
+    RepositoryFactory.getRequestRepositoty()
+        .getStoreWidgets(url)
+        .observe(refresh)
+        .observeOn(Schedulers.io())
+        .subscribe(getStoreWidgets -> {
+          loadGetStoreWidgets(getStoreWidgets, refresh);
+        }, this::finishLoading);
+  }
+}
