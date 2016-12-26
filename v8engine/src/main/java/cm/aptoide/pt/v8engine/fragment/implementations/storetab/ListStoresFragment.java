d0581fed1799781e96a620d686200a9fa3e7f730
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-26 22:53:50 +0000

    Created ListStoresFragment.java.

---
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/ListStoresFragment.java
@@ -0,0 +1,48 @@
+package cm.aptoide.pt.v8engine.fragment.implementations.storetab;
+
+import android.os.Bundle;
+import cm.aptoide.pt.dataprovider.ws.v7.store.ListStoresRequest;
+import cm.aptoide.pt.model.v7.store.ListStores;
+import cm.aptoide.pt.model.v7.store.Store;
+import cm.aptoide.pt.v8engine.repository.AdsRepository;
+import cm.aptoide.pt.v8engine.repository.RepositoryFactory;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.grid.GridStoreDisplayable;
+import cm.aptoide.pt.v8engine.view.recycler.listeners.EndlessRecyclerOnScrollListener;
+import java.util.LinkedList;
+import java.util.List;
+import rx.functions.Action1;
+
+/**
+ * Created by neuro on 26-12-2016.
+ */
+
+public class ListStoresFragment extends StoreGridRecyclerFragment {
+
+  private static final AdsRepository adsRepository = new AdsRepository();
+
+  @Override public void load(boolean create, boolean refresh, Bundle savedInstanceState) {
+    super.load(create, refresh, savedInstanceState);
+
+    ListStoresRequest listStoresRequest =
+        RepositoryFactory.getRequestRepositoty().getListStores(url);
+    Action1<ListStores> listStoresAction = listStores -> {
+
+      // Load sub nodes
+      List<Store> list = listStores.getDatalist().getList();
+
+      displayables = new LinkedList<>();
+      for (Store store : list) {
+        displayables.add(new GridStoreDisplayable(store));
+      }
+
+      addDisplayables(displayables);
+    };
+
+    recyclerView.clearOnScrollListeners();
+    endlessRecyclerOnScrollListener =
+        new EndlessRecyclerOnScrollListener(this.getAdapter(), listStoresRequest, listStoresAction,
+            errorRequestListener);
+    recyclerView.addOnScrollListener(endlessRecyclerOnScrollListener);
+    endlessRecyclerOnScrollListener.onLoadMore(refresh);
+  }
+}
