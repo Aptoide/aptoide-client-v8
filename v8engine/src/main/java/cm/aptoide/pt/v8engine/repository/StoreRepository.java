c586d5457c1ed22ee5ab418bb64245fea5ec6eb2
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-26 22:28:05 +0000

    StoreRepository is now always gotten through RepositoryFactory.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/repository/StoreRepository.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/repository/StoreRepository.java
@@ -3,15 +3,18 @@ package cm.aptoide.pt.v8engine.repository;
 import cm.aptoide.pt.database.accessors.StoreAccessor;
 import cm.aptoide.pt.database.realm.Store;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import rx.Observable;
 
 /**
  * Created by sithengineer on 11/10/2016.
  */
 
-@AllArgsConstructor public class StoreRepository implements Repository {
+public class StoreRepository implements Repository {
   private final StoreAccessor storeAccessor;
+
+  public StoreRepository(StoreAccessor storeAccessor) {
+    this.storeAccessor = storeAccessor;
+  }
 
   public Observable<Boolean> isSubscribed(long storeId) {
     return storeAccessor.get(storeId).map(store -> store != null);
