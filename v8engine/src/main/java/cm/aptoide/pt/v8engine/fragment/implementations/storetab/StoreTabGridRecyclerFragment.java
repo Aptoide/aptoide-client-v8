7f8da2347c3c73cacb8fa28ce0c890d4cba0fea4
Neurophobic Animal <fredericosantinho@gmail.com>
2016-12-28 15:03:33 +0000

    Removed corner cases from load method of StoreTabGridRecyclerFragment.java.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/StoreTabGridRecyclerFragment.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/fragment/implementations/storetab/StoreTabGridRecyclerFragment.java
@@ -8,45 +8,18 @@ package cm.aptoide.pt.v8engine.fragment.implementations.storetab;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import cm.aptoide.accountmanager.AptoideAccountManager;
-import cm.aptoide.pt.dataprovider.DataProvider;
-import cm.aptoide.pt.dataprovider.repository.IdsRepositoryImpl;
-import cm.aptoide.pt.dataprovider.util.DataproviderUtils;
-import cm.aptoide.pt.dataprovider.ws.v7.BaseRequestWithStore;
 import cm.aptoide.pt.dataprovider.ws.v7.V7;
-import cm.aptoide.pt.dataprovider.ws.v7.WSWidgetsUtils;
-import cm.aptoide.pt.dataprovider.ws.v7.store.GetMyStoreListRequest;
-import cm.aptoide.pt.dataprovider.ws.v7.store.GetStoreWidgetsRequest;
 import cm.aptoide.pt.model.v7.Event;
-import cm.aptoide.pt.model.v7.GetStoreWidgets;
 import cm.aptoide.pt.model.v7.Layout;
-import cm.aptoide.pt.model.v7.store.ListStores;
-import cm.aptoide.pt.model.v7.store.Store;
-import cm.aptoide.pt.networkclient.interfaces.ErrorRequestListener;
-import cm.aptoide.pt.preferences.secure.SecurePreferencesImplementation;
 import cm.aptoide.pt.v8engine.R;
-import cm.aptoide.pt.v8engine.V8Engine;
 import cm.aptoide.pt.v8engine.fragment.GridRecyclerSwipeFragment;
 import cm.aptoide.pt.v8engine.fragment.implementations.MyStoresFragment;
 import cm.aptoide.pt.v8engine.repository.RepositoryFactory;
 import cm.aptoide.pt.v8engine.repository.StoreRepository;
-import cm.aptoide.pt.v8engine.util.StoreUtils;
 import cm.aptoide.pt.v8engine.util.Translator;
 import cm.aptoide.pt.v8engine.view.recycler.displayable.Displayable;
-import cm.aptoide.pt.v8engine.view.recycler.displayable.DisplayablesFactory;
-import cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.grid.GridStoreDisplayable;
-import cm.aptoide.pt.v8engine.view.recycler.displayable.implementations.grid.RecommendedStoreDisplayable;
-import cm.aptoide.pt.v8engine.view.recycler.listeners.EndlessRecyclerOnScrollListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import rx.Observable;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by neuro on 29-04-2016.
@@ -178,113 +151,13 @@ public abstract class StoreTabGridRecyclerFragment extends GridRecyclerSwipeFrag
       }
 
       // TODO: 28-12-2016 neuro martelo martelo martelo
-      switch (name) {
-        case getStoresRecommended:
-        case getMyStoresSubscribed:
-          caseMyStores(url, refresh);
-          break;
-        default:
-          Observable<List<? extends Displayable>> displayablesObservable =
-              buildDisplayables(refresh, url);
-          if (displayablesObservable != null) {
-            displayablesObservable.compose(bindUntilEvent(LifecycleEvent.DESTROY_VIEW))
-                .subscribe(this::setDisplayables);
-          }
+      Observable<List<? extends Displayable>> displayablesObservable =
+          buildDisplayables(refresh, url);
+      if (displayablesObservable != null) {
+        displayablesObservable.compose(bindUntilEvent(LifecycleEvent.DESTROY_VIEW))
+            .subscribe(this::setDisplayables);
       }
     }
-  }
-
-  /**
-   * @deprecated á espera da ajuda do trinkies :/
-   */
-  @Deprecated private Subscription caseGetStoreWidgets(String url,
-      BaseRequestWithStore.StoreCredentials storeCredentials, boolean refresh) {
-    return GetStoreWidgetsRequest.ofAction(url, storeCredentials,
-        AptoideAccountManager.getAccessToken(),
-        new IdsRepositoryImpl(SecurePreferencesImplementation.getInstance(),
-            DataProvider.getContext()).getAptoideClientUUID())
-        .observe(refresh)
-        .observeOn(Schedulers.io())
-        .subscribe(getStoreWidgets -> {
-
-          // Load sub nodes
-          List<GetStoreWidgets.WSWidget> list = getStoreWidgets.getDatalist().getList();
-          CountDownLatch countDownLatch = new CountDownLatch(list.size());
-
-          String aptoideClientUuid =
-              new IdsRepositoryImpl(SecurePreferencesImplementation.getInstance(),
-                  DataProvider.getContext()).getAptoideClientUUID();
-
-          Observable.from(list)
-              .forEach(wsWidget -> WSWidgetsUtils.loadInnerNodes(wsWidget,
-                  wsWidget.getView() != null ? StoreUtils.getStoreCredentialsFromUrl(
-                      wsWidget.getView()) : new BaseRequestWithStore.StoreCredentials(),
-                  countDownLatch, refresh, throwable -> finishLoading(throwable),
-                  AptoideAccountManager.getAccessToken(), aptoideClientUuid,
-                  DataproviderUtils.AdNetworksUtils.isGooglePlayServicesAvailable(
-                      V8Engine.getContext()), DataProvider.getConfiguration().getPartnerId(),
-                  AptoideAccountManager.isMatureSwitchOn()));
-
-          try {
-            countDownLatch.await(5, TimeUnit.SECONDS);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-
-          List<Displayable> displayables = DisplayablesFactory.parse(getStoreWidgets, storeTheme,
-              RepositoryFactory.getStoreRepository());
-          setDisplayables(displayables);
-        }, throwable -> finishLoading(throwable));
-  }
-
-  /**
-   * @deprecated á espera da ajuda do trinkies :/
-   */
-  @Deprecated private void caseMyStores(String url, boolean refresh) {
-    StoreRepository storeRepository = RepositoryFactory.getStoreRepository();
-    GetMyStoreListRequest request =
-        GetMyStoreListRequest.of(url, AptoideAccountManager.getAccessToken(),
-            new IdsRepositoryImpl(SecurePreferencesImplementation.getInstance(),
-                DataProvider.getContext()).getAptoideClientUUID(), true);
-
-    Action1<ListStores> listStoresAction =
-        listStores -> addDisplayables(getStoresDisplayable(listStores.getDatalist().getList()));
-
-    ErrorRequestListener errorListener = (throwable) -> {
-      recyclerView.clearOnScrollListeners();
-      LinkedList<String> errorsList = new LinkedList<>();
-      errorsList.add(WSWidgetsUtils.USER_NOT_LOGGED_ERROR);
-      if (WSWidgetsUtils.shouldAddObjectView(errorsList, throwable)) {
-        DisplayablesFactory.loadLocalSubscribedStores(storeRepository)
-            .compose(bindUntilEvent(LifecycleEvent.DESTROY_VIEW))
-            .subscribe(stores -> addDisplayables(getStoresDisplayable(stores)));
-      } else {
-        finishLoading(throwable);
-      }
-    };
-
-    recyclerView.clearOnScrollListeners();
-    EndlessRecyclerOnScrollListener endlessRecyclerOnScrollListener =
-        new EndlessRecyclerOnScrollListener(this.getAdapter(), request, listStoresAction,
-            errorListener, refresh);
-
-    recyclerView.addOnScrollListener(endlessRecyclerOnScrollListener);
-    endlessRecyclerOnScrollListener.onLoadMore(refresh);
-  }
-
-  @NonNull private ArrayList<Displayable> getStoresDisplayable(List<Store> list) {
-    ArrayList<Displayable> storesDisplayables = new ArrayList<>(list.size());
-    Collections.sort(list, (store, t1) -> store.getName().compareTo(t1.getName()));
-    for (int i = 0; i < list.size(); i++) {
-      if (i == 0 || list.get(i - 1).getId() != list.get(i).getId()) {
-        if (layout == Layout.LIST) {
-          storesDisplayables.add(new RecommendedStoreDisplayable(list.get(i), storeRepository));
-        } else {
-          storesDisplayables.add(new GridStoreDisplayable(list.get(i)));
-        }
-      }
-    }
-    return storesDisplayables;
   }
 
   @Override public int getContentViewId() {
