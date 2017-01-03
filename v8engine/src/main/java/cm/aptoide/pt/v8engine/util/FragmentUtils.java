6704cdd9e27eb02689be6b597991f17249345a9f
Neurophobic Animal <fredericosantinho@gmail.com>
2017-01-03 17:49:46 +0000

    Fixed bad commit.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/util/FragmentUtils.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/util/FragmentUtils.java
@@ -5,11 +5,8 @@
 
 package cm.aptoide.pt.v8engine.util;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import cm.aptoide.pt.v8engine.R;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,54 +20,60 @@ public final class FragmentUtils {
 
   private static final AtomicInteger atomicInt = new AtomicInteger(0);
 
-  public static void replaceFragment(@NonNull Activity activity, @NonNull Fragment fragment,
-      String tag) {
+  public static void replaceFragment(@NonNull android.app.Activity activity,
+      @NonNull android.app.Fragment fragment, String tag) {
 
     activity.getFragmentManager().beginTransaction()
         //				.setCustomAnimations(ENTER_ANIMATION, EXIT_ANIMATION, ENTER_ANIMATION, EXIT_ANIMATION)
         .addToBackStack(tag).replace(R.id.fragment_placeholder, fragment, tag).commit();
   }
 
-  public static void replaceFragment(@NonNull Activity activity, @NonNull Fragment fragment) {
+  public static void replaceFragment(@NonNull android.app.Activity activity,
+      @NonNull android.app.Fragment fragment) {
     replaceFragment(activity, fragment,
         fragment.getClass().getSimpleName() + "_" + atomicInt.incrementAndGet());
   }
 
-  public static Fragment getFirstFragment(@NonNull Activity activity) {
-    FragmentManager.BackStackEntry backStackEntry =
+  public static android.app.Fragment getFirstFragment(@NonNull android.app.Activity activity) {
+    android.app.FragmentManager.BackStackEntry backStackEntry =
         activity.getFragmentManager().getBackStackEntryAt(0);
     return activity.getFragmentManager().findFragmentByTag(backStackEntry.getName());
   }
 
-  public static Fragment getLastFragment(@NonNull Activity activity) {
+  public static android.app.Fragment getLastFragment(@NonNull android.app.Activity activity) {
     FragmentManager fragmentManager = activity.getFragmentManager();
-    FragmentManager.BackStackEntry backStackEntry =
+    android.app.FragmentManager.BackStackEntry backStackEntry =
         fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1);
     return activity.getFragmentManager().findFragmentByTag(backStackEntry.getName());
   }
 
-  public static void replaceFragmentV4(@NonNull FragmentActivity fragmentActivity,
-      @NonNull Fragment fragment, String tag) {
+  public static void replaceFragmentV4(
+      @NonNull android.support.v4.app.FragmentActivity fragmentActivity,
+      @NonNull android.support.v4.app.Fragment fragment, String tag) {
     fragmentActivity.getSupportFragmentManager()
         .beginTransaction()
         .setCustomAnimations(ENTER_ANIMATION, EXIT_ANIMATION, ENTER_ANIMATION, EXIT_ANIMATION)
         .addToBackStack(tag)
-        .replace(R.id.fragment_placeholder, fragment, tag).commitAllowingStateLoss();
+        .replace(R.id.fragment_placeholder, fragment, tag)
+        .commitAllowingStateLoss();
   }
 
-  public static void replaceFragmentV4(@NonNull FragmentActivity fragmentActivity,
-      @NonNull Fragment fragment) {
+  public static void replaceFragmentV4(
+      @NonNull android.support.v4.app.FragmentActivity fragmentActivity,
+      @NonNull android.support.v4.app.Fragment fragment) {
     replaceFragmentV4(fragmentActivity, fragment,
         fragment.getClass().getSimpleName() + "_" + atomicInt.incrementAndGet());
   }
 
-  public static Fragment getFirstFragmentV4(@NonNull FragmentActivity fragmentActivity) {
+  public static android.support.v4.app.Fragment getFirstFragmentV4(
+      @NonNull android.support.v4.app.FragmentActivity fragmentActivity) {
     android.support.v4.app.FragmentManager.BackStackEntry backStackEntry =
         fragmentActivity.getSupportFragmentManager().getBackStackEntryAt(0);
     return fragmentActivity.getSupportFragmentManager().findFragmentByTag(backStackEntry.getName());
   }
 
-  public static Fragment getLastFragmentV4(@NonNull FragmentActivity fragmentActivity) {
+  public static android.support.v4.app.Fragment getLastFragmentV4(
+      @NonNull android.support.v4.app.FragmentActivity fragmentActivity) {
     android.support.v4.app.FragmentManager fragmentManager =
         fragmentActivity.getSupportFragmentManager();
     android.support.v4.app.FragmentManager.BackStackEntry backStackEntry =
