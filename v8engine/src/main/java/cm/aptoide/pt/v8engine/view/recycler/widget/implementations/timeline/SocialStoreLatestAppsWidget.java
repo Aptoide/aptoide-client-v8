0613462b9e6172c81fd33dc8791fa5411008abde
jdandrade <joao.andrade@aptoide.com>
2017-01-23 15:59:35 +0000

    [AN-1158] Bug fix: Social Store card - Shared store icon is now clickable, goes into store view.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/implementations/timeline/SocialStoreLatestAppsWidget.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/implementations/timeline/SocialStoreLatestAppsWidget.java
@@ -169,6 +169,22 @@ public class SocialStoreLatestAppsWidget
           V8Engine.getFragmentProvider().newStoreFragment(displayable.getStoreName()));
     }));
 
+    compositeSubscription.add(RxView.clicks(sharedStoreAvatar).subscribe(click -> {
+      knockWithSixpackCredentials(displayable.getAbTestingUrl());
+      Analytics.AppsTimeline.clickOnCard(getCardTypeName(), Analytics.AppsTimeline.BLANK,
+          Analytics.AppsTimeline.BLANK, displayable.getSharedStore().getName(),
+          Analytics.AppsTimeline.OPEN_STORE);
+      displayable.sendClickEvent(SendEventRequest.Body.Data.builder()
+          .cardType(getCardTypeName())
+          .source(TimelineClickEvent.SOURCE_APTOIDE)
+          .specific(
+              SendEventRequest.Body.Specific.builder().store(displayable.getSharedStore().getName()).build())
+          .build(), TimelineClickEvent.OPEN_STORE);
+      ((FragmentShower) getContext()).pushFragmentV4(
+          V8Engine.getFragmentProvider().newStoreFragment(displayable.getSharedStore().getName()));
+    }));
+
+
     StoreThemeEnum storeThemeEnum = StoreThemeEnum.get(displayable.getSharedStore());
 
     followStore.setBackgroundDrawable(storeThemeEnum.getButtonLayoutDrawable());
