3f8e3ed57e0ab948a3d5eb95a7a685cbb5355ca2
Ana Lara <ana.simoes@aptoide.com>
2017-02-01 18:01:56 +0000

    reformat crash reports

--- crashreports/src/main/java/cm/aptoide/pt/crashreports/CrashReport.java
+++ crashreports/src/main/java/cm/aptoide/pt/crashreports/CrashReport.java
@@ -12,10 +12,8 @@ public class CrashReport implements CrashLogger {
   //
 
   private static CrashReport singleton = new CrashReport();
-
-  public static CrashReport getInstance() {
-    return singleton;
-  }
+  // TODO: 12/1/2017 sithengineer should we protect this list from concurrent modifications?
+  private ArrayList<CrashLogger> crashLoggers;
 
   private CrashReport() {
     // TODO: 12/1/2017 sithengineer is lazy initialization necessary here?
@@ -26,16 +24,13 @@ public class CrashReport implements CrashLogger {
   // CrashReporter methods
   //
 
-  // TODO: 12/1/2017 sithengineer should we protect this list from concurrent modifications?
-  private ArrayList<CrashLogger> crashLoggers;
+  public static CrashReport getInstance() {
+    return singleton;
+  }
 
   public CrashReport addLogger(CrashLogger crashLogger) {
     crashLoggers.add(crashLogger);
     return this;
-  }
-
-  private boolean isInitialized() {
-    return crashLoggers != null && !crashLoggers.isEmpty();
   }
 
   @Override public void log(Throwable throwable) {
@@ -47,6 +42,10 @@ public class CrashReport implements CrashLogger {
     for (int i = 0; i < crashLoggers.size(); i++) {
       crashLoggers.get(i).log(throwable);
     }
+  }
+
+  private boolean isInitialized() {
+    return crashLoggers != null && !crashLoggers.isEmpty();
   }
 
   @Override public void log(String key, String value) {
