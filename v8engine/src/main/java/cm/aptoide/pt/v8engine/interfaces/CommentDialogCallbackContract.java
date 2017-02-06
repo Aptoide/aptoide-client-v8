6bcf85f39bdd3027801a29b186abaa3bbdb69f71
jdandrade <joao.andrade@aptoide.com>
2017-02-06 10:59:51 +0000

    [AN-1183] Reverting commits done so far 490f1cd85a816304d2140eba666ee1f25f9a515c 0daf57687ddf0e57c42416086c8c3c7cc543fc19

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/interfaces/CommentDialogCallbackContract.java
+++
@@ -1,8 +0,0 @@
-package cm.aptoide.pt.v8engine.interfaces;
-
-/**
- * Created by jdandrade on 03/02/2017.
- */
-public interface CommentDialogCallbackContract {
-  void okSelected(String text, long idAsLong, Long previousCommentId, String inputText);
-}
