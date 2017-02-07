3b5f323db56453a0f09d31844cf1c03de2a27502
Marcelo Benites <marcelo.benites@aptoide.com>
2017-02-07 12:10:04 +0000

    emit connection status in google play connection observable

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/presenter/GoogleLoginPresenter.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/presenter/GoogleLoginPresenter.java
@@ -30,7 +30,7 @@ public class GoogleLoginPresenter implements Presenter {
   @Override public void present() {
 
     view.getLifecycle()
-        .filter(event -> event.equals(View.LifecycleEvent.CREATE))
+        .filter(event -> event.equals(View.LifecycleEvent.RESUME))
         .flatMap(created -> playServicesConnection.isAvailable().toObservable())
         .doOnNext(available -> {
           if (available) {
@@ -40,17 +40,26 @@ public class GoogleLoginPresenter implements Presenter {
           }
         })
         .filter(available -> available)
-        .flatMap(available -> view.onGoogleLoginSelection()
-            .flatMap(selected -> playServicesConnection.connect()
-                .doOnCompleted(() -> view.showSuccess())
-                .doOnError(throwable -> showError(throwable))
-                .onErrorComplete()
-                .toObservable()))
+        .flatMap(available -> googleLoginSelection())
         .compose(view.bindUntilEvent(View.LifecycleEvent.DESTROY))
         .subscribe();
   }
 
-  private void showError(Throwable throwable) {
+  private Observable<GooglePlayServicesConnection.Status> googleLoginSelection() {
+    return view.onGoogleLoginSelection()
+        .flatMap(selected -> connectOrShowError())
+        .retry()
+        .compose(view.bindUntilEvent(View.LifecycleEvent.PAUSE));
+  }
+
+  private Observable<GooglePlayServicesConnection.Status> connectOrShowError() {
+    return playServicesConnection.connect()
+        .filter(status -> status.equals(GooglePlayServicesConnection.Status.CONNECTED))
+        .doOnNext(status -> view.showSuccess())
+        .doOnError(throwable -> showErrorOrResolution(throwable));
+  }
+
+  private void showErrorOrResolution(Throwable throwable) {
     if (throwable instanceof GooglePlayServicesException) {
       if (((GooglePlayServicesException) throwable).isResolvable()) {
         view.showResolution(((GooglePlayServicesException) throwable).getErrorCode());
