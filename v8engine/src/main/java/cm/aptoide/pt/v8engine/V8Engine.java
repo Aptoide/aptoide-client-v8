e095baa37e8e95f74ebb7b93e8ec292f7fc4c159
trinkes <fabiojscosta1583@gmail.com>
2017-02-13 16:40:26 +0000

    [Settings/AN-1165] - Section "Root", subsection "allow root install", => default value for root phones = true

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/V8Engine.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/V8Engine.java
@@ -213,6 +213,7 @@ public abstract class V8Engine extends DataProvider {
       }, e -> {
         CrashReport.getInstance().log(e);
       });
+      ManagerPreferences.setAllowRootInstallation(AptoideUtils.SystemU.isRooted());
     } else {
       loadInstalledApps().subscribe();
     }
