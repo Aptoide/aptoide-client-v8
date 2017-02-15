469e31fccb45ad7ef130150967c55c3bada60586
Neurophobic Animal <fredericosantinho@gmail.com>
2017-02-15 02:27:26 +0000

    Added timeout dependency to AptoideServerSocket.java.

--- shareapps/src/main/java/cm/aptoide/pt/shareapps/socket/example/ExampleUsageMultiThread.java
+++ shareapps/src/main/java/cm/aptoide/pt/shareapps/socket/example/ExampleUsageMultiThread.java
@@ -30,8 +30,6 @@ public class ExampleUsageMultiThread {
 
     Thread.sleep(1000);
 
-    Thread.sleep(1000);
-
     for (int i = 0; i < 2; i++) {
       newAptoideMessageClientSocket().startAsync();
     }
