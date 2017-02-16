fd182d08139aca6f7952f12efd331a30e7755252
Pedro Ribeiro <ribasss@gmail.com>
2017-02-16 14:22:41 +0000

    [BUG] - Fixed usage of wrong getPath method.

--- aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/CreateStoreActivity.java
+++ aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/CreateStoreActivity.java
@@ -665,7 +665,7 @@ public class CreateStoreActivity extends PermissionsBaseActivity
       storeAvatarPath = fileUtils.getPathAlt(avatarUrl, getApplicationContext());
     } else if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
       avatarUrl = data.getData();
-      storeAvatarPath = fileUtils.getPath(avatarUrl, getApplicationContext());
+      storeAvatarPath = fileUtils.getPathAlt(avatarUrl, getApplicationContext());
     }
     checkAvatarRequirements(storeAvatarPath, avatarUrl);
   }
