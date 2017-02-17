06a3450dce9bba0046ef0b3bdcac5922b97086c7
trinkes <fabiojscosta1583@gmail.com>
2017-02-17 17:48:26 +0000

    [Store/AN-1147] - store with influencer channel and without description has blank space where the description should be

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/implementations/grid/GridStoreMetaWidget.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/implementations/grid/GridStoreMetaWidget.java
@@ -148,7 +148,13 @@ public class GridStoreMetaWidget extends MetaStoresBaseWidget<GridStoreMetaDispl
     editStoreButton.setTextColor(color);
 
     name.setText(store.getName());
-    description.setText(store.getAppearance().getDescription());
+    String descriptionText = store.getAppearance().getDescription();
+    if (TextUtils.isEmpty(descriptionText)) {
+      description.setVisibility(View.GONE);
+    } else {
+      description.setText(descriptionText);
+      description.setVisibility(View.VISIBLE);
+    }
     appsCount.setText(
         NumberFormat.getNumberInstance(Locale.getDefault()).format(store.getStats().getApps()));
     downloadsCount.setText(AptoideUtils.StringU.withSuffix(store.getStats().getDownloads()));
