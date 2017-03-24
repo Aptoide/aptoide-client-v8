6892acf5954e3295d340d8f248ec31a45579eea4
Marcelo Benites <marcelo.benites@aptoide.com>
2017-03-24 10:44:48 +0000

    create unit tests for credentials validator

--- aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/CredentialsValidator.java
+++ aptoide_accountmanager/src/main/java/cm/aptoide/accountmanager/CredentialsValidator.java
@@ -1,19 +1,18 @@
 package cm.aptoide.accountmanager;
 
-import android.text.TextUtils;
 import rx.Completable;
 
 public class CredentialsValidator {
 
   public Completable validate(String email, String password, boolean validatePassword) {
     return Completable.defer(() -> {
-      if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+      if (isEmpty(email) && isEmpty(password)) {
         return Completable.error(
             new AccountValidationException(AccountValidationException.EMPTY_EMAIL_AND_PASSWORD));
-      } else if (TextUtils.isEmpty(password)) {
+      } else if (isEmpty(password)) {
         return Completable.error(
             new AccountValidationException(AccountValidationException.EMPTY_PASSWORD));
-      } else if (TextUtils.isEmpty(email)) {
+      } else if (isEmpty(email)) {
         return Completable.error(
             new AccountValidationException(AccountValidationException.EMPTY_EMAIL));
       } else if (validatePassword && (password.length() < 8 || !has1number1letter(password))) {
@@ -47,5 +46,13 @@ public class CredentialsValidator {
     }
 
     return hasNumber && hasLetter;
+  }
+
+  private boolean isEmpty(CharSequence str) {
+    if (str == null || str.length() == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
