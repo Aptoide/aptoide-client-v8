17928af279d5e6231d51205218dc6f9248002418
Ana Lara <ana.simoes@aptoide.com>
2017-03-27 12:35:29 +0100

    [AN-1444] - home cache - list apps does not add store_id neither store_name

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/ListAppsRequest.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/ListAppsRequest.java
@@ -21,20 +21,20 @@ import rx.Observable;
  * Created by neuro on 27-04-2016.
  */
 @Data @EqualsAndHashCode(callSuper = true) public class ListAppsRequest
-    extends BaseRequestWithStore<ListApps, ListAppsRequest.Body> {
+    extends V7<ListApps, ListAppsRequest.Body> {
 
   private static final int LINES_PER_REQUEST = 6;
   private String url;
 
   private ListAppsRequest(String url, Body body, BodyInterceptor bodyInterceptor) {
-    super(body,
+    super(body, BASE_HOST,
         OkHttpClientFactory.getSingletonClient(() -> SecurePreferences.getUserAgent(), false),
         WebService.getDefaultConverter(), bodyInterceptor);
     this.url = url;
   }
 
-  public static ListAppsRequest ofAction(String url, StoreCredentials storeCredentials,
-      BodyInterceptor bodyInterceptor) {
+  public static ListAppsRequest ofAction(String url,
+      BaseRequestWithStore.StoreCredentials storeCredentials, BodyInterceptor bodyInterceptor) {
     V7Url listAppsV7Url = new V7Url(url).remove("listApps");
     if (listAppsV7Url.containsLimit()) {
       return new ListAppsRequest(listAppsV7Url.get(), new Body(storeCredentials), bodyInterceptor);
@@ -50,21 +50,28 @@ import rx.Observable;
     return interfaces.listApps(url != null ? url : "", body, bypassCache);
   }
 
-  @EqualsAndHashCode(callSuper = true) public static class Body extends BaseBodyWithStore
+  @EqualsAndHashCode(callSuper = true) public static class Body extends BaseBody
       implements Endless {
 
+    @Getter private String storeUser;
+    @Getter private String storePassSha1;
     @Getter private Integer limit;
     @Getter @Setter private int offset;
     @Getter @Setter private Integer groupId;
     @Getter private String notApkTags;
 
-    public Body(StoreCredentials storeCredentials) {
-      super(storeCredentials);
+    public Body(BaseRequestWithStore.StoreCredentials storeCredentials) {
+      super();
+
+      this.storeUser = storeCredentials.getUsername();
+      this.storePassSha1 = storeCredentials.getPasswordSha1();
       setNotApkTags();
     }
 
-    public Body(StoreCredentials storeCredentials, int limit) {
-      super(storeCredentials);
+    public Body(BaseRequestWithStore.StoreCredentials storeCredentials, int limit) {
+      super();
+      this.storeUser = storeCredentials.getUsername();
+      this.storePassSha1 = storeCredentials.getPasswordSha1();
       this.limit = limit;
       setNotApkTags();
     }
