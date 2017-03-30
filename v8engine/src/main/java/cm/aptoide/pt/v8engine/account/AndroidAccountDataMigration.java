011643f6149145f8a69cedcae21893215dd6c983
Marcelo Benites <marcelo.benites@aptoide.com>
2017-03-30 11:07:21 +0100

    move logic to obtain old version to from V8 engine to account migration class

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/account/AndroidAccountDataMigration.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/account/AndroidAccountDataMigration.java
@@ -3,10 +3,12 @@ package cm.aptoide.pt.v8engine.account;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.text.TextUtils;
 import android.util.Log;
 import cm.aptoide.pt.preferences.secure.SecureCoderDecoder;
 import cm.aptoide.pt.v8engine.V8Engine;
+import cm.aptoide.pt.v8engine.deprecated.SQLiteDatabaseHelper;
 import rx.Completable;
 
 /**
@@ -29,17 +31,19 @@ public class AndroidAccountDataMigration {
   private final AccountManager accountManager;
   private final SecureCoderDecoder secureCoderDecoder;
   private final int currentVersion;
+  private String databasePath;
+
   private int oldVersion;
 
   public AndroidAccountDataMigration(SharedPreferences secureSharedPreferences,
       SharedPreferences defaultSharedPreferences, AccountManager accountManager,
-      SecureCoderDecoder secureCoderDecoder, int oldVersion, int currentVersion) {
+      SecureCoderDecoder secureCoderDecoder, int currentVersion, String databasePath) {
     this.secureSharedPreferences = secureSharedPreferences;
     this.defaultSharedPreferences = defaultSharedPreferences;
     this.accountManager = accountManager;
     this.secureCoderDecoder = secureCoderDecoder;
-    this.oldVersion = oldVersion;
     this.currentVersion = currentVersion;
+    this.databasePath = databasePath;
   }
 
   Completable migrate() {
@@ -54,6 +58,8 @@ public class AndroidAccountDataMigration {
         if (isMigrated()) {
           return Completable.complete();
         }
+
+        generateOldVersion();
 
         Log.i(TAG, String.format("Migrating from version %d to %d", oldVersion, currentVersion));
 
@@ -223,5 +229,21 @@ public class AndroidAccountDataMigration {
       }
     }
     return false;
+  }
+
+  private void generateOldVersion() {
+    int oldVersion = SQLiteDatabaseHelper.DATABASE_VERSION;
+    try {
+      SQLiteDatabase db =
+          SQLiteDatabase.openDatabase(databasePath, null, SQLiteDatabase.OPEN_READONLY, null);
+      oldVersion = db.getVersion();
+      if (db.isOpen()) {
+        db.close();
+      }
+    } catch (Exception ex) {
+      // db does not exist. it's a fresh install
+    }
+    ;
+    this.oldVersion = oldVersion;
   }
 }
