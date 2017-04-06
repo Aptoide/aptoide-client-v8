53ac991b83e89ec34807813943c47f35a9c6264d
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-04-06 15:01:56 +0100

    Added methods to flag and remove ghosts

--- spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/GroupValidator.java
+++ spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/GroupValidator.java
@@ -2,6 +2,7 @@ package cm.aptoide.pt.spotandshareandroid;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by filipe on 31-03-2017.
@@ -21,24 +22,40 @@ public class GroupValidator {
     return false;
   }
 
-  public ArrayList<Group> removeGhosts(ArrayList<Group> groupsList) {
+  public ArrayList<Group> flagGhosts(ArrayList<Group> groupsList) {
     ghostsClearHashmap = new HashMap<>();
+    List<Group> ruleOneGroups = new ArrayList<>();
     for (int i = 0; i < groupsList.size(); i++) {
       String groupDeviceID = groupsList.get(i).getDeviceID();
       String hotspotCounter = groupsList.get(i).getHotspotControlCounter();
 
-      if (!groupDeviceID.equals("")) {//to avoid rule 1
+      if (!groupDeviceID.equals("")) {//to avoid rule 1 - default groupDeviceID = ""
         if (!ghostsClearHashmap.containsKey(groupDeviceID)) {
           ghostsClearHashmap.put(groupDeviceID, groupsList.get(i));
+          groupsList.get(i).setGhostFlag(false);
         } else if ((int) hotspotCounter.charAt(0) > (int) ghostsClearHashmap.get(groupDeviceID)
             .getHotspotControlCounter()
             .charAt(0)) {
           ghostsClearHashmap.put(groupDeviceID, groupsList.get(i));
+          groupsList.get(i).setGhostFlag(true);
         }
+      } else {
+        ruleOneGroups.add(groupsList.get(i));
       }
     }
 
     ArrayList<Group> list = new ArrayList<Group>(ghostsClearHashmap.values());
+    list.addAll(ruleOneGroups);
     return list;
+  }
+
+  public ArrayList<Group> removeGhosts(ArrayList<Group> clientsList) {
+    ArrayList<Group> clearedList = new ArrayList<>();
+    for (int i = 0; i < clientsList.size(); i++) {
+      if (!clientsList.get(i).isGhost()) {
+        clearedList.add(clientsList.get(i));
+      }
+    }
+    return clearedList;
   }
 }
