3a4cb791acd8776a529cd1f38fc49229637782a5
trinkes <fabiojscosta1583@gmail.com>
2017-04-21 11:10:14 +0100

    [Root install/AN-1533] - Install event now has metrics that show if phone has root or not and if root install is enable on settings

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/AptoideAnalytics/events/InstallEvent.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/AptoideAnalytics/events/InstallEvent.java
@@ -2,6 +2,7 @@ package cm.aptoide.pt.v8engine.analytics.AptoideAnalytics.events;
 
 import cm.aptoide.pt.dataprovider.ws.v7.BaseBody;
 import cm.aptoide.pt.dataprovider.ws.v7.BodyInterceptor;
+import lombok.Setter;
 
 /**
  * Created by trinkes on 05/01/2017.
@@ -9,6 +10,8 @@ import cm.aptoide.pt.dataprovider.ws.v7.BodyInterceptor;
 
 public class InstallEvent extends DownloadInstallBaseEvent {
   private static final String EVENT_NAME = "INSTALL";
+  @Setter private boolean aptoideSettings;
+  @Setter private boolean isPhoneRooted;
 
   public InstallEvent(Action action, Origin origin, String packageName, String url, String obbUrl,
       String patchObbUrl, AppContext context, int versionCode,
@@ -16,5 +19,13 @@ public class InstallEvent extends DownloadInstallBaseEvent {
       BodyInterceptor<BaseBody> bodyInterceptor) {
     super(action, origin, packageName, url, obbUrl, patchObbUrl, context, versionCode,
         downloadInstallEventConverter, EVENT_NAME, bodyInterceptor);
+  }
+
+  public boolean getAptoideSettings() {
+    return aptoideSettings;
+  }
+
+  public boolean getIsPhoneRooted() {
+    return isPhoneRooted;
   }
 }
--- v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/AptoideAnalytics/events/InstallEventConverter.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/AptoideAnalytics/events/InstallEventConverter.java
@@ -3,6 +3,8 @@ package cm.aptoide.pt.v8engine.analytics.AptoideAnalytics.events;
 import cm.aptoide.pt.dataprovider.ws.v7.BaseBody;
 import cm.aptoide.pt.dataprovider.ws.v7.BodyInterceptor;
 import cm.aptoide.pt.dataprovider.ws.v7.analyticsbody.DownloadInstallAnalyticsBaseBody;
+import cm.aptoide.pt.preferences.managed.ManagerPreferences;
+import cm.aptoide.pt.utils.AptoideUtils;
 
 /**
  * Created by trinkes on 05/01/2017.
@@ -19,13 +21,21 @@ public class InstallEventConverter extends DownloadInstallEventConverter<Install
   @Override
   protected DownloadInstallAnalyticsBaseBody.Data convertSpecificFields(InstallEvent report,
       DownloadInstallAnalyticsBaseBody.Data data) {
+    DownloadInstallAnalyticsBaseBody.Root root = new DownloadInstallAnalyticsBaseBody.Root();
+    root.setAptoideSettings(report.getAptoideSettings());
+    root.setPhone(report.getIsPhoneRooted());
+    data.setRoot(root);
     return data;
   }
 
   @Override protected InstallEvent createEventObject(DownloadInstallBaseEvent.Action action,
       DownloadInstallBaseEvent.Origin origin, String packageName, String url, String obbUrl,
       String patchObbUrl, DownloadInstallBaseEvent.AppContext context, int versionCode) {
-    return new InstallEvent(action, origin, packageName, url, obbUrl, patchObbUrl, context,
-        versionCode, this, bodyInterceptor);
+    InstallEvent installEvent =
+        new InstallEvent(action, origin, packageName, url, obbUrl, patchObbUrl, context,
+            versionCode, this, bodyInterceptor);
+    installEvent.setAptoideSettings(ManagerPreferences.allowRootInstallation());
+    installEvent.setPhoneRooted(AptoideUtils.SystemU.isRooted());
+    return installEvent;
   }
 }
