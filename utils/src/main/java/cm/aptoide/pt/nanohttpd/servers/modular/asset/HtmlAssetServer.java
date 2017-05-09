3cd5894bde8242c41a75473800039ba92e050800
Neurophobic Animal <fredericosantinho@gmail.com>
2017-05-09 14:33:58 +0100

    [AN-1527] - Added base asset modules.

---
+++ utils/src/main/java/cm/aptoide/pt/nanohttpd/servers/modular/asset/HtmlAssetServer.java
@@ -0,0 +1,22 @@
+package cm.aptoide.pt.nanohttpd.servers.modular.asset;
+
+import fi.iki.elonen.NanoHTTPD;
+
+import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
+
+/**
+ * Created by neuro on 08-05-2017.
+ */
+public class HtmlAssetServer extends AbstractAssetServer {
+
+  private final String htmlPath;
+
+  public HtmlAssetServer(String accepts, String htmlPath) {
+    super(accepts);
+    this.htmlPath = htmlPath;
+  }
+
+  @Override public NanoHTTPD.Response serve(NanoHTTPD.IHTTPSession session) {
+    return newFixedLengthResponse(loadTextAsset(htmlPath));
+  }
+}
