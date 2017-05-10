a71bf967f5ff0e4a1fb6d5100e9e29a2ae4a9c90
Pedro Ribeiro <ribasss@gmail.com>
2017-05-10 18:33:37 +0100

    [AN-1487] - AppView Open Video event.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/app/screenshots/ScreenshotsAdapter.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/app/screenshots/ScreenshotsAdapter.java
@@ -116,6 +116,7 @@ public class ScreenshotsAdapter
       play_button.setVisibility(View.VISIBLE);
 
       itemView.setOnClickListener(v -> {
+        appViewAnalytics.sendOpenVideoEvent();
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getUrl()));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
