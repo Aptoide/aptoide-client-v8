cbe501acd3565b9e7a76b0f0e4c8591effc17473
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-05-19 16:29:34 +0100

    [AN-1527] - Extracted hotspot creation to an Async task

--- spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/ShareAptoideManager.java
+++ spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/ShareAptoideManager.java
@@ -1,19 +1,61 @@
 package cm.aptoide.pt.spotandshareandroid;
 
+import android.os.AsyncTask;
+
 /**
  * Created by filipe on 17-05-2017.
  */
 
 public class ShareAptoideManager {
 
-  private static final String SSID = "Aptoide_Share";
+  public static final String SSID = "Aptoide_Share";
   private HotspotManager hotspotManager;
+  private EnabledHotspotListener listener;
 
   public ShareAptoideManager(HotspotManager hotspotManager) {
     this.hotspotManager = hotspotManager;
   }
 
-  public int enableHotspot() {
-    return hotspotManager.enableHotspot(SSID);
+  public void enableHotspot(EnabledHotspotListener listener) {
+    this.listener = listener;
+    new EnableHotspotTask().execute();
+  }
+
+  public void stop() {
+    this.hotspotManager.stop();
+    this.listener = null;
+  }
+
+  public interface EnabledHotspotListener {
+    void onResult(boolean result);
+  }
+
+  private class EnableHotspotTask extends AsyncTask<Void, Void, Integer> {
+
+    @Override protected Integer doInBackground(Void... params) {
+      return hotspotManager.enableOpenHotspot(SSID);
+    }
+
+    @Override protected void onPreExecute() {
+      super.onPreExecute();
+    }
+
+    @Override protected void onPostExecute(Integer integer) {
+      super.onPostExecute(integer);
+      if (listener != null) {
+        if (integer == ConnectionManager.SUCCESS_HOTSPOT_CREATION) {
+          listener.onResult(true);
+        } else {
+          listener.onResult(false);
+        }
+      }
+    }
+
+    @Override protected void onCancelled() {
+      super.onCancelled();
+      if (listener != null) {
+        listener = null;
+      }
+    }
   }
 }
