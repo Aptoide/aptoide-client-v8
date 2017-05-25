cb5b0a1f77c8494b9452ae11533b9a569296b228
Pedro Ribeiro <ribasss@gmail.com>
2017-05-25 16:41:50 +0100

    Corrected Search_No_Results parameter name.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/search/SearchAnalytics.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/search/SearchAnalytics.java
@@ -14,7 +14,6 @@ import java.util.Map;
 public class SearchAnalytics extends AptoideAnalytics {
 
   private static final String QUERY = "search_term";
-  private static final String SOURCE = "search_source";
   private static final String SEARCH = "Search";
   private static final String NO_RESULTS = "Search_No_Results";
   private static final String APP_CLICK = "Search_Results_App_View_Click";
@@ -33,7 +32,7 @@ public class SearchAnalytics extends AptoideAnalytics {
   }
 
   public void searchNoResults(String query) {
-    analytics.sendEvent(new FacebookEvent(facebook, NO_RESULTS, createBundleData(SOURCE, query)));
+    analytics.sendEvent(new FacebookEvent(facebook, NO_RESULTS, createBundleData(QUERY, query)));
   }
 
   public void searchAppClick(String query, String packageName) {
