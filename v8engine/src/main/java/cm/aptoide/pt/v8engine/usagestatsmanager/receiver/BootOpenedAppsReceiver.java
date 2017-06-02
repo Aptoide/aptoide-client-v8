b03696c716253841b3d510da59d88eb034afe29d
Neurophobic Animal <fredericosantinho@gmail.com>
2017-06-02 18:06:25 +0100

    Better logging.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/usagestatsmanager/receiver/BootOpenedAppsReceiver.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/usagestatsmanager/receiver/BootOpenedAppsReceiver.java
@@ -3,13 +3,19 @@ package cm.aptoide.pt.v8engine.usagestatsmanager.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import cm.aptoide.pt.logger.Logger;
 import cm.aptoide.pt.v8engine.usagestatsmanager.utils.AlarmHelper;
 
 public class BootOpenedAppsReceiver extends BroadcastReceiver {
 
+  private static final String TAG = BootOpenedAppsReceiver.class.getSimpleName();
+
   public static final long ALARM_INTERVAL = 60000;
 
   @Override public void onReceive(Context context, Intent intent) {
+    Logger.d(TAG,
+        "onReceive() called with: " + "context = [" + context + "], intent = [" + intent + "]");
+
     if (intent.getAction()
         .equals(Intent.ACTION_BOOT_COMPLETED)) {
       new AlarmHelper(context, OpenedAppsReceiver.class).setupAlarm(ALARM_INTERVAL);
