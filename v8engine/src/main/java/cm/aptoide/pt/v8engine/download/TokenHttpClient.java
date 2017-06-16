4c5f20bfacc078c2a474bf53005b3be95df1f331
diogoloureiro <!a12110650>
2017-06-16 15:01:34 +0100

    increased timeout and auto retries

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/download/TokenHttpClient.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/download/TokenHttpClient.java
@@ -88,9 +88,9 @@ public class TokenHttpClient {
             return response;
           }
         })
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS);
+        .connectTimeout(45, TimeUnit.SECONDS)
+        .writeTimeout(45, TimeUnit.SECONDS)
+        .readTimeout(45, TimeUnit.SECONDS);
   }
 
   private void addMirrorToDownloadEvent(String v, String packageName, int fileType,
