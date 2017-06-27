b3305e0033b073e06aafed21ca81aa68f05f5eef
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-06-27 16:58:51 +0100

    Added module Spot and Share App for testing purposes.

---
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/MainActivity.java
@@ -0,0 +1,18 @@
+package cm.aptoide.pt.spotandshareapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by neuro on 27-06-2017.
+ */
+
+public class MainActivity extends Activity {
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.empty_frame);
+  }
+}
