127f83a4ae0c1d65841a99b65fd16a927d288ccb
Pedro Ribeiro <ribasss@gmail.com>
2017-06-27 10:17:41 +0100

    [AN-1706] - Changed FlurryEvent class to support no data events.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/events/FlurryEvent.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/analytics/events/FlurryEvent.java
@@ -2,17 +2,20 @@ package cm.aptoide.pt.v8engine.analytics.events;
 
 import cm.aptoide.pt.v8engine.analytics.Event;
 import com.flurry.android.FlurryAgent;
-import java.util.HashMap;
 import java.util.Map;
 
 public class FlurryEvent implements Event {
 
   private final String eventName;
-  private final Map<String, String> map;
+  private Map<String, String> map;
 
   public FlurryEvent(String eventName, Map<String, String> map) {
     this.eventName = eventName;
     this.map = map;
+  }
+
+  public FlurryEvent(String eventName) {
+    this.eventName = eventName;
   }
 
   @Override public void send() {
