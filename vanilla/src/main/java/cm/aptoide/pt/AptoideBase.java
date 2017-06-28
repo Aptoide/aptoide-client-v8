6c9d15ec830546b45ec5d6ec8f0ea87e03d4a0a7
Ana Lara <ana.simoes@aptoide.com>
2017-06-28 16:31:17 +0100

    AN-1725 - AppsFlyer

--- vanilla/src/main/java/cm/aptoide/pt/AptoideBase.java
+++ vanilla/src/main/java/cm/aptoide/pt/AptoideBase.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import cm.aptoide.pt.preferences.AptoidePreferencesConfiguration;
 import cm.aptoide.pt.v8engine.V8Engine;
+import com.appsflyer.AppsFlyerLib;
 
 /**
  * Created by neuro on 10-05-2016.
@@ -19,6 +20,18 @@ public class AptoideBase extends V8Engine {
     setupCrashReports(BuildConfig.CRASH_REPORTS_DISABLED);
     super.onCreate();
     //setupStrictMode();
+
+    /*
+     * AppsFlyer
+     */
+    AppsFlyerLib.getInstance()
+        .setAndroidIdData(((V8Engine) getApplicationContext()).getIdsRepository()
+            .getUniqueIdentifier());
+
+    AppsFlyerLib.getInstance()
+        .startTracking(this, getString(R.string.APPS_FLYER_DEV_KEY));
+    AppsFlyerLib.getInstance()
+        .reportTrackSession(this);
   }
 
   @Override protected void attachBaseContext(Context base) {
