935cc3d07f411ff2c543fc653fc8df42721cf413
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-07-05 17:08:29 +0100

    Removed unused code

--- spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/presenter/SpotAndShareAppSelectionPresenter.java
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/presenter/SpotAndShareAppSelectionPresenter.java
@@ -55,9 +55,6 @@ public class SpotAndShareAppSelectionPresenter implements Presenter {
   }
 
   private void leaveGroup() {
-    //// TODO: 03-07-2017 filipe call spot&share lib to leave the group
-    System.out.println("LEAVING THE GROUP");
-    //view.navigateBack();
     spotAndShare.leaveGroup(success -> view.navigateBack(), err -> view.onLeaveGroupError());
   }
 
