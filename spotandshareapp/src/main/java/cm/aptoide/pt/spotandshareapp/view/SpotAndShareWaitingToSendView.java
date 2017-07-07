96a1103e39e8b43952b5690fc91fac7ba3520792
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-07-07 18:30:49 +0100

    Handling backbutton clicks on waiting to send view

--- spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/view/SpotAndShareWaitingToSendView.java
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/view/SpotAndShareWaitingToSendView.java
@@ -1,8 +1,20 @@
 package cm.aptoide.pt.spotandshareapp.view;
 
+import cm.aptoide.pt.v8engine.presenter.View;
+import rx.Observable;
+
 /**
  * Created by filipe on 07-07-2017.
  */
 
-public interface SpotAndShareWaitingToSendView {
+public interface SpotAndShareWaitingToSendView extends View {
+  Observable<Void> backButtonEvent();
+
+  void showExitWarning();
+
+  Observable<Void> exitEvent();
+
+  void navigateBack();
+
+  void onLeaveGroupError();
 }
