c8f39c2d26f01d07c12015bdfab9215c1593753f
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-07-12 22:22:20 +0100

    Added WifiManagerReflect.java.

---
+++ spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/reflect/WifiManagerReflect.java
@@ -0,0 +1,96 @@
+package cm.aptoide.pt.spotandshareandroid.reflect;
+
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by neuro on 12-07-2017.
+ */
+public class WifiManagerReflect {
+
+  private final WifiManager wifiManager;
+
+  private Method setWifiApConfiguration;
+  private Method setWifiApEnabled;
+  private Method getWifiApConfiguration;
+  private Method isWifiApEnabled;
+  private Method getWifiApState;
+
+  public WifiManagerReflect(WifiManager wifiManager) {
+    this.wifiManager = wifiManager;
+  }
+
+  private Method findMethods(String methodName) {
+
+    Method[] wmMethods = wifiManager.getClass()
+        .getDeclaredMethods();
+    for (Method method : wmMethods) {
+      if (method.getName()
+          .equals(methodName)) {
+        return method;
+      }
+    }
+
+    throw new RuntimeException("Couldn't find method!");
+  }
+
+  private Object invokeWithReturn(Method method, Object... objects) {
+    try {
+      return method.invoke(wifiManager, objects);
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+
+    throw new IllegalArgumentException("invokeWithReturn failed for method " + method.getName());
+  }
+
+  private void invoke(Method method, Object... objects) {
+    try {
+      method.invoke(wifiManager, objects);
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void setWifiApEnabled(WifiConfiguration wifiConfiguration, boolean enable) {
+    if (setWifiApEnabled == null) {
+      setWifiApEnabled = findMethods("setWifiApEnabled");
+    }
+    invoke(setWifiApEnabled, wifiConfiguration, enable);
+  }
+
+  public WifiConfiguration getWifiApConfiguration() {
+    if (getWifiApConfiguration == null) {
+      getWifiApConfiguration = findMethods("getWifiApConfiguration");
+    }
+    return (WifiConfiguration) invokeWithReturn(getWifiApConfiguration);
+  }
+
+  public void setWifiApConfiguration(WifiConfiguration wifiConfiguration) {
+    if (setWifiApConfiguration == null) {
+      setWifiApConfiguration = findMethods("setWifiApConfiguration");
+    }
+
+    invoke(setWifiApConfiguration, wifiConfiguration);
+  }
+
+  public boolean isWifiApEnabled() {
+    if (isWifiApEnabled == null) {
+      isWifiApEnabled = findMethods("isWifiApEnabled");
+    }
+    return (boolean) invokeWithReturn(isWifiApEnabled);
+  }
+
+  public int getWifiApState() {
+    if (getWifiApState == null) {
+      getWifiApState = findMethods("getWifiApState");
+    }
+    return (int) invokeWithReturn(getWifiApState);
+  }
+}
