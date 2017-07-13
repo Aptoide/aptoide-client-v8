080ab14af97a4bbc5901b3146a9e57f043efeaa0
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-07-13 12:13:31 +0100

    Created spotandshare install manager

---
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/SpotAndShareInstallManager.java
@@ -0,0 +1,89 @@
+package cm.aptoide.pt.spotandshareapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import cm.aptoide.pt.utils.FileUtils;
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by filipe on 13-07-2017.
+ */
+
+public class SpotAndShareInstallManager {
+
+  private final ExecutorService singleThreadedExecutorService = Executors.newSingleThreadExecutor();
+  private Context context;
+
+  public SpotAndShareInstallManager(Context context) {
+    this.context = context;
+  }
+
+  public void installAppAsync(String filePath, String packageName) {
+    singleThreadedExecutorService.execute(() -> installApp(filePath, packageName));
+  }
+
+  private void installApp(String filePath, String packageName) {
+    moveObbs(filePath, packageName);
+    install(filePath);
+  }
+
+  private void install(String filePath) {
+    startInstallIntent(context, new File(filePath));
+  }
+
+  private void moveObbs(String filePath, String packageName) {
+
+    FileUtils fileUtils = new FileUtils();
+    String obbsFilePath =
+        Environment.getExternalStoragePublicDirectory("/") + "/Android/Obb/" + packageName + "/";
+    String appFolderPath = getAppFolder(filePath);
+    File appFolder = new File(appFolderPath);
+    File[] filesList = appFolder.listFiles();
+
+    for (File file : filesList) {
+      String fileName = file.getName();
+      if (fileName.endsWith("obb")) {
+        String[] fileNameArray = fileName.split("\\.");
+        String prefix = fileNameArray[0];
+        if (prefix.equalsIgnoreCase("main") || prefix.equalsIgnoreCase("patch")) {
+          fileUtils.copyFile(appFolderPath, obbsFilePath, fileName);
+        }
+      }
+    }
+  }
+
+  private String getAppFolder(String filePath) {
+    String[] filePathArray = filePath.split("/");
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < filePathArray.length - 1; i++) {
+      sb.append(filePathArray[i] + "/");
+    }
+    return sb.toString();
+  }
+
+  private void startInstallIntent(Context context, File file) {
+    Intent intent = new Intent(Intent.ACTION_VIEW);
+
+    Uri photoURI = null;
+    if (Build.VERSION.SDK_INT > 23) {
+      //content://....apk for nougat
+      photoURI = FileProvider.getUriForFile(context, "cm.aptoide.pt.provider", file);
+      //todo only works on release this is the package name + provider
+    } else {
+      //file://....apk for < nougat
+      photoURI = Uri.fromFile(file);
+    }
+
+    intent.setDataAndType(photoURI, "application/vnd.android.package-archive");
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+        | Intent.FLAG_GRANT_READ_URI_PERMISSION
+        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+    context.startActivity(intent);
+  }
+}
