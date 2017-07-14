3eccb0c1aa2658f4e0e4f38bb29e55ec3496390c
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-07-14 14:39:14 +0100

    Code cleanup.

--- spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/transfermanager/Transfer.java
+++ spotandshareandroid/src/main/java/cm/aptoide/pt/spotandshareandroid/transfermanager/Transfer.java
@@ -1,9 +1,7 @@
 package cm.aptoide.pt.spotandshareandroid.transfermanager;
 
 import cm.aptoide.pt.spotandshare.socket.entities.AndroidAppInfo;
-import cm.aptoide.pt.spotandshare.socket.interfaces.TransferLifecycle;
 import com.jakewharton.rxrelay.BehaviorRelay;
-import java.io.IOException;
 import lombok.Getter;
 
 /**
@@ -28,43 +26,6 @@ public abstract class Transfer<T extends Transfer> {
 
   public enum State {
     PENDING_ACCEPTION, PENDING, RECEIVING, RECEIVED, ERROR, SERVING,
-  }
-
-  private static class TransferLifecycleRelay implements TransferLifecycle<AndroidAppInfo> {
-
-    private final Transfer transfer;
-    private final TransferManager transferManager;
-
-    private TransferLifecycleRelay(Transfer transfer, TransferManager transferManager) {
-      this.transfer = transfer;
-      this.transferManager = transferManager;
-    }
-
-    private void notifyListeners() {
-      transfer.behaviorRelay.call(transfer);
-      transferManager.callRelay();
-    }
-
-    @Override public void onStartTransfer(AndroidAppInfo androidAppInfo) {
-      transfer.state = State.PENDING;
-      notifyListeners();
-    }
-
-    @Override public void onFinishTransfer(AndroidAppInfo androidAppInfo) {
-      transfer.state = State.RECEIVED;
-      notifyListeners();
-    }
-
-    @Override public void onProgressChanged(AndroidAppInfo androidAppInfo, float progress) {
-      transfer.state = State.RECEIVING;
-      transfer.progress = progress;
-      notifyListeners();
-    }
-
-    @Override public void onError(IOException e) {
-      transfer.state = State.ERROR;
-      notifyListeners();
-    }
   }
 
   public boolean isPendingAcception() {
