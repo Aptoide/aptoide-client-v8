e56e743029ef508d042f9632062b3e38d42ec479
Trinkes <fabiojscosta1583@gmail.com>
2017-07-17 11:02:22 +0100

    [TimelinePost/AN-1701] - webservices Cache now only caches responses with 200 as response code

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/cache/POSTCacheInterceptor.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/cache/POSTCacheInterceptor.java
@@ -21,13 +21,11 @@ public class POSTCacheInterceptor implements Interceptor {
   @Override public Response intercept(Chain chain) throws IOException {
     final Request request = chain.request();
 
-    // we only intercept and cache POST requests
     if (!request.method()
         .equalsIgnoreCase("POST")) {
       return chain.proceed(request);
     }
 
-    // we shouldn't cache the response if the client explicitly asked us not to
     Headers headers = request.headers();
     if (headers.size() > 0) {
       for (String bypassCacheHeaderValue : headers.values(WebService.BYPASS_HEADER_KEY)) {
@@ -37,7 +35,6 @@ public class POSTCacheInterceptor implements Interceptor {
       }
     }
 
-    // hit the cache to get the response
     if (cache.contains(request)) {
       Response cachedResponse = cache.get(request);
       if (cachedResponse != null) {
@@ -48,12 +45,10 @@ public class POSTCacheInterceptor implements Interceptor {
       Logger.v(TAG, String.format("cache hit but with null result '%s'", request.url()));
     }
 
-    // in case of a cache miss, go to the network
     Logger.v(TAG, String.format("cache miss '%s'", request.url()));
     Response response = chain.proceed(request);
 
-    // we only cache successful responses
-    if (response.isSuccessful()) {
+    if (response.code() == 200) {
       cache.put(request, response);
     }
     return response;
