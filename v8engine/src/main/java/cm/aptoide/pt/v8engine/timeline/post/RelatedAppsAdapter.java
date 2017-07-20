f69e10e8d83cf5fd09dba43e019f27627d6a557c
Trinkes <fabiojscosta1583@gmail.com>
2017-07-20 14:53:41 +0100

    [TimelinePost/AN-1701] - added a selection animation when user choose a related app

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/timeline/post/RelatedAppsAdapter.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/timeline/post/RelatedAppsAdapter.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 import cm.aptoide.pt.v8engine.R;
@@ -153,9 +155,11 @@ class RelatedAppsAdapter extends RecyclerView.Adapter {
           .load(app.getImage(), image);
       this.name.setText(app.getName());
 
-      // TODO: 13/07/2017 trinkes support pre lollipop devices
       ObjectAnimator animator;
       if (app.isSelected()) {
+        if (shouldAnimateSelection()) {
+          scaleView(checkIndicator);
+        }
         animator = ObjectAnimator.ofFloat(itemView, "elevation", ViewCompat.getElevation(itemView),
             SELECTED_ELEVATION);
       } else {
@@ -166,6 +170,19 @@ class RelatedAppsAdapter extends RecyclerView.Adapter {
 
       animator.setDuration(200);
       animator.start();
+    }
+
+    private boolean shouldAnimateSelection() {
+      return checkIndicator.getVisibility() != View.VISIBLE;
+    }
+
+    private void scaleView(View v) {
+      Animation anim =
+          new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF,
+              1f);
+      anim.setFillAfter(true);
+      anim.setDuration(200);
+      v.startAnimation(anim);
     }
   }
 }
