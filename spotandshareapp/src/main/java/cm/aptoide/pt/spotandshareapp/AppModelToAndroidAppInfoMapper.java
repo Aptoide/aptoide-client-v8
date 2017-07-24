403757efcb380f0056e7bdd3237239256a33f758
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-07-24 19:03:16 +0100

    [AN-1800] Created model to map app model to android app info

---
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/AppModelToAndroidAppInfoMapper.java
@@ -0,0 +1,39 @@
+package cm.aptoide.pt.spotandshareapp;
+
+import cm.aptoide.pt.spotandshare.socket.entities.AndroidAppInfo;
+import java.io.File;
+
+/**
+ * Created by filipe on 24-07-2017.
+ */
+
+public class AppModelToAndroidAppInfoMapper {
+
+  private ObbsProvider obbsProvider;
+
+  public AppModelToAndroidAppInfoMapper(ObbsProvider obbsProvider) {
+    this.obbsProvider = obbsProvider;
+  }
+
+  public AndroidAppInfo convertAppModelToAndroidAppInfo(AppModel appModel) {
+
+    String appName = appModel.getAppName();
+    String packageName = appModel.getPackageName();
+    File apk = new File(appModel.getFilePath());
+    byte[] bitmapdata = appModel.getAppIconAsByteArray();
+
+    AndroidAppInfo androidAppInfo;
+    if (!appModel.getObbsFilePath()
+        .equals(InstalledRepositoryDummy.NO_OBBS)) {
+
+      File[] obbsList = obbsProvider.getObbsList(appModel.getObbsFilePath());
+
+      androidAppInfo =
+          new AndroidAppInfo(appName, packageName, apk, obbsList[0], obbsList[1], bitmapdata, null);
+    } else {
+      androidAppInfo = new AndroidAppInfo(appName, packageName, apk, null, null, bitmapdata, null);
+    }
+
+    return androidAppInfo;
+  }
+}
