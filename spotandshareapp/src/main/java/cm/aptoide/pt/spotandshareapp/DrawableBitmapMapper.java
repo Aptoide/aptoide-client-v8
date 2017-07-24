e7bf5c947861efb71502e2391a4f8f7440e01f9d
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-07-24 18:16:10 +0100

    [AN-1800] Created method inside DrawableBitmapMapper to convert byteArray to Drawable

--- spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/DrawableBitmapMapper.java
+++ spotandshareapp/src/main/java/cm/aptoide/pt/spotandshareapp/DrawableBitmapMapper.java
@@ -1,6 +1,8 @@
 package cm.aptoide.pt.spotandshareapp;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import java.io.ByteArrayOutputStream;
@@ -9,7 +11,13 @@ import java.io.ByteArrayOutputStream;
  * Created by filipe on 10-07-2017.
  */
 
-public class DrawableToBitmapMapper {
+public class DrawableBitmapMapper {
+
+  private Context context;
+
+  public DrawableBitmapMapper(Context context) {
+    this.context = context;
+  }
 
   public byte[] convertDrawableToBitmap(Drawable drawable) {
     Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
@@ -17,5 +25,10 @@ public class DrawableToBitmapMapper {
     bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
     byte[] bitmapdata = stream.toByteArray();
     return bitmapdata;
+  }
+
+  public Drawable convertBitmapToDrawable(byte[] array) {
+    return new BitmapDrawable(context.getResources(),
+        BitmapFactory.decodeByteArray(array, 0, array.length));
   }
 }
