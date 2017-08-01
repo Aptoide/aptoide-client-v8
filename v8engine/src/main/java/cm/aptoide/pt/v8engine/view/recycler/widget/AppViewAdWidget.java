d87cc70594dfb2f346fa34d83117b0b926d20521
Neurophobic Animal <fredericosantinho@gmail.com>
2017-08-01 14:36:51 +0100

    [AN-1840] - AppViewAdWidget.java now has rating as well.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/AppViewAdWidget.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/AppViewAdWidget.java
@@ -1,6 +1,9 @@
 package cm.aptoide.pt.v8engine.view.recycler.widget;
 
 import android.view.View;
+import android.widget.RatingBar;
+import cm.aptoide.pt.v8engine.R;
+import cm.aptoide.pt.v8engine.view.recycler.displayable.GridAdDisplayable;
 
 /**
  * Created by neuro on 01-08-2017.
@@ -8,7 +11,22 @@ import android.view.View;
 
 public class AppViewAdWidget extends GridAdWidget {
 
+  private RatingBar ratingBar;
+
   public AppViewAdWidget(View itemView) {
     super(itemView);
+  }
+
+  @Override protected void assignViews(View itemView) {
+    super.assignViews(itemView);
+
+    ratingBar = (RatingBar) itemView.findViewById(R.id.ratingbar);
+  }
+
+  @Override public void bindView(GridAdDisplayable displayable) {
+    super.bindView(displayable);
+
+    ratingBar.setRating(displayable.getPojo()
+        .getStars());
   }
 }
