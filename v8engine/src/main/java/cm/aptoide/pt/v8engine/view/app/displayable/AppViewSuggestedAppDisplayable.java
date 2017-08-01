209372a6dbb432fb99d6734e6c178f673599cf59
Neurophobic Animal <fredericosantinho@gmail.com>
2017-08-01 11:17:09 +0100

    [AN-1840] - Added AppViewSuggestedAppDisplayable.java.

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/view/app/displayable/AppViewSuggestedAppDisplayable.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/app/displayable/AppViewSuggestedAppDisplayable.java
@@ -5,35 +5,28 @@
 
 package cm.aptoide.pt.v8engine.view.app.displayable;
 
-import cm.aptoide.pt.database.realm.MinimalAd;
+import cm.aptoide.pt.dataprovider.model.v7.listapp.App;
 import cm.aptoide.pt.v8engine.R;
-import cm.aptoide.pt.v8engine.app.AppViewAnalytics;
-import cm.aptoide.pt.v8engine.view.recycler.displayable.DisplayablePojo;
+import cm.aptoide.pt.v8engine.view.app.GridAppDisplayable;
 
 /**
  * Created on 04/05/16.
  */
-public class AppViewSuggestedAppDisplayable extends DisplayablePojo<MinimalAd> {
-
-  private AppViewAnalytics appViewAnalytics;
+public class AppViewSuggestedAppDisplayable extends GridAppDisplayable {
 
   public AppViewSuggestedAppDisplayable() {
   }
 
-  public AppViewSuggestedAppDisplayable(MinimalAd minimalAd, AppViewAnalytics appViewAnalytics) {
-    super(minimalAd);
-    this.appViewAnalytics = appViewAnalytics;
+  public AppViewSuggestedAppDisplayable(App app) {
+    // TODO: 01-08-2017 neuro tags
+    super(app, null, true);
   }
 
   @Override protected Configs getConfig() {
-    return new Configs(1, true);
+    return new Configs(3, true);
   }
 
   @Override public int getViewLayout() {
     return R.layout.displayable_app_view_suggested_app;
-  }
-
-  public AppViewAnalytics getAppViewAnalytics() {
-    return appViewAnalytics;
   }
 }
