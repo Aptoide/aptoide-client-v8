8d44b36c4e52cc61a1fbb0f4f29b0a8945367151
Neurophobic Animal <fredericosantinho@gmail.com>
2017-08-01 11:14:55 +0100

    [AN-1840] - Added AppViewAdDisplayable.java.

---
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/view/recycler/widget/AppViewAdWidget.java
@@ -0,0 +1,14 @@
+package cm.aptoide.pt.v8engine.view.recycler.widget;
+
+import android.view.View;
+
+/**
+ * Created by neuro on 01-08-2017.
+ */
+
+public class AppViewAdWidget extends GridAdWidget {
+
+  public AppViewAdWidget(View itemView) {
+    super(itemView);
+  }
+}
