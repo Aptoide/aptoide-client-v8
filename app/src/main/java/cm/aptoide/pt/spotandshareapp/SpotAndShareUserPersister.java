0900bdeac313d74f7640eec071b8db6e036c304b
Filipe Gonçalves <filipe.goncalves@aptoide.com>
2017-08-10 19:00:38 +0100

    [AN-1861] User persister now stores avatar path too

--- app/src/main/java/cm/aptoide/pt/spotandshareapp/SpotAndShareUserPersister.java
+++ app/src/main/java/cm/aptoide/pt/spotandshareapp/SpotAndShareUserPersister.java
@@ -8,9 +8,15 @@ import android.content.SharedPreferences;
 
 public class SpotAndShareUserPersister {
 
+  public static final String DEFAULT_USERNAME = "userwithnoname";
+  public static final int DEFAULT_AVATAR_ID = 0;
+  public static final String DEFAULT_AVATAR_PATH =
+      "android.resource://spotandshareapp.dev/drawable/spotandshare_avatar_01";
+
   public static final String SHARED_PREFERENCES_NAME = "cm.aptoide.pt";
   public static final String SPOTANDSHARE_ACCOUNT_USERNAME = "SPOTANDSHARE_ACCOUNT_USERNAME";
-  public static final String SPOTANDSHARE_ACCOUNT_AVATAR = "SPOTANDSHARE_ACCOUNT_AVATAR";
+  public static final String SPOTANDSHARE_ACCOUNT_AVATAR_ID = "SPOTANDSHARE_ACCOUNT_AVATAR_ID";
+  public static final String SPOTANDSHARE_ACCOUNT_AVATAR_PATH = "SPOTANDSHARE_ACCOUNT_AVATAR_PATH";
   private SharedPreferences sharedPreferences;
 
   public SpotAndShareUserPersister(SharedPreferences sharedPreferences) {
@@ -22,14 +28,20 @@ public class SpotAndShareUserPersister {
         .putString(SPOTANDSHARE_ACCOUNT_USERNAME, user.getUsername())
         .apply();
     sharedPreferences.edit()
-        .putInt(SPOTANDSHARE_ACCOUNT_AVATAR, user.getAvatar()
+        .putInt(SPOTANDSHARE_ACCOUNT_AVATAR_ID, user.getAvatar()
             .getAvatarId())
+        .apply();
+    sharedPreferences.edit()
+        .putString(SPOTANDSHARE_ACCOUNT_AVATAR_PATH, user.getAvatar()
+            .getString())
         .apply();
   }
 
   public SpotAndShareUser get() {
-    String username = sharedPreferences.getString(SPOTANDSHARE_ACCOUNT_USERNAME, "userwithnoname");
-    int avatar = sharedPreferences.getInt(SPOTANDSHARE_ACCOUNT_AVATAR, 0);
-    return new SpotAndShareUser(username, new SpotAndShareUserAvatar(avatar, "", false));
+    String username = sharedPreferences.getString(SPOTANDSHARE_ACCOUNT_USERNAME, DEFAULT_USERNAME);
+    int avatarId = sharedPreferences.getInt(SPOTANDSHARE_ACCOUNT_AVATAR_ID, DEFAULT_AVATAR_ID);
+    String avatarPath =
+        sharedPreferences.getString(SPOTANDSHARE_ACCOUNT_AVATAR_PATH, DEFAULT_AVATAR_PATH);
+    return new SpotAndShareUser(username, new SpotAndShareUserAvatar(avatarId, avatarPath, false));
   }
 }
