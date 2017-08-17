5981af6ea71798457a4a051785b6267460230303
Ana Lara <ana.simoes@aptoide.com>
2017-08-17 09:29:33 +0100

    AN-1759 - minor changes - easier to read

--- v8engine/src/main/java/cm/aptoide/pt/v8engine/account/AndroidAccountDataMigration.java
+++ v8engine/src/main/java/cm/aptoide/pt/v8engine/account/AndroidAccountDataMigration.java
@@ -120,8 +120,13 @@ public class AndroidAccountDataMigration {
     return res;
   }
 
+  private boolean isMigrated() {
+    return oldVersion == currentVersion;
+  }
+
   private Completable migrateAccountFromVersion59To60() {
-    if (oldVersion <= 59) {
+    if (oldVersion < 60) {
+      Log.w(TAG, "migrateAccountFromVersion59To60");
       return Completable.defer(() -> Completable.fromCallable(() -> {
         final android.accounts.Account[] accounts = accountManager.getAccountsByType(accountType);
         final Account oldAccount = accounts[0];
@@ -139,125 +144,123 @@ public class AndroidAccountDataMigration {
     return Completable.complete();
   }
 
-  private boolean isMigrated() {
-    return oldVersion == currentVersion;
-  }
-
   //V7
   private Completable migrateAccountFromPreviousThan43() {
-    if (oldVersion >= 43) {
-      return Completable.complete();
+    if (oldVersion < 43) {
+      Log.w(TAG, "migrateAccountFromPreviousThan43");
+      return Completable.defer(() -> Completable.fromCallable(() -> {
+        //
+        // migration from v7 to this v8
+        //
+
+        // here we will migrate from V7 directly to the new Account Manager and Account
+        // all data is saved in shared prefs except the refresh token, which was saved in the
+        // secure shared prefs
+
+        Log.i(TAG, "migrating from v7");
+
+        final android.accounts.Account[] accounts = accountManager.getAccountsByType(accountType);
+        final Account oldAccount = accounts[0];
+
+        String sharedPrefsData;
+        for (String key : MIGRATION_KEYS) {
+          sharedPrefsData = defaultSharedPreferences.getString(key, null);
+          if (!TextUtils.isEmpty(sharedPrefsData)) {
+            accountManager.setUserData(oldAccount, key, sharedPrefsData);
+          }
+        }
+
+        for (String key : MIGRATION_KEYS) {
+          sharedPrefsData = secureSharedPreferences.getString(key, null);
+          if (!TextUtils.isEmpty(sharedPrefsData)) {
+            accountManager.setUserData(oldAccount, key, sharedPrefsData);
+          }
+        }
+
+        String refreshToken = accountManager.blockingGetAuthToken(oldAccount, "Full access", false);
+        if (!TextUtils.isEmpty(refreshToken)) {
+          accountManager.setUserData(oldAccount, "refresh_token", refreshToken);
+        }
+
+        String matureSwitchKey = "aptoide_account_manager_mature_switch";
+        sharedPrefsData = defaultSharedPreferences.getString(matureSwitchKey, "false");
+        accountManager.setUserData(oldAccount, matureSwitchKey, sharedPrefsData);
+
+        String accountManagerLoginModeKeyOld = "loginType";
+        String accountManagerLoginModeKeyNew = "aptoide_account_manager_login_mode";
+        sharedPrefsData = defaultSharedPreferences.getString(accountManagerLoginModeKeyOld, "");
+        accountManager.setUserData(oldAccount, accountManagerLoginModeKeyNew, sharedPrefsData);
+
+        cleanKeysFromPreferences(MIGRATION_KEYS, defaultSharedPreferences);
+        cleanKeysFromPreferences(MIGRATION_KEYS, secureSharedPreferences);
+
+        return Completable.complete();
+      }));
     }
-    return Completable.defer(() -> Completable.fromCallable(() -> {
-      //
-      // migration from v7 to this v8
-      //
-
-      // here we will migrate from V7 directly to the new Account Manager and Account
-      // all data is saved in shared prefs except the refresh token, which was saved in the
-      // secure shared prefs
-
-      Log.i(TAG, "migrating from v7");
-
-      final android.accounts.Account[] accounts = accountManager.getAccountsByType(accountType);
-      final Account oldAccount = accounts[0];
-
-      String sharedPrefsData;
-      for (String key : MIGRATION_KEYS) {
-        sharedPrefsData = defaultSharedPreferences.getString(key, null);
-        if (!TextUtils.isEmpty(sharedPrefsData)) {
-          accountManager.setUserData(oldAccount, key, sharedPrefsData);
-        }
-      }
-
-      for (String key : MIGRATION_KEYS) {
-        sharedPrefsData = secureSharedPreferences.getString(key, null);
-        if (!TextUtils.isEmpty(sharedPrefsData)) {
-          accountManager.setUserData(oldAccount, key, sharedPrefsData);
-        }
-      }
-
-      String refreshToken = accountManager.blockingGetAuthToken(oldAccount, "Full access", false);
-      if (!TextUtils.isEmpty(refreshToken)) {
-        accountManager.setUserData(oldAccount, "refresh_token", refreshToken);
-      }
-
-      String matureSwitchKey = "aptoide_account_manager_mature_switch";
-      sharedPrefsData = defaultSharedPreferences.getString(matureSwitchKey, "false");
-      accountManager.setUserData(oldAccount, matureSwitchKey, sharedPrefsData);
-
-      String accountManagerLoginModeKeyOld = "loginType";
-      String accountManagerLoginModeKeyNew = "aptoide_account_manager_login_mode";
-      sharedPrefsData = defaultSharedPreferences.getString(accountManagerLoginModeKeyOld, "");
-      accountManager.setUserData(oldAccount, accountManagerLoginModeKeyNew, sharedPrefsData);
-
-      cleanKeysFromPreferences(MIGRATION_KEYS, defaultSharedPreferences);
-      cleanKeysFromPreferences(MIGRATION_KEYS, secureSharedPreferences);
-
-      return Completable.complete();
-    }));
+    return Completable.complete();
   }
 
   //v8
   private Completable migrateAccountFrom43to59() {
-    if (oldVersion < 43 || oldVersion >= 59) {
-      return Completable.complete();
-    }
-    return Completable.defer(() -> Completable.fromCallable(() -> {
+    if (oldVersion >= 43 && oldVersion < 59) {
+      Log.w(TAG, "migrateAccountFrom43to59");
+      return Completable.defer(() -> Completable.fromCallable(() -> {
 
-      //
-      // migration from an older v8.x to this v8
-      //
+        //
+        // migration from an older v8.x to this v8
+        //
 
-      final Account[] accounts = accountManager.getAccountsByType(accountType);
+        final Account[] accounts = accountManager.getAccountsByType(accountType);
 
-      if (!accountHasKeysForMigration(MIGRATION_KEYS, secureSharedPreferences)
-          || accounts.length == 0) {
+        if (!accountHasKeysForMigration(MIGRATION_KEYS, secureSharedPreferences)
+            || accounts.length == 0) {
 
-        Log.e(TAG,
-            "Account migration from <8.1.2.1 to >8.2.0.0 failed. the required keys were not available.");
-        markMigrated();
+          Log.e(TAG,
+              "Account migration from <8.1.2.1 to >8.2.0.0 failed. the required keys were not available.");
+          markMigrated();
+          return Completable.complete();
+        }
+
+        final Account oldAccount = accounts[0];
+
+        String encryptedPassword = accountManager.getPassword(oldAccount);
+        // new SecureCoderDecoder.Builder(context).create()
+        String plainTextPassword = secureCoderDecoder.decrypt(encryptedPassword);
+
+        if (oldVersion <= 55 || TextUtils.isEmpty(plainTextPassword)) {
+          // previously we store the password encrypted but we stopped doing it at DB version 55
+          plainTextPassword = encryptedPassword;
+        }
+
+        String sharedPrefsData;
+        for (String key : MIGRATION_KEYS) {
+          sharedPrefsData = secureSharedPreferences.getString(key, null);
+          accountManager.setUserData(oldAccount, key, sharedPrefsData);
+        }
+
+        String matureSwitchKey = "aptoide_account_manager_mature_switch";
+        sharedPrefsData = secureSharedPreferences.getString(matureSwitchKey, "false");
+        accountManager.setUserData(oldAccount, matureSwitchKey, sharedPrefsData);
+
+        // access_confirmed is registered in the default shared preferences and not in the
+        // secure shared preferences
+        String accessConfirmedKey = "access_confirmed";
+        sharedPrefsData =
+            Boolean.toString(defaultSharedPreferences.getBoolean(accessConfirmedKey, false));
+        accountManager.setUserData(oldAccount, accessConfirmedKey, sharedPrefsData);
+
+        // account.name -> user email. we don't need to change this
+        accountManager.setPassword(oldAccount, plainTextPassword);
+
+        // remove all keys from shared preferences
+        cleanKeysFromPreferences(MIGRATION_KEYS, defaultSharedPreferences);
+        cleanKeysFromPreferences(MIGRATION_KEYS, secureSharedPreferences);
+
+        Log.i(TAG, "Account migration from <8.1.2.1 to >8.2.0.0 succeeded");
         return Completable.complete();
-      }
-
-      final Account oldAccount = accounts[0];
-
-      String encryptedPassword = accountManager.getPassword(oldAccount);
-      // new SecureCoderDecoder.Builder(context).create()
-      String plainTextPassword = secureCoderDecoder.decrypt(encryptedPassword);
-
-      if (oldVersion <= 55 || TextUtils.isEmpty(plainTextPassword)) {
-        // previously we store the password encrypted but we stopped doing it at DB version 55
-        plainTextPassword = encryptedPassword;
-      }
-
-      String sharedPrefsData;
-      for (String key : MIGRATION_KEYS) {
-        sharedPrefsData = secureSharedPreferences.getString(key, null);
-        accountManager.setUserData(oldAccount, key, sharedPrefsData);
-      }
-
-      String matureSwitchKey = "aptoide_account_manager_mature_switch";
-      sharedPrefsData = secureSharedPreferences.getString(matureSwitchKey, "false");
-      accountManager.setUserData(oldAccount, matureSwitchKey, sharedPrefsData);
-
-      // access_confirmed is registered in the default shared preferences and not in the
-      // secure shared preferences
-      String accessConfirmedKey = "access_confirmed";
-      sharedPrefsData =
-          Boolean.toString(defaultSharedPreferences.getBoolean(accessConfirmedKey, false));
-      accountManager.setUserData(oldAccount, accessConfirmedKey, sharedPrefsData);
-
-      // account.name -> user email. we don't need to change this
-      accountManager.setPassword(oldAccount, plainTextPassword);
-
-      // remove all keys from shared preferences
-      cleanKeysFromPreferences(MIGRATION_KEYS, defaultSharedPreferences);
-      cleanKeysFromPreferences(MIGRATION_KEYS, secureSharedPreferences);
-
-      Log.i(TAG, "Account migration from <8.1.2.1 to >8.2.0.0 succeeded");
-      return Completable.complete();
-    }));
+      }));
+    }
+    return Completable.complete();
   }
 
   private void cleanKeysFromPreferences(String[] migrationKeys,
