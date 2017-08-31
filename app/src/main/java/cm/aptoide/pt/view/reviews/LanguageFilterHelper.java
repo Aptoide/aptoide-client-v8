e2620c98669f3611abdd4cd9beeaf15d21519916
Neurophobic Animal <FredericoSantinho@gmail.com>
2017-08-31 16:44:41 +0100

    [AN-1733] - Added countryCodes to LanguageFilterHelper.java.

--- app/src/main/java/cm/aptoide/pt/view/reviews/LanguageFilterHelper.java
+++ app/src/main/java/cm/aptoide/pt/view/reviews/LanguageFilterHelper.java
@@ -4,9 +4,11 @@ import android.content.res.Resources;
 import android.support.annotation.StringRes;
 import cm.aptoide.pt.R;
 import cm.aptoide.pt.utils.AptoideUtils;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import lombok.Getter;
+import lombok.experimental.Delegate;
 
 /**
  * Created by neuro on 28-08-2017.
@@ -20,7 +22,8 @@ import lombok.Getter;
   private final String currentCountryCode;
 
   LanguageFilterHelper(Resources resources) {
-    all = new LanguageFilter(R.string.comments_filter_comments_by_language_all, null);
+    all = new LanguageFilter(R.string.comments_filter_comments_by_language_all,
+        Collections.emptyList());
     currentCountryCode = AptoideUtils.SystemU.getCountryCode(resources);
     currentLanguageFirst =
         new LanguageFilter(R.string.comments_filter_comments_by_language_current_language_first,
@@ -31,11 +34,30 @@ import lombok.Getter;
   @Getter static class LanguageFilter {
 
     @StringRes private final int stringId;
-    private final String value;
+    private final List<String> countryCodes;
+    @Delegate private final String value = computeLazyValue();
 
-    LanguageFilter(@StringRes int stringId, String value) {
+    LanguageFilter(@StringRes int stringId, String countryCode) {
+      this(stringId, Collections.singletonList(countryCode));
+    }
+
+    LanguageFilter(@StringRes int stringId, List<String> countryCodes) {
       this.stringId = stringId;
-      this.value = value;
+      this.countryCodes = countryCodes;
+    }
+
+    private String computeLazyValue() {
+      String result = null;
+
+      for (String countryCode : countryCodes) {
+        if (result == null) {
+          result = countryCode;
+        } else {
+          result += "," + countryCode;
+        }
+      }
+
+      return result;
     }
   }
 
