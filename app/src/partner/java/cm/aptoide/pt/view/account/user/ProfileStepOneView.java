c28dd38d22ed2a0480d6c0a5e3ac12c1f729e7ce
danielchen <daniel.chen@aptoide.com>
2017-09-07 11:18:14 +0100

    My account view partners

---
+++ app/src/partner/java/cm/aptoide/pt/view/account/user/ProfileStepOneView.java
@@ -0,0 +1,22 @@
+package cm.aptoide.pt.view.account.user;
+
+/**
+ * Created by danielchen on 07/09/17.
+ */
+
+import android.support.annotation.NonNull;
+import cm.aptoide.pt.presenter.View;
+import rx.Completable;
+import rx.Observable;
+
+interface ProfileStepOneView extends View {
+    @NonNull Observable<Boolean> continueButtonClick();
+
+    @NonNull Observable<Void> moreInfoButtonClick();
+
+    void showWaitDialog();
+
+    void dismissWaitDialog();
+
+    Completable showGenericErrorMessage();
+}
