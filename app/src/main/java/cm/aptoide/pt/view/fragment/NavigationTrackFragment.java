25904a762f0eb1f036f6bc222f6b63bae350f21d
Pedro Ribeiro <ribasss@gmail.com>
2017-09-14 15:10:20 +0100

    [AN-1736] - Created new Fragment that will be responsible for registering opened views.

---
+++ app/src/main/java/cm/aptoide/pt/view/fragment/NavigationTrackFragment.java
@@ -0,0 +1,21 @@
+package cm.aptoide.pt.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import cm.aptoide.pt.AptoideApplication;
+import cm.aptoide.pt.analytics.AptoideNavigationTracker;
+
+/**
+ * Created by pedroribeiro on 14/09/17.
+ */
+
+public class NavigationTrackFragment extends FragmentView {
+
+  private AptoideNavigationTracker navigationTracker;
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    navigationTracker =
+        ((AptoideApplication) getContext().getApplicationContext()).getAptoideNavigationTracker();
+  }
+}
