76147f43fcc84304b9ef80956e8752e4b42487c7
Pedro Ribeiro <ribasss@gmail.com>
2017-09-18 11:27:07 +0100

    [AN-1736] - Added checks in order to only register necessary views.

--- app/src/main/java/cm/aptoide/pt/analytics/AptoideNavigationTracker.java
+++ app/src/main/java/cm/aptoide/pt/analytics/AptoideNavigationTracker.java
@@ -1,6 +1,10 @@
 package cm.aptoide.pt.analytics;
 
 import cm.aptoide.pt.logger.Logger;
+import cm.aptoide.pt.view.account.LoginSignUpCredentialsFragment;
+import cm.aptoide.pt.view.store.GetStoreFragment;
+import cm.aptoide.pt.view.store.home.HomeFragment;
+import cm.aptoide.pt.view.wizard.WizardFragment;
 import java.util.List;
 
 /**
@@ -11,24 +15,44 @@ public class AptoideNavigationTracker implements NavigationTracker {
 
   public static final String DO_NOT_REGISTER_VIEW = "do_not_register_view";
   private List<String> viewList;
+  private boolean insert;
 
   public AptoideNavigationTracker(List<String> viewList) {
     this.viewList = viewList;
   }
 
   @Override public void registerView(String viewName) {
-    viewList.add(viewName);
-    Logger.d(this.getClass()
-        .getName(), "View is: " + viewName);
+    insert = filter(viewName);
+    if (!getCurrentViewName().equals(viewName) && insert) {
+      viewList.add(viewName.equals(HomeFragment.class.getSimpleName())
+          ? GetStoreFragment.class.getSimpleName() : viewName);
+      Logger.d(this.getClass()
+          .getName(), "View is: " + getCurrentViewName());
+    }
   }
 
   @Override public String getPreviousViewName() {
-    // TODO: 07/09/2017 fix this logic
+    if (viewList.isEmpty()) {
+      return "";
+    }
     return viewList.get(viewList.size() - 1);
   }
 
   @Override public String getCurrentViewName() {
-    // TODO: 07/09/2017 fix this logic
+    if (viewList.isEmpty()) {
+      return "";
+    }
     return viewList.get(viewList.size() - 1);
+  }
+
+  private boolean filter(String viewName) {
+    if (viewName.equals(WizardFragment.class.getSimpleName())) {
+      insert = false;
+    } else if (viewName.equals(LoginSignUpCredentialsFragment.class.getSimpleName())) {
+      insert = false;
+    } else {
+      insert = true;
+    }
+    return insert;
   }
 }
