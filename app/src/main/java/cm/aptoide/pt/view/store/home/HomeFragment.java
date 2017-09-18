d91506e172cf680a7b47b1aa4a86c551a946b636
Pedro Ribeiro <ribasss@gmail.com>
2017-09-18 18:07:22 +0100

    [AN-1736] - Drawer register.

--- app/src/main/java/cm/aptoide/pt/view/store/home/HomeFragment.java
+++ app/src/main/java/cm/aptoide/pt/view/store/home/HomeFragment.java
@@ -267,6 +267,7 @@ public class HomeFragment extends StoreFragment {
     toolbar.setNavigationOnClickListener(v -> {
       drawerLayout.openDrawer(GravityCompat.START);
       drawerAnalytics.drawerOpen();
+      navigationTracker.registerView("Drawer");
     });
   }
 
