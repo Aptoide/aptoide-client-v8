74687e121c4cb3cbb7508328b46c0abe6d2ad3f4
Fábio Dias <fabio.dias@aptoide.com>
2017-09-19 15:29:52 +0100

    migrating SearchFragment to SearchView with MVP pattern

---
+++ app/src/main/java/cm/aptoide/pt/view/search/SearchPresenter.java
@@ -0,0 +1,18 @@
+package cm.aptoide.pt.view.search;
+
+import android.os.Bundle;
+import cm.aptoide.pt.presenter.Presenter;
+
+public class SearchPresenter implements Presenter {
+  @Override public void present() {
+
+  }
+
+  @Override public void saveState(Bundle state) {
+
+  }
+
+  @Override public void restoreState(Bundle state) {
+
+  }
+}
