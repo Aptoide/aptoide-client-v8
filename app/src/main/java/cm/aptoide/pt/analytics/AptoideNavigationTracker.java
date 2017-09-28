e9191d9b976804dfb9d5633d4b7907469c2a38e2
Pedro Ribeiro <ribasss@gmail.com>
2017-09-28 13:43:52 +0100

    [AN-1982] - Changed view name of GetStoreWidgetsFragment to HomeFragment.

--- app/src/main/java/cm/aptoide/pt/analytics/AptoideNavigationTracker.java
+++ app/src/main/java/cm/aptoide/pt/analytics/AptoideNavigationTracker.java
@@ -4,6 +4,7 @@ import cm.aptoide.pt.account.view.LoginSignUpCredentialsFragment;
 import cm.aptoide.pt.logger.Logger;
 import cm.aptoide.pt.view.search.SearchPagerTabFragment;
 import cm.aptoide.pt.view.store.GetStoreFragment;
+import cm.aptoide.pt.view.store.GetStoreWidgetsFragment;
 import cm.aptoide.pt.view.store.home.HomeFragment;
 import cm.aptoide.pt.view.wizard.WizardFragment;
 import java.util.List;
@@ -24,8 +25,7 @@ public class AptoideNavigationTracker implements NavigationTracker {
   @Override public void registerView(String viewName) {
     insert = filter(viewName);
     if (insert) {
-      viewList.add(viewName.equals(GetStoreFragment.class.getSimpleName())
-          ? HomeFragment.class.getSimpleName() : viewName);
+      viewList.add(checkViewname(viewName));
       Logger.d(this.getClass()
           .getName(), "View is: " + getCurrentViewName());
     }
@@ -43,6 +43,15 @@ public class AptoideNavigationTracker implements NavigationTracker {
       return "";
     }
     return viewList.get(viewList.size() - 1);
+  }
+
+  private String checkViewname(String viewName) {
+    if (viewName.equals(GetStoreFragment.class.getSimpleName()) || viewName.equals(
+        GetStoreWidgetsFragment.class.getSimpleName())) {
+      return HomeFragment.class.getSimpleName();
+    } else {
+      return viewName;
+    }
   }
 
   private boolean filter(String viewName) {
