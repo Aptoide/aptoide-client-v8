f1332a403e62dbc93f10b4809779e9bc7ab5d130
jdandrade <joao.andrade@aptoide.com>
2017-10-06 11:47:15 +0100

    [AN-2028] Making the code prettier.

--- app/src/main/java/cm/aptoide/pt/social/data/TimelineCardFilter.java
+++ app/src/main/java/cm/aptoide/pt/social/data/TimelineCardFilter.java
@@ -51,10 +51,7 @@ public class TimelineCardFilter {
           .toSingle()
           .onErrorResumeNext(err -> Single.just(null))
           .flatMapObservable(update -> {
-            if (update == null) {
-              return Observable.empty();
-            }
-            if (update.isExcluded()) {
+            if (update == null || update.isExcluded()) {
               return Observable.empty();
             } else {
               return Observable.just(timelineCard);
