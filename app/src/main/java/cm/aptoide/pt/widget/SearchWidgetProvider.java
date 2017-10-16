95e31fac5e3680c5261de76fd318e2a27c55979c
paquiquinho <paquiquinho@gmail.com>
2017-10-16 12:35:52 +0100

    [AN-1986] Widget exists but redirects to main activity;

---
+++ app/src/main/java/cm/aptoide/pt/widget/SearchWidgetProvider.java
@@ -0,0 +1,34 @@
+package cm.aptoide.pt.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.search.view.SearchActivity;
+import cm.aptoide.pt.view.MainActivity;
+
+/**
+ * Created by franciscocalado on 10/16/17.
+ */
+
+public class SearchWidgetProvider extends AppWidgetProvider {
+
+  public void onUpdate(Context context, AppWidgetManager manager, int[] appWidgetIds){
+    final int N = appWidgetIds.length;
+
+    for(int i=0; i<N; i++){
+      int appWidgetId = appWidgetIds[i];
+
+      Intent intent  = new Intent(context, MainActivity.class);
+      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.search_widget);
+      views.setOnClickPendingIntent(R.id.widget_search_bar, pendingIntent);
+
+      manager.updateAppWidget(appWidgetId,views);
+    }
+  }
+}
