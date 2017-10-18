21801df36b2dc1cb7f7866b929cc7644b904db62
José <jose.messejana@gmail.com>
2017-10-18 16:53:07 +0100

    [AN-2010] Mature and Landscape tests added. All tests requested created, some tests are not fully working yet

--- app/src/androidTest/java/cm/aptoide/pt/EspressoTests.java
+++ app/src/androidTest/java/cm/aptoide/pt/EspressoTests.java
@@ -12,6 +12,7 @@ import android.provider.MediaStore;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.GeneralLocation;
 import android.support.test.espresso.action.GeneralSwipeAction;
 import android.support.test.espresso.action.Press;
@@ -22,8 +23,13 @@ import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiSelector;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import cm.aptoide.pt.view.MainActivity;
 import java.io.IOException;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,8 +47,12 @@ import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.intending;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by jose_messejana on 26-09-2017.
@@ -57,10 +67,12 @@ public class EspressoTests {
   private final String LOGINEMAIL = "jose.messejana@aptoide.com";
   private final String PASS = "aptoide1234";
   private final String APP_TO_SEARCH = "Hearthstone";
-  private final String LIGHT_APP_TO_SEARCH = "Counter++";
+  private final String LIGHT_APP_TO_SEARCH = "Cut the rope";
+  private final String MATURE_SEARCH = "sexo";
   private final int WAIT_TIME = 550;
   private final int LONGER_WAIT_TIME = 2000;
   private final int NUMBER_OF_RETRIES = 3;
+  private final int NUMBER_OF_RESULTS = 5;
   @Rule public IntentsTestRule<MainActivity> mActivityRule =
       new IntentsTestRule<>(MainActivity.class);
   //@Rule public RetryTestRule retry = new RetryTestRule(NUMBER_OF_RETRIES);
@@ -70,6 +82,21 @@ public class EspressoTests {
   private static ViewAction swipeRigthOnLeftMost() {
     return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER_LEFT,
         GeneralLocation.CENTER_RIGHT, Press.FINGER);
+  }
+
+  public static Matcher<View> withRecyclerViewSize(final int size) {
+    return new TypeSafeMatcher<View>() {
+
+      @Override public boolean matchesSafely(final View view) {
+        final int actualListSize = ((RecyclerView) view).getAdapter()
+            .getItemCount();
+        return actualListSize >= size;
+      }
+
+      @Override public void describeTo(final Description description) {
+        description.appendText("RecyclerView should have " + size + " items");
+      }
+    };
   }
 
   @Before public void setUp() throws IOException {
@@ -141,6 +168,8 @@ public class EspressoTests {
     onView(withText(R.string.ws_error_WOP_9)).check(matches(isDisplayed()));
   }
 
+  // Third batch of tests
+
   @Test public void signUp() throws InterruptedException {
     goToMyAccount();
     performSignUp(SIGNUPEMAILTEST, PASS);
@@ -149,8 +178,6 @@ public class EspressoTests {
     }
     completeSignUp();
   }
-
-  // Third batch of tests
 
   @Test public void signUpPrivate() throws InterruptedException {
     goToMyAccount();
@@ -214,6 +241,7 @@ public class EspressoTests {
     createImageandIntend();
     onView(withId(R.id.button_camera)).perform(click());
     allow_permission(mDevice, "CAMERA");
+    allow_permission(mDevice, "WRITE_EXTERNAL_STORAGE");
     intended(hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
     Thread.sleep(LONGER_WAIT_TIME * 2);
     onView(withId(R.id.create_user_create_profile)).perform(click());
@@ -229,31 +257,98 @@ public class EspressoTests {
     goToMyAccount();
     onView(withId(R.id.my_account_edit_user_store)).perform(click());
     Thread.sleep(WAIT_TIME);
-    onView(withId(R.id.create_user_image_action)).perform(click());
+    onView(withId(R.id.create_store_image_action)).perform(click());
     Thread.sleep(WAIT_TIME);
     createImageandIntend();
     onView(withId(R.id.button_camera)).perform(click());
     allow_permission(mDevice, "CAMERA");
+    allow_permission(mDevice, "WRITE_EXTERNAL_STORAGE");
     intended(hasAction(MediaStore.ACTION_IMAGE_CAPTURE));
     Thread.sleep(LONGER_WAIT_TIME * 2);
-    onView(withId(R.id.create_user_create_profile)).perform(click());
+    onView(withId(R.id.create_store_action)).perform(click());
   }
 
   //Landscape tests
 
+  @Test public void matureTest() throws InterruptedException {
+    goToSettings();
+    onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+    onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+    Thread.sleep(WAIT_TIME);
+    try {
+      onView(withText(R.string.yes)).perform(click());
+    } catch (NoMatchingViewException e) {
+      Thread.sleep(LONGER_WAIT_TIME);
+      onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+      onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+      onView(withText(R.string.yes)).perform(click());
+    }
+    pressBackButton();
+    Thread.sleep(WAIT_TIME);
+    barOnlySearchApp(MATURE_SEARCH);
+    onView(withId(R.id.search_src_text)).perform(pressImeActionButton());
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.all_stores_result_list)).check(
+        matches(withRecyclerViewSize(NUMBER_OF_RESULTS)));
+    pressBackButton();
+    goToSettings();
+    onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+    onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+    Thread.sleep(WAIT_TIME);
+    pressBackButton();
+    Thread.sleep(WAIT_TIME);
+    barOnlySearchApp(MATURE_SEARCH);
+    onView(withId(R.id.search_src_text)).perform(pressImeActionButton());
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.all_stores_result_list)).check(
+        matches(not(withRecyclerViewSize(NUMBER_OF_RESULTS))));
+  }
+
+  @Test public void landscapewizard() throws InterruptedException {
+    Activity activity = mActivityRule.getActivity();
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.next_icon)).perform(swipeLeft());
+    Thread.sleep(WAIT_TIME);
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    Thread.sleep(WAIT_TIME);
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.next_icon)).perform(swipeLeft());
+    Thread.sleep(WAIT_TIME);
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    Thread.sleep(WAIT_TIME);
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.skip_text)).perform(click());
+    Thread.sleep(WAIT_TIME);
+    onView(withText(R.string.stores)).perform(click());
+  }
+
+  @Test public void landscapehometab() throws InterruptedException {
+    Activity activity = mActivityRule.getActivity();
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    Thread.sleep(WAIT_TIME);
+    onView(withText(R.string.stores)).perform(click());
+    onView(withText(R.string.home_title)).perform(click());
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(1, click()));
+  }
+
   @Test public void landscapeSearch() throws InterruptedException {
     Activity activity = mActivityRule.getActivity();
-    searchApp();
+    barOnlySearchApp(APP_TO_SEARCH);
     Thread.sleep(WAIT_TIME);
     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     Thread.sleep(LONGER_WAIT_TIME);
-    searchApp();
+    barOnlySearchApp(APP_TO_SEARCH);
     Thread.sleep(WAIT_TIME);
     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     Thread.sleep(LONGER_WAIT_TIME);
     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     Thread.sleep(WAIT_TIME);
-    searchApp();
+    barOnlySearchApp(APP_TO_SEARCH);
     Thread.sleep(WAIT_TIME);
     onView(withId(R.id.search_src_text)).perform(pressImeActionButton());
     onView(withId(R.id.all_stores_result_list)).check(matches(isDisplayed()));
@@ -270,7 +365,7 @@ public class EspressoTests {
   @Test public void landscapeAppView() throws InterruptedException {
     UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
     Activity activity = mActivityRule.getActivity();
-    goToApp(APP_TO_SEARCH);
+    goToApp(APP_TO_SEARCH, 0);
     cancelIfDownloading();
     Thread.sleep(WAIT_TIME);
     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
@@ -297,6 +392,44 @@ public class EspressoTests {
 
   // Initial tests
 
+  @Test public void settingsLandscape() throws InterruptedException {
+    boolean checked;
+    Activity activity = mActivityRule.getActivity();
+    goToSettings();
+    Thread.sleep(WAIT_TIME);
+    onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+    onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+    Thread.sleep(WAIT_TIME);
+    try {
+      onView(withText("YES")).perform(click());
+      checked = true;
+    } catch (Exception e) {
+      checked = false;
+    }
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    Thread.sleep(LONGER_WAIT_TIME);
+    onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+    onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+    Thread.sleep(WAIT_TIME);
+    if (!checked) {
+      onView(withText(R.string.yes)).perform(click());
+      checked = true;
+    } else {
+      onView(withText(R.string.yes)).check(matches(not(isDisplayed())));
+      checked = false;
+    }
+    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    onView(withId(R.id.list)).perform(RecyclerViewActions.scrollToPosition(11));
+    onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(11, click()));
+    Thread.sleep(WAIT_TIME);
+    if (!checked) {
+      onView(withText(R.string.yes)).perform(click());
+    } else {
+      onView(withText(R.string.yes)).check(matches(not(isDisplayed())));
+    }
+    Thread.sleep(LONGER_WAIT_TIME);
+  }
+
   @Test public void signIn() throws InterruptedException {
     goToMyAccount();
     performLogin(LOGINEMAIL, PASS);
@@ -311,7 +444,7 @@ public class EspressoTests {
 
   @Test public void download() throws InterruptedException {
     UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    goToApp(APP_TO_SEARCH);
+    goToApp(APP_TO_SEARCH, 0);
     cancelIfDownloading();
     while (!hasOpenedAppView()) {
       Thread.sleep(WAIT_TIME);
@@ -328,9 +461,11 @@ public class EspressoTests {
     onView(withId(R.id.action_btn)).check(matches(withText(R.string.appview_button_install)));
   }
 
+  //Boolean Methods
+
   @Test public void download_and_install() throws InterruptedException {
     UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    goToApp(LIGHT_APP_TO_SEARCH);
+    goToApp(LIGHT_APP_TO_SEARCH, 1);
     cancelIfDownloading();
     while (!hasOpenedAppView()) {
       Thread.sleep(WAIT_TIME);
@@ -339,15 +474,11 @@ public class EspressoTests {
     closePopUp();
     allow_permission(mDevice, "WRITE_EXTERNAL_STORAGE");
     closeIfIsNotLoggedInOnDownload();
-    Thread.sleep(WAIT_TIME);
-    onView(withId(R.id.download_progress)).check(matches(isDisplayed()));
-    Thread.sleep(LONGER_WAIT_TIME * 5);
+    Thread.sleep(LONGER_WAIT_TIME * 15);
     installApp(mDevice);
     Thread.sleep(LONGER_WAIT_TIME);
     onView(withId(R.id.action_btn)).check(matches(withText(R.string.open)));
   }
-
-  //Boolean Methods
 
   private boolean isFirstTime() {
     try {
@@ -408,14 +539,14 @@ public class EspressoTests {
     }
   }
 
+  // IfElse Actions
+
   private boolean hasPermission(String permission) {
     Context context = InstrumentationRegistry.getTargetContext();
     String finalpermission = "android.permission." + permission;
     int permissionStatus = ContextCompat.checkSelfPermission(context, finalpermission);
     return (permissionStatus == PackageManager.PERMISSION_GRANTED);
   }
-
-  // IfElse Actions
 
   private void closePopUp() throws InterruptedException {
     try {
@@ -458,6 +589,8 @@ public class EspressoTests {
     }
   }
 
+  //Refactored Methods
+
   private void cancelIfDownloading() {
     try {
       onView(withId(R.id.ic_action_cancel)).perform(click());
@@ -465,12 +598,10 @@ public class EspressoTests {
     }
   }
 
-  //Refactored Methods
-
   private void goToMyAccount() throws InterruptedException {
     onView(withId(R.id.toolbar)).perform(swipeRigthOnLeftMost());
     Thread.sleep(WAIT_TIME);
-    onView(withText(R.string.my_account_title_my_account)).perform(click());
+    onView(withText(R.string.drawer_title_my_account)).perform(click());
   }
 
   private void performLogin(String email, String pass) throws InterruptedException {
@@ -503,13 +634,13 @@ public class EspressoTests {
     onView(withId(R.id.skip_text)).perform(click());
   }
 
-  private void goToApp(String app) throws InterruptedException {
+  private void goToApp(String app, int pos) throws InterruptedException {
     onView(withId(R.id.action_search)).perform(click());
     Thread.sleep(WAIT_TIME);
     onView(withId(R.id.search_src_text)).perform(replaceText(app), pressImeActionButton());
     Thread.sleep(LONGER_WAIT_TIME);
     onView(withId(R.id.all_stores_result_list)).perform(
-        RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        RecyclerViewActions.actionOnItemAtPosition(pos, click()));
   }
 
   private void testIcActionButtons() throws InterruptedException {
@@ -579,10 +710,10 @@ public class EspressoTests {
     onView(withId(R.id.create_store_action)).perform(click());
   }
 
-  private void searchApp() throws InterruptedException {
+  private void barOnlySearchApp(String app) throws InterruptedException {
     onView(withId(R.id.action_search)).perform(click());
     Thread.sleep(WAIT_TIME);
-    onView(withId(R.id.search_src_text)).perform(replaceText(APP_TO_SEARCH));
+    onView(withId(R.id.search_src_text)).perform(replaceText(app));
   }
 
   private void installApp(UiDevice mDevice) {
@@ -603,11 +734,25 @@ public class EspressoTests {
   private void createImageandIntend() {
     Bitmap icon = BitmapFactory.decodeResource(InstrumentationRegistry.getTargetContext()
         .getResources(), R.mipmap.ic_launcher);
+
     // Build a result to return from the Camera app
     Intent resultData = new Intent();
     resultData.putExtra("data", icon);
     Instrumentation.ActivityResult result =
         new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
     intending(hasAction(MediaStore.ACTION_IMAGE_CAPTURE)).respondWith(result);
+  }
+
+  private void goToSettings() throws InterruptedException {
+    onView(withId(R.id.toolbar)).perform(swipeRigthOnLeftMost());
+    Thread.sleep(WAIT_TIME);
+    onView(withText(R.string.drawer_title_settings)).perform(click());
+  }
+
+  private void pressBackButton() {
+    ViewInteraction appCompatImageButton2 = onView(
+        allOf(withContentDescription("Navigate up"), withParent(withId(R.id.toolbar)),
+            isDisplayed()));
+    appCompatImageButton2.perform(click());
   }
 }
