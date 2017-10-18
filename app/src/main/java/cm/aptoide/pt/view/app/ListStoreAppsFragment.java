f73cb3725b7f6407732b04570d8330e2a08d414e
Trinkes <fabiojscosta1583@gmail.com>
2017-10-18 16:53:44 +0100

    [MyStoreMeta/AN-1580] - the number of columns of apps showed in ListStoreAppsFragment is now dynamic and it changes according to screen width

--- app/src/main/java/cm/aptoide/pt/view/app/ListStoreAppsFragment.java
+++ app/src/main/java/cm/aptoide/pt/view/app/ListStoreAppsFragment.java
@@ -1,5 +1,6 @@
 package cm.aptoide.pt.view.app;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import cm.aptoide.pt.AptoideApplication;
 import cm.aptoide.pt.R;
 import cm.aptoide.pt.analytics.ScreenTagHistory;
@@ -16,6 +18,7 @@ import cm.aptoide.pt.database.AccessorFactory;
 import cm.aptoide.pt.database.realm.Store;
 import cm.aptoide.pt.dataprovider.WebService;
 import cm.aptoide.pt.store.StoreCredentialsProviderImpl;
+import cm.aptoide.pt.utils.AptoideUtils;
 import cm.aptoide.pt.view.BackButtonFragment;
 import java.util.ArrayList;
 import java.util.List;
@@ -64,7 +67,7 @@ public class ListStoreAppsFragment extends BackButtonFragment implements ListSto
     RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
     adapter = new ListStoreAppsAdapter(new ArrayList<>(), appClicks);
     recyclerView.setAdapter(adapter);
-    recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+    recyclerView.setLayoutManager(new GridLayoutManager(getContext(), getSpanSize(3)));
   }
 
   @Override public ScreenTagHistory getHistoryTracker() {
@@ -84,5 +87,11 @@ public class ListStoreAppsFragment extends BackButtonFragment implements ListSto
 
   @Override public Observable<Application> getAppClick() {
     return appClicks;
+  }
+
+  public int getSpanSize(int defaultSpan) {
+    return (int) (AptoideUtils.ScreenU.getScreenWidthInDip(
+        (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE),
+        getContext().getResources()) / AptoideUtils.ScreenU.REFERENCE_WIDTH_DPI * defaultSpan);
   }
 }
