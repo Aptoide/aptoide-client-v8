4715d090ea135cf79619d12696adb080c183830f
Fábio Dias <fabio.dias@aptoide.com>
2017-10-20 12:16:18 +0100

    fix for issues AN-2064 and AN-2065

--- app/src/main/java/cm/aptoide/pt/view/store/StoreFragment.java
+++ app/src/main/java/cm/aptoide/pt/view/store/StoreFragment.java
@@ -6,6 +6,7 @@
 package cm.aptoide.pt.view.store;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -109,6 +110,7 @@ public class StoreFragment extends BasePagerToolbarFragment {
   private String marketName;
   private String defaultTheme;
   private Runnable registerViewpagerCurrentItem;
+  private SearchBuilder searchBuilder;
 
   public static StoreFragment newInstance(long userId, String storeTheme, OpenType openType) {
     return newInstance(userId, storeTheme, null, openType);
@@ -158,6 +160,17 @@ public class StoreFragment extends BasePagerToolbarFragment {
 
   @Override public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    final SearchManager searchManager =
+        (SearchManager) getContext().getSystemService(Context.SEARCH_SERVICE);
+
+    final String defaultStore = getDefaultStore();
+
+    final SearchNavigator searchNavigator =
+        new SearchNavigator(getFragmentNavigator(), storeName, defaultStore);
+
+    searchBuilder = new SearchBuilder(searchManager, searchNavigator);
+
     defaultTheme = ((AptoideApplication) getContext().getApplicationContext()).getDefaultTheme();
     tokenInvalidator =
         ((AptoideApplication) getContext().getApplicationContext()).getTokenInvalidator();
@@ -334,11 +347,11 @@ public class StoreFragment extends BasePagerToolbarFragment {
   @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     inflater.inflate(R.menu.menu_search, menu);
-    String defaultStore = getDefaultStore();
-    SearchBuilder searchBuilder =
-        new SearchBuilder(menu.findItem(R.id.action_search), getActivity(),
-            new SearchNavigator(getFragmentNavigator(), storeName, defaultStore));
-    searchBuilder.validateAndAttachSearch();
+    if (searchBuilder != null && searchBuilder.isValid()) {
+      searchBuilder.attachSearch(getContext(), menu.findItem(R.id.action_search));
+    } else {
+      menu.removeItem(R.id.action_search);
+    }
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
