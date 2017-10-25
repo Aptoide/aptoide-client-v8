92664e10f2adf7d2464abb1d6d5fb4b502aa0635
diogoloureiro <!a12110650>
2017-10-25 17:11:23 +0100

    [AN-300] moved AdsRepository, GetStoreWidgetsRequestFactory and RequestFactory to the Vanilla dimension.  implemented AdsRepository, GetStoreWidgetsRequestFactory and RequestFactory on the Partners dimension.  first install related methods are now only on Partners dimension implementation.

---
+++ app/src/vanilla/java/cm/aptoide/pt/ads/AdsRepository.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2016.
+ * Modified on 04/08/2016.
+ */
+
+package cm.aptoide.pt.ads;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import cm.aptoide.accountmanager.AptoideAccountManager;
+import cm.aptoide.pt.database.realm.MinimalAd;
+import cm.aptoide.pt.dataprovider.model.v2.GetAdsResponse;
+import cm.aptoide.pt.dataprovider.ws.v2.aptwords.AdsApplicationVersionCodeProvider;
+import cm.aptoide.pt.dataprovider.ws.v2.aptwords.GetAdsRequest;
+import cm.aptoide.pt.networking.IdsRepository;
+import cm.aptoide.pt.preferences.managed.ManagerPreferences;
+import cm.aptoide.pt.utils.q.QManager;
+import java.util.LinkedList;
+import java.util.List;
+import okhttp3.OkHttpClient;
+import retrofit2.Converter;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by marcelobenites on 7/27/16.
+ */
+public class AdsRepository {
+
+  private final IdsRepository idsRepository;
+  private final AptoideAccountManager accountManager;
+  private final GooglePlayServicesAvailabilityChecker googlePlayServicesAvailabilityChecker;
+  private final PartnerIdProvider partnerIdProvider;
+  private final OkHttpClient httpClient;
+  private final Converter.Factory converterFactory;
+  private final QManager qManager;
+  private final SharedPreferences sharedPreferences;
+  private final Context context;
+  private final ConnectivityManager connectivityManager;
+  private final Resources resources;
+  private final AdsApplicationVersionCodeProvider versionCodeProvider;
+  private final MinimalAdMapper adMapper;
+
+  public AdsRepository(IdsRepository idsRepository, AptoideAccountManager accountManager,
+      OkHttpClient httpClient, Converter.Factory converterFactory, QManager qManager,
+      SharedPreferences sharedPreferences, Context applicationContext,
+      ConnectivityManager connectivityManager, Resources resources,
+      AdsApplicationVersionCodeProvider versionCodeProvider,
+      GooglePlayServicesAvailabilityChecker googlePlayServicesAvailabilityChecker,
+      PartnerIdProvider partnerIdProvider, MinimalAdMapper adMapper) {
+    this.idsRepository = idsRepository;
+    this.accountManager = accountManager;
+    this.versionCodeProvider = versionCodeProvider;
+    this.googlePlayServicesAvailabilityChecker = googlePlayServicesAvailabilityChecker;
+    this.partnerIdProvider = partnerIdProvider;
+    this.httpClient = httpClient;
+    this.converterFactory = converterFactory;
+    this.qManager = qManager;
+    this.sharedPreferences = sharedPreferences;
+    this.context = applicationContext;
+    this.connectivityManager = connectivityManager;
+    this.resources = resources;
+    this.adMapper = adMapper;
+  }
+
+  public static boolean validAds(List<GetAdsResponse.Ad> ads) {
+    return ads != null
+        && !ads.isEmpty()
+        && ads.get(0) != null
+        && ads.get(0)
+        .getPartner() != null
+        && ads.get(0)
+        .getPartner()
+        .getData() != null;
+  }
+
+  public static boolean validAds(GetAdsResponse getAdsResponse) {
+    return getAdsResponse != null && validAds(getAdsResponse.getAds());
+  }
+
+  public Observable<MinimalAd> getAdsFromAppView(String packageName, String storeName) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAd(GetAdsRequest.ofAppviewOrganic(packageName, storeName,
+            idsRepository.getUniqueIdentifier(),
+            googlePlayServicesAvailabilityChecker.isAvailable(context),
+            partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+            converterFactory,
+            qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+            sharedPreferences, connectivityManager, resources, versionCodeProvider)
+            .observe()));
+  }
+
+  private Observable<MinimalAd> mapToMinimalAd(
+      Observable<GetAdsResponse> getAdsResponseObservable) {
+    return getAdsResponseObservable.map((getAdsResponse) -> getAdsResponse.getAds())
+        .flatMap(ads -> {
+          if (!validAds(ads)) {
+            return Observable.error(new IllegalStateException("Invalid ads returned from server"));
+          }
+          return Observable.just(ads.get(0));
+        })
+        .map((ad) -> adMapper.map(ad));
+  }
+
+  public Observable<List<MinimalAd>> getAdsFromHomepageMore(boolean refresh) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAds(
+            GetAdsRequest.ofHomepageMore(idsRepository.getUniqueIdentifier(),
+                googlePlayServicesAvailabilityChecker.isAvailable(context),
+                partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+                converterFactory,
+                qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+                sharedPreferences, connectivityManager, resources, versionCodeProvider)
+                .observe(refresh)));
+  }
+
+  private Observable<List<MinimalAd>> mapToMinimalAds(
+      Observable<GetAdsResponse> getAdsResponseObservable) {
+    return getAdsResponseObservable.flatMap(ads -> {
+      if (!validAds(ads)) {
+        return Observable.error(new IllegalStateException("Invalid ads returned from server"));
+      }
+      return Observable.just(ads);
+    })
+        .map((getAdsResponse) -> getAdsResponse.getAds())
+        .map(ads -> {
+          List<MinimalAd> minimalAds = new LinkedList<>();
+          for (GetAdsResponse.Ad ad : ads) {
+            minimalAds.add(adMapper.map(ad));
+          }
+          return minimalAds;
+        });
+  }
+
+  public Observable<List<MinimalAd>> getAdsFromAppviewSuggested(String packageName,
+      List<String> keywords) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAds(
+            GetAdsRequest.ofAppviewSuggested(keywords, idsRepository.getUniqueIdentifier(),
+                googlePlayServicesAvailabilityChecker.isAvailable(context), packageName,
+                partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+                converterFactory,
+                qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+                sharedPreferences, connectivityManager, resources, versionCodeProvider)
+                .observe()).subscribeOn(Schedulers.io()));
+  }
+
+  public Observable<MinimalAd> getAdsFromSearch(String query) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAd(
+            GetAdsRequest.ofSearch(query, idsRepository.getUniqueIdentifier(),
+                googlePlayServicesAvailabilityChecker.isAvailable(context),
+                partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+                converterFactory,
+                qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+                sharedPreferences, connectivityManager, resources, versionCodeProvider)
+                .observe()));
+  }
+
+  public Observable<MinimalAd> getAdsFromSecondInstall(String packageName) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAd(
+            GetAdsRequest.ofSecondInstall(packageName, idsRepository.getUniqueIdentifier(),
+                googlePlayServicesAvailabilityChecker.isAvailable(context),
+                partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+                converterFactory,
+                qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+                sharedPreferences, connectivityManager, resources, versionCodeProvider)
+                .observe()));
+  }
+
+  public Observable<MinimalAd> getAdsFromSecondTry(String packageName) {
+    return accountManager.accountStatus()
+        .first()
+        .flatMap(account -> mapToMinimalAd(
+            GetAdsRequest.ofSecondTry(packageName, idsRepository.getUniqueIdentifier(),
+                googlePlayServicesAvailabilityChecker.isAvailable(context),
+                partnerIdProvider.getPartnerId(), account.isAdultContentEnabled(), httpClient,
+                converterFactory,
+                qManager.getFilters(ManagerPreferences.getHWSpecsFilter(sharedPreferences)),
+                sharedPreferences, connectivityManager, resources, versionCodeProvider)
+                .observe()));
+  }
+}
---
+++ app/src/vanilla/java/cm/aptoide/pt/repository/request/RequestFactory.java
@@ -0,0 +1,120 @@
+package cm.aptoide.pt.repository.request;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.view.WindowManager;
+import cm.aptoide.pt.dataprovider.interfaces.TokenInvalidator;
+import cm.aptoide.pt.dataprovider.ws.BodyInterceptor;
+import cm.aptoide.pt.dataprovider.ws.v2.aptwords.AdsApplicationVersionCodeProvider;
+import cm.aptoide.pt.dataprovider.ws.v7.BaseBody;
+import cm.aptoide.pt.dataprovider.ws.v7.GetRecommendedRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.ListAppsRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.ListFullReviewsRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.store.GetRecommendedStoresRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.store.GetStoreRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.store.GetStoreWidgetsRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.store.GetUserRequest;
+import cm.aptoide.pt.dataprovider.ws.v7.store.ListStoresRequest;
+import cm.aptoide.pt.store.StoreCredentialsProvider;
+import okhttp3.OkHttpClient;
+import retrofit2.Converter;
+
+/**
+ * Created by neuro on 26-12-2016.
+ */
+
+public class RequestFactory {
+
+  private final ListStoresRequestFactory listStoresRequestFactory;
+  private final ListAppsRequestFactory listAppsRequestFactory;
+  private final ListFullReviewsRequestFactory listFullReviewsRequestFactory;
+  private final GetStoreRequestFactory getStoreRequestFactory;
+  private final GetStoreWidgetsRequestFactory getStoreWidgetsRequestFactory;
+  private final StoreCredentialsProvider storeCredentialsProvider;
+  private final GetUserRequestFactory getUserRequestFactory;
+  private final GetStoreRecommendedRequestFactory getStoreRecommendedRequestFactory;
+  private final GetRecommendedRequestFactory getRecommendedRequestFactory;
+  private final boolean googlePlayServicesAvailable;
+
+  public RequestFactory(StoreCredentialsProvider storeCredentialsProvider,
+      BodyInterceptor<BaseBody> bodyInterceptor, OkHttpClient httpClient,
+      Converter.Factory converterFactory, TokenInvalidator tokenInvalidator,
+      SharedPreferences sharedPreferences, Resources resources, WindowManager windowManager,
+      String clientUniqueId, String partnerId, boolean accountMature, String filters,
+      ConnectivityManager connectivityManager,
+      AdsApplicationVersionCodeProvider versionCodeProvider, boolean googlePlayServicesAvailable) {
+    this.storeCredentialsProvider = storeCredentialsProvider;
+    this.googlePlayServicesAvailable = googlePlayServicesAvailable;
+    listStoresRequestFactory =
+        new ListStoresRequestFactory(bodyInterceptor, httpClient, converterFactory,
+            tokenInvalidator, sharedPreferences);
+    listAppsRequestFactory =
+        new ListAppsRequestFactory(bodyInterceptor, storeCredentialsProvider, httpClient,
+            converterFactory, tokenInvalidator, sharedPreferences, resources, windowManager);
+    listFullReviewsRequestFactory =
+        new ListFullReviewsRequestFactory(bodyInterceptor, httpClient, converterFactory,
+            tokenInvalidator, sharedPreferences);
+    getStoreRequestFactory =
+        new GetStoreRequestFactory(storeCredentialsProvider, bodyInterceptor, httpClient,
+            converterFactory, tokenInvalidator, sharedPreferences, resources, windowManager);
+    getStoreWidgetsRequestFactory =
+        new GetStoreWidgetsRequestFactory(storeCredentialsProvider, bodyInterceptor, httpClient,
+            converterFactory, tokenInvalidator, sharedPreferences, resources, windowManager,
+            clientUniqueId, partnerId, accountMature, filters, connectivityManager,
+            versionCodeProvider);
+    getUserRequestFactory =
+        new GetUserRequestFactory(bodyInterceptor, httpClient, converterFactory, tokenInvalidator,
+            sharedPreferences, resources, windowManager, storeCredentialsProvider, clientUniqueId,
+            partnerId, accountMature, filters, connectivityManager, versionCodeProvider);
+
+    getStoreRecommendedRequestFactory =
+        new GetStoreRecommendedRequestFactory(bodyInterceptor, httpClient, converterFactory,
+            tokenInvalidator, sharedPreferences);
+    getRecommendedRequestFactory =
+        new GetRecommendedRequestFactory(bodyInterceptor, httpClient, converterFactory,
+            tokenInvalidator, sharedPreferences);
+  }
+
+  public ListStoresRequest newListStoresRequest(int offset, int limit) {
+    return this.listStoresRequestFactory.newListStoresRequest(offset, limit);
+  }
+
+  public ListStoresRequest newListStoresRequest(String url) {
+    return this.listStoresRequestFactory.newListStoresRequest(url);
+  }
+
+  public ListAppsRequest newListAppsRequest(String url) {
+    return this.listAppsRequestFactory.newListAppsRequest(url);
+  }
+
+  public ListAppsRequest newListAppsRequest(int storeId, Long groupId, int limit,
+      ListAppsRequest.Sort sort) {
+    return this.listAppsRequestFactory.newListAppsRequest(storeId, groupId, limit, sort);
+  }
+
+  public ListFullReviewsRequest newListFullReviews(String url, boolean refresh) {
+    return this.listFullReviewsRequestFactory.newListFullReviews(url, refresh,
+        storeCredentialsProvider.fromUrl(url));
+  }
+
+  public GetStoreRequest newStore(String url) {
+    return this.getStoreRequestFactory.newStore(url);
+  }
+
+  public GetStoreWidgetsRequest newStoreWidgets(String url) {
+    return this.getStoreWidgetsRequestFactory.newStoreWidgets(url, googlePlayServicesAvailable);
+  }
+
+  public GetUserRequest newGetUser(String url) {
+    return this.getUserRequestFactory.newGetUser(url, googlePlayServicesAvailable);
+  }
+
+  public GetRecommendedStoresRequest newGetRecommendedStores(String url) {
+    return this.getStoreRecommendedRequestFactory.newRecommendedStore(url);
+  }
+
+  public GetRecommendedRequest newGetRecommendedRequest(int limit, String packageName) {
+    return this.getRecommendedRequestFactory.newGetRecommendedRequest(limit, packageName);
+  }
+}
