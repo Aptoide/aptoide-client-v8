2b22c5d26ffa83f920cd315b4c743c72a35a7b63
Marcelo Benites <marcelo.benites@aptoide.com>
2017-11-02 13:04:58 +0000

    fix partners flavor build

--- app/src/partner/java/cm/aptoide/pt/remotebootconfig/datamodel/Multistore.java
+++ app/src/partner/java/cm/aptoide/pt/remotebootconfig/datamodel/Multistore.java
@@ -1,23 +1,28 @@
 package cm.aptoide.pt.remotebootconfig.datamodel;
 
-import lombok.Data;
-
 /**
  * Created by diogoloureiro on 18/01/2017.
  *
  * Multistore class
  */
 
-@SuppressWarnings("WeakerAccess") @Data public class Multistore {
+public class Multistore {
   private boolean enable;
   private boolean search;
 
-  /**
-   * @param enable allows the user to view other Store views
-   * @param search allows users to search apps on other Stores
-   */
-  public Multistore(boolean enable, boolean search) {
+  public boolean isEnable() {
+    return enable;
+  }
+
+  public void setEnable(boolean enable) {
     this.enable = enable;
+  }
+
+  public boolean isSearch() {
+    return search;
+  }
+
+  public void setSearch(boolean search) {
     this.search = search;
   }
 }
--- app/src/partner/java/cm/aptoide/pt/remotebootconfig/datamodel/Switches.java
+++ app/src/partner/java/cm/aptoide/pt/remotebootconfig/datamodel/Switches.java
@@ -1,23 +1,28 @@
 package cm.aptoide.pt.remotebootconfig.datamodel;
 
-import lombok.Data;
-
 /**
  * Created by diogoloureiro on 18/01/2017.
  *
  * Switches class
  */
 
-@SuppressWarnings("WeakerAccess") @Data public class Switches {
+public class Switches {
   private Mature mature;
   private Options options;
 
-  /**
-   * @param mature mature options configs
-   * @param options partner store options
-   */
-  public Switches(Mature mature, Options options) {
+  public Mature getMature() {
+    return mature;
+  }
+
+  public void setMature(Mature mature) {
     this.mature = mature;
+  }
+
+  public Options getOptions() {
+    return options;
+  }
+
+  public void setOptions(Options options) {
     this.options = options;
   }
 }
