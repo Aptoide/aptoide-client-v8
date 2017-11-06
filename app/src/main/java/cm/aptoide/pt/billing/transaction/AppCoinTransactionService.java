9999afafc44fb4ec9e62a2e86688cf3296f13117
Neurophobic Animal <NeurophobicAnimal@gmail.com>
2017-11-06 16:03:10 +0000

    Changed AppCoinTransactionService.java RECEIVER_ADDR.

--- app/src/main/java/cm/aptoide/pt/billing/transaction/AppCoinTransactionService.java
+++ app/src/main/java/cm/aptoide/pt/billing/transaction/AppCoinTransactionService.java
@@ -38,7 +38,7 @@ public class AppCoinTransactionService implements TransactionService {
   private SharedPreferences sharedPreferences;
   private Map<String, Product> products = new HashMap<>();
   public static final String CONTRACT_ADDRESS = "8dbf4349cbeca08a02cc6b5b0862f9dd42c585b9";
-  private static final String RECEIVER_ADDR = "62a5c1680554A61334F5c6f6D7dA6044b6AFbFe8";
+  private static final String RECEIVER_ADDR = "9bf504d8f8abeca13863c3d260d7be237fa3fd18";
   private static final boolean REALTRANSACTION = true;
 
   public AppCoinTransactionService(TransactionMapper transactionMapper,
