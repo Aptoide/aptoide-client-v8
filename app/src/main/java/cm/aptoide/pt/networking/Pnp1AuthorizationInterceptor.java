8a6eee3a625c5f9fd25145a07248ab12dbc1c1ca
Trinkes <fabiojscosta1583@gmail.com>
2017-11-06 19:10:03 +0000

    [SocialNotifications/AN-2068] - token invalidation and addition is now made by okhttp interceptor.

---
+++ app/src/main/java/cm/aptoide/pt/networking/Pnp1AuthorizationInterceptor.java
@@ -0,0 +1,63 @@
+package cm.aptoide.pt.networking;
+
+import cm.aptoide.pt.dataprovider.interfaces.TokenInvalidator;
+import java.io.IOException;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Pnp1AuthorizationInterceptor implements Interceptor {
+  public static final String ACCESS_TOKEN_KEY = "access_token";
+  private final AuthenticationPersistence authenticationPersistence;
+  private final TokenInvalidator tokenInvalidator;
+  private long timeStamp;
+
+  public Pnp1AuthorizationInterceptor(AuthenticationPersistence authenticationPersistence,
+      TokenInvalidator tokenInvalidator) {
+    this.authenticationPersistence = authenticationPersistence;
+    this.tokenInvalidator = tokenInvalidator;
+    timeStamp = 0;
+  }
+
+  @Override public Response intercept(Chain chain) throws IOException {
+    Request request = chain.request();
+
+    HttpUrl url = request.url()
+        .newBuilder()
+        .addQueryParameter(ACCESS_TOKEN_KEY, authenticationPersistence.getAuthentication()
+            .toBlocking()
+            .value()
+            .getAccessToken())
+        .build();
+    request = request.newBuilder()
+        .url(url)
+        .build();
+
+    Response response = chain.proceed(request);
+    if (response.code() == 401) {
+      invalidateToken();
+      url = request.url()
+          .newBuilder()
+          .setQueryParameter(ACCESS_TOKEN_KEY, authenticationPersistence.getAuthentication()
+              .toBlocking()
+              .value()
+              .getAccessToken())
+          .build();
+      Request invalidatedRequest = request.newBuilder()
+          .url(url)
+          .build();
+      response = chain.proceed(invalidatedRequest);
+    }
+
+    return response;
+  }
+
+  private synchronized void invalidateToken() {
+    if (System.currentTimeMillis() - timeStamp > 5000) {
+      tokenInvalidator.invalidateAccessToken()
+          .await();
+      timeStamp = System.currentTimeMillis();
+    }
+  }
+}
--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v1/notification/PullSocialNotificationRequest.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v1/notification/PullSocialNotificationRequest.java
@@ -8,49 +8,35 @@ package cm.aptoide.pt.dataprovider.ws.v1.notification;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.text.TextUtils;
-import cm.aptoide.pt.dataprovider.exception.AptoideWsV7Exception;
-import cm.aptoide.pt.dataprovider.interfaces.TokenInvalidator;
 import cm.aptoide.pt.dataprovider.model.v1.GetPullNotificationsResponse;
-import cm.aptoide.pt.dataprovider.model.v7.BaseV7Response;
-import cm.aptoide.pt.dataprovider.ws.BodyInterceptor;
 import cm.aptoide.pt.dataprovider.ws.v1.PnpV1WebService;
 import cm.aptoide.pt.dataprovider.ws.v1.Service;
 import cm.aptoide.pt.preferences.toolbox.ToolboxManager;
 import cm.aptoide.pt.utils.AptoideUtils;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import okhttp3.OkHttpClient;
 import retrofit2.Converter;
-import retrofit2.adapter.rxjava.HttpException;
 import rx.Observable;
 
 public class PullSocialNotificationRequest
     extends PnpV1WebService<List<GetPullNotificationsResponse>> {
   private static List<Integer> pretendedNotificationTypes;
-  private final String INVALID_ACCESS_TOKEN_CODE = "AUTH-2";
   private final Map<String, String> options;
   private final String id;
-  private final TokenInvalidator tokenInvalidator;
-  private final BodyInterceptor<Map<String, String>> interceptor;
-  private boolean accessTokenRetry = false;
 
   private PullSocialNotificationRequest(String id, Map<String, String> options,
-      OkHttpClient okHttpClient, Converter.Factory converterFactory,
-      TokenInvalidator tokenInvalidator, BodyInterceptor<Map<String, String>> interceptor) {
+      OkHttpClient okHttpClient, Converter.Factory converterFactory) {
     super(okHttpClient, converterFactory);
     this.options = options;
     this.id = id;
-    this.tokenInvalidator = tokenInvalidator;
-    this.interceptor = interceptor;
   }
 
   public static PullSocialNotificationRequest of(String uniqueIdentifier, String versionName,
       String appId, OkHttpClient httpClient, Converter.Factory converterFactory, String oemid,
-      SharedPreferences sharedPreferences, Resources resources, TokenInvalidator tokenInvalidator,
-      BodyInterceptor<Map<String, String>> interceptor, boolean isLogged) {
+      SharedPreferences sharedPreferences, Resources resources, boolean isLogged) {
 
     Map<String, String> options = new HashMap<>();
     pretendedNotificationTypes = new ArrayList<>();
@@ -76,48 +62,12 @@ public class PullSocialNotificationRequest
     options.put("status_in_json", String.valueOf(true));
 
     return new PullSocialNotificationRequest(uniqueIdentifier, options, httpClient,
-        converterFactory, tokenInvalidator, interceptor);
+        converterFactory);
   }
 
   @Override
   protected Observable<List<GetPullNotificationsResponse>> loadDataFromNetwork(Service interfaces,
       boolean bypassCache) {
-    return interceptor.intercept(options)
-        .toObservable()
-        .flatMap(options -> handleToken(
-            interfaces.getPullSocialNotifications(true, id, pretendedNotificationTypes, options),
-            true));
-  }
-
-  private Observable<List<GetPullNotificationsResponse>> handleToken(
-      Observable<List<GetPullNotificationsResponse>> observable, boolean bypassCache) {
-
-    return observable.onErrorResumeNext(throwable -> {
-      if (throwable instanceof HttpException) {
-        try {
-
-          BaseV7Response genericResponse =
-              (BaseV7Response) converterFactory.responseBodyConverter(BaseV7Response.class, null,
-                  null)
-                  .convert(((HttpException) throwable).response()
-                      .errorBody());
-          if (INVALID_ACCESS_TOKEN_CODE.equals(genericResponse.getError()
-              .getCode())) {
-            if (!accessTokenRetry) {
-              accessTokenRetry = true;
-              return tokenInvalidator.invalidateAccessToken()
-                  .andThen(observe(bypassCache));
-            }
-          } else {
-            AptoideWsV7Exception exception = new AptoideWsV7Exception(throwable);
-            exception.setBaseResponse(genericResponse);
-            return Observable.error(exception);
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-      return Observable.error(throwable);
-    });
+    return interfaces.getPullSocialNotifications(true, id, pretendedNotificationTypes, options);
   }
 }
