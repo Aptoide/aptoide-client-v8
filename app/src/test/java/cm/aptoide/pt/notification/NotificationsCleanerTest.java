476ef0485c06c1c04cce40045381e9d3f2bdbe9a
Ana Lara <ana.simoes@aptoide.com>
2017-12-11 20:46:02 +0000

    code style

--- app/src/test/java/cm/aptoide/pt/notification/NotificationsCleanerTest.java
+++ app/src/test/java/cm/aptoide/pt/notification/NotificationsCleanerTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import rx.Completable;
 import rx.Observable;
