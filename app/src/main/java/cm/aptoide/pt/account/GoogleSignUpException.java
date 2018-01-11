091f32b8d901f2118ae0502e08151ce3630490e6
jdandrade <joao.andrade@aptoide.com>
2018-01-11 16:32:12 +0000

    [AN-2233] add login invalid status code

--- app/src/main/java/cm/aptoide/pt/account/GoogleSignUpException.java
+++ app/src/main/java/cm/aptoide/pt/account/GoogleSignUpException.java
@@ -3,9 +3,15 @@ package cm.aptoide.pt.account;
 public class GoogleSignUpException extends Exception {
 
   private final String error;
+  private final int statusCode;
 
-  public GoogleSignUpException(String error) {
+  public GoogleSignUpException(String error, int statusCode) {
     this.error = error;
+    this.statusCode = statusCode;
+  }
+
+  public int getStatusCode() {
+    return statusCode;
   }
 
   public String getError() {
