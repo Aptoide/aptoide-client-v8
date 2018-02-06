334c67978f999b4c332cfb3624fd3785cac71906
José <jose.messejana@gmail.com>
2018-02-06 10:39:26 +0000

    [AN-2094] Added static imports now needed to run espresso tests

--- app/src/androidTest/java/cm/aptoide/pt/LandscapeUITests.java
+++ app/src/androidTest/java/cm/aptoide/pt/LandscapeUITests.java
@@ -18,6 +18,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static cm.aptoide.pt.UITests.goToSettings;
 import static cm.aptoide.pt.UITests.skipWizard;
 
