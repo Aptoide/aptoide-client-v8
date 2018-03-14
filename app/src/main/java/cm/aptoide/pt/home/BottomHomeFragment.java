cfae319b582b82862f5bcfa6df9beecc84380d06
jdandrade <joao.andrade@aptoide.com>
2018-03-14 10:37:03 +0000

    [ASV-177] refactor: extract fake bundles to a fake bundle data source for testing.

--- app/src/main/java/cm/aptoide/pt/home/BottomHomeFragment.java
+++ app/src/main/java/cm/aptoide/pt/home/BottomHomeFragment.java
@@ -11,7 +11,6 @@ import android.widget.ProgressBar;
 import cm.aptoide.pt.R;
 import cm.aptoide.pt.dataprovider.model.v2.GetAdsResponse;
 import cm.aptoide.pt.view.app.Application;
-import cm.aptoide.pt.view.app.FeatureGraphicApplication;
 import cm.aptoide.pt.view.fragment.FragmentView;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -107,50 +106,5 @@ public class BottomHomeFragment extends FragmentView implements HomeView {
 
   @Override public Observable<GetAdsResponse.Ad> adClicked() {
     return adClickedEvents;
-  }
-
-  public List<AppBundle> getFakeBundles() {
-    List<Application> tmp = new ArrayList<>();
-    String icon = "https://placeimg.com/640/480/any";
-    Application aptoide = new Application("Aptoide", icon, 0, 1000, "cm.aptoide.pt", 300);
-    tmp.add(aptoide);
-    Application facebook =
-        new Application("Facebook", icon, (float) 4.2, 1000, "katana.facebook.com", 30);
-    tmp.add(facebook);
-    tmp.add(aptoide);
-    tmp.add(facebook);
-    tmp.add(aptoide);
-    tmp.add(facebook);
-    tmp.add(aptoide);
-    tmp.add(facebook);
-
-    List<Application> tmp1 = new ArrayList<>();
-    FeatureGraphicApplication aptoideFeatureGraphic =
-        new FeatureGraphicApplication("Aptoide", icon, 0, 1000, "cm.aptoide.pt", 300, icon);
-    tmp.add(aptoideFeatureGraphic);
-    FeatureGraphicApplication facebookFeatureGraphic =
-        new FeatureGraphicApplication("Facebook", icon, (float) 4.2, 1000, "katana.facebook.com",
-            30, icon);
-    tmp1.add(facebookFeatureGraphic);
-    tmp1.add(aptoideFeatureGraphic);
-    tmp1.add(facebookFeatureGraphic);
-    tmp1.add(aptoideFeatureGraphic);
-    tmp1.add(facebookFeatureGraphic);
-    tmp1.add(aptoideFeatureGraphic);
-    tmp1.add(facebookFeatureGraphic);
-    tmp1.add(aptoideFeatureGraphic);
-    tmp1.add(facebookFeatureGraphic);
-    AppBundle appBundle =
-        new AppBundle("As escolhas do filipe", tmp1, AppBundle.BundleType.EDITORS, null, "");
-    AppBundle appBundle1 =
-        new AppBundle("piores apps locais", tmp, AppBundle.BundleType.APPS, null, "");
-    AppBundle appBundle2 =
-        new AppBundle("um pouco melhor apps", tmp, AppBundle.BundleType.APPS, null, "");
-
-    List<AppBundle> appBundles = new ArrayList<>();
-    appBundles.add(appBundle);
-    appBundles.add(appBundle1);
-    appBundles.add(appBundle2);
-    return appBundles;
   }
 }
