8bf7aa9845ba6d1fed7a6646e6799be7ba1bf84c
José <jose.messejana@gmail.com>
2018-03-15 11:07:59 +0000

    [ASV-174] Adapted branch to new changes

--- app/src/main/java/cm/aptoide/pt/store/view/my/MyStoresView.java
+++ app/src/main/java/cm/aptoide/pt/store/view/my/MyStoresView.java
@@ -1,11 +1,13 @@
 package cm.aptoide.pt.store.view.my;
 
-import cm.aptoide.pt.home.BottomNavigationFragment;
+import cm.aptoide.pt.presenter.View;
 
 /**
  * Created by D01 on 14/03/18.
  */
 
-public interface MyStoresView extends BottomNavigationFragment {
+public interface MyStoresView extends View {
+
+  void scrollToTop();
 
 }
