0844a66c42460899124cab1cc0e850b1e40a3de4
paquiquinho <paquiquinho@gmail.com>
2018-03-21 10:33:35 +0000

    [ASV-172] Fixed mistake with string and icons in the new account fragment;

--- aptoidepreferences/src/main/java/cm/aptoide/pt/preferences/managed/ManagerPreferences.java
+++ aptoidepreferences/src/main/java/cm/aptoide/pt/preferences/managed/ManagerPreferences.java
@@ -56,7 +56,7 @@ public class ManagerPreferences {
   }
 
   public static boolean getGeneralDownloadsMobile(SharedPreferences sharedPreferences) {
-    return sharedPreferences.getBoolean(ManagedKeys.GENERAL_DOWNLOADS_MOBILE, true);
+    return !sharedPreferences.getBoolean(ManagedKeys.GENERAL_DOWNLOADS_MOBILE, true);
   }
 
   public static boolean getAnimationsEnabledStatus(SharedPreferences defaultSharedPreferences) {
