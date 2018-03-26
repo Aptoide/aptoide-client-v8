e541956d639d739eb37367e7d1f6b46bbab1015e
phillipsgoncalves <filisgoncalves@gmail.com>
2018-03-26 22:59:01 +0100

    [ASV-175] Created apps navigator

---
+++ app/src/main/java/cm/aptoide/pt/home/apps/AppsNavigator.java
@@ -0,0 +1,23 @@
+package cm.aptoide.pt.home.apps;
+
+import cm.aptoide.pt.app.view.AppViewFragment;
+import cm.aptoide.pt.navigator.FragmentNavigator;
+
+/**
+ * Created by filipegoncalves on 3/26/18.
+ */
+
+public class AppsNavigator {
+
+  private final FragmentNavigator fragmentNavigator;
+
+  public AppsNavigator(FragmentNavigator fragmentNavigator) {
+    this.fragmentNavigator = fragmentNavigator;
+  }
+
+  public void navigateToAppView(long appId, String packageName) {
+    fragmentNavigator.navigateTo(
+        AppViewFragment.newInstance(appId, packageName, AppViewFragment.OpenType.OPEN_ONLY, ""),
+        true);
+  }
+}
