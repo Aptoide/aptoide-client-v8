e3a7ae9dd1afb92a4262e80cc22a28b3bcc928f9
José <jose.messejana@gmail.com>
2018-03-27 09:08:18 +0100

    [ASV-214] Cast to LinearLayout on empty_state. Removed previous unused analyticsManager injection

--- app/src/main/java/cm/aptoide/pt/notification/view/InboxFragment.java
+++ app/src/main/java/cm/aptoide/pt/notification/view/InboxFragment.java
@@ -7,17 +7,16 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import cm.aptoide.pt.AptoideApplication;
 import cm.aptoide.pt.R;
 import cm.aptoide.pt.analytics.ScreenTagHistory;
-import cm.aptoide.pt.analytics.analytics.AnalyticsManager;
 import cm.aptoide.pt.crashreports.CrashReport;
 import cm.aptoide.pt.navigator.ActivityResultNavigator;
 import cm.aptoide.pt.notification.AptoideNotification;
 import cm.aptoide.pt.view.fragment.BaseToolbarFragment;
 import java.util.Collections;
 import java.util.List;
-import javax.inject.Inject;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.subjects.PublishSubject;
@@ -28,11 +27,10 @@ import rx.subjects.PublishSubject;
 
 public class InboxFragment extends BaseToolbarFragment implements InboxView {
 
-  @Inject AnalyticsManager analyticsManager;
   private RecyclerView list;
   private InboxAdapter adapter;
   private PublishSubject<AptoideNotification> notificationSubject;
-  private View emptyState;
+  private LinearLayout emptyState;
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
     int itemId = item.getItemId();
@@ -64,7 +62,7 @@ public class InboxFragment extends BaseToolbarFragment implements InboxView {
     list.setAdapter(adapter);
     list.setLayoutManager(new LinearLayoutManager(getContext()));
 
-    emptyState = view.findViewById(R.id.empty_state);
+    emptyState = (LinearLayout) view.findViewById(R.id.empty_state);
 
     AptoideApplication application = ((AptoideApplication) getContext().getApplicationContext());
     attachPresenter(
