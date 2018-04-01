ee5f060d214be8fb9c2efca8370fffaad560010e
Ana Lara <ana.simoes@aptoide.com>
2018-04-01 20:55:52 +0100

    asv-262 no need to add more items into appviewsuggestedadwidget

--- app/src/main/java/cm/aptoide/pt/app/view/widget/AppViewSuggestedAdWidget.java
+++ app/src/main/java/cm/aptoide/pt/app/view/widget/AppViewSuggestedAdWidget.java
@@ -1,11 +1,7 @@
 package cm.aptoide.pt.app.view.widget;
 
 import android.view.View;
-import android.widget.TextView;
-import cm.aptoide.pt.R;
-import cm.aptoide.pt.view.recycler.displayable.GridAdDisplayable;
 import cm.aptoide.pt.view.recycler.widget.GridAdWidget;
-import java.text.DecimalFormat;
 
 /**
  * Created by neuro on 01-08-2017.
@@ -13,27 +9,7 @@ import java.text.DecimalFormat;
 
 public class AppViewSuggestedAdWidget extends GridAdWidget {
 
-  private TextView rating;
-
   public AppViewSuggestedAdWidget(View itemView) {
     super(itemView);
-  }
-
-  @Override protected void assignViews(View itemView) {
-    super.assignViews(itemView);
-
-    rating = (TextView) itemView.findViewById(R.id.rating_label);
-  }
-
-  @Override public void bindView(GridAdDisplayable displayable) {
-    super.bindView(displayable);
-
-    try {
-      DecimalFormat oneDecimalFormatter = new DecimalFormat("#.#");
-      rating.setText(oneDecimalFormatter.format(displayable.getPojo()
-          .getStars()));
-    } catch (Exception e) {
-      rating.setText(R.string.appcardview_title_no_starts);
-    }
   }
 }
