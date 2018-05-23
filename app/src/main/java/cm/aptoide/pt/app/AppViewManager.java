577417c1ad14c49ae2edd99676468a775db39239
jdandrade <joao.andrade@aptoide.com>
2018-05-23 16:29:00 +0100

    [ASV-419] task: remove unused code.

--- app/src/main/java/cm/aptoide/pt/app/AppViewManager.java
+++ app/src/main/java/cm/aptoide/pt/app/AppViewManager.java
@@ -5,7 +5,6 @@ import cm.aptoide.pt.account.view.store.StoreManager;
 import cm.aptoide.pt.analytics.analytics.AnalyticsManager;
 import cm.aptoide.pt.appview.PreferencesManager;
 import cm.aptoide.pt.database.realm.Download;
-import cm.aptoide.pt.dataprovider.model.v7.GetAppMeta;
 import cm.aptoide.pt.download.DownloadFactory;
 import cm.aptoide.pt.install.InstallManager;
 import cm.aptoide.pt.notification.NotificationAnalytics;
@@ -214,43 +213,6 @@ public class AppViewManager {
 
   public void saveRootInstallWarning(Boolean answer) {
     installManager.rootInstallAllowed(answer);
-  }
-
-  private GetAppMeta.App buildAppMockedApp() {
-    GetAppMeta.App app = new GetAppMeta.App();
-    app.setId(37032862);
-    app.setName("AutoDoc");
-    app.setPackageName("de.autodoc.gmbh");
-    app.setSize(29101728);
-    app.setIcon("http://pool.img.aptoide.com/wadogo/28e6bfc1151ed8da26a028f1118cc353_icon.png");
-    app.setGraphic(
-        "http://pool.img.aptoide.com/wadogo/c5ff946d644c0162963fee037149af88_fgraphic_705x345.png");
-    app.setAdded("2017-04-27 20:19:19");
-    app.setModified("2018-05-02 23:20:59");
-    GetAppMeta.GetAppMetaFile file = buildAppFile();
-    app.setFile(file);
-    return app;
-  }
-
-  private GetAppMeta.GetAppMetaFile buildAppFile() {
-    GetAppMeta.GetAppMetaFile file = new GetAppMeta.GetAppMetaFile();
-    file.setVername("1.4.6");
-    file.setVercode(149);
-    file.setMd5sum("e900c63e3ca3da65f7c4aa4390b1304c");
-    file.setPath(
-        "http://pool.apk.aptoide.com/wadogo/de-autodoc-gmbh-149-37032862-e900c63e3ca3da65f7c4aa4390b1304c.apk");
-    file.setPathAlt(
-        "http://pool.apk.aptoide.com/wadogo/alt/ZGUtYXV0b2RvYy1nbWJoLTE0OS0zNzAzMjg2Mi1lOTAwYzYzZTNjYTNkYTY1ZjdjNGFhNDM5MGIxMzA0Yw.apk");
-    GetAppMeta.GetAppMetaFile.Signature signature = buildSignature();
-    file.setSignature(signature);
-    return file;
-  }
-
-  private GetAppMeta.GetAppMetaFile.Signature buildSignature() {
-    GetAppMeta.GetAppMetaFile.Signature signature = new GetAppMeta.GetAppMetaFile.Signature();
-    signature.setOwner("CN=Alexej Erdle, OU=CEO, O=Autodoc GmbH, L=Berlin, ST=Berlin, C=DE");
-    signature.setSha1("C5:49:29:2E:5E:4C:B5:64:3E:44:30:7C:B0:78:98:26:9C:40:00:99");
-    return signature;
   }
 
   public Completable downloadApp(DownloadAppViewModel.Action downloadAction, String packageName,
