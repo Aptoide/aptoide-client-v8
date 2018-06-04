711f0fc7805c7c070a23a38eacd6352abd311a09
nunocosta <nuno.pinto.costa@gmail.com>
2018-06-04 18:18:11 +0100

    [ASV-490] Updated glide to 4.7.1

--- app/src/main/java/cm/aptoide/pt/networking/image/GlideModifications.java
+++ app/src/main/java/cm/aptoide/pt/networking/image/GlideModifications.java
@@ -1,23 +1,26 @@
 package cm.aptoide.pt.networking.image;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.GlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 public class GlideModifications implements GlideModule {
   @Override public void applyOptions(Context context, GlideBuilder builder) {
-    builder.setDecodeFormat(DecodeFormat.PREFER_RGB_565);
 
+    builder.setDefaultRequestOptions(RequestOptions.formatOf(DecodeFormat.PREFER_RGB_565));
     // disk cache config
     //builder.setDiskCache(new ExternalCacheDiskCacheFactory(context));
     // using defaults
 
-    MemorySizeCalculator calculator = new MemorySizeCalculator(context);
+    MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context).build();
 
     // size for memory cache
     int defaultMemoryCacheSize = calculator.getMemoryCacheSize();
@@ -28,7 +31,8 @@ public class GlideModifications implements GlideModule {
     builder.setBitmapPool(new LruBitmapPool(defaultBitmapPoolSize));
   }
 
-  @Override public void registerComponents(Context context, Glide glide) {
+  @Override public void registerComponents(@NonNull Context context, @NonNull Glide glide,
+      @NonNull Registry registry) {
     // does nothing
   }
 }
