f58ac1b852fa75186e7b68154f41f659386d830b
eram <m.eram@tiara.studio>
2018-07-13 23:59:45 +0430

    added/ shamed model response

---
+++ aban/src/main/java/com/eram/aban/model/Shamed.java
@@ -0,0 +1,14 @@
+package com.eram.aban.model;
+
+public class Shamed {
+
+
+    public String success;
+    public Data data;
+
+    public class Data {
+        public String application_name;
+        public String package_name;
+        public String shamed_code;
+    }
+}
