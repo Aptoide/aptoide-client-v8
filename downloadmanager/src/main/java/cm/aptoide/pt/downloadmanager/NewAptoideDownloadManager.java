dea72bd583ebb1c0c5712073d7acb4e3c0539b1e
phillipsgoncalves <filisgoncalves@gmail.com>
2018-08-23 15:18:40 +0100

    [ASV-853] Setting the download in queue while starting it

--- downloadmanager/src/main/java/cm/aptoide/pt/downloadmanager/NewAptoideDownloadManager.java
+++ downloadmanager/src/main/java/cm/aptoide/pt/downloadmanager/NewAptoideDownloadManager.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import rx.Completable;
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by filipegoncalves on 7/27/18.
@@ -14,6 +15,7 @@ public class NewAptoideDownloadManager implements DownloadManager {
 
   private DownloadsRepository downloadsRepository;
   private HashMap<String, AppDownloader> appDownloaderMap;
+  private DownloadService downloadService;
 
   public NewAptoideDownloadManager(DownloadsRepository downloadsRepository) {
     this.downloadsRepository = downloadsRepository;
@@ -28,11 +30,16 @@ public class NewAptoideDownloadManager implements DownloadManager {
   }
 
   @Override public Completable startDownload(Download download) {
-    return downloadsRepository.save(download);
+    return Completable.fromAction(() -> {
+      download.setOverallDownloadStatus(Download.IN_QUEUE);
+      download.setTimeStamp(System.currentTimeMillis());
+      downloadsRepository.save(download);
+    })
+        .subscribeOn(Schedulers.computation());
   }
 
   @Override public Observable<Download> getDownload(String md5) {
-    return null;
+    return downloadsRepository.getDownload(md5);
   }
 
   @Override public Observable<Download> getDownloadsByMd5(String md5) {
@@ -68,5 +75,24 @@ public class NewAptoideDownloadManager implements DownloadManager {
   }
 
   private void dispatchDownloads() {
+    /*
+     downloadsRepository.getDownloads()
+     .flatMapIterable(downloadsList -> downloadsList)
+     .filter(download -> download.getOverallDownloadStatus() == PENDING)
+     .toList()
+     .filter(downloads -> !downloads.isEmpty())
+     .map(downloads -> downloads.get(0))
+     .flatMap(download -> getAppDownloader(download.getMd5()).flatMap(
+     appDownloader -> downloadService.download(download, appDownloader)))
+     .flatMapCompletable(download -> downloadsRepository.save(download))
+     .subscribe(__ -> {
+     }, throwable -> {
+     throw new OnErrorNotImplementedException();
+     });
+     **/
+  }
+
+  private Observable<AppDownloader> getAppDownloader(String md5) {
+    return Observable.just(appDownloaderMap.get(md5));
   }
 }
