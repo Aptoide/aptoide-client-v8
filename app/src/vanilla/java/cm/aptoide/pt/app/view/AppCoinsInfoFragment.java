8bc1d04b49cc359cca975478ac97d238c39650f2
phillipsgoncalves <filisgoncalves@gmail.com>
2018-08-24 18:29:23 +0100

    [ASV-975] Renamed app coins info layout

--- app/src/vanilla/java/cm/aptoide/pt/app/view/AppCoinsInfoFragment.java
+++ app/src/vanilla/java/cm/aptoide/pt/app/view/AppCoinsInfoFragment.java
@@ -206,6 +206,6 @@ public class AppCoinsInfoFragment extends BackButtonFragment
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_appcoints_info, container, false);
+    return inflater.inflate(R.layout.fragment_appcoins_info, container, false);
   }
 }
