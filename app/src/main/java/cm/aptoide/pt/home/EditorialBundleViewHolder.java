09903d56f1c02b0cf7caa66a3d6299875b170fd2
paquiquinho <paquiquinho@gmail.com>
2018-08-30 14:28:30 +0100

    [ASV-963] Added editorial home card along with everything needed to process it and display it in home;

---
+++ app/src/main/java/cm/aptoide/pt/home/EditorialBundleViewHolder.java
@@ -0,0 +1,44 @@
+package cm.aptoide.pt.home;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.networking.image.ImageLoader;
+import cm.aptoide.pt.view.Translator;
+import rx.subjects.PublishSubject;
+
+/**
+ * Created by franciscocalado on 29/08/2018.
+ */
+
+public class EditorialBundleViewHolder extends AppBundleViewHolder {
+  private final PublishSubject<HomeEvent> uiEventsListener;
+  private final View editorialCard;
+  private final TextView editorialTitle;
+  private final TextView editorialSubtitle;
+  private final ImageView backgroundImage;
+
+  public EditorialBundleViewHolder(View view, PublishSubject<HomeEvent> uiEventsListener) {
+    super(view);
+    this.uiEventsListener = uiEventsListener;
+    this.editorialCard = view.findViewById(R.id.editorial_card);
+    this.editorialTitle = (TextView) view.findViewById(R.id.editorial_title);
+    this.editorialSubtitle = (TextView) view.findViewById(R.id.editorial_subtitle);
+    this.backgroundImage = (ImageView) view.findViewById(R.id.background_image);
+  }
+
+  @Override public void setBundle(HomeBundle homeBundle, int position) {
+    ActionBundle actionBundle = (ActionBundle) homeBundle;
+    ActionItem actionItem = actionBundle.getActionItem();
+
+    ImageLoader.with(itemView.getContext())
+        .load(actionItem.getIcon(), backgroundImage);
+    editorialTitle.setText(Translator.translate(actionItem.getTitle(), itemView.getContext(), ""));
+    editorialSubtitle.setText(
+        Translator.translate(actionItem.getMessage(), itemView.getContext(), ""));
+    editorialCard.setOnClickListener(view -> {
+      uiEventsListener.onNext(new HomeEvent(homeBundle, position, HomeEvent.Type.EDITORIAL));
+    });
+  }
+}
