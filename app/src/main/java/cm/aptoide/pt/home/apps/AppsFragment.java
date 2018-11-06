c742e10c9af6829643431d64e63b5e6ccd59bc63
phillipsgoncalves <filisgoncalves@gmail.com>
2018-11-06 11:55:49 +0000

    [ASV-1091] Removed animation after setting an item on standby state.

--- app/src/main/java/cm/aptoide/pt/home/apps/AppsFragment.java
+++ app/src/main/java/cm/aptoide/pt/home/apps/AppsFragment.java
@@ -8,7 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -141,10 +140,7 @@ public class AppsFragment extends NavigationTrackFragment implements AppsFragmen
     recyclerView.setAdapter(adapter);
     recyclerView.setLayoutManager(
         new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-    RecyclerView.ItemAnimator animator = recyclerView.getItemAnimator();
-    if (animator instanceof SimpleItemAnimator) {
-      ((SimpleItemAnimator) animator).setSupportsChangeAnimations(false);
-    }
+    recyclerView.setItemAnimator(null);
   }
 
   @Nullable @Override
