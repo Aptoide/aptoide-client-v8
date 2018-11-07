fe72747f06edede57e3922e935810730e45af8c5
jdandrade <joao.andrade@aptoide.com>
2018-11-07 09:46:02 +0000

    [ASV-1138] refactor: delete unused code.

--- app/src/partner/java/cm/aptoide/pt/view/recycler/DisplayableWidgetMapping.java
+++ app/src/partner/java/cm/aptoide/pt/view/recycler/DisplayableWidgetMapping.java
@@ -76,12 +76,8 @@ import cm.aptoide.pt.store.view.recommended.RecommendedStoreDisplayable;
 import cm.aptoide.pt.store.view.recommended.RecommendedStoreWidget;
 import cm.aptoide.pt.timeline.view.displayable.FollowStoreDisplayable;
 import cm.aptoide.pt.timeline.view.displayable.FollowUserDisplayable;
-import cm.aptoide.pt.timeline.view.displayable.TimeLineStatsDisplayable;
-import cm.aptoide.pt.timeline.view.displayable.TimeLineStatsWidget;
 import cm.aptoide.pt.timeline.view.follow.FollowStoreWidget;
 import cm.aptoide.pt.timeline.view.follow.FollowUserWidget;
-import cm.aptoide.pt.timeline.view.login.TimelineLoginDisplayable;
-import cm.aptoide.pt.timeline.view.login.TimelineLoginWidget;
 import cm.aptoide.pt.updates.view.UpdateDisplayable;
 import cm.aptoide.pt.updates.view.UpdateWidget;
 import cm.aptoide.pt.updates.view.UpdatesHeaderDisplayable;
@@ -273,16 +269,10 @@ public class DisplayableWidgetMapping {
         new DisplayableWidgetMapping((OfficialAppWidget.class), OfficialAppDisplayable.class));
 
     displayableWidgetMappings.add(
-        new DisplayableWidgetMapping((TimeLineStatsWidget.class), TimeLineStatsDisplayable.class));
-
-    displayableWidgetMappings.add(
         new DisplayableWidgetMapping((FollowUserWidget.class), FollowUserDisplayable.class));
 
     displayableWidgetMappings.add(new DisplayableWidgetMapping((MessageWhiteBgWidget.class),
         MessageWhiteBgDisplayable.class));
-
-    displayableWidgetMappings.add(
-        new DisplayableWidgetMapping((TimelineLoginWidget.class), TimelineLoginDisplayable.class));
 
     displayableWidgetMappings.add(
         new DisplayableWidgetMapping((FollowStoreWidget.class), FollowStoreDisplayable.class));
