76499038244bc309e852014ed7370e4049aa2bb8
Tiago Pedrinho <rz.tiagopedrinho@gmail.com>
2018-11-08 10:04:39 +0000

    [ASV-770] Removed flavourApplicationModule

--- app/src/main/java/cm/aptoide/pt/AptoideApplication.java
+++ app/src/main/java/cm/aptoide/pt/AptoideApplication.java
@@ -356,7 +356,6 @@ public abstract class AptoideApplication extends Application {
     if (applicationComponent == null) {
       applicationComponent = DaggerApplicationComponent.builder()
           .applicationModule(new ApplicationModule(this, getAptoideMd5sum()))
-          .flavourApplicationModule(new FlavourApplicationModule(this))
           .build();
     }
     return applicationComponent;
