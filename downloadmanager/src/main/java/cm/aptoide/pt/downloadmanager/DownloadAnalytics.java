6763ca5ed56c2b56bec560f7fc563e3adbed508d
phillipsgoncalves <filisgoncalves@gmail.com>
2018-11-26 18:38:29 +0000

    [ASV-1153] Renamed download error analytics to download analytics to also include the download complete events

--- downloadmanager/src/main/java/cm/aptoide/pt/downloadmanager/DownloadAnalytics.java
+++ downloadmanager/src/main/java/cm/aptoide/pt/downloadmanager/DownloadAnalytics.java
@@ -1,6 +1,8 @@
 package cm.aptoide.pt.downloadmanager;
 
-public interface DownloadErrorAnalytics {
+public interface DownloadAnalytics {
+
+  void onDownloadComplete(String md5, String packageName, int versionCode);
 
   void onError(String packageName, int versionCode, Throwable throwable);
 }
