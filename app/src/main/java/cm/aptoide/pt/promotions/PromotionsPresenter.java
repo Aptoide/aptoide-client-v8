b845ad4abd66df0239a98d1aa1afd91385b20110
phillipsgoncalves <filisgoncalves@gmail.com>
2018-11-30 10:55:27 +0000

    [ASV-1219] Showing promotion fake apps to download

--- app/src/main/java/cm/aptoide/pt/promotions/PromotionsPresenter.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionsPresenter.java
@@ -1,15 +1,33 @@
 package cm.aptoide.pt.promotions;
 
 import cm.aptoide.pt.presenter.Presenter;
+import cm.aptoide.pt.presenter.View;
+import rx.exceptions.OnErrorNotImplementedException;
 
 public class PromotionsPresenter implements Presenter {
 
   private PromotionsView view;
+  private PromotionsManager promotionsManager;
 
-  public PromotionsPresenter(PromotionsView view) {
+  public PromotionsPresenter(PromotionsView view, PromotionsManager promotionsManager) {
     this.view = view;
+    this.promotionsManager = promotionsManager;
   }
 
   @Override public void present() {
+    getPromotionApps();
+  }
+
+  private void getPromotionApps() {
+
+    view.getLifecycleEvent()
+        .filter(lifecycleEvent -> lifecycleEvent.equals(View.LifecycleEvent.CREATE))
+        .flatMap(__ -> promotionsManager.getPromotionApps())
+        .doOnNext(appsList -> view.showPromotionApps(appsList))
+        .compose(view.bindUntilEvent(View.LifecycleEvent.DESTROY))
+        .subscribe(__ -> {
+        }, throwable -> {
+          throw new OnErrorNotImplementedException(throwable);
+        });
   }
 }
--- app/src/main/java/cm/aptoide/pt/promotions/PromotionsView.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionsView.java
@@ -1,7 +1,9 @@
 package cm.aptoide.pt.promotions;
 
 import cm.aptoide.pt.presenter.View;
+import java.util.List;
 
 public interface PromotionsView extends View {
 
+  void showPromotionApps(List<PromotionApp> appsList);
 }
