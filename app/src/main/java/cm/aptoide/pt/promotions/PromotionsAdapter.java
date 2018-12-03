713233082f505b8408629b4f71b8dce9bd52a29c
phillipsgoncalves <filisgoncalves@gmail.com>
2018-12-03 16:39:01 +0000

    [ASV-1219] Matching app states of promotion apps with the correct view types

--- app/src/main/java/cm/aptoide/pt/promotions/PromotionsAdapter.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionsAdapter.java
@@ -4,7 +4,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 import java.util.List;
 
-public class PromotionsAdapter extends RecyclerView.Adapter<PromotionsViewHolder> {
+public class PromotionsAdapter extends RecyclerView.Adapter<PromotionAppsViewHolder> {
+
+  static final int UPDATE = 0;
+  static final int DOWNLOAD = 1;
+  static final int DOWNLOADING = 2;
+  static final int INSTALL = 3;
+  static final int CLAIM = 4;
+  static final int CLAIMED = 5;
 
   private List<PromotionApp> appsList;
   private PromotionsViewHolderFactory viewHolderFactory;
@@ -15,15 +22,48 @@ public class PromotionsAdapter extends RecyclerView.Adapter<PromotionsViewHolder
     this.viewHolderFactory = viewHolderFactory;
   }
 
-  @Override public PromotionsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+  @Override public PromotionAppsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
     return viewHolderFactory.createViewHolder(parent, viewType);
   }
 
-  @Override public void onBindViewHolder(PromotionsViewHolder holder, int position) {
+  @Override public void onBindViewHolder(PromotionAppsViewHolder holder, int position) {
     holder.setApp(appsList.get(position));
+  }
+
+  @Override public int getItemViewType(int position) {
+    PromotionApp app = appsList.get(position);
+    int state;
+    switch (app.getState()) {
+      case UPDATE:
+        state = UPDATE;
+        break;
+      case DOWNLOAD:
+        state = DOWNLOAD;
+        break;
+      case DOWNLOADING:
+        state = DOWNLOADING;
+        break;
+      case INSTALL:
+        state = INSTALL;
+        break;
+      case CLAIM:
+        state = CLAIM;
+        break;
+      case CLAIMED:
+        state = CLAIMED;
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid type of app state");
+    }
+    return state;
   }
 
   @Override public int getItemCount() {
     return appsList.size();
+  }
+
+  public void setPromotionApps(List<PromotionApp> appsList) {
+    this.appsList = appsList;
+    notifyDataSetChanged();
   }
 }
