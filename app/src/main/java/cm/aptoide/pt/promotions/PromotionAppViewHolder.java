2295188a990f13ffad3334cf892c03e41057fcc4
phillipsgoncalves <filisgoncalves@gmail.com>
2018-12-04 17:28:05 +0000

    [ASV-1219] Setting correct message on promotion app action button

--- app/src/main/java/cm/aptoide/pt/promotions/PromotionAppViewHolder.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionAppViewHolder.java
@@ -1,12 +1,19 @@
 package cm.aptoide.pt.promotions;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import cm.aptoide.pt.R;
 import cm.aptoide.pt.networking.image.ImageLoader;
 import cm.aptoide.pt.utils.AptoideUtils;
 import java.text.DecimalFormat;
+
+import static cm.aptoide.pt.promotions.PromotionsAdapter.CLAIM;
+import static cm.aptoide.pt.promotions.PromotionsAdapter.CLAIMED;
+import static cm.aptoide.pt.promotions.PromotionsAdapter.DOWNLOAD;
+import static cm.aptoide.pt.promotions.PromotionsAdapter.INSTALL;
+import static cm.aptoide.pt.promotions.PromotionsAdapter.UPDATE;
 
 public class PromotionAppViewHolder extends GeneralPromotionAppsViewHolder {
 
@@ -17,6 +24,7 @@ public class PromotionAppViewHolder extends GeneralPromotionAppsViewHolder {
   private TextView appSize;
   private TextView numberOfDownloads;
   private TextView rating;
+  private Button promotionAction;
 
   public PromotionAppViewHolder(View itemView, int appState) {
     super(itemView);
@@ -27,10 +35,36 @@ public class PromotionAppViewHolder extends GeneralPromotionAppsViewHolder {
     numberOfDownloads = itemView.findViewById(R.id.number_of_downloads);
     appSize = itemView.findViewById(R.id.app_size);
     rating = itemView.findViewById(R.id.rating);
+    promotionAction = itemView.findViewById(R.id.promotion_app_action_button);
   }
 
   @Override public void setApp(PromotionApp app) {
     setAppCardHeader(app);
+    promotionAction.setText(getButtonMessage(appState));
+  }
+
+  private String getButtonMessage(int appState) {
+    String message = "";
+    switch (appState) {
+      case UPDATE:
+        message = "Update to get 25 APPC";
+        break;
+      case DOWNLOAD:
+        message = "Install to get 25 APPC";
+        break;
+      case INSTALL:
+        message = "Install to get 25 APPC";
+        break;
+      case CLAIM:
+        message = "Claim your 25 appc";
+        break;
+      case CLAIMED:
+        message = "Claimed";
+        break;
+      default:
+        throw new IllegalArgumentException("Wrong view type of promotion app");
+    }
+    return message;
   }
 
   private void setAppCardHeader(PromotionApp app) {
