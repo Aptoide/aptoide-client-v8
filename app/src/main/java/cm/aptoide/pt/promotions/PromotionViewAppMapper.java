eab076d16157a31a4d8cb48295878217548d99fe
phillipsgoncalves <filisgoncalves@gmail.com>
2018-12-06 12:18:37 +0000

    [ASV-1219] Created promotion view app mapper to map promotion apps to promotion view apps

---
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionViewAppMapper.java
@@ -0,0 +1,30 @@
+package cm.aptoide.pt.promotions;
+
+import cm.aptoide.pt.app.DownloadModel;
+import cm.aptoide.pt.app.DownloadStateParser;
+import cm.aptoide.pt.install.Install;
+
+public class PromotionViewAppMapper {
+
+  private final DownloadStateParser downloadStateParser;
+
+  public PromotionViewAppMapper(DownloadStateParser downloadStateParser) {
+    this.downloadStateParser = downloadStateParser;
+  }
+
+  public PromotionViewApp mapInstallToPromotionApp(Install install, PromotionApp promotionApp) {
+    return new PromotionViewApp(
+        getDownloadModel(install.getType(), install.getProgress(), install.getState()),
+        promotionApp.getName(), promotionApp.getPackageName(), promotionApp.getAppId(),
+        promotionApp.getDownloadPath(), promotionApp.getAlternativePath(),
+        promotionApp.getAppIcon(), promotionApp.isClaimed(), promotionApp.getDescription(),
+        promotionApp.getSize(), promotionApp.getRating(), promotionApp.getNumberOfDownloads(),
+        promotionApp.getMd5(), promotionApp.getVersionCode());
+  }
+
+  private DownloadModel getDownloadModel(Install.InstallationType type, int progress,
+      Install.InstallationStatus state) {
+    return new DownloadModel(downloadStateParser.parseDownloadType(type, false, false), progress,
+        downloadStateParser.parseDownloadState(state), null);
+  }
+}
