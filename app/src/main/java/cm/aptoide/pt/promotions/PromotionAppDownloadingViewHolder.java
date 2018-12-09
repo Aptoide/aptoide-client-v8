bbbb0e98f6e8f188cdf5b771880f3abc4f04d49b
phillipsgoncalves <filisgoncalves@gmail.com>
2018-12-09 23:09:29 +0000

    [ASV-1219] Showing download progress for the active download cases

--- app/src/main/java/cm/aptoide/pt/promotions/PromotionAppDownloadingViewHolder.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionAppDownloadingViewHolder.java
@@ -2,8 +2,11 @@ package cm.aptoide.pt.promotions;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import cm.aptoide.pt.R;
+import cm.aptoide.pt.app.DownloadModel;
 import cm.aptoide.pt.networking.image.ImageLoader;
 import cm.aptoide.pt.utils.AptoideUtils;
 import java.text.DecimalFormat;
@@ -19,6 +22,13 @@ class PromotionAppDownloadingViewHolder extends GeneralPromotionAppsViewHolder {
   private TextView numberOfDownloads;
   private TextView rating;
 
+  private ProgressBar downloadProgressBar;
+  private TextView downloadProgressValue;
+  private ImageView pauseDownload;
+  private ImageView cancelDownload;
+  private ImageView resumeDownload;
+  private LinearLayout downloadControlsLayout;
+
   public PromotionAppDownloadingViewHolder(View itemView,
       PublishSubject<PromotionAppClick> promotionAppClick) {
     super(itemView);
@@ -29,10 +39,20 @@ class PromotionAppDownloadingViewHolder extends GeneralPromotionAppsViewHolder {
     appSize = itemView.findViewById(R.id.app_size);
     rating = itemView.findViewById(R.id.rating);
     this.promotionAppClick = promotionAppClick;
+
+    downloadProgressBar = itemView.findViewById(R.id.promotions_download_progress_bar);
+    downloadProgressValue = itemView.findViewById(R.id.promotions_download_progress_number);
+    pauseDownload = itemView.findViewById(R.id.promotions_download_pause_download);
+    cancelDownload = itemView.findViewById(R.id.promotions_download_cancel_button);
+    resumeDownload = itemView.findViewById(R.id.promotions_download_resume_download);
+    downloadControlsLayout = itemView.findViewById(R.id.install_controls_layout);
   }
 
   @Override public void setApp(PromotionViewApp app) {
     setAppCardHeader(app);
+    setDownloadState(app.getDownloadModel()
+        .getProgress(), app.getDownloadModel()
+        .getDownloadState());
   }
 
   private void setAppCardHeader(PromotionViewApp app) {
@@ -47,5 +67,56 @@ class PromotionAppDownloadingViewHolder extends GeneralPromotionAppsViewHolder {
       rating.setText(new DecimalFormat("0.0").format(app.getRating()));
     }
     numberOfDownloads.setText(String.valueOf(app.getNumberOfDownloads()));
+  }
+
+  private void setDownloadState(int progress, DownloadModel.DownloadState downloadState) {
+
+    LinearLayout.LayoutParams pauseShowing =
+        new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT, 4f);
+    LinearLayout.LayoutParams pauseHidden =
+        new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT, 2f);
+    switch (downloadState) {
+      case ACTIVE:
+        downloadProgressBar.setIndeterminate(false);
+        downloadProgressBar.setProgress(progress);
+        downloadProgressValue.setText(String.valueOf(progress) + "%");
+        pauseDownload.setVisibility(View.VISIBLE);
+        cancelDownload.setVisibility(View.GONE);
+        resumeDownload.setVisibility(View.GONE);
+        downloadControlsLayout.setLayoutParams(pauseShowing);
+        break;
+      case INDETERMINATE:
+        downloadProgressBar.setIndeterminate(true);
+        pauseDownload.setVisibility(View.VISIBLE);
+        cancelDownload.setVisibility(View.GONE);
+        resumeDownload.setVisibility(View.GONE);
+        downloadControlsLayout.setLayoutParams(pauseShowing);
+        break;
+      case PAUSE:
+        downloadProgressBar.setIndeterminate(false);
+        downloadProgressBar.setProgress(progress);
+        downloadProgressValue.setText(String.valueOf(progress) + "%");
+        pauseDownload.setVisibility(View.GONE);
+        cancelDownload.setVisibility(View.VISIBLE);
+        resumeDownload.setVisibility(View.VISIBLE);
+        downloadControlsLayout.setLayoutParams(pauseHidden);
+        break;
+      case COMPLETE:
+        downloadProgressBar.setIndeterminate(true);
+        pauseDownload.setVisibility(View.VISIBLE);
+        cancelDownload.setVisibility(View.GONE);
+        resumeDownload.setVisibility(View.GONE);
+        downloadControlsLayout.setLayoutParams(pauseShowing);
+        break;
+      case ERROR:
+        //showErrorDialog("", getContext().getString(R.string.error_occured));
+        break;
+      case NOT_ENOUGH_STORAGE_ERROR:
+        //showErrorDialog(getContext().getString(R.string.out_of_space_dialog_title),
+        //getContext().getString(R.string.out_of_space_dialog_message));
+        break;
+    }
   }
 }
