5946faf4c9a9a6064851c67fdd79ce30e9b036cd
phillipsgoncalves <filisgoncalves@gmail.com>
2018-12-14 11:37:00 +0000

    [ASV-1239] Listening to the install button even after the user cancelled the permission

--- app/src/main/java/cm/aptoide/pt/promotions/PromotionsPresenter.java
+++ app/src/main/java/cm/aptoide/pt/promotions/PromotionsPresenter.java
@@ -103,13 +103,13 @@ public class PromotionsPresenter implements Presenter {
   private void installButtonClick() {
     view.getLifecycleEvent()
         .filter(lifecycleEvent -> lifecycleEvent.equals(View.LifecycleEvent.CREATE))
-        .flatMap(__ -> view.installButtonClick())
-        .filter(promotionViewApp -> promotionViewApp.getDownloadModel()
-            .isDownloadable())
-        .flatMapCompletable(promotionViewApp -> downloadApp(promotionViewApp))
-        .observeOn(viewScheduler)
-        .doOnError(throwable -> throwable.printStackTrace())
-        .retry()
+        .flatMap(__ -> view.installButtonClick()
+            .filter(promotionViewApp -> promotionViewApp.getDownloadModel()
+                .isDownloadable())
+            .flatMapCompletable(promotionViewApp -> downloadApp(promotionViewApp))
+            .observeOn(viewScheduler)
+            .doOnError(throwable -> throwable.printStackTrace())
+            .retry())
         .compose(view.bindUntilEvent(View.LifecycleEvent.DESTROY))
         .subscribe(created -> {
         }, error -> {
