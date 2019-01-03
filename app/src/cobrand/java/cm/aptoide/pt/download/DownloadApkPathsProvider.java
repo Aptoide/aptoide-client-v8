b26112db7e9e6b773975e75ed1e446f2c653f7f4
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-03 17:03:49 +0000

    [ASV-1215] Extracted logic to add download action to the download path from DownloadFactory to the DownloadApkPathsProvider

---
+++ app/src/cobrand/java/cm/aptoide/pt/download/DownloadApkPathsProvider.java
@@ -0,0 +1,28 @@
+package cm.aptoide.pt.download;
+
+import cm.aptoide.pt.database.realm.Download;
+
+public class DownloadApkPathsProvider {
+
+  private static final String UPDATE_ACTION = "?action=update";
+  private static final String INSTALL_ACTION = "?action=install";
+  private static final String DOWNGRADE_ACTION = "?action=downgrade";
+
+  public ApkPaths getDownloadPaths(int downloadAction, String path, String altPath) {
+    switch (downloadAction) {
+      case Download.ACTION_INSTALL:
+        path += INSTALL_ACTION;
+        altPath += INSTALL_ACTION;
+        break;
+      case Download.ACTION_DOWNGRADE:
+        path += DOWNGRADE_ACTION;
+        altPath += DOWNGRADE_ACTION;
+        break;
+      case Download.ACTION_UPDATE:
+        path += UPDATE_ACTION;
+        altPath += UPDATE_ACTION;
+        break;
+    }
+    return new ApkPaths(path, altPath);
+  }
+}
