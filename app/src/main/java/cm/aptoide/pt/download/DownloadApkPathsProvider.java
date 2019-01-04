b3da3ac0df3582365028188c6c1ac0695b3c26e7
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-04 14:55:59 +0000

    [ASV-1215] Calculating oemid query on the downloads apk path provider to add it to the links

--- app/src/main/java/cm/aptoide/pt/download/DownloadApkPathsProvider.java
+++ app/src/main/java/cm/aptoide/pt/download/DownloadApkPathsProvider.java
@@ -7,22 +7,38 @@ public class DownloadApkPathsProvider {
   private static final String UPDATE_ACTION = "?action=update";
   private static final String INSTALL_ACTION = "?action=install";
   private static final String DOWNGRADE_ACTION = "?action=downgrade";
+  private static final String OEMID_QUERY = "&oemid=";
+  private final OemidProvider oemidProvider;
+
+  public DownloadApkPathsProvider(OemidProvider oemidProvider) {
+    this.oemidProvider = oemidProvider;
+  }
 
   public ApkPaths getDownloadPaths(int downloadAction, String path, String altPath) {
+    String oemid = getOemidQueryString();
     switch (downloadAction) {
       case Download.ACTION_INSTALL:
-        path += INSTALL_ACTION;
-        altPath += INSTALL_ACTION;
+        path += INSTALL_ACTION + oemid;
+        altPath += INSTALL_ACTION + oemid;
         break;
       case Download.ACTION_DOWNGRADE:
-        path += DOWNGRADE_ACTION;
-        altPath += DOWNGRADE_ACTION;
+        path += DOWNGRADE_ACTION + oemid;
+        altPath += DOWNGRADE_ACTION + oemid;
         break;
       case Download.ACTION_UPDATE:
-        path += UPDATE_ACTION;
-        altPath += UPDATE_ACTION;
+        path += UPDATE_ACTION + oemid;
+        altPath += UPDATE_ACTION + oemid;
         break;
     }
     return new ApkPaths(path, altPath);
+  }
+
+  private String getOemidQueryString() {
+    String oemid = oemidProvider.getOemid();
+    if (oemid.isEmpty()) {
+      return "";
+    } else {
+      return OEMID_QUERY + oemid;
+    }
   }
 }
