4d9f30bdc347c6b0bb5f081cfc224bbf9b17e54f
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-17 14:10:45 +0000

    [ASV-1367] Loading search native ad unit placement id

--- app/src/main/java/cm/aptoide/pt/search/view/SearchResultFragment.java
+++ app/src/main/java/cm/aptoide/pt/search/view/SearchResultFragment.java
@@ -344,7 +344,7 @@ public class SearchResultFragment extends BackButtonFragment
   @Override public void addAllStoresResult(List<SearchAppResult> dataList, String query) {
     RequestParameters requestParameters = new RequestParameters.Builder().keywords(query)
         .build();
-    moPubRecyclerAdapter.loadAds(BuildConfig.MOPUB_SEARCH_NATIVE_PLACEMENT_ID, requestParameters);
+    moPubRecyclerAdapter.loadAds(BuildConfig.MOPUB_NATIVE_HOME_PLACEMENT_ID_T12, requestParameters);
     allStoresResultAdapter.addResultForSearch(dataList);
     viewModel.addAllStoresSearchAppResults(dataList);
   }
