c2a85458fb4e0c6f0ad910ba48145239cbdee07f
jdandrade <joao.andrade@aptoide.com>
2019-01-22 21:15:08 +0000

    [ASV-1327] feature: add STORE_META widget type support to widget utils. Behind it does a store/getMeta request.

--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/V7.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/V7.java
@@ -309,7 +309,8 @@ public abstract class V7<U, B extends RefreshBody> extends WebService<V7.Interfa
         @Path(value = "url", encoded = true) String path, @Body GetHomeMetaRequest.Body body,
         @Header(WebService.BYPASS_HEADER_KEY) boolean bypassCache);
 
-    @POST("store/getMeta") Observable<GetStoreMeta> getStoreMeta(@Body GetHomeMetaRequest.Body body,
+    @POST("store/getMeta{url}") Observable<GetStoreMeta> getStoreMeta(
+        @Path(value = "url", encoded = true) String path, @Body GetHomeMetaRequest.Body body,
         @Header(WebService.BYPASS_HEADER_KEY) boolean bypassCache);
 
     @POST("listSearchApps") Observable<ListSearchApps> listSearchApps(
--- dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/store/GetStoreMetaRequest.java
+++ dataprovider/src/main/java/cm/aptoide/pt/dataprovider/ws/v7/store/GetStoreMetaRequest.java
@@ -6,6 +6,7 @@ import cm.aptoide.pt.dataprovider.model.v7.store.GetStoreMeta;
 import cm.aptoide.pt.dataprovider.ws.BodyInterceptor;
 import cm.aptoide.pt.dataprovider.ws.v7.BaseBody;
 import cm.aptoide.pt.dataprovider.ws.v7.BaseRequestWithStore;
+import cm.aptoide.pt.dataprovider.ws.v7.V7Url;
 import okhttp3.OkHttpClient;
 import retrofit2.Converter;
 import rx.Observable;
@@ -17,23 +18,35 @@ import rx.Observable;
 public class GetStoreMetaRequest
     extends BaseRequestWithStore<GetStoreMeta, GetHomeMetaRequest.Body> {
 
-  public GetStoreMetaRequest(GetHomeMetaRequest.Body body,
+  private final String url;
+
+  public GetStoreMetaRequest(String url, GetHomeMetaRequest.Body body,
       BodyInterceptor<BaseBody> bodyInterceptor, OkHttpClient httpClient,
       Converter.Factory converterFactory, TokenInvalidator tokenInvalidator,
       SharedPreferences sharedPreferences) {
     super(body, httpClient, converterFactory, bodyInterceptor, tokenInvalidator, sharedPreferences);
+    this.url = url;
   }
 
   public static GetStoreMetaRequest of(StoreCredentials storeCredentials,
       BodyInterceptor<BaseBody> bodyInterceptor, OkHttpClient httpClient,
       Converter.Factory converterFactory, TokenInvalidator tokenInvalidator,
       SharedPreferences sharedPreferences) {
-    return new GetStoreMetaRequest(new GetHomeMetaRequest.Body(storeCredentials), bodyInterceptor,
-        httpClient, converterFactory, tokenInvalidator, sharedPreferences);
+    return new GetStoreMetaRequest("", new GetHomeMetaRequest.Body(storeCredentials),
+        bodyInterceptor, httpClient, converterFactory, tokenInvalidator, sharedPreferences);
+  }
+
+  public static GetStoreMetaRequest ofAction(String url, StoreCredentials storeCredentials,
+      BodyInterceptor<BaseBody> bodyInterceptor, OkHttpClient httpClient,
+      Converter.Factory converterFactory, TokenInvalidator tokenInvalidator,
+      SharedPreferences sharedPreferences) {
+    return new GetStoreMetaRequest(new V7Url(url).remove("getStoreMeta")
+        .get(), new GetHomeMetaRequest.Body(storeCredentials), bodyInterceptor, httpClient,
+        converterFactory, tokenInvalidator, sharedPreferences);
   }
 
   @Override protected Observable<GetStoreMeta> loadDataFromNetwork(Interfaces interfaces,
       boolean bypassCache) {
-    return interfaces.getStoreMeta(body, bypassCache);
+    return interfaces.getStoreMeta(url != null ? url : "", body, bypassCache);
   }
 }
