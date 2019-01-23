b7dffec1a8e1c5b83b2ba673b683c0724dcc98ff
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-23 16:46:54 +0000

    [ASV-1377] Created small banner ad view holder to show banner ads in home

---
+++ app/src/vanilla/java/cm/aptoide/pt/home/SmallBannerAdBundleViewHolder.java
@@ -0,0 +1,55 @@
+package cm.aptoide.pt.home;
+
+import android.view.View;
+import cm.aptoide.pt.BuildConfig;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.logger.Logger;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubView;
+
+class SmallBannerAdBundleViewHolder extends AppBundleViewHolder {
+
+  private final MoPubView bannerView;
+  private final String TAG = "Mopub";
+  private boolean hasLoaded;
+
+  public SmallBannerAdBundleViewHolder(View view) {
+    super(view);
+    hasLoaded = false;
+    bannerView = view.findViewById(R.id.banner);
+    bannerView.setBannerAdListener(new MoPubView.BannerAdListener() {
+      @Override public void onBannerLoaded(MoPubView banner) {
+        Logger.getInstance()
+            .d(TAG, "Banner loaded");
+      }
+
+      @Override public void onBannerFailed(MoPubView banner, MoPubErrorCode errorCode) {
+        Logger.getInstance()
+            .e(TAG, "Banner error : " + errorCode.toString());
+      }
+
+      @Override public void onBannerClicked(MoPubView banner) {
+        Logger.getInstance()
+            .d(TAG, "Banner clicked");
+      }
+
+      @Override public void onBannerExpanded(MoPubView banner) {
+        Logger.getInstance()
+            .d(TAG, "Banner expanded");
+      }
+
+      @Override public void onBannerCollapsed(MoPubView banner) {
+        Logger.getInstance()
+            .d(TAG, "Banner collapsed");
+      }
+    });
+    bannerView.setAdUnitId(BuildConfig.MOPUB_BANNER_90_HOME_PLACEMENT_ID_T12);
+  }
+
+  @Override public void setBundle(HomeBundle homeBundle, int position) {
+    if (!hasLoaded) {
+      bannerView.loadAd();
+      hasLoaded = true;
+    }
+  }
+}
