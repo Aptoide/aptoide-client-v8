9a9e3bef59e91d795312bf024ad0b235d438117d
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-24 11:08:10 +0000

    [ASV-1377] Renamed ab test event key name to the ab test experience name

--- app/src/main/java/cm/aptoide/pt/ads/MoPubAnalytics.java
+++ app/src/main/java/cm/aptoide/pt/ads/MoPubAnalytics.java
@@ -8,7 +8,7 @@ public class MoPubAnalytics {
 
   public void setMoPubAbTestGroup(boolean isControlGroup) {
     Bundle bundle = new Bundle();
-    bundle.putString("ab_test_mopub", isControlGroup ? "a_without_mopub" : "b_with_mopub");
+    bundle.putString("ASV-1377-MoPub-Ads", isControlGroup ? "a_without_mopub" : "b_with_mopub");
     AppEventsLogger.updateUserProperties(bundle, response -> Logger.getInstance()
         .d("Facebook Analytics: ", response.toString()));
   }
