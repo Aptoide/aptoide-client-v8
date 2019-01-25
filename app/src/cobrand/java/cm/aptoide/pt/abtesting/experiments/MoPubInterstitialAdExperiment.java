5380af88e402503b3c83857b615cd76e18952e26
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-25 11:45:18 +0000

    Fixed ab test record experience on Interstitial Ad Experiment

--- app/src/cobrand/java/cm/aptoide/pt/abtesting/experiments/MoPubInterstitialAdExperiment.java
+++ app/src/cobrand/java/cm/aptoide/pt/abtesting/experiments/MoPubInterstitialAdExperiment.java
@@ -1,11 +1,21 @@
 package cm.aptoide.pt.abtesting.experiments;
 
+import rx.Observable;
+
 public class MoPubInterstitialAdExperiment {
 
   public MoPubInterstitialAdExperiment() {
   }
 
   public Observable<Boolean> loadInterstitial() {
+    return Observable.just(false);
+  }
+
+  public Observable<Boolean> recordAdImpression() {
+    return Observable.just(false);
+  }
+
+  public Observable<Boolean> recordAdClick() {
     return Observable.just(false);
   }
 }
