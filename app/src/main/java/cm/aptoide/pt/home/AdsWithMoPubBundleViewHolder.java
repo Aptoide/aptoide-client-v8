ccc6a81d05b7a34595118845d8c63ed34ba72cd2
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-25 16:58:00 +0000

    [ASV-1377] Created ads with mopub viewholder to represent the viewholder of the ad cards with mopub

---
+++ app/src/main/java/cm/aptoide/pt/home/AdsWithMoPubBundleViewHolder.java
@@ -0,0 +1,79 @@
+package cm.aptoide.pt.home;
+
+import android.graphics.Rect;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.utils.AptoideUtils;
+import cm.aptoide.pt.view.Translator;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import rx.subjects.PublishSubject;
+
+public class AdsWithMoPubBundleViewHolder extends AppBundleViewHolder {
+
+  private static final int ADS = R.layout.ads_bundle_item;
+  private final TextView bundleTitle;
+  private final Button moreButton;
+  private final AdsInBundleAdapter appsInBundleAdapter;
+  private final PublishSubject<HomeEvent> uiEventsListener;
+  private final PublishSubject<AdHomeEvent> adClickedEvents;
+  private final DecimalFormat oneDecimalFormatter;
+  private final RecyclerView appsList;
+  private final String marketName;
+
+  public AdsWithMoPubBundleViewHolder(View view, PublishSubject<HomeEvent> uiEventsListener,
+      DecimalFormat oneDecimalFormatter, PublishSubject<AdHomeEvent> adClickedEvents,
+      String marketName) {
+    super(view);
+    this.uiEventsListener = uiEventsListener;
+    this.oneDecimalFormatter = oneDecimalFormatter;
+    this.adClickedEvents = adClickedEvents;
+    this.marketName = marketName;
+
+    bundleTitle = (TextView) view.findViewById(R.id.bundle_title);
+    moreButton = (Button) view.findViewById(R.id.bundle_more);
+    appsList = (RecyclerView) view.findViewById(R.id.apps_list);
+    appsInBundleAdapter =
+        new AdsInBundleAdapter(new ArrayList<>(), oneDecimalFormatter, adClickedEvents);
+    LinearLayoutManager layoutManager =
+        new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+    appsList.addItemDecoration(new RecyclerView.ItemDecoration() {
+      @Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+          RecyclerView.State state) {
+        int margin = AptoideUtils.ScreenU.getPixelsForDip(5, view.getResources());
+        outRect.set(margin, margin, 0, margin);
+      }
+    });
+    appsList.setLayoutManager(layoutManager);
+    appsList.setAdapter(appsInBundleAdapter);
+  }
+
+  @Override public void setBundle(HomeBundle homeBundle, int position) {
+    if (!(homeBundle instanceof AdBundle)) {
+      throw new IllegalStateException(this.getClass()
+          .getName() + " is getting non AdBundle instance!");
+    }
+    bundleTitle.setText(
+        Translator.translate(homeBundle.getTitle(), itemView.getContext(), marketName));
+
+    appsInBundleAdapter.updateBundle(homeBundle, position);
+    appsInBundleAdapter.update((List<AdClick>) homeBundle.getContent());
+
+    appsList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+      @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        if (dx > 0) {
+          uiEventsListener.onNext(new HomeEvent(homeBundle, position, HomeEvent.Type.SCROLL_RIGHT));
+        }
+      }
+    });
+
+    moreButton.setOnClickListener(
+        v -> uiEventsListener.onNext(new HomeEvent(homeBundle, position, HomeEvent.Type.MORE)));
+  }
+}
