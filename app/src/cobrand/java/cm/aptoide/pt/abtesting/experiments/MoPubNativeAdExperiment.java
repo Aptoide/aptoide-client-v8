2aca778a877b034c1a05686779d62cca3c29ca69
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-25 10:26:13 +0000

    [ASV-1377] Created mopub native ad experiment for cobrand

---
+++ app/src/cobrand/java/cm/aptoide/pt/abtesting/experiments/MoPubNativeAdExperiment.java
@@ -0,0 +1,23 @@
+package cm.aptoide.pt.abtesting.experiments;
+
+import rx.Single;
+
+public class MoPubNativeAdExperiment {
+
+  private final String EXPERIMENT_ID = "ASV-1377-MoPub-Ads";
+
+  public MoPubNativeAdExperiment() {
+  }
+
+  public Single<Boolean> loadNative() {
+    return Single.just(false);
+  }
+
+  public Single<Boolean> recordAdImpression() {
+    return Single.just(false);
+  }
+
+  public Single<Boolean> recordAdClick() {
+    return Single.just(false);
+  }
+}
