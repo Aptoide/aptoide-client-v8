88906d5413d4be67daecfe48d80db928f9c60f87
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-27 19:27:17 +0000

    [ASV-1377] Checking if native ads should be shown in ad bundle in order to create ad bundle with mopub ads

--- app/src/main/java/cm/aptoide/pt/home/AdsBundlesViewHolderFactory.java
+++ app/src/main/java/cm/aptoide/pt/home/AdsBundlesViewHolderFactory.java
@@ -14,19 +14,20 @@ public class AdsBundlesViewHolderFactory {
   private final PublishSubject<AdHomeEvent> adClickedEvents;
   private final DecimalFormat oneDecimalFormatter;
   private final String marketName;
+  private final boolean showNatives;
 
   public AdsBundlesViewHolderFactory(PublishSubject<HomeEvent> uiEventsListener,
       PublishSubject<AdHomeEvent> adClickedEvents, DecimalFormat oneDecimalFormatter,
-      String marketName) {
+      String marketName, boolean showNatives) {
     this.uiEventsListener = uiEventsListener;
     this.adClickedEvents = adClickedEvents;
     this.oneDecimalFormatter = oneDecimalFormatter;
     this.marketName = marketName;
+    this.showNatives = showNatives;
   }
 
   public AppBundleViewHolder createViewHolder(ViewGroup parent) {
-
-    if (Build.VERSION.SDK_INT >= 21) {
+    if (Build.VERSION.SDK_INT >= 21 && showNatives) {
       return new AdsWithMoPubBundleViewHolder(LayoutInflater.from(parent.getContext())
           .inflate(ADS, parent, false), uiEventsListener, oneDecimalFormatter, adClickedEvents,
           marketName);
