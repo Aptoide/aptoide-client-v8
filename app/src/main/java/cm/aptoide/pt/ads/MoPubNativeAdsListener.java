67ebef37548796f9542904a47f5e59c5a95355b6
phillipsgoncalves <filisgoncalves@gmail.com>
2019-01-28 11:54:28 +0000

    [ASV-1377] removed commented code

--- app/src/main/java/cm/aptoide/pt/ads/MoPubNativeAdsListener.java
+++ app/src/main/java/cm/aptoide/pt/ads/MoPubNativeAdsListener.java
@@ -4,8 +4,6 @@ import com.mopub.nativeads.MoPubNativeAdLoadedListener;
 
 public class MoPubNativeAdsListener implements MoPubNativeAdLoadedListener {
   @Override public void onAdLoaded(int position) {
-    // homeAnalytics.sendAdImpressionEvent(0, "Ad", position, "ads-highlighted", HomeEvent.Type.AD,
-    //   ApplicationAd.Network.MOPUB);
   }
 
   @Override public void onAdRemoved(int position) {
