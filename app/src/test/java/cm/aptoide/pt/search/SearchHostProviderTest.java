3471448e7178ec2dbcf8c7441ee9a4750d9b8d51
jdandrade <joao.andrade@aptoide.com>
2019-02-07 19:35:41 +0000

    [ASV-1332] task: add search host provider test when toolbox is http enabled and aptoide scheme is http

--- app/src/test/java/cm/aptoide/pt/search/SearchHostProviderTest.java
+++ app/src/test/java/cm/aptoide/pt/search/SearchHostProviderTest.java
@@ -19,4 +19,11 @@ public class SearchHostProviderTest {
     String searchHost = searchHostProvider.getSearchHost();
     assertEquals(searchHost, "https://buzz.aptoide.com:10002/v1/");
   }
+
+  @Test public void getSearchHost_toolBoxEnabled_aptoideHttpScheme() {
+    SearchHostProvider searchHostProvider =
+        new SearchHostProvider(true, "http", "buzz.aptoide.com:10001", "buzz.aptoide.com:10002");
+    String searchHost = searchHostProvider.getSearchHost();
+    assertEquals(searchHost, "http://buzz.aptoide.com:10001/v1/");
+  }
 }
