4fdca62ec540d0af38d667de746af8e90b8b77f0
phillipsgoncalves <filisgoncalves@gmail.com>
2019-02-11 12:13:58 +0000

    [ASV-1396] Created base adapter configuration for startapp

---
+++ app/src/main/java/com/mopub/nativeads/StartAppBaseConfiguration.java
@@ -0,0 +1,42 @@
+package com.mopub.nativeads;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import cm.aptoide.pt.BuildConfig;
+import com.mopub.common.BaseAdapterConfiguration;
+import com.mopub.common.OnNetworkInitializationFinishedListener;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
+import java.util.Map;
+
+public class StartAppBaseConfiguration extends BaseAdapterConfiguration {
+
+  private final String NETWORK_SDK_VERSION = "3.10.1";
+
+  @NonNull @Override public String getAdapterVersion() {
+    return NETWORK_SDK_VERSION + ".0";
+  }
+
+  @Nullable @Override public String getBiddingToken(@NonNull Context context) {
+    return null;
+  }
+
+  @NonNull @Override public String getMoPubNetworkName() {
+    return "startapp";
+  }
+
+  @NonNull @Override public String getNetworkSdkVersion() {
+    return NETWORK_SDK_VERSION;
+  }
+
+  @Override public void initializeNetwork(@NonNull Context context,
+      @Nullable Map<String, String> configuration,
+      @NonNull OnNetworkInitializationFinishedListener listener) {
+    StartAppSDK.init(context, BuildConfig.STARTAPP_APPLICATION_ID, false);
+    StartAppAd.disableSplash();
+    listener.onNetworkInitializationFinished(this.getClass(),
+        MoPubErrorCode.ADAPTER_INITIALIZATION_SUCCESS);
+  }
+}
