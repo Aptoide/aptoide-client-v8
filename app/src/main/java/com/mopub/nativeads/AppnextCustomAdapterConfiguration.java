4ff008cb9afdedc4c45966f5ecf23e67656f369f
phillipsgoncalves <filisgoncalves@gmail.com>
2019-02-14 17:01:55 +0000

    [ASV-1426] Implemented appnext custom adapter configuration

--- app/src/main/java/com/mopub/nativeads/AppnextCustomAdapterConfiguration.java
+++ app/src/main/java/com/mopub/nativeads/AppnextCustomAdapterConfiguration.java
@@ -3,13 +3,15 @@ package com.mopub.nativeads;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import com.appnext.base.Appnext;
 import com.mopub.common.BaseAdapterConfiguration;
 import com.mopub.common.OnNetworkInitializationFinishedListener;
+import com.mopub.mobileads.MoPubErrorCode;
 import java.util.Map;
 
 public class AppnextCustomAdapterConfiguration extends BaseAdapterConfiguration {
   @NonNull @Override public String getAdapterVersion() {
-    return null;
+    return "2.4.4.472";
   }
 
   @Nullable @Override public String getBiddingToken(@NonNull Context context) {
@@ -17,16 +19,18 @@ public class AppnextCustomAdapterConfiguration extends BaseAdapterConfiguration 
   }
 
   @NonNull @Override public String getMoPubNetworkName() {
-    return null;
+    return "appnext";
   }
 
   @NonNull @Override public String getNetworkSdkVersion() {
-    return null;
+    return "2.4.4";
   }
 
   @Override public void initializeNetwork(@NonNull Context context,
       @Nullable Map<String, String> configuration,
       @NonNull OnNetworkInitializationFinishedListener listener) {
-
+    Appnext.init(context);
+    listener.onNetworkInitializationFinished(this.getClass(),
+        MoPubErrorCode.ADAPTER_INITIALIZATION_SUCCESS);
   }
 }
