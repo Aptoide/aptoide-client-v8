1694258b668ca8f32c6fbaa48f4ec9160e347cde
José <jose.messejana@gmail.com>
2019-02-20 12:24:03 +0000

    [AS-105] Added dot to the new Bottom navigation item

--- app/src/main/java/cm/aptoide/pt/bottomNavigation/BottomNavigationMapper.java
+++ app/src/main/java/cm/aptoide/pt/bottomNavigation/BottomNavigationMapper.java
@@ -8,9 +8,9 @@ import cm.aptoide.pt.R;
 
 public class BottomNavigationMapper {
 
+  public static final int CURATION_POSITION = 1;
   public static final int APPS_POSITION = 4;
   static final int HOME_POSITION = 0;
-  static final int CURATION_POSITION = 1;
   static final int SEARCH_POSITION = 2;
   static final int STORES_POSITION = 3;
 
