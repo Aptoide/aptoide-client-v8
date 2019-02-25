268324d41f23e2700eb7b579d62ed0754c12de13
José <jose.messejana@gmail.com>
2019-02-25 12:35:48 +0000

    Simplified Model constructor

--- app/src/main/java/cm/aptoide/pt/autoupdate/kotlin/AutoUpdateModel.kt
+++ app/src/main/java/cm/aptoide/pt/autoupdate/kotlin/AutoUpdateModel.kt
@@ -5,13 +5,9 @@ data class AutoUpdateModel(val versionCode: Int, val uri: String, val md5: Strin
                            val minSdk: String, val packageName: String, val shouldUpdate: Boolean = false,
                            var error: Error? = null, var loading: Boolean = false) {
 
-    constructor(error: Error?) : this(-1, "", "", "", "") {
-        this.error = error
-    }
+    constructor(error: Error?) : this(-1, "", "", "", "", error = error)
 
-    constructor(loading: Boolean) : this(-1, "", "", "", "") {
-        this.loading = loading
-    }
+    constructor(loading: Boolean) : this(-1, "", "", "", "", loading = loading)
 
     fun hasError(): Boolean = error != null
 }
