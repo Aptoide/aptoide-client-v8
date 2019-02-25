219fd335d0fb44793a82135c6b58399a01179bd2
phillipsgoncalves <filisgoncalves@gmail.com>
2019-02-25 11:55:08 +0000

    [ASV-1403] Generalized info action item layout so that it could be re-used for the wallet ads promotion offer

--- app/src/main/java/cm/aptoide/pt/home/InfoBundleViewHolder.java
+++ app/src/main/java/cm/aptoide/pt/home/InfoBundleViewHolder.java
@@ -1,6 +1,7 @@
 package cm.aptoide.pt.home;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import cm.aptoide.pt.R;
@@ -10,8 +11,8 @@ import rx.subjects.PublishSubject;
 
 class InfoBundleViewHolder extends AppBundleViewHolder {
   private final PublishSubject<HomeEvent> uiEventsListener;
-  private final View knowMoreButton;
-  private final View dismissButton;
+  private final Button knowMoreButton;
+  private final Button dismissButton;
   private final ImageView icon;
   private final TextView title;
   private final TextView message;
@@ -19,11 +20,11 @@ class InfoBundleViewHolder extends AppBundleViewHolder {
   public InfoBundleViewHolder(View view, PublishSubject<HomeEvent> uiEventsListener) {
     super(view);
     this.uiEventsListener = uiEventsListener;
-    this.knowMoreButton = view.findViewById(R.id.know_more_button);
+    this.knowMoreButton = view.findViewById(R.id.action_button);
     this.dismissButton = view.findViewById(R.id.dismiss_button);
-    this.icon = (ImageView) view.findViewById(R.id.icon);
-    this.title = (TextView) view.findViewById(R.id.title);
-    this.message = (TextView) view.findViewById(R.id.message);
+    this.icon = view.findViewById(R.id.icon);
+    this.title = view.findViewById(R.id.title);
+    this.message = view.findViewById(R.id.message);
   }
 
   @Override public void setBundle(HomeBundle homeBundle, int position) {
@@ -31,6 +32,8 @@ class InfoBundleViewHolder extends AppBundleViewHolder {
     ActionItem actionItem = actionBundle.getActionItem();
     ImageLoader.with(itemView.getContext())
         .load(actionItem.getIcon(), icon);
+    knowMoreButton.setText(R.string.all_button_know_more);
+    dismissButton.setText(R.string.all_button_got_it);
     title.setText(Translator.translate(actionItem.getTitle(), itemView.getContext(), ""));
     message.setText(Translator.translate(actionItem.getSubTitle(), itemView.getContext(), ""));
     knowMoreButton.setOnClickListener(view -> uiEventsListener.onNext(
--- app/src/vanilla/java/cm/aptoide/pt/home/WalletAdsOfferViewHolder.java
+++ app/src/vanilla/java/cm/aptoide/pt/home/WalletAdsOfferViewHolder.java
@@ -21,7 +21,7 @@ class WalletAdsOfferViewHolder extends AppBundleViewHolder {
   public WalletAdsOfferViewHolder(View view, PublishSubject<HomeEvent> uiEventsListener) {
     super(view);
     this.uiEventsListener = uiEventsListener;
-    this.installWalletButton = view.findViewById(R.id.know_more_button);
+    this.installWalletButton = view.findViewById(R.id.action_button);
     this.dismissButton = view.findViewById(R.id.dismiss_button);
     this.icon = view.findViewById(R.id.icon);
     this.title = view.findViewById(R.id.title);
@@ -33,6 +33,8 @@ class WalletAdsOfferViewHolder extends AppBundleViewHolder {
     ActionItem actionItem = actionBundle.getActionItem();
     ImageLoader.with(itemView.getContext())
         .load(actionItem.getIcon(), icon);
+    installWalletButton.setText(R.string.all_button_know_more);
+    dismissButton.setText(R.string.all_button_got_it);
     title.setText(Translator.translate(actionItem.getTitle(), itemView.getContext(), ""));
     message.setText(Translator.translate(actionItem.getSubTitle(), itemView.getContext(), ""));
     installWalletButton.setOnClickListener(view -> uiEventsListener.onNext(
