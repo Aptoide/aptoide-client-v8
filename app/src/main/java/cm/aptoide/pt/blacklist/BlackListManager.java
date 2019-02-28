77c3a9578e14cccebc659374260185ccdab99bf8
phillipsgoncalves <filisgoncalves@gmail.com>
2019-02-28 16:55:06 +0000

    [ASV-1403] Created BlacklistManager and blacklist persistence to abstract communication with shared preferences and store all the items that should be blacklisted

---
+++ app/src/main/java/cm/aptoide/pt/blacklist/BlackListManager.java
@@ -0,0 +1,45 @@
+package cm.aptoide.pt.blacklist;
+
+public class BlackListManager {
+
+  private BlacklistPersistence blacklistPersistence;
+
+  public BlackListManager(BlacklistPersistence blacklistPersistence) {
+    this.blacklistPersistence = blacklistPersistence;
+  }
+
+  public boolean isBlacklisted(BlacklistUnit blacklistUnit) {
+    return blacklistPersistence.isBlacklisted(blacklistUnit.getId(),
+        blacklistUnit.getMaxPossibleImpressions());
+  }
+
+  public void addImpression(BlacklistUnit blacklistUnit) {
+    blacklistPersistence.addImpression(blacklistUnit.getId(),
+        blacklistUnit.getMaxPossibleImpressions());
+  }
+
+  public void blacklist(BlacklistUnit blacklistUnit) {
+    blacklistPersistence.blacklist(blacklistUnit.getId(),
+        blacklistUnit.getMaxPossibleImpressions());
+  }
+
+  public enum BlacklistUnit {
+    WALLET_ADS_OFFER("Wallet_Ads_Offer", 10);
+
+    private String id;
+    private int maxPossibleImpressions;
+
+    BlacklistUnit(String id, int maxPossibleImpressions) {
+      this.id = id;
+      this.maxPossibleImpressions = maxPossibleImpressions;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public int getMaxPossibleImpressions() {
+      return maxPossibleImpressions;
+    }
+  }
+}
---
+++ app/src/main/java/cm/aptoide/pt/blacklist/BlacklistPersistence.java
@@ -0,0 +1,32 @@
+package cm.aptoide.pt.blacklist;
+
+import android.content.SharedPreferences;
+
+class BlacklistPersistence {
+
+  private SharedPreferences sharedPreferences;
+
+  public BlacklistPersistence(SharedPreferences sharedPreferences) {
+    this.sharedPreferences = sharedPreferences;
+  }
+
+  public boolean isBlacklisted(String id, int maxPossibleImpressions) {
+    return sharedPreferences.getInt(id, 0) == maxPossibleImpressions;
+  }
+
+  public void addImpression(String id, int maxImpressions) {
+    int actualImpressions = sharedPreferences.getInt(id, maxImpressions);
+    if (actualImpressions >= 0) {
+      actualImpressions--;
+      sharedPreferences.edit()
+          .putInt(id, actualImpressions)
+          .apply();
+    }
+  }
+
+  public void blacklist(String id, int maxPossibleImpressions) {
+    sharedPreferences.edit()
+        .putInt(id, maxPossibleImpressions)
+        .apply();
+  }
+}
