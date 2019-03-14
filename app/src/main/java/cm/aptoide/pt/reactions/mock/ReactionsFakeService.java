6a9c275198bc901ac676c1d2cdb7e5e28a6ca696
Tiago Pedrinho <rz.tiagopedrinho@gmail.com>
2019-03-14 14:36:01 +0000

    Setup reactions interactor and network model.

---
+++ app/src/main/java/cm/aptoide/pt/reactions/mock/ReactionsFakeService.java
@@ -0,0 +1,11 @@
+package cm.aptoide.pt.reactions.mock;
+
+import cm.aptoide.pt.reactions.data.ReactionType;
+import cm.aptoide.pt.reactions.network.ReactionsService;
+import rx.Completable;
+
+public class ReactionsFakeService implements ReactionsService {
+  @Override public Completable setReaction(String id, ReactionType like) {
+    return Completable.complete();
+  }
+}
