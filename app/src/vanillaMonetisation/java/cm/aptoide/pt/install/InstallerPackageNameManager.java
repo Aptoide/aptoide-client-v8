5113010a585bb183ecd9e02be0135d70027c53f2
jdandrade <joao.andrade@aptoide.com>
2019-03-14 17:11:37 +0000

    [ASV-1370] feature: installer package name is only set in vanilla dev/internal/leak/monetisation and cobrand flavours.

---
+++ app/src/vanillaMonetisation/java/cm/aptoide/pt/install/InstallerPackageNameManager.java
@@ -0,0 +1,9 @@
+package cm.aptoide.pt.install;
+
+import cm.aptoide.pt.database.realm.Download;
+
+public class InstallerPackageNameManager {
+  boolean shouldSetInstallerPackageName(Download download) {
+    return false;
+  }
+}
