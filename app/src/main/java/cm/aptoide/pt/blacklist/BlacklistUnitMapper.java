6e6d3e3f257f872ceafa16c3f4778948f53c70a8
phillipsgoncalves <filisgoncalves@gmail.com>
2019-03-15 10:59:11 +0000

    [ASV-1403] Using switch case to identify blacklistunits to improve readability

--- app/src/main/java/cm/aptoide/pt/blacklist/BlacklistUnitMapper.java
+++ app/src/main/java/cm/aptoide/pt/blacklist/BlacklistUnitMapper.java
@@ -4,13 +4,14 @@ public class BlacklistUnitMapper {
 
   public BlacklistManager.BlacklistUnit mapToBlacklistUnit(String blacklistKey) {
 
-    if (blacklistKey.equals("WALLET_ADS_OFFER_51")) {
-      return BlacklistManager.BlacklistUnit.WALLET_ADS_OFFER;
-    } else if (blacklistKey.equals("appc_card_info_1")) {
-      return BlacklistManager.BlacklistUnit.APPC_CARD_INFO;
-    } else {
-      throw new IllegalArgumentException(
-          "Wrong blacklist key. Please, make sure you are passing the correct type and id.");
+    switch (blacklistKey) {
+      case "WALLET_ADS_OFFER_51":
+        return BlacklistManager.BlacklistUnit.WALLET_ADS_OFFER;
+      case "appc_card_info_1":
+        return BlacklistManager.BlacklistUnit.APPC_CARD_INFO;
+      default:
+        throw new IllegalArgumentException(
+            "Wrong blacklist key. Please, make sure you are passing the correct type and id.");
     }
   }
 }
