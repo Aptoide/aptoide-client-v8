ddc7c44761df195ec47471d81dbd8bf9038930b1
José <jose.messejana@gmail.com>
2019-03-26 16:19:15 +0000

    [AS-123] Added user id to load reaction model

--- app/src/main/java/cm/aptoide/pt/reactions/network/LoadReactionModel.java
+++ app/src/main/java/cm/aptoide/pt/reactions/network/LoadReactionModel.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 public class LoadReactionModel {
 
+  private final String userId;
   private final List<TopReaction> topReactionList;
   private final int total;
   private final String myReaction;
 
-  public LoadReactionModel(int total, String myReaction, List<TopReaction> topReactionList) {
+  public LoadReactionModel(int total, String myReaction, String userId,
+      List<TopReaction> topReactionList) {
+    this.userId = userId;
     this.topReactionList = topReactionList;
     this.total = total;
     this.myReaction = myReaction;
@@ -19,6 +22,7 @@ public class LoadReactionModel {
   public LoadReactionModel() {
     total = -1;
     myReaction = "";
+    userId = "";
     topReactionList = Collections.emptyList();
   }
 
@@ -32,5 +36,9 @@ public class LoadReactionModel {
 
   public String getMyReaction() {
     return myReaction;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 }
