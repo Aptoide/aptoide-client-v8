209bed553008cdcc1aaed66026a6560e39e6e364
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-04-15 10:36:47 +0100

    ASV-1562 - Renamed MoPubsAdsService and its method Removed unnecessary CompositeSubscription

--- app/src/main/java/cm/aptoide/pt/ads/AdsUserPropertyManager.java
+++ app/src/main/java/cm/aptoide/pt/ads/AdsUserPropertyManager.java
@@ -5,29 +5,28 @@ import cm.aptoide.pt.install.InstalledRepository;
 import rx.Scheduler;
 import rx.subscriptions.CompositeSubscription;
 
-public class MoPubAdsService {
+public class AdsUserPropertyManager {
 
   private static final String WALLET_PACKAGE = "com.appcoins.wallet";
 
-  private final CompositeSubscription subscriptions;
   private final MoPubAdsManager moPubAdsManager;
   private final InstalledRepository installedRepository;
   private final MoPubAnalytics moPubAnalytics;
   private final CrashReport crashReport;
   private final Scheduler ioScheduler;
 
-  public MoPubAdsService(MoPubAdsManager moPubAdsManager, InstalledRepository installedRepository,
+  public AdsUserPropertyManager(MoPubAdsManager moPubAdsManager,
+      InstalledRepository installedRepository,
       MoPubAnalytics moPubAnalytics, CrashReport crashReport, Scheduler ioScheduler) {
     this.moPubAdsManager = moPubAdsManager;
     this.installedRepository = installedRepository;
     this.crashReport = crashReport;
     this.moPubAnalytics = moPubAnalytics;
     this.ioScheduler = ioScheduler;
-    this.subscriptions = new CompositeSubscription();
   }
 
-  public void observeWalletInstalation() {
-    subscriptions.add(installedRepository.isInstalled(WALLET_PACKAGE)
+  public void start() {
+    installedRepository.isInstalled(WALLET_PACKAGE)
         .observeOn(ioScheduler)
         .distinctUntilChanged()
         .flatMap(__ -> moPubAdsManager.getAdsVisibilityStatus()
@@ -36,6 +35,6 @@ public class MoPubAdsService {
         .takeWhile(offerResponseStatus -> offerResponseStatus
             != WalletAdsOfferManager.OfferResponseStatus.NO_ADS)
         .subscribe(created -> {
-        }, error -> crashReport.log(error)));
+        }, error -> crashReport.log(error));
   }
 }
