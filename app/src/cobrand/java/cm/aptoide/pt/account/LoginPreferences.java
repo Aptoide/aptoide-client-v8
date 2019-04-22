d1dd6a9a588de26e2a311dbd0ba92c2dad6f4dbd
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-04-22 15:13:16 +0100

    ASV-1410 - Created LoginPreferences cobrand version to hide Google login

---
+++ app/src/cobrand/java/cm/aptoide/pt/account/LoginPreferences.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017.
+ * Modified by Marcelo Benites on 10/02/2017.
+ */
+
+package cm.aptoide.pt.account;
+
+import android.content.Context;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class LoginPreferences {
+
+  private final Context context;
+  private final GoogleApiAvailability googleApiAvailability;
+
+  public LoginPreferences(Context context, GoogleApiAvailability googleApiAvailability) {
+    this.context = context;
+    this.googleApiAvailability = googleApiAvailability;
+  }
+
+  public boolean isGoogleLoginEnabled() {
+    return false;
+  }
+
+  public boolean isFacebookLoginEnabled() {
+    return true;
+  }
+}
