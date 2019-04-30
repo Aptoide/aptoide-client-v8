849f4ed22fbc7071e0d32e624d394192c2eb3610
jdandrade <joao.andrade@aptoide.com>
2019-04-30 17:35:09 +0100

    [ASV-1370] fix: realm migration - schema was not bumped last release.

--- aptoide-database/src/main/java/cm/aptoide/pt/database/accessors/RealmToRealmDatabaseMigration.java
+++ aptoide-database/src/main/java/cm/aptoide/pt/database/accessors/RealmToRealmDatabaseMigration.java
@@ -419,9 +419,12 @@ public class RealmToRealmDatabaseMigration implements RealmMigration {
       oldVersion++;
     }
     if (oldVersion == 8096) {
-      schema.get("Update")
-          .addField("appcUpgrade", boolean.class)
-          .transform(upgrade -> upgrade.set("appcUpgrade", false));
+      if (!schema.get("Update")
+          .hasField("appcUpgrade")) {
+        schema.get("Update")
+            .addField("appcUpgrade", boolean.class)
+            .transform(upgrade -> upgrade.set("appcUpgrade", false));
+      }
 
       oldVersion++;
     }
