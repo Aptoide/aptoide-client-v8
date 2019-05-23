8281983118cbedf60e37db62785733800bb66513
paquiquinho <paquico_9@hotmail.com>
2019-05-23 17:59:47 +0100

    [ASV-1638] Integrated admob;

--- app/src/main/java/cm/aptoide/pt/AptoideApplication.java
+++ app/src/main/java/cm/aptoide/pt/AptoideApplication.java
@@ -107,6 +107,7 @@ import com.jakewharton.rxrelay.PublishRelay;
 import com.mopub.common.MoPub;
 import com.mopub.common.SdkConfiguration;
 import com.mopub.common.logging.MoPubLog;
+import com.mopub.mobileads.GooglePlayServicesAdapterConfiguration;
 import com.mopub.nativeads.AppLovinBaseAdapterConfiguration;
 import com.mopub.nativeads.AppnextBaseAdapterConfiguration;
 import com.mopub.nativeads.InMobiBaseAdapterConfiguration;
@@ -165,8 +166,7 @@ public abstract class AptoideApplication extends Application {
   @Inject AdsRepository adsRepository;
   @Inject SyncStorage syncStorage;
   @Inject NavigationTracker navigationTracker;
-  @Inject @Named("mature-pool-v7") BodyInterceptor<BaseBody>
-      accountSettingsBodyInterceptorPoolV7;
+  @Inject @Named("mature-pool-v7") BodyInterceptor<BaseBody> accountSettingsBodyInterceptorPoolV7;
   @Inject TrendingManager trendingManager;
   @Inject AdultContentAnalytics adultContentAnalytics;
   @Inject NotificationAnalytics notificationAnalytics;
@@ -356,6 +356,8 @@ public abstract class AptoideApplication extends Application {
             getMediatedNetworkConfigurationWithAppIdMap(
                 BuildConfig.MOPUB_BANNER_50_HOME_PLACEMENT_ID,
                 BuildConfig.MOPUB_FYBER_APPLICATION_ID))
+        .withMediatedNetworkConfiguration(GooglePlayServicesAdapterConfiguration.class.getName(),
+            getAdMobAdsPreferencesMap())
         .withLogLevel(MoPubLog.LogLevel.DEBUG)
         .build();
 
@@ -375,6 +377,12 @@ public abstract class AptoideApplication extends Application {
         getMediatedNetworkConfigurationBaseMap(mediatedNetworkPlacementId);
     mediationNetworkConfiguration.put("appID", appId);
     return mediationNetworkConfiguration;
+  }
+
+  private Map<String, String> getAdMobAdsPreferencesMap() {
+    HashMap<String, String> result = new HashMap<>();
+    result.put("npa", "1");
+    return result;
   }
 
   public ApplicationComponent getApplicationComponent() {
