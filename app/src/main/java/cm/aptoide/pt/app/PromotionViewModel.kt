77a75dd7bcd9d0776bc54f699a7bf7287359f3b6
jdandrade <joao.andrade@aptoide.com>
2019-06-06 18:24:49 +0100

    ASV-1536 - Added relevant promotion models

---
+++ app/src/main/java/cm/aptoide/pt/app/PromotionViewModel.kt
@@ -0,0 +1,11 @@
+package cm.aptoide.pt.app
+
+import cm.aptoide.pt.promotions.Promotion
+import cm.aptoide.pt.promotions.WalletApp
+
+data class PromotionViewModel(
+    var walletApp: WalletApp,
+    var promotion: Promotion,
+    var isWalletInstalled: Boolean,
+    var isAppViewAppInstalled: Boolean
+)
