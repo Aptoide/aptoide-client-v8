67aca205dcd294707df1ecfc1b0c9d130a892987
jdandrade <joao.andrade@aptoide.com>
2019-06-25 14:18:29 +0100

    [ASV-1744] fix: replace bds-store with catappult (new store for appcoins certified developers)

--- app/src/main/java/cm/aptoide/pt/view/AppCoinsInfoNavigator.java
+++ app/src/main/java/cm/aptoide/pt/view/AppCoinsInfoNavigator.java
@@ -36,7 +36,7 @@ public class AppCoinsInfoNavigator {
     AppViewFragment appViewFragment = new AppViewFragment();
     Bundle bundle = new Bundle();
     bundle.putString(AppViewFragment.BundleKeys.PACKAGE_NAME.name(), APPC_WALLET_PACKAGE_NAME);
-    bundle.putString(AppViewFragment.BundleKeys.STORE_NAME.name(), "bds-store");
+    bundle.putString(AppViewFragment.BundleKeys.STORE_NAME.name(), "catappult");
     appViewFragment.setArguments(bundle);
     fragmentNavigator.navigateTo(appViewFragment, true);
   }
