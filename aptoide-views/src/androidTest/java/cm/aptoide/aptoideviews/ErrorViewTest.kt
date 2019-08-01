17ca5a035331f192c96978afc0bb8f7d19e98764
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-08-01 18:16:51 +0100

    ASV-1687 - Extracted resources to new module Added test

---
+++ aptoide-views/src/androidTest/java/cm/aptoide/aptoideviews/ErrorViewTest.kt
@@ -0,0 +1,13 @@
+package cm.aptoide.aptoideviews
+
+import cm.aptoide.aptoideviews.base.BaseTestView
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ErrorViewTest : BaseTestView() {
+
+  @Test
+  fun testCheck() {
+    assertEquals(true, true)
+  }
+}
