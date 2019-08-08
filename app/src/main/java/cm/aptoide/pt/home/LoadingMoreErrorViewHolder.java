0e0f38ab47a18a375a00ebdbcfa793e399c81a9c
phillipsgoncalves <filisgoncalves@gmail.com>
2019-08-08 09:57:27 +0100

    [ASV-1807] Created viewholder for loading more error

---
+++ app/src/main/java/cm/aptoide/pt/home/LoadingMoreErrorViewHolder.java
@@ -0,0 +1,14 @@
+package cm.aptoide.pt.home;
+
+import android.view.View;
+
+class LoadingMoreErrorViewHolder extends AppBundleViewHolder {
+
+  public LoadingMoreErrorViewHolder(View view) {
+    super(view);
+  }
+
+  @Override public void setBundle(HomeBundle homeBundle, int position) {
+
+  }
+}
