cb30a194f59c5cd3850edfadd530d5bb357498c2
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-10-04 14:49:28 +0100

    ASV-1941 - Code style

--- aptoide-views/src/main/java/cm/aptoide/aptoideviews/skeleton/SkeletonViewPreferences.kt
+++ aptoide-views/src/main/java/cm/aptoide/aptoideviews/skeleton/SkeletonViewPreferences.kt
@@ -3,8 +3,6 @@ package cm.aptoide.aptoideviews.skeleton
 import android.graphics.Color
 import cm.aptoide.aptoideviews.skeleton.mask.Border
 import cm.aptoide.aptoideviews.skeleton.mask.Shape
-import cm.aptoide.aptoideviews.skeleton.mask.Size
-import cm.aptoide.aptoideviews.skeleton.mask.SizeDimension
 
 internal data class SkeletonViewPreferences(
     var shape: Shape = Shape.Rect(Color.parseColor("#EDEEF2"), 0),
