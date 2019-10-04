54495bc982f56d42125937140b99c0fef178fb9a
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-10-04 14:50:08 +0100

    ASV-1941 - Code style

--- aptoide-views/src/main/java/cm/aptoide/aptoideviews/skeleton/mask/Shape.kt
+++ aptoide-views/src/main/java/cm/aptoide/aptoideviews/skeleton/mask/Shape.kt
@@ -4,6 +4,6 @@ import androidx.annotation.ColorInt
 import androidx.annotation.Px
 
 internal sealed class Shape(@ColorInt val color: Int) {
-  class Rect(@ColorInt color: Int, @Px val cornerRadius: Int): Shape(color)
-  class Circle(@ColorInt color: Int): Shape(color)
+  class Rect(@ColorInt color: Int, @Px val cornerRadius: Int) : Shape(color)
+  class Circle(@ColorInt color: Int) : Shape(color)
 }
