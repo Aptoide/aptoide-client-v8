29ae3a9fc72b737d035dd9afeab8f709e4a41d0f
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-10-28 16:35:38 +0000

    ASV-2061 - Added DownloadProgressView to project

---
+++ aptoide-views/src/main/java/cm/aptoide/aptoideviews/common/AnimatedImageView.kt
@@ -0,0 +1,54 @@
+package cm.aptoide.aptoideviews.common
+
+import android.content.Context
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.widget.ImageView
+import androidx.appcompat.content.res.AppCompatResources
+import cm.aptoide.aptoideviews.R
+
+class AnimatedImageView : ImageView {
+
+  var animation: Animatable? = null
+  var reverse: Animatable? = null
+  var isAnimationsEnabled = true
+
+  private var isReverse = false
+
+  constructor(context: Context) : this(context, null)
+  constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs,
+      defStyleAttr) {
+    retrievePreferences(attrs, defStyleAttr)
+  }
+
+  private fun retrievePreferences(attrs: AttributeSet?, defStyleAttr: Int) {
+    val typedArray =
+        context.obtainStyledAttributes(attrs, R.styleable.AnimatedImageView, defStyleAttr, 0)
+    val animationId = typedArray.getResourceId(R.styleable.AnimatedImageView_animation, -1)
+    val reverseId = typedArray.getResourceId(R.styleable.AnimatedImageView_reverseAnimation, -1)
+
+    if (animationId != -1) {
+      animation = AppCompatResources.getDrawable(context, animationId) as Animatable
+    }
+    if (reverseId != -1) {
+      reverse = AppCompatResources.getDrawable(context, reverseId) as Animatable
+    }
+    typedArray.recycle()
+  }
+
+  fun play() {
+    val drawable: Animatable? = if (!isReverse) animation else reverse
+    drawable?.let {
+      setImageDrawable(drawable as Drawable)
+      if (isAnimationsEnabled) drawable.start()
+      isReverse = !isReverse
+    }
+  }
+
+  fun setReverseAsDefault() {
+    setImageDrawable(reverse as Drawable)
+  }
+
+}
