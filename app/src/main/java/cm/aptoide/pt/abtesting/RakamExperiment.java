d9000ae28a31a433769b2695981b85fa86419a55
phillipsgoncalves <filisgoncalves@gmail.com>
2019-10-30 12:26:59 +0000

    [ASV-2053] Created base experiment and consequent type experiments that should represent all of the created experiments. Future experiments should all be either of Rakam or Wasabi type, unless we create a new one. If we do so, then we should create create a new class that implements BaseExperiment interface.

---
+++ app/src/main/java/cm/aptoide/pt/abtesting/RakamExperiment.java
@@ -0,0 +1,8 @@
+package cm.aptoide.pt.abtesting;
+
+public class RakamExperiment implements BaseExperiment {
+
+  @Override public ExperimentType getType() {
+    return ExperimentType.RAKAM;
+  }
+}
