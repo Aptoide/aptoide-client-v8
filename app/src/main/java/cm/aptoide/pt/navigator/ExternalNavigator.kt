2ffaeafe96fe431adff2e4b7d8dcc536e93d1a7a
paquiquinho <paquico_9@hotmail.com>
2019-11-14 12:41:32 +0000

    [ASV-2037] Fixed tests;

--- app/src/main/java/cm/aptoide/pt/navigator/ExternalNavigator.kt
+++ app/src/main/java/cm/aptoide/pt/navigator/ExternalNavigator.kt
@@ -3,7 +3,7 @@ package cm.aptoide.pt.navigator
 import android.content.Context
 import cm.aptoide.pt.link.CustomTabsHelper
 
-class ExternalNavigator(val context: Context, val theme: String) {
+open class ExternalNavigator(val context: Context, val theme: String) {
 
   fun navigateToCatappultWebsite() {
     CustomTabsHelper.getInstance()
