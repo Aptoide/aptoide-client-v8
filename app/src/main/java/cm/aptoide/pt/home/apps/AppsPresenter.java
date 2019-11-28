0340155b0cd78d6ad354f74a2a8ce5b2628865ed
Francisco Aleixo <francisco.aleixo16@gmail.com>
2019-11-28 10:51:47 +0000

    ASV-2060 - Removed comment

--- app/src/main/java/cm/aptoide/pt/home/apps/AppsPresenter.java
+++ app/src/main/java/cm/aptoide/pt/home/apps/AppsPresenter.java
@@ -48,8 +48,6 @@ public class AppsPresenter implements Presenter {
 
     observeAppModelState();
 
-    //handleAppcUpgradesSeeMoreClick();
-
     handlePauseDownloadClick();
 
     handleResumeClick();
