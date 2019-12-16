52ae3cd04510f60a96fe5288af653fb10a1be452
phillipsgoncalves <filisgoncalves@gmail.com>
2019-12-16 18:09:30 +0000

    [ASV-1766] Setting system theme after opening the app

--- app/src/main/java/cm/aptoide/pt/view/ThemedActivityView.java
+++ app/src/main/java/cm/aptoide/pt/view/ThemedActivityView.java
@@ -1,5 +1,6 @@
 package cm.aptoide.pt.view;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -10,6 +11,32 @@ public abstract class ThemedActivityView extends ActivityView {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getActivityComponent().inject(this);
+    //ThemeUtils.setStatusBarThemeColor(this, theme);
+    //ThemeUtils.setAptoideTheme(this, "dark");
+
+    setSystemTheme(getResources().getConfiguration());
+  }
+
+  private void setSystemTheme(Configuration configuration) {
+    int currentNightMode = configuration.uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    switch (currentNightMode) {
+      case Configuration.UI_MODE_NIGHT_NO:
+        // Night mode is not active, we're using the light theme
+        setLightTheme();
+        break;
+      case Configuration.UI_MODE_NIGHT_YES:
+        // Night mode is active, we're using dark theme
+        setDarkTheme();
+        break;
+    }
+  }
+
+  private void setLightTheme() {
+    ThemeUtils.setStatusBarThemeColor(this, theme);
+    ThemeUtils.setAptoideTheme(this, theme);
+  }
+
+  private void setDarkTheme() {
     ThemeUtils.setStatusBarThemeColor(this, theme);
     ThemeUtils.setAptoideTheme(this, "dark");
   }
