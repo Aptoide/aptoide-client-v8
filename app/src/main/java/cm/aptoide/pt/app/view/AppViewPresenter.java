4cbedd005e6e6fb410f5bfa3a5060648107fe22c
Francisco Aleixo <francisco.aleixo16@gmail.com>
2020-01-24 15:15:08 +0000

    MOB-166 - Fixed status bar

--- app/src/main/java/cm/aptoide/pt/app/view/AppViewPresenter.java
+++ app/src/main/java/cm/aptoide/pt/app/view/AppViewPresenter.java
@@ -735,9 +735,7 @@ public class AppViewPresenter implements Presenter {
           appViewAnalytics.sendReadAllEvent();
           appViewNavigator.navigateToRateAndReview(model.getAppId(), model.getAppName(),
               model.getStore()
-                  .getName(), model.getPackageName(), model.getStore()
-                  .getAppearance()
-                  .getTheme());
+                  .getName(), model.getPackageName(), "default");
         })
         .compose(view.bindUntilEvent(View.LifecycleEvent.DESTROY))
         .subscribe(__ -> {
