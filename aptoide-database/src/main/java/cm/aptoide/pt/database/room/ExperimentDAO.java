0cb3b45d132aa5880f1b05de52be25de806327c9
phillipsgoncalves <filisgoncalves@gmail.com>
2020-02-27 16:40:58 +0000

    [MOB-75] Fixed experiment dao query

--- aptoide-database/src/main/java/cm/aptoide/pt/database/room/ExperimentDAO.java
+++ aptoide-database/src/main/java/cm/aptoide/pt/database/room/ExperimentDAO.java
@@ -3,14 +3,14 @@ package cm.aptoide.pt.database.room;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
-import io.reactivex.Observable;
+import io.reactivex.Single;
 
 import static androidx.room.OnConflictStrategy.REPLACE;
 
 @Dao public interface ExperimentDAO {
 
-  @Query("SELECT * FROM experiment where experimentName LIKE :identifier")
-  Observable<RoomExperiment> get(String identifier);
+  @Query("SELECT * FROM experiment WHERE experimentName = :identifier") Single<RoomExperiment> get(
+      String identifier);
 
   @Insert(onConflict = REPLACE) void save(RoomExperiment experiment);
 }
