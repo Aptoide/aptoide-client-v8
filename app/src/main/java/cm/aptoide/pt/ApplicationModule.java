dbf20bc2fcdf1bb77ec25a949300aa138c179320
phillipsgoncalves <filisgoncalves@gmail.com>
2020-03-06 15:49:02 +0000

    [MOB-76] Using correct persistence in Room

--- app/src/main/java/cm/aptoide/pt/ApplicationModule.java
+++ app/src/main/java/cm/aptoide/pt/ApplicationModule.java
@@ -205,8 +205,8 @@ import cm.aptoide.pt.networking.UserAgentInterceptor;
 import cm.aptoide.pt.networking.UserAgentInterceptorV8;
 import cm.aptoide.pt.notification.NotificationAnalytics;
 import cm.aptoide.pt.notification.NotificationProvider;
-import cm.aptoide.pt.notification.RealmLocalNotificationSyncMapper;
-import cm.aptoide.pt.notification.RealmLocalNotificationSyncPersistence;
+import cm.aptoide.pt.notification.RoomLocalNotificationSyncMapper;
+import cm.aptoide.pt.notification.RoomLocalNotificationSyncPersistence;
 import cm.aptoide.pt.notification.sync.LocalNotificationSyncManager;
 import cm.aptoide.pt.packageinstaller.AppInstaller;
 import cm.aptoide.pt.preferences.AptoideMd5Manager;
@@ -855,7 +855,7 @@ import static com.google.android.gms.auth.api.Auth.GOOGLE_SIGN_IN_API;
   }
 
   @Singleton @Provides SyncStorage provideSyncStorage(
-      RealmLocalNotificationSyncPersistence persistence) {
+      RoomLocalNotificationSyncPersistence persistence) {
     return new SyncStorage(new HashMap<>(), persistence);
   }
 
@@ -1949,10 +1949,10 @@ import static com.google.android.gms.auth.api.Auth.GOOGLE_SIGN_IN_API;
   }
 
   @Singleton @Provides
-  RealmLocalNotificationSyncPersistence providesRealmLocalNotificationSyncPersistence(
-      Database database, NotificationProvider provider) {
-    return new RealmLocalNotificationSyncPersistence(database,
-        new RealmLocalNotificationSyncMapper(), provider);
+  RoomLocalNotificationSyncPersistence providesRealmLocalNotificationSyncPersistence(
+      AptoideDatabase database, NotificationProvider provider) {
+    return new RoomLocalNotificationSyncPersistence(new RoomLocalNotificationSyncMapper(), provider,
+        database.localNotificationSyncDao());
   }
 
   @Singleton @Provides LocalNotificationSyncManager providesLocalNotificationSyncManager(
