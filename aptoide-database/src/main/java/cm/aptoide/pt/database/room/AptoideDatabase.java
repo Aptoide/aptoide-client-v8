0ef7bd8e0cdb572f378ad7bc7c73659a97fbf24e
phillipsgoncalves <filisgoncalves@gmail.com>
2020-03-30 19:14:10 +0100

    [MOB-264] Created new room store database table and its dao to allow accessing data

--- aptoide-database/src/main/java/cm/aptoide/pt/database/room/AptoideDatabase.java
+++ aptoide-database/src/main/java/cm/aptoide/pt/database/room/AptoideDatabase.java
@@ -13,7 +13,7 @@ import static cm.aptoide.pt.database.room.AptoideDatabase.VERSION;
 @Database(entities = {
     RoomEvent.class, RoomExperiment.class, RoomStoredMinimalAd.class, RoomNotification.class,
     RoomLocalNotificationSync.class, RoomInstalled.class, RoomInstallation.class, RoomUpdate.class,
-    RoomDownload.class
+    RoomDownload.class, RoomStore.class
 }, version = VERSION) @TypeConverters({
     SplitTypeConverter.class, StringTypeConverter.class, FileToDownloadTypeConverter.class
 }) public abstract class AptoideDatabase extends RoomDatabase {
@@ -40,4 +40,6 @@ import static cm.aptoide.pt.database.room.AptoideDatabase.VERSION;
   public abstract UpdateDao updateDao();
 
   public abstract DownloadDAO downloadDAO();
+
+  public abstract StoreDao storeDao();
 }
---
+++ aptoide-database/src/main/java/cm/aptoide/pt/database/room/StoreDao.java
@@ -0,0 +1,29 @@
+package cm.aptoide.pt.database.room;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import io.reactivex.Observable;
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao public interface StoreDao {
+
+  @Query("SELECT * from store") Observable<List<RoomStore>> getAll();
+
+  @Query("SELECT * from store where storeName = :storeName") Observable<RoomStore> getByStoreName(
+      String storeName);
+
+  @Query("SELECT * from store where storeName = :storeId") Observable<RoomStore> getByStoreId(
+      long storeId);
+
+  @Query("SELECT COUNT(*) from store where storeName = :storeId") Observable<Integer> isSubscribed(
+      long storeId);
+
+  @Query("DELETE FROM store where storeName = :storeName") void removeByStoreName(String storeName);
+
+  @Insert(onConflict = REPLACE) void insert(RoomStore store);
+
+  @Query("SELECT COUNT(*) FROM store") Observable<Long> countAll();
+}
