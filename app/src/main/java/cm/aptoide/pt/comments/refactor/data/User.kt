73817211376cb3385f2af3155bb9afd09fb9dee5
phillipsgoncalves <filisgoncalves@gmail.com>
2020-04-21 11:20:33 +0100

    [MOB-406] Created a new default constructor in User order to convert local accounts to User, which have no id, avatar or nickname

--- app/src/main/java/cm/aptoide/pt/comments/refactor/data/User.kt
+++ app/src/main/java/cm/aptoide/pt/comments/refactor/data/User.kt
@@ -1,3 +1,6 @@
 package cm.aptoide.pt.comments.refactor.data
 
-data class User(val id: Long, val avatar: String, val name: String)
+data class User(val id: Long, val avatar: String, val name: String) {
+
+  constructor(avatar: String, name: String) : this(-1, avatar, name)
+}
