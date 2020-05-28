90236ea41f6c64807cefe3931cde1cf29ca59460
jdandradex <joao.andrade@aptoide.com>
2020-05-28 16:03:24 +0100

    [MOB-555] feature: improve remote authentication service calls to be made on a specific context (IO) and return a Response retrofit wrapper object so we can validate http codes.

--- aptoide-authentication-core/src/main/kotlin/com/aptoide/authentication/network/RemoteAuthenticationService.kt
+++ aptoide-authentication-core/src/main/kotlin/com/aptoide/authentication/network/RemoteAuthenticationService.kt
@@ -1,10 +1,14 @@
 package com.aptoide.authentication.network
 
+import com.aptoide.authentication.AuthenticationException
 import com.aptoide.authentication.model.CodeAuth
 import com.aptoide.authentication.model.OAuth2
 import com.aptoide.authentication.service.AuthenticationService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -21,25 +25,44 @@ class RemoteAuthenticationService :
           .create(AuthorizationV7::class.java)
 
   override suspend fun sendMagicLink(email: String): CodeAuth {
-    return authorizationV7.sendMagicLink(email, Type.EMAIL, arrayOf("CODE:TOKEN:EMAIL"))
+    return withContext(Dispatchers.IO) {
+      val sendMagicLinkResponse =
+          authorizationV7.sendMagicLink(email, Type.EMAIL, arrayOf("CODE:TOKE:EMAIL"))
+      val codeAuth = sendMagicLinkResponse.body()
+      if (sendMagicLinkResponse.isSuccessful && codeAuth != null) {
+        return@withContext codeAuth!!
+      } else {
+        throw AuthenticationException(
+            sendMagicLinkResponse.code().toString() + " " + sendMagicLinkResponse.message())
+      }
+    }
   }
 
   override suspend fun authenticate(magicToken: String, state: String, agent: String): OAuth2 {
-    return authorizationV7.authenticate(magicToken, Type.CODE, arrayOf("OAUTH2"), state, agent)
+    return withContext(Dispatchers.IO) {
+      val authenticateResponse =
+          authorizationV7.authenticate(magicToken, Type.CODE, arrayOf("OAUTH2"), state, agent)
+      val oAuth2 = authenticateResponse.body()
+      if (authenticateResponse.isSuccessful && oAuth2 != null) {
+        return@withContext oAuth2!!
+      } else
+        throw AuthenticationException(
+            authenticateResponse.code().toString() + " " + authenticateResponse.message())
+    }
   }
-
 
   interface AuthorizationV7 {
     @GET("user/authorize")
     suspend fun authenticate(@Query("credential") credential: String,
                              @Query("type") type: Type, @Query(
             "supported") supported: Array<String>,
-                             @Query("state") state: String, @Query("agent") agent: String): OAuth2
+                             @Query("state") state: String,
+                             @Query("agent") agent: String): Response<OAuth2>
 
     @GET("user/authorize")
     suspend fun sendMagicLink(@Query("credential") credential: String,
                               @Query("type") type: Type, @Query(
-            "supported") supported: Array<String>): CodeAuth
+            "supported") supported: Array<String>): Response<CodeAuth>
 
   }
 }
