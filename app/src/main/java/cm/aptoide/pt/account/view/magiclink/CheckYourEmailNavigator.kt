dbdcda0aed118e84b40c6deff93bb3ac467829fd
franciscoaleixo <francisco.aleixo16@gmail.com>
2020-05-29 10:52:04 +0100

    MOB-555 - Fixed email new task

--- app/src/main/java/cm/aptoide/pt/account/view/magiclink/CheckYourEmailNavigator.kt
+++ app/src/main/java/cm/aptoide/pt/account/view/magiclink/CheckYourEmailNavigator.kt
@@ -8,6 +8,7 @@ class CheckYourEmailNavigator(private val activityNavigator: ActivityNavigator?)
   fun navigateToEmailApp() {
     val intent = Intent(Intent.ACTION_MAIN)
     intent.addCategory(Intent.CATEGORY_APP_EMAIL)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     activityNavigator?.navigateWithIntent(intent)
   }
 }
