fb7bdb0659a5a7c3df23b49a0847e538c9003244
franciscoaleixo <francisco.aleixo16@gmail.com>
2020-06-25 00:51:18 +0100

    MOB-656 - Fixed styling

--- aptoide-views/src/main/java/cm/aptoide/aptoideviews/appcoins/BonusAppcView.kt
+++ aptoide-views/src/main/java/cm/aptoide/aptoideviews/appcoins/BonusAppcView.kt
@@ -2,9 +2,13 @@ package cm.aptoide.aptoideviews.appcoins
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.widget.FrameLayout
+import androidx.annotation.Dimension
+import androidx.annotation.Px
 import cm.aptoide.aptoideviews.R
 import kotlinx.android.synthetic.main.bonus_appc_view.view.*
+
 
 class BonusAppcView : FrameLayout {
 
@@ -21,6 +25,13 @@ class BonusAppcView : FrameLayout {
     val typedArray =
         context.obtainStyledAttributes(attrs, R.styleable.BonusAppcView, defStyleAttr, 0)
 
+    if (!typedArray.getBoolean(R.styleable.BonusAppcView_enable_card_padding, true)) {
+      val params = card_view.layoutParams
+      params.width = dpToPx(66).toInt()
+      params.height = dpToPx(72).toInt()
+      card_view.layoutParams = params
+    }
+
     setSide(Side.values()[typedArray.getInt(R.styleable.BonusAppcView_side, 0)])
     typedArray.recycle()
   }
@@ -34,6 +45,12 @@ class BonusAppcView : FrameLayout {
 
   fun setPercentage(percentage: Int) {
     percentage_number_textview.text = percentage.toString()
+  }
+
+  @Px
+  private fun dpToPx(@Dimension(unit = Dimension.DP) dp: Int): Float {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(),
+        resources.displayMetrics)
   }
 
   enum class Side { LEFT, RIGHT }
