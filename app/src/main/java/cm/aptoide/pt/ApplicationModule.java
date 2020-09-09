5859d3774a8d32fa0dec51b66e30577951bae3c1
franciscoaleixo <francisco.aleixo16@gmail.com>
2020-09-09 18:47:46 +0100

    MOB-786 - Incremented db and centralized migrations

--- app/src/main/java/cm/aptoide/pt/ApplicationModule.java
+++ app/src/main/java/cm/aptoide/pt/ApplicationModule.java
@@ -20,11 +20,8 @@ import android.telephony.TelephonyManager;
 import android.text.format.DateUtils;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
-import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.room.Room;
-import androidx.room.migration.Migration;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 import cm.aptoide.accountmanager.AccountFactory;
 import cm.aptoide.accountmanager.AccountPersistence;
 import cm.aptoide.accountmanager.AccountService;
@@ -1049,16 +1046,17 @@ import static com.google.android.gms.auth.api.Auth.GOOGLE_SIGN_IN_API;
         logger);
   }
 
-  @Singleton @Provides AptoideDatabase providesAptoideDataBase() {
+  @Singleton @Provides AptoideDatabase providesAptoideDataBase(
+      RoomMigrationProvider roomMigrationProvider) {
     return Room.databaseBuilder(application.getApplicationContext(), AptoideDatabase.class,
         BuildConfig.ROOM_DATABASE_NAME)
         .fallbackToDestructiveMigrationFrom(getSQLiteIntArrayVersions())
-        .addMigrations(new Migration(100, 101) {
-          @Override public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE download " + " ADD COLUMN attributionId TEXT");
-          }
-        })
+        .addMigrations(roomMigrationProvider.getMigrations())
         .build();
+  }
+
+  @Singleton @Provides RoomMigrationProvider providesRoomMigrationProvider() {
+    return new RoomMigrationProvider();
   }
 
   private int[] getSQLiteIntArrayVersions() {
---
+++ app/src/main/java/cm/aptoide/pt/RoomMigrationProvider.kt
@@ -0,0 +1,17 @@
+package cm.aptoide.pt
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+class RoomMigrationProvider {
+  val migrations = arrayOf(object : Migration(100, 101) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+      database.execSQL("ALTER TABLE download " + " ADD COLUMN attributionId TEXT")
+    }
+  }, object : Migration(101, 102) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+      // Boolean attributes must be defined as non-null integers with set default
+      database.execSQL("ALTER TABLE installed " + " ADD COLUMN enabled INTEGER DEFAULT 0 NOT NULL")
+    }
+  })
+}
