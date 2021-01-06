96292b848e1356688ebb9f9a55f5f87ba3a15246
Mykhailo Dobosh <mykhailo.dobosh@goloballogic.com>
2021-01-06 16:46:33 +0200

    changed default store to smarttech-iq

--- app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
+++ app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
@@ -1,7 +1,7 @@
 package cm.aptoide.pt.store.view.my;
 
 public class SMARTStore {
-    public static final String STORE_NAME = "jacksonland2";
+    public static final String STORE_NAME = "smarttech-iq";
     public static final String STORE_COLOR = "red";
 
     private SMARTStore() {}
