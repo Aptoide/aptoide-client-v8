8a877c667b50e1d9eb52e61d89b8542de2fd8434
phillipsgoncalves <filisgoncalves@gmail.com>
2021-03-02 01:02:29 +0000

    [APP-135] Created social media navigator to open the social media page that the user clicked on

---
+++ app/src/main/java/cm/aptoide/pt/socialMedia/SocialMediaNavigator.kt
@@ -0,0 +1,32 @@
+package cm.aptoide.pt.socialMedia
+
+import android.content.Context
+import cm.aptoide.aptoideviews.socialmedia.SocialMediaView
+import cm.aptoide.pt.R
+import cm.aptoide.pt.link.CustomTabsHelper
+import cm.aptoide.pt.themes.ThemeManager
+
+class SocialMediaNavigator(val context: Context, val themeManager: ThemeManager) {
+
+  companion object {
+    var FACEBOOK_URL = "https://pt-pt.facebook.com/aptoide/"
+    var TWITTER_URL = "https://twitter.com/aptoide"
+    var INSTAGRAM_URL = "https://www.instagram.com/aptoideteam/"
+  }
+
+  fun navigateToSocialMediaWebsite(socialMediaType: SocialMediaView.SocialMediaType) {
+    val socialMediaUrl: String = mapSocialMediaTypeToUrl(socialMediaType)
+    CustomTabsHelper.getInstance()
+        .openInChromeCustomTab(socialMediaUrl, context,
+            themeManager.getAttributeForTheme(
+                R.attr.colorPrimary).resourceId)
+  }
+
+  private fun mapSocialMediaTypeToUrl(socialMediaType: SocialMediaView.SocialMediaType): String {
+    return when (socialMediaType) {
+      SocialMediaView.SocialMediaType.FACEBOOK_CLICK -> FACEBOOK_URL
+      SocialMediaView.SocialMediaType.TWITTER_CLICK -> TWITTER_URL
+      SocialMediaView.SocialMediaType.INSTAGRAM_CLICK -> INSTAGRAM_URL
+    }
+  }
+}
