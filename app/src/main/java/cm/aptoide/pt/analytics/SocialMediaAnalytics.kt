8efe88ec3d07dc5a1d2a778f6cda9921b8fa0638
phillipsgoncalves <filisgoncalves@gmail.com>
2021-03-02 00:18:14 +0000

    [APP-135] Created SocialMediaAnalytics to send analytics regarding the promote social media feature

---
+++ app/src/main/java/cm/aptoide/pt/analytics/SocialMediaAnalytics.kt
@@ -0,0 +1,39 @@
+package cm.aptoide.pt.analytics
+
+import cm.aptoide.analytics.AnalyticsManager
+import cm.aptoide.analytics.implementation.navigation.NavigationTracker
+import cm.aptoide.aptoideviews.socialmedia.SocialMediaView
+import java.util.*
+
+open class SocialMediaAnalytics(val analyticsManager: AnalyticsManager,
+                                val navigationTracker: NavigationTracker) {
+
+  companion object {
+    @JvmField
+    var PROMOTE_SOCIAL_MEDIA_EVENT_NAME: String = "promote_social_media_click"
+    var FACEBOOK_ACTION: String = "facebook"
+    var TWITTER_ACTION: String = "twitter"
+    var INSTAGRAM_ACTION: String = "instagram"
+    var CONTEXT = "context"
+    var ACTION = "action"
+
+  }
+
+  fun sendPromoteSocialMediaClickEvent(socialMediaType: SocialMediaView.SocialMediaType) {
+    val map = HashMap<String, Any>()
+    map[ACTION] = mapSocialMediaTypeToAction(socialMediaType)
+    map[CONTEXT] = navigationTracker.getViewName(true)
+    analyticsManager.logEvent(map, PROMOTE_SOCIAL_MEDIA_EVENT_NAME,
+        AnalyticsManager.Action.CLICK,
+        navigationTracker.getViewName(true))
+  }
+
+  private fun mapSocialMediaTypeToAction(socialMediaType: SocialMediaView.SocialMediaType): String {
+    return when (socialMediaType) {
+      SocialMediaView.SocialMediaType.FACEBOOK_CLICK -> FACEBOOK_ACTION
+      SocialMediaView.SocialMediaType.TWITTER_CLICK -> TWITTER_ACTION
+      SocialMediaView.SocialMediaType.INSTAGRAM_CLICK -> INSTAGRAM_ACTION
+    }
+  }
+
+}
