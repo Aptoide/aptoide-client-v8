d32c2d889afe15c00d4e1761de09542dbcf4806c
phillipsgoncalves <filisgoncalves@gmail.com>
2021-03-04 10:58:07 +0000

    [APP-135] Fixed event social media event with duplicated context attribute

--- app/src/main/java/cm/aptoide/pt/socialMedia/SocialMediaAnalytics.kt
+++ app/src/main/java/cm/aptoide/pt/socialMedia/SocialMediaAnalytics.kt
@@ -14,14 +14,12 @@ open class SocialMediaAnalytics(val analyticsManager: AnalyticsManager,
     var FACEBOOK_ACTION: String = "facebook"
     var TWITTER_ACTION: String = "twitter"
     var INSTAGRAM_ACTION: String = "instagram"
-    var CONTEXT = "context"
     var ACTION = "action"
   }
 
   fun sendPromoteSocialMediaClickEvent(socialMediaType: SocialMediaView.SocialMediaType) {
     val map = HashMap<String, Any>()
     map[ACTION] = mapSocialMediaTypeToAction(socialMediaType)
-    map[CONTEXT] = navigationTracker.getViewName(true)
     analyticsManager.logEvent(map, PROMOTE_SOCIAL_MEDIA_EVENT_NAME,
         AnalyticsManager.Action.CLICK,
         navigationTracker.getViewName(true))
