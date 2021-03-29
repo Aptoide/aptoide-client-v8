e587d17d12628d13306c589d27aaf1748f431de2
phillipsgoncalves <filisgoncalves@gmail.com>
2021-03-29 16:18:23 +0100

    [APP-30] created OutOfSpaceManager to create list of apps to uninstall and uninstall apps for the out of space dialog feature

---
+++ app/src/main/java/cm/aptoide/pt/download/view/outofspace/OutOfSpaceManager.kt
@@ -0,0 +1,29 @@
+package cm.aptoide.pt.download.view.outofspace
+
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import cm.aptoide.pt.install.InstallManager
+import cm.aptoide.pt.utils.AptoideUtils
+import rx.Completable
+import rx.Observable
+import java.io.File
+
+
+class OutOfSpaceManager(private val packageManager: PackageManager,
+                        private val installManager: InstallManager) {
+  fun getInstalledApps(): Observable<List<InstalledApp>> {
+    return Observable.from(
+        packageManager.getInstalledApplications(PackageManager.GET_META_DATA))
+        .filter { (it.flags and ApplicationInfo.FLAG_SYSTEM) != 0 }
+        .map {
+          InstalledApp(packageManager.getApplicationLabel(it).toString(), it.packageName,
+              AptoideUtils.SystemU.getApkIconPath(
+                  packageManager.getPackageInfo(it.packageName, PackageManager.GET_META_DATA)),
+              File(packageManager.getApplicationInfo(it.packageName, 0).publicSourceDir).length())
+        }.toList()
+  }
+
+  fun uninstallApp(packageName: String?): Completable {
+    return installManager.uninstallApp(packageName)
+  }
+}
