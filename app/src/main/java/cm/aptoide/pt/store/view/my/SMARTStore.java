1e0068f7017b846b49d54704acbcdf87261de656
mdobosh-smarttech <74913516+mdobosh-smarttech@users.noreply.github.com>
2021-04-09 11:16:23 +0300

    added one more store url for debug/release builds (#21)
    Co-authored-by: Mykhailo Dobosh <mykhailo.dobosh@goloballogic.com>

--- app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
+++ app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
@@ -1,7 +1,12 @@
 package cm.aptoide.pt.store.view.my;
 
+import android.os.Build;
+
 public class SMARTStore {
-    public static final String STORE_NAME = "smarttech-iq";
+    private static final boolean DEBUG = "userdebug".equals(Build.TYPE);
+
+    //TODO replace url with original one when provided
+    public static final String STORE_NAME = DEBUG ? "smarttech-iq" : "pein95";
     public static final String STORE_COLOR = "red";
 
     private SMARTStore() {}
