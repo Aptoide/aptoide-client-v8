e06cdf5366174425bbdbfc1ca703d483f4c561b6
mdobosh-smarttech <74913516+mdobosh-smarttech@users.noreply.github.com>
2021-04-09 16:49:37 +0300

    Feature/us118172 using debug store url (#22)
    * added one more store url for debug/release builds
    
    * changed app store uri
    
    * changed app store uri
    
    Co-authored-by: Mykhailo Dobosh <mykhailo.dobosh@goloballogic.com>

--- app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
+++ app/src/main/java/cm/aptoide/pt/store/view/my/SMARTStore.java
@@ -5,8 +5,7 @@ import android.os.Build;
 public class SMARTStore {
     private static final boolean DEBUG = "userdebug".equals(Build.TYPE);
 
-    //TODO replace url with original one when provided
-    public static final String STORE_NAME = DEBUG ? "smarttech-iq" : "pein95";
+    public static final String STORE_NAME = DEBUG ? "aptoide-test-store" : "smarttech-iq";
     public static final String STORE_COLOR = "red";
 
     private SMARTStore() {}
