053e7119b6d9eb5316ad2c49b5fb515de6f5296d
jdandradex <joao.andrade@aptoide.com>
2021-06-21 16:32:34 +0100

    [APP-307] feature: support impression and click events for promotionals pointing to editorial

--- app/src/main/java/cm/aptoide/pt/home/bundles/base/HomeBundle.java
+++ app/src/main/java/cm/aptoide/pt/home/bundles/base/HomeBundle.java
@@ -26,5 +26,12 @@ public interface HomeBundle {
       return this.equals(APPS) || this.equals(EDITORS) || this.equals(ADS) || this.equals(
           APPCOINS_ADS) || this.equals(FEATURED_BONUS_APPC);
     }
+
+    public boolean isPromotional() {
+      return this.equals(NEW_APP)
+          || this.equals(NEWS_ITEM)
+          || this.equals(NEW_APP_VERSION)
+          || this.equals(IN_GAME_EVENT);
+    }
   }
 }
