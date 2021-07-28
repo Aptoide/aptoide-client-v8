d09de142431c36a3bb84c0586eb74974617f1a85
jdandradex <joao.andrade@aptoide.com>
2021-07-28 14:58:02 +0100

    [APP-450] fix: remove unnecessary imports

--- app/src/main/java/cm/aptoide/pt/home/EskillsPreferencesManager.java
+++ app/src/main/java/cm/aptoide/pt/home/EskillsPreferencesManager.java
@@ -3,7 +3,6 @@ package cm.aptoide.pt.home;
 import cm.aptoide.pt.appview.PreferencesPersister;
 
 import static cm.aptoide.pt.preferences.managed.ManagedKeys.SHOW_ESKILLS_DIALOG;
-import static cm.aptoide.pt.preferences.managed.ManagedKeys.SHOW_PROMOTIONS_DIALOG;
 
 public class EskillsPreferencesManager {
 
