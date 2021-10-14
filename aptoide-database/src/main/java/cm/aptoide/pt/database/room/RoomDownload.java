5c72454eb858fa839d90229ed75bbd5dae290be7
phillipsgoncalves <filisgoncalves@gmail.com>
2021-10-15 00:52:37 +0100

    [APP-557] Created getter for the splits of a download

--- aptoide-database/src/main/java/cm/aptoide/pt/database/room/RoomDownload.java
+++ aptoide-database/src/main/java/cm/aptoide/pt/database/room/RoomDownload.java
@@ -13,6 +13,7 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(tableName = "download") public class RoomDownload {
@@ -247,6 +248,16 @@ import java.util.List;
       }
     }
     return false;
+  }
+
+  public List<RoomFileToDownload> getSplits() {
+    List<RoomFileToDownload> splitsList = new ArrayList<>();
+    for (RoomFileToDownload fileToDownload : filesToDownload) {
+      if (fileToDownload.getFileType() == RoomFileToDownload.SPLIT) {
+        splitsList.add(fileToDownload);
+      }
+    }
+    return splitsList;
   }
 
   public String getStoreName() {
