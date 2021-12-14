382c2c0a14712f84156b67cefb83ac3d533ce729
phillipsgoncalves <filisgoncalves@gmail.com>
2021-12-14 15:46:56 +0000

    [APP-676] Removed rx java workmanager and using normal worker as that dependency was causing project issues

--- app/src/main/java/cm/aptoide/pt/notification/ComingSoonNotificationWorker.kt
+++ app/src/main/java/cm/aptoide/pt/notification/ComingSoonNotificationWorker.kt
@@ -5,8 +5,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.work.RxWorker
 import androidx.work.WorkManager
+import androidx.work.Worker
 import androidx.work.WorkerParameters
 import cm.aptoide.pt.AptoideApplication
 import cm.aptoide.pt.DeepLinkIntentReceiver
@@ -15,29 +15,11 @@ import cm.aptoide.pt.app.aptoideinstall.ComingSoonApp
 import cm.aptoide.pt.networking.image.ImageLoader
 import cm.aptoide.pt.view.app.AppCenter
 import cm.aptoide.pt.view.app.DetailedAppRequestResult
-import hu.akarnokd.rxjava.interop.RxJavaInterop
-import io.reactivex.Single
 
 class ComingSoonNotificationWorker(private val context: Context,
                                    workerParameters: WorkerParameters,
                                    private val appCenter: AppCenter) :
-    RxWorker(context, workerParameters) {
-
-  override fun createWork(): Single<Result> {
-    val packageName = inputData.getString(ComingSoonNotificationManager.PACKAGE_NAME)
-    return RxJavaInterop.toV2Single(appCenter.loadDetailedApp(
-        packageName, "catappult")
-        .doOnSuccess { detailedAppResult: DetailedAppRequestResult? ->
-          if (detailedAppResult != null && detailedAppResult.detailedApp != null) {
-            cancelComingSoonVerification(packageName)
-            handleAppArrived(ComingSoonApp(
-                detailedAppResult.detailedApp.name,
-                detailedAppResult.detailedApp.icon, detailedAppResult.detailedApp.md5,
-                detailedAppResult.detailedApp.store.name, detailedAppResult.detailedApp.packageName
-            ))
-          }
-        }).map { Result.success() }.onErrorReturn { Result.success() }
-  }
+    Worker(context, workerParameters) {
 
   private fun handleAppArrived(comingSoonApp: ComingSoonApp) {
     val resultIntent = Intent(applicationContext,
@@ -75,5 +57,21 @@ class ComingSoonNotificationWorker(private val context: Context,
       WorkManager.getInstance(context)
           .cancelAllWorkByTag(ComingSoonNotificationManager.WORKER_TAG + packageName)
     }
+  }
+
+  override fun doWork(): Result {
+    val packageName = inputData.getString(ComingSoonNotificationManager.PACKAGE_NAME)
+    appCenter.loadDetailedApp(packageName, "catappult")
+        .doOnSuccess { detailedAppResult: DetailedAppRequestResult? ->
+          if (detailedAppResult != null && detailedAppResult.detailedApp != null) {
+            cancelComingSoonVerification(packageName)
+            handleAppArrived(ComingSoonApp(
+                detailedAppResult.detailedApp.name,
+                detailedAppResult.detailedApp.icon, detailedAppResult.detailedApp.md5,
+                detailedAppResult.detailedApp.store.name, detailedAppResult.detailedApp.packageName
+            ))
+          }
+        }.toBlocking().value()
+    return Result.success()
   }
 }
