069069d6408b91d1c79beaf2795d551eebcf3db4
jdandradex <joao.andrade@aptoide.com>
2022-01-03 16:52:08 +0000

    [APP-776] fix: separate and bump schema migration so it doesn't break nightly users.

--- app/src/main/java/cm/aptoide/pt/RoomMigrationProvider.kt
+++ app/src/main/java/cm/aptoide/pt/RoomMigrationProvider.kt
@@ -34,6 +34,9 @@ class RoomMigrationProvider {
   }, object : Migration(105, 106) {
     override fun migrate(database: SupportSQLiteDatabase) {
       database.execSQL("ALTER TABLE notification ADD COLUMN whitelistedPackages TEXT")
+    }
+  }, object : Migration(106, 107) {
+    override fun migrate(database: SupportSQLiteDatabase) {
       database.execSQL(
           "CREATE TABLE IF NOT EXISTS appComingSoonRegistration (`packageName`TEXT NOT NULL, PRIMARY KEY(`packageName`) )")
     }
