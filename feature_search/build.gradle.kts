eafa72197d3e4191047cbe7aa5c89ad03e0dff89
phillipsgoncalves <filisgoncalves@gmail.com>
2022-02-01 11:54:01 +0000

    [APP-897] Created feature search module to implement the search feature

---
+++ feature_search/build.gradle.kts
@@ -0,0 +1,60 @@
+import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
+
+plugins {
+  id(GradlePluginId.ANDROID_APPLICATION)
+  id(GradlePluginId.KOTLIN_ANDROID)
+}
+
+android {
+  compileSdkVersion(AndroidConfig.COMPILE_SDK)
+
+  defaultConfig {
+    minSdkVersion(AndroidConfig.MIN_SDK)
+    targetSdkVersion(AndroidConfig.TARGET_SDK)
+    versionCode = AndroidConfig.VERSION_CODE
+    versionName = AndroidConfig.VERSION_NAME
+    testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+  }
+
+  buildTypes {
+    getByName(BuildType.RELEASE) {
+      isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+      isShrinkResources = BuildTypeRelease.shrinkResources
+      proguardFiles("proguard-android.txt", "proguard-rules.pro")
+      //signingConfig = signingConfigs.getByName("signingConfigRelease")
+    }
+    getByName(BuildType.DEBUG) {
+      isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+      isShrinkResources = BuildTypeDebug.shrinkResources
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_1_8.toString()
+  }
+}
+
+dependencies {
+
+  implementation(LibraryDependency.CORE_KTX)
+  implementation(LibraryDependency.CORE_KTX)
+  implementation(LibraryDependency.APP_COMPAT)
+  implementation(LibraryDependency.MATERIAL)
+  implementation(LibraryDependency.CONSTRAINT_LAYOUT)
+  implementation(LibraryDependency.KOTLIN)
+  implementation(LibraryDependency.RETROFIT)
+  implementation(LibraryDependency.RETROFIT_MOSHI_CONVERTER)
+  implementation(LibraryDependency.OK_HTTP)
+  implementation(LibraryDependency.LOGGING_INTERCEPTOR)
+  implementation(LibraryDependency.COROUTINES)
+  testImplementation(TestLibraryDependency.COROUTINES_TEST)
+  implementation(LibraryDependency.ROOM)
+  annotationProcessor(LibraryDependency.ROOM_COMPILER)
+  androidTestImplementation(TestLibraryDependency.ROOM_TESTING)
+  testImplementation(TestLibraryDependency.JUNIT)
+  androidTestImplementation(TestLibraryDependency.JUNIT_ANDROIDX)
+  androidTestImplementation(TestLibraryDependency.ESPRESSO_CORE)
+}
