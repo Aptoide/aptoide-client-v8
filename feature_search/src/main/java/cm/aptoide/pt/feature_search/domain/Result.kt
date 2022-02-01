ea6bf596bd87ddaa10276021d1340882198b3f91
phillipsgoncalves <filisgoncalves@gmail.com>
2022-02-01 11:55:47 +0000

    [APP-897] Created Result class to be used as entity to propagate the result (success, loading, error) from a coroutine call

---
+++ feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/Result.kt
@@ -0,0 +1,16 @@
+package cm.aptoide.pt.feature_search.domain
+
+sealed class Result<out R> {
+
+  data class Success<out T>(val data: T) : Result<T>()
+  data class Error(val exception: Exception) : Result<Nothing>()
+  object Loading : Result<Nothing>()
+
+  override fun toString(): String {
+    return when (this) {
+      is Success<*> -> "Success[data=$data]"
+      is Error -> "Error[exception=$exception]"
+      Loading -> "Loading"
+    }
+  }
+}
