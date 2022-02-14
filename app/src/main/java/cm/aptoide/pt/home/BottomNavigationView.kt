ddfb8411a0ea4b08cbd2a2634cd471fd03750b01
jdandradex <joao.andrade@aptoide.com>
2022-02-14 16:46:26 +0000

    [APP-858] add actionbar to bottom navigation view.

--- app/src/main/java/cm/aptoide/pt/home/BottomNavigationView.kt
+++ app/src/main/java/cm/aptoide/pt/home/BottomNavigationView.kt
@@ -1,11 +1,14 @@
 package cm.aptoide.pt.home
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SportsEsports
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -14,34 +17,50 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import cm.aptoide.pt.home.appcoins.BonusScreen
 import cm.aptoide.pt.feature_apps.presentation.AppsScreen
+import cm.aptoide.pt.home.appcoins.BonusScreen
 import cm.aptoide.pt.home.games.GamesScreen
 import cm.aptoide.pt.home.search.SearchScreen
 import cm.aptoide.pt.home.updates.UpdatesScreen
 import cm.aptoide.pt.theme.AppTheme
+import cm.aptoide.pt.theme.AptoideTheme
 
-@Preview
 @Composable
 fun MainView() {
   val navController = rememberNavController()
   Scaffold(
-      bottomBar = {
-        BottomNavigation(navController)
-      }
+    topBar = {
+      AptoideActionBar()
+    },
+    bottomBar = {
+      BottomNavigation(navController)
+    }
   ) {
     NavigationGraph(navController)
+  }
+}
+
+@Preview
+@Composable
+fun AptoideActionBar() {
+  AptoideTheme {
+    TopAppBar(
+      backgroundColor = AppTheme.colors.background, elevation = Dp(0f)
+    ) {
+      Icon(imageVector = Icons.Outlined.SportsEsports, contentDescription = null)
+      Text("Aptoide")
+    }
   }
 }
 
 @Composable
 private fun BottomNavigation(navController: NavHostController) {
   val items = listOf(
-      BottomNavigationMenus.Games,
-      BottomNavigationMenus.Apps,
-      BottomNavigationMenus.AppCoins,
-      BottomNavigationMenus.Search,
-      BottomNavigationMenus.Updates
+    BottomNavigationMenus.Games,
+    BottomNavigationMenus.Apps,
+    BottomNavigationMenus.AppCoins,
+    BottomNavigationMenus.Search,
+    BottomNavigationMenus.Updates
   )
   CompositionLocalProvider(LocalElevationOverlay provides null) {
     BottomNavigation(backgroundColor = AppTheme.colors.surface) {
@@ -50,30 +69,32 @@ private fun BottomNavigation(navController: NavHostController) {
       items.forEach { screen ->
         val selected = currentDestination?.hierarchy?.any { it.route == screen.route }
         BottomNavigationItem(
-            icon = { Icon(imageVector = screen.icon, contentDescription = null) },
-            selected = selected == true,
-            label = {
-              Text(text = stringResource(id = screen.resourceId),
-                  color = if (selected == true) AppTheme.colors.primary else AppTheme.colors.unselectedLabelColor)
-            },
-            selectedContentColor = AppTheme.colors.primary,
-            unselectedContentColor = AppTheme.colors.unselectedLabelColor,
-            alwaysShowLabel = true,
-            onClick = {
-              navController.navigate(screen.route) {
-                // Pop up to the start destination of the graph to
-                // avoid building up a large stack of destinations
-                // on the back stack as users select items
-                popUpTo(navController.graph.findStartDestination().id) {
-                  saveState = true
-                }
-                // Avoid multiple copies of the same destination when
-                // reselecting the same item
-                launchSingleTop = true
-                // Restore state when reselecting a previously selected item
-                restoreState = true
+          icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+          selected = selected == true,
+          label = {
+            Text(
+              text = stringResource(id = screen.resourceId),
+              color = if (selected == true) AppTheme.colors.primary else AppTheme.colors.unselectedLabelColor
+            )
+          },
+          selectedContentColor = AppTheme.colors.primary,
+          unselectedContentColor = AppTheme.colors.unselectedLabelColor,
+          alwaysShowLabel = true,
+          onClick = {
+            navController.navigate(screen.route) {
+              // Pop up to the start destination of the graph to
+              // avoid building up a large stack of destinations
+              // on the back stack as users select items
+              popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
               }
-            })
+              // Avoid multiple copies of the same destination when
+              // reselecting the same item
+              launchSingleTop = true
+              // Restore state when reselecting a previously selected item
+              restoreState = true
+            }
+          })
       }
     }
   }
@@ -82,8 +103,8 @@ private fun BottomNavigation(navController: NavHostController) {
 @Composable
 private fun NavigationGraph(navController: NavHostController) {
   NavHost(
-      navController = navController,
-      startDestination = BottomNavigationMenus.Games.route
+    navController = navController,
+    startDestination = BottomNavigationMenus.Games.route
   ) {
     composable(BottomNavigationMenus.Games.route) {
       GamesScreen()
