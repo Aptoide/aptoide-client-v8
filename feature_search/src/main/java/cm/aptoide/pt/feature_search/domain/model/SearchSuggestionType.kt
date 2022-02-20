4530369793c6629d38a52f6b5bcb8ff433a34d3d
phillipsgoncalves <filisgoncalves@gmail.com>
2022-02-20 22:24:12 +0000

    [APP-897] Passing the type of search suggestion to the other layers

---
+++ feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/model/SearchSuggestionType.kt
@@ -0,0 +1,5 @@
+package cm.aptoide.pt.feature_search.domain.model
+
+enum class SearchSuggestionType(name: String) {
+  SEARCH_HISTORY("Recent searches"), TOP_APTOIDE_SEARCH("Top Aptoide Searches")
+}
--- feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/usecase/GetSearchSuggestionsUseCase.kt
+++ feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/usecase/GetSearchSuggestionsUseCase.kt
@@ -1,22 +1,34 @@
 package cm.aptoide.pt.feature_search.domain.usecase
 
-import cm.aptoide.pt.feature_search.domain.model.SearchSuggestion
+import cm.aptoide.pt.feature_search.domain.model.SearchSuggestionType
+import cm.aptoide.pt.feature_search.domain.model.SearchSuggestions
 import cm.aptoide.pt.feature_search.domain.repository.SearchRepository
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @ViewModelScoped
 class GetSearchSuggestionsUseCase @Inject constructor(private val searchRepository: SearchRepository) {
 
-  fun getSearchSuggestions(): Flow<List<SearchSuggestion>> {
+  fun getSearchSuggestions(): Flow<SearchSuggestions> {
     return searchRepository.getSearchHistory().flatMapMerge {
       if (it.isEmpty()) {
-        return@flatMapMerge searchRepository.getTopSearchedApps()
+        return@flatMapMerge searchRepository.getTopSearchedApps().map { topSearchedApps ->
+          SearchSuggestions(
+            SearchSuggestionType.TOP_APTOIDE_SEARCH, topSearchedApps
+          )
+        }
       } else {
-        return@flatMapMerge flow { it }
+        return@flatMapMerge flow {
+          it.map { searchHistory ->
+            SearchSuggestions(
+              SearchSuggestionType.SEARCH_HISTORY, it
+            )
+          }
+        }
       }
     }
   }
