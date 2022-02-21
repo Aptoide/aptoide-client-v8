f79a073a604668b4ce83e80e56c784af902a43fd
phillipsgoncalves <filisgoncalves@gmail.com>
2022-02-21 00:08:06 +0000

    [APP-897] Fixed view, search suggestion now is aligned with icon

--- feature_search/src/main/java/cm/aptoide/pt/feature_search/presentation/search/SearchView.kt
+++ feature_search/src/main/java/cm/aptoide/pt/feature_search/presentation/search/SearchView.kt
@@ -1,18 +1,20 @@
 package cm.aptoide.pt.feature_search.presentation.search
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import cm.aptoide.pt.feature_search.R
 
 @Preview
 @Composable
@@ -44,8 +46,20 @@ fun SearchSuggestions(title: String, suggestions: List<String>) {
 
 @Composable
 fun SearchSuggestionItem(item: String) {
-  Text(
-    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
-    text = item
-  )
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+  ) {
+    Image(
+      modifier = Modifier
+        .size(21.dp, 18.dp)
+        .wrapContentHeight(CenterVertically),
+      painter = painterResource(id = R.drawable.ic_search_history_icon),
+      contentDescription = "Suggestion icon"
+    )
+    Text(
+      modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+      text = item
+    )
+  }
 }
