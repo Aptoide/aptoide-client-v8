b4ae8b71685838da555ec33d28ba4cc20b3a83a9
shansen <shawnhansen@smarttech.com>
2022-02-24 10:41:58 -0700

    Fix some null pointer exceptions in Aptoide (possibly related to a field issue)

--- app/src/main/java/cm/aptoide/pt/smart/appfiltering/AppHttpInterceptor.java
+++ app/src/main/java/cm/aptoide/pt/smart/appfiltering/AppHttpInterceptor.java
@@ -21,11 +21,33 @@ public class AppHttpInterceptor implements Interceptor {
         Response originalResponse = chain.proceed(chain.request());
         String path = chain.request().url().uri().getPath();
         if (path.contains(LIST_APP) || path.contains(SEARCH_APP)) {
-            String bodyString = originalResponse.body().string();
-            JSONObject objectAfterRemove = removeHelper.filterResponse(bodyString, FilteringList.removingList);
-            JSONObject objectAfterAdd = addHelper.filterResponse(path, objectAfterRemove.toString(), AddingList.addingList);
-            if (objectAfterAdd != null) {
-                return rebuildResponse(originalResponse, objectAfterAdd, chain);
+
+            ResponseBody responseBody = originalResponse.body();
+            if(responseBody != null) {
+                String bodyString = responseBody.string();
+                JSONObject objectAfterRemove = removeHelper.filterResponse(bodyString, FilteringList.removingList);
+                JSONObject objectAfterAdd = null;
+
+                // Here's the order:
+                // 1) Apply the filter/removing list first to the original response.
+                // 2) If that succeeds, use the returned list & then apply the added list.
+                //   - If it fails, use the original response & apply the added list.
+                // 3) If the added list is non-null, then use it - it will have apps filtered/removed (if step 2 succeeded) + added apps.
+                //   - If it is null, then just use the filtered/removed list (if it's not null) since adding apps failed.
+                // 4) If any of this fails, then just return the original response (at the end of the function).
+
+                if (objectAfterRemove != null) {
+                    objectAfterAdd = addHelper.filterResponse(path, objectAfterRemove.toString(), AddingList.addingList);
+                } else {
+                    objectAfterAdd = addHelper.filterResponse(path, bodyString, AddingList.addingList);
+                }
+
+                if (objectAfterAdd != null) {
+                    return rebuildResponse(originalResponse, objectAfterAdd, chain);
+                }
+                if (objectAfterRemove != null) {
+                    return rebuildResponse(originalResponse, objectAfterRemove, chain);
+                }
             }
         }
 
