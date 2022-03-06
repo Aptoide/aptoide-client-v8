8e330cf31faade0717b3786ce556ed0b8ac91dee
phillipsgoncalves <filisgoncalves@gmail.com>
2022-03-06 23:49:01 +0000

    [APP-898] Changed the domain search app model to support the view requirements

--- feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/model/SearchApp.kt
+++ feature_search/src/main/java/cm/aptoide/pt/feature_search/domain/model/SearchApp.kt
@@ -2,7 +2,7 @@ package cm.aptoide.pt.feature_search.domain.model
 
 data class SearchApp(
   val appName: String,
-  val packageName: String,
   val rating: Double,
-  val downloads: Int
+  val downloads: Int,
+  val malware: String
 )
