f8d40c180f71f28cea28ee1426bc0872e6109675
phillipsgoncalves <filisgoncalves@gmail.com>
2022-03-17 23:34:37 +0000

    [APP-878] Created updates viewmodel

--- feature_updates/src/main/java/cm/aptoide/pt/feature_updates/presentation/UpdatesViewModel.kt
+++ feature_updates/src/main/java/cm/aptoide/pt/feature_updates/presentation/UpdatesViewModel.kt
@@ -1,10 +1,41 @@
 package cm.aptoide.pt.feature_updates.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cm.aptoide.pt.feature_updates.domain.model.InstalledApp
 import cm.aptoide.pt.feature_updates.domain.usecase.GetInstalledAppsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UpdatesViewModel @Inject constructor(private val getInstalledAppsUseCase: GetInstalledAppsUseCase) :
-  ViewModel()
+  ViewModel() {
+
+
+  private val viewModelState = MutableStateFlow(
+    UpdatesViewModelState(emptyList())
+  )
+
+  val uiState = viewModelState.map { it.toUiState() }
+    .stateIn(
+      viewModelScope,
+      SharingStarted.Eagerly,
+      viewModelState.value.toUiState()
+    )
+
+  init {
+    viewModelScope.launch {
+      getInstalledAppsUseCase.getInstalledApps().collect { installedAppsList ->
+        viewModelState.update { it.copy(installedAppsList = installedAppsList) }
+      }
+    }
+  }
+
+
+}
+
+private data class UpdatesViewModelState(val installedAppsList: List<InstalledApp>) {
+  fun toUiState(): UpdatesUiState = UpdatesUiState(installedAppsList)
+}
