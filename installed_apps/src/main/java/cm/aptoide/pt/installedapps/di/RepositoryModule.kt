003e15909fe477e2402b15b9b63b2fdc07ff5dcf
phillipsgoncalves <filisgoncalves@gmail.com>
2022-03-18 10:59:30 +0000

    [APP-878] Implemented repository module to provide repository related dependencies for the installed apps module

---
+++ installed_apps/src/main/java/cm/aptoide/pt/installedapps/di/RepositoryModule.kt
@@ -0,0 +1,37 @@
+package cm.aptoide.pt.installedapps.di
+
+import android.content.Context
+import androidx.room.Room
+import cm.aptoide.pt.installedapps.data.database.InstalledAppsDatabase
+import cm.aptoide.pt.installedapps.data.database.LocalInstalledAppsRepository
+import cm.aptoide.pt.installedapps.data.fake.FakeInstalledAppsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+  @Singleton
+  @Provides
+  fun provideLocalInstalledAppsRepository(database: InstalledAppsDatabase): LocalInstalledAppsRepository {
+    return FakeInstalledAppsRepository()
+    //return database.installedAppsDao()
+  }
+
+  @Singleton
+  @Provides
+  fun provideInstalledAppsDatabase(@ApplicationContext appContext: Context): InstalledAppsDatabase {
+    return Room.databaseBuilder(
+      appContext,
+      InstalledAppsDatabase::class.java,
+      "aptoide_installed_apps.db"
+    )
+      .build()
+  }
+
+}
