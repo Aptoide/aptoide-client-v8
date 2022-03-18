dbc1de1fb5d488a5576341b3b4bddbcc8b295486
phillipsgoncalves <filisgoncalves@gmail.com>
2022-03-18 11:05:20 +0000

    [APP-878] Created use case module to provide all use cases related to the installed apps management on the updates view feature

--- feature_updates/src/main/java/cm/aptoide/pt/feature_updates/di/UseCaseModule.kt
+++ feature_updates/src/main/java/cm/aptoide/pt/feature_updates/di/UseCaseModule.kt
@@ -1,3 +1,18 @@
 package cm.aptoide.pt.feature_updates.di
 
-class UseCaseModule
+import cm.aptoide.pt.feature_updates.domain.repository.UpdatesRepository
+import cm.aptoide.pt.feature_updates.domain.usecase.GetInstalledAppsUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object UseCaseModule {
+
+  @Provides
+  fun provideGetInstalledAppsUseCase(updatesRepository: UpdatesRepository): GetInstalledAppsUseCase {
+    return GetInstalledAppsUseCase(updatesRepository)
+  }
+}
