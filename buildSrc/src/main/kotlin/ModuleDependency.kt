d7901a51301e373cf5611b3561a73a05f7b28db6
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-14 01:33:09 +0100

    [APP-866] Created appview module dependency name

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -5,4 +5,5 @@ object ModuleDependency {
   const val FEATURE_APPS = ":feature_apps"
   const val FEATURE_SEARCH = ":feature_search"
   const val APTOIDE_NETWORK = ":aptoide-network"
+  const val FEATURE_APPVIEW = ":feature_appview"
 }
