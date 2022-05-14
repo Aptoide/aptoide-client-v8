52fd1cfb6435a1bc517c94d1429a22157e66994a
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-14 13:02:36 +0100

    [APP-866] Implemented GetAppInfo use case

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/usecase/GetAppInfoUseCase.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/usecase/GetAppInfoUseCase.kt
@@ -1,3 +1,15 @@
 package cm.aptoide.pt.feature_appview.domain.usecase
 
-class GetAppInfoUseCase
+import cm.aptoide.pt.feature_appview.domain.repository.AppViewRepository
+import cm.aptoide.pt.feature_appview.domain.repository.AppViewResult
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ViewModelScoped
+class GetAppInfoUseCase @Inject constructor(private val appViewRepository: AppViewRepository) {
+
+  fun getAppInfo(packageName: String): Flow<AppViewResult> {
+    return appViewRepository.getAppInfo(packageName = packageName)
+  }
+}
