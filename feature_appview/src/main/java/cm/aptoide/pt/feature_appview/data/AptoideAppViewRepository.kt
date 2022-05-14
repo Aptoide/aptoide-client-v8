c9db44f083ef88ffb2c97cf40982a579ade2fdc9
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-14 12:59:05 +0100

    [APP-866] using AppsRepository on AppViewRepository to make the requests regarding the app info

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/data/AptoideAppViewRepository.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/data/AptoideAppViewRepository.kt
@@ -1,5 +1,28 @@
 package cm.aptoide.pt.feature_appview.data
 
+import cm.aptoide.pt.feature_apps.data.AppResult
+import cm.aptoide.pt.feature_apps.data.AppsRepository
 import cm.aptoide.pt.feature_appview.domain.repository.AppViewRepository
+import cm.aptoide.pt.feature_appview.domain.repository.AppViewResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class AptoideAppViewRepository : AppViewRepository
+@Singleton
+class AptoideAppViewRepository @Inject constructor(val appsRepository: AppsRepository) :
+  AppViewRepository {
+
+  override fun getAppInfo(packageName: String): Flow<AppViewResult> {
+    return appsRepository.getApp(packageName = packageName).map {
+      when (it) {
+        is AppResult.Success -> {
+          AppViewResult.Success(it.data)
+        }
+        is AppResult.Error -> {
+          AppViewResult.Error(it.e)
+        }
+      }
+    }
+  }
+}
--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
@@ -1,3 +1,13 @@
 package cm.aptoide.pt.feature_appview.domain.repository
 
-interface AppViewRepository
+import cm.aptoide.pt.feature_apps.data.App
+import kotlinx.coroutines.flow.Flow
+
+interface AppViewRepository {
+  fun getAppInfo(packageName: String): Flow<AppViewResult>
+}
+
+sealed interface AppViewResult {
+  data class Success(val data: App) : AppViewResult
+  data class Error(val error: Throwable) : AppViewResult
+}
