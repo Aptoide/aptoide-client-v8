f6fc000c1d0a01c6b697fc1b8a5d4a185b161699
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-15 02:30:11 +0100

    [APP-866] Created appview tab class to represent tabs on appview

---
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/AppViewTab.kt
@@ -0,0 +1,10 @@
+package cm.aptoide.pt.feature_appview.presentation
+
+enum class AppViewTab(var tabName: String, var index: Int) {
+  DETAILS("Details", 0),
+  REVIEWS("Reviews", 1),
+  NFT("NFT", 2),
+  RELATED("Related", 3),
+  VERSIONS("Versions", 4),
+  INFO("Info", 5)
+}
