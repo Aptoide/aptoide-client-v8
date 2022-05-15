bd9c0af889152aab8c1f9cc2d12784b0ed0f1864
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-16 00:54:39 +0100

    [APP-866] Implemented appview get related content request

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
@@ -1,12 +1,14 @@
 package cm.aptoide.pt.feature_appview.domain.repository
 
 import cm.aptoide.pt.feature_apps.data.App
+import cm.aptoide.pt.feature_appview.domain.model.RelatedCard
 import kotlinx.coroutines.flow.Flow
 
 interface AppViewRepository {
   fun getAppInfo(packageName: String): Flow<AppViewResult>
   fun getSimilarApps(packageName: String): Flow<SimilarAppsResult>
   fun getOtherVersions(packageName: String): Flow<OtherVersionsResult>
+  fun getRelatedContent(packageName: String): Flow<RelatedContentResult>
 }
 
 sealed interface AppViewResult {
@@ -22,4 +24,9 @@ sealed interface SimilarAppsResult {
 sealed interface OtherVersionsResult {
   data class Success(val otherVersionsList: List<App>) : OtherVersionsResult
   data class Error(val error: Throwable) : OtherVersionsResult
+}
+
+sealed interface RelatedContentResult {
+  data class Success(val relatedContent: List<RelatedCard>) : RelatedContentResult
+  data class Error(val error: Throwable) : RelatedContentResult
 }
