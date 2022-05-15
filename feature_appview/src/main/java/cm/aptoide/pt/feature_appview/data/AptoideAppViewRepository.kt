49bb66d1a855629f515b4f974a128ca4311603a6
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-15 20:08:19 +0100

    [APP-866] Merging both getRecommended requests to be able to get both similar apps (appc similar apps and regular similar apps)

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/data/AptoideAppViewRepository.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/data/AptoideAppViewRepository.kt
@@ -2,9 +2,13 @@ package cm.aptoide.pt.feature_appview.data
 
 import cm.aptoide.pt.feature_apps.data.AppResult
 import cm.aptoide.pt.feature_apps.data.AppsRepository
+import cm.aptoide.pt.feature_apps.data.AppsResult
 import cm.aptoide.pt.feature_appview.domain.repository.AppViewRepository
 import cm.aptoide.pt.feature_appview.domain.repository.AppViewResult
+import cm.aptoide.pt.feature_appview.domain.repository.SimilarAppsResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -24,5 +28,28 @@ class AptoideAppViewRepository @Inject constructor(val appsRepository: AppsRepos
         }
       }
     }
+  }
+
+  override fun getSimilarApps(packageName: String): Flow<SimilarAppsResult> {
+    return appsRepository.getRecommended("packageName=$packageName/section=appc")
+      .flatMapMerge { appcResult ->
+        when (appcResult) {
+          is AppsResult.Success -> {
+            return@flatMapMerge appsRepository.getRecommended("packageName=$packageName").map {
+              when (it) {
+                is AppsResult.Success -> {
+                  return@map SimilarAppsResult.Success(it.data, appcResult.data)
+                }
+                is AppsResult.Error -> {
+                  return@map SimilarAppsResult.Error(it.e)
+                }
+              }
+            }
+          }
+          is AppsResult.Error -> {
+            return@flatMapMerge flowOf(SimilarAppsResult.Error(appcResult.e))
+          }
+        }
+      }
   }
 }
--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/repository/AppViewRepository.kt
@@ -5,9 +5,15 @@ import kotlinx.coroutines.flow.Flow
 
 interface AppViewRepository {
   fun getAppInfo(packageName: String): Flow<AppViewResult>
+  fun getSimilarApps(packageName: String): Flow<SimilarAppsResult>
 }
 
 sealed interface AppViewResult {
   data class Success(val data: App) : AppViewResult
   data class Error(val error: Throwable) : AppViewResult
+}
+
+sealed interface SimilarAppsResult {
+  data class Success(val similarApps: List<App>, val appcSimilarApps: List<App>) : SimilarAppsResult
+  data class Error(val error: Throwable) : SimilarAppsResult
 }
