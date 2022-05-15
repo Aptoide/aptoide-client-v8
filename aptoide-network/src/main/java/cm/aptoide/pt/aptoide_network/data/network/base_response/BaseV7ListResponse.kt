d3ea4f414e273a4c1a9282f50ea029f87afae3dc
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-15 21:55:35 +0100

    [APP-866] Created basev7list response

---
+++ aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/base_response/BaseV7ListResponse.kt
@@ -0,0 +1,11 @@
+package cm.aptoide.pt.aptoide_network.data.network.base_response
+
+class BaseV7ListResponse<T> : BaseV7Response() {
+  var list: List<T>? = null
+  var total: Int = 0
+    get() = if (hasData()) list!!.size else 0
+
+  private fun hasData(): Boolean {
+    return list != null
+  }
+}
