ebb70c493d930c17c801de9340a9e358a4c0d364
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-16 00:55:54 +0100

    [APP-866] GetRelatedContent use case making the request for the related content

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/usecase/GetRelatedContentUseCase.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/domain/usecase/GetRelatedContentUseCase.kt
@@ -1,3 +1,17 @@
 package cm.aptoide.pt.feature_appview.domain.usecase
 
-class GetRelatedContentUseCase
+import cm.aptoide.pt.feature_appview.domain.repository.AppViewRepository
+import cm.aptoide.pt.feature_appview.domain.repository.RelatedContentResult
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ViewModelScoped
+class GetRelatedContentUseCase @Inject constructor(private val appViewRepository: AppViewRepository) {
+
+  fun getRelatedContent(packageName: String): Flow<RelatedContentResult> {
+    return appViewRepository.getRelatedContent(packageName)
+  }
+
+
+}
