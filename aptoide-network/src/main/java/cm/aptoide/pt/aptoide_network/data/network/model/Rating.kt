32eac32a441f035edb5a707ab592d25a279ec24c
phillipsgoncalves <filisgoncalves@gmail.com>
2022-05-23 11:57:14 +0100

    [APP-866] Fixed Rating object attributes

--- aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/model/Rating.kt
+++ aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/model/Rating.kt
@@ -1,5 +1,5 @@
 package cm.aptoide.pt.aptoide_network.data.network.model
 
-data class Rating(val avg: Double, val total: Long, val votes: List<Votes>)
+data class Rating(val avg: Double, val total: Long, val votes: List<Votes>?)
 
-data class Votes(var value: Int, var count: Int)
+data class Votes(val value: Int, val count: Int)
