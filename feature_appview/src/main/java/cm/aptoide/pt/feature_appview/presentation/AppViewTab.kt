66a59ed5a873e36e0e3005292e9b795d1d4267df
phillipsgoncalves <filisgoncalves@gmail.com>
2022-06-24 12:23:29 +0100

    [APP-866] Fixed bug due to the removal of nft appview viewpager tab

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/AppViewTab.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/AppViewTab.kt
@@ -3,7 +3,7 @@ package cm.aptoide.pt.feature_appview.presentation
 enum class AppViewTab(var tabName: String, var index: Int) {
   DETAILS("Details", 0),
   REVIEWS("Reviews", 1),
-  RELATED("Related", 3),
-  VERSIONS("Versions", 4),
-  INFO("Info", 5)
+  RELATED("Related", 2),
+  VERSIONS("Versions", 3),
+  INFO("Info", 4)
 }
