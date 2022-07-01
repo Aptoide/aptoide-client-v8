4522e0c7b1687c10cdf7651082ac24e21cb79b56
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-01 10:05:46 +0100

    [APP-866] Increased version of compose

--- buildSrc/src/main/kotlin/LibraryDependency.kt
+++ buildSrc/src/main/kotlin/LibraryDependency.kt
@@ -47,7 +47,7 @@ private object LibraryVersion {
   const val COIL = "1.4.0"
   const val ROOM = "2.4.0"
   const val ACTIVITY_COMPOSE = "1.3.1"
-  const val COMPOSE = "1.0.5"
+  const val COMPOSE = "1.2.0-rc02"
   const val VIEWMODEL_COMPOSE = "1.0.0-alpha07"
   const val NAVIGATION_COMPOSE = "2.4.0-rc01"
   const val MATERIAL_ICONS_EXTENDED = "1.0.0"
