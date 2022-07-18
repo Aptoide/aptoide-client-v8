010e5d18e966e528339852c8307d2b3c13e367ed
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-18 16:03:12 +0100

    [APP-884] Created base module download view

---
+++ download-view/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+  id(GradlePluginId.ANDROID_LIBRARY)
+  id(GradlePluginId.KOTLIN_ANDROID)
+  id(GradlePluginId.KOTLIN_KAPT)
+  id(GradlePluginId.HILT_PLUGIN)
+}
+
+android {
+  compileSdk = AndroidConfig.COMPILE_SDK
+
+  defaultConfig {
+    minSdk = AndroidConfig.MIN_SDK
+    targetSdk = AndroidConfig.TARGET_SDK
+
+    testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+  }
+
+  buildTypes {
+    getByName(BuildType.RELEASE) {
+      isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+      proguardFiles("proguard-android.txt", "proguard-rules.pro")
+    }
+    getByName(BuildType.DEBUG) {
+      isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+    }
+  }
+
+  buildFeatures {
+    // Enables Jetpack Compose for this module
+    compose = true
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_1_8.toString()
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = CoreVersion.KT_COMPILER_EXTENSION
+  }
+}
+
+dependencies {
+
+  implementation(project(ModuleDependency.APTOIDE_UI))
+  implementation(project(ModuleDependency.FEATURE_APPS))
+  implementation(project(ModuleDependency.APTOIDE_INSTALLER))
+
+  implementation(LibraryDependency.CORE_KTX)
+  implementation(LibraryDependency.APP_COMPAT)
+  implementation(LibraryDependency.MATERIAL)
+  implementation(LibraryDependency.KOTLIN)
+
+
+  //compose
+  implementation(LibraryDependency.MATERIAL_COMPOSE)
+  implementation(LibraryDependency.ANIMATION_COMPOSE)
+  implementation(LibraryDependency.UI_TOOLING_COMPOSE)
+  implementation(LibraryDependency.VIEWMODEL_COMPOSE)
+  implementation(LibraryDependency.NAVIGATION_COMPOSE)
+
+  //di
+  implementation(LibraryDependency.HILT)
+  implementation(LibraryDependency.HILT_NAV_COMPOSE)
+  kapt(LibraryDependency.HILT_COMPILER)
+
+  //imageloader
+  implementation(LibraryDependency.COIL_COMPOSE)
+
+  //logger
+  implementation(LibraryDependency.TIMBER)
+
+
+  testImplementation(TestLibraryDependency.JUNIT)
+  androidTestImplementation(TestLibraryDependency.JUNIT_ANDROIDX)
+  androidTestImplementation(TestLibraryDependency.ESPRESSO_CORE)
+}
