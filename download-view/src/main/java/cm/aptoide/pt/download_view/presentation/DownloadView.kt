b5caa141eaa634083c924f6dd625a19b9fc8b5eb
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-20 15:38:07 +0100

    [APP-884] Created base download view

---
+++ download-view/src/main/java/cm/aptoide/pt/download_view/presentation/DownloadView.kt
@@ -0,0 +1,376 @@
+package cm.aptoide.pt.download_view.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import cm.aptoide.pt.feature_apps.R
+import cm.aptoide.pt.feature_apps.data.App
+import cm.aptoide.pt.theme.AppTheme
+import cm.aptoide.pt.theme.AptoideTheme
+import coil.compose.rememberImagePainter
+import coil.transform.RoundedCornersTransformation
+
+@Preview
+@Composable
+fun DownloadViewScreen(downloadViewViewModel: DownloadViewViewModel = hiltViewModel()) {
+
+  val uiState by downloadViewViewModel.uiState.collectAsState()
+
+  AptoideTheme {
+    MainDownloadView(uiState, onDownloadApp = {
+      downloadViewViewModel.downloadApp(it)
+    })
+  }
+}
+
+@Composable
+fun MainDownloadView(uiState: DownloadViewUiState, onDownloadApp: (App) -> Unit) {
+  when (uiState.downloadViewType) {
+    DownloadViewType.NO_APPCOINS -> {
+      NoAppCoinsDownloadView(uiState.app, uiState.downloadViewState, onDownloadApp)
+    }
+    DownloadViewType.APPCOINS -> {
+      AppCoinsDownloadView(uiState.app, uiState.downloadViewState, onDownloadApp)
+    }
+    DownloadViewType.ESKILLS -> {
+      ESkillsDownloadView(uiState.app, uiState.downloadViewState, onDownloadApp)
+    }
+  }
+}
+
+@Composable
+fun ESkillsDownloadView(
+  app: App?,
+  downloadViewState: DownloadViewState,
+  onDownloadApp: (App) -> Unit
+) {
+  Column(
+    modifier = Modifier
+      .width(344.dp)
+      .height(96.dp)
+  ) {
+    DownloadState(downloadViewState, app, onDownloadApp)
+    ESkillsBanner()
+  }
+}
+
+@Composable
+fun ESkillsBanner() {
+  Row {
+    // TODO: add eskills icon
+    Image(
+      painter = rememberImagePainter(R.drawable.ic_placeholder,
+        builder = {
+          placeholder(R.drawable.ic_placeholder)
+          transformations(RoundedCornersTransformation())
+        }),
+      contentDescription = "Eskills icon",
+      modifier = Modifier
+        .width(24.dp)
+        .height(24.dp)
+        .padding(bottom = 8.dp, top = 8.dp, start = 16.dp, end = 8.dp)
+    )
+    Text(text = "Earn Money")
+    Text(text = "Beat other players with e-Skills")
+  }
+}
+
+@Composable
+fun AppCoinsDownloadView(
+  app: App?,
+  downloadViewState: DownloadViewState,
+  onDownloadApp: (App) -> Unit
+) {
+  Column(
+    modifier = Modifier
+      .width(344.dp)
+      .height(96.dp)
+  ) {
+    DownloadState(downloadViewState, app, onDownloadApp)
+    AppCoinsBanner()
+  }
+}
+
+@Composable
+fun AppCoinsBanner() {
+  Row {
+    // TODO: add appcoins icon
+    Image(
+      painter = rememberImagePainter(R.drawable.ic_placeholder,
+        builder = {
+          placeholder(R.drawable.ic_placeholder)
+          transformations(RoundedCornersTransformation())
+        }),
+      contentDescription = "AppCoins icon",
+      modifier = Modifier
+        .width(24.dp)
+        .height(24.dp)
+        .padding(bottom = 8.dp, top = 8.dp, start = 16.dp, end = 8.dp)
+    )
+    Text(text = "Up to 25% bonus")
+    Text(text = "In-app purchases with AppCoins")
+  }
+}
+
+@Composable
+fun NoAppCoinsDownloadView(
+  app: App?,
+  downloadViewState: DownloadViewState,
+  onDownloadApp: (App) -> Unit
+) {
+  Column(
+    modifier = Modifier
+      .width(344.dp)
+      .height(56.dp)
+  ) {
+    DownloadState(downloadViewState, app, onDownloadApp)
+  }
+}
+
+@Composable
+fun DownloadState(downloadViewState: DownloadViewState, app: App?, onDownloadApp: (App) -> Unit) {
+  when (downloadViewState) {
+    DownloadViewState.INSTALL -> {
+      app?.let { InstallButton(onDownloadApp, it) }
+    }
+    DownloadViewState.PROCESSING -> {
+      app?.let { ProcessingDownloadView(it.isAppCoins) }
+    }
+    DownloadViewState.DOWNLOADING -> {
+      app?.let { DownloadingDownloadView(it.isAppCoins, 10f, app.appSize) }
+    }
+    DownloadViewState.INSTALLING -> {
+      app?.let { InstallingDownloadView(it.isAppCoins) }
+    }
+    DownloadViewState.INSTALLED -> {
+      OpenButton()
+    }
+    DownloadViewState.ERROR -> {
+      ErrorDownloadView()
+    }
+    DownloadViewState.READY_TO_INSTALL -> {
+      ReadyToInstallView()
+    }
+  }
+}
+
+@Composable
+fun ReadyToInstallView() {
+  Button(
+    onClick = { TODO("Handle install app") },
+    shape = CircleShape,
+    modifier = Modifier
+      .height(56.dp)
+      .fillMaxWidth()
+  ) {
+    Text("READY TO INSTALL", maxLines = 1)
+  }
+}
+
+
+@Composable
+fun InstallButton(onDownloadApp: (App) -> Unit, app: App) {
+  Button(
+    onClick = { onDownloadApp(app) },
+    shape = CircleShape,
+    modifier = Modifier
+      .height(56.dp)
+      .fillMaxWidth()
+  ) {
+    Text("INSTALL", maxLines = 1)
+  }
+}
+
+@Composable
+fun ErrorDownloadView() {
+  Row(modifier = Modifier.fillMaxWidth()) {
+    Box(modifier = Modifier.weight(1f)) {
+      GeneralErrorLabel()
+    }
+    Button(
+      onClick = { TODO("Handle open app") },
+      shape = CircleShape,
+      modifier = Modifier
+        .height(56.dp)
+        .width(140.dp)
+    ) {
+      Text("RETRY", maxLines = 1)
+    }
+  }
+}
+
+@Composable
+fun GeneralErrorLabel() {
+  TODO("Not yet implemented")
+}
+
+@Composable
+fun OpenButton() {
+  Button(
+    onClick = { TODO("Handle open app") },
+    shape = CircleShape,
+    modifier = Modifier
+      .height(56.dp)
+      .fillMaxWidth()
+  ) {
+    Text("OPEN", maxLines = 1)
+  }
+}
+
+@Composable
+fun ProcessingDownloadView(isAppCoins: Boolean) {
+  if (isAppCoins) {
+    IndeterminateDownloadView(
+      label = "Downloading",
+      AppTheme.colors.appCoinsColor,
+      AppTheme.colors.appCoinsColor
+    )
+  } else {
+    IndeterminateDownloadView(
+      label = "Downloading",
+      AppTheme.colors.primary,
+      AppTheme.colors.primary
+    )
+  }
+}
+
+@Composable
+fun InstallingDownloadView(isAppCoins: Boolean) {
+  if (isAppCoins) {
+    IndeterminateDownloadView(
+      label = "Installing",
+      AppTheme.colors.appCoinsColor,
+      AppTheme.colors.appCoinsColor
+    )
+  } else {
+    IndeterminateDownloadView(
+      label = "Installing",
+      AppTheme.colors.primary,
+      AppTheme.colors.primary
+    )
+  }
+}
+
+@Composable
+fun DownloadingDownloadView(isAppCoins: Boolean, progress: Float, appSize: Long) {
+  if (isAppCoins) {
+    AppCoinsDownloadingDownloadView(progress, appSize)
+  } else {
+    NoAppCoinsDownloadingDownloadView(progress, appSize)
+  }
+}
+
+@Composable
+fun NoAppCoinsDownloadingDownloadView(progress: Float, appSize: Long) {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(start = 16.dp, top = 12.dp, end = 16.dp, bottom = 19.dp)
+  ) {
+    DownloadingProgressLabel(AppTheme.colors.primary, progress, appSize)
+    DownloadingProgressBar(AppTheme.colors.primary, progress)
+  }
+}
+
+@Composable
+fun AppCoinsDownloadingDownloadView(progress: Float, appSize: Long) {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(start = 16.dp, top = 12.dp, end = 16.dp, bottom = 19.dp)
+  ) {
+    DownloadingProgressLabel(AppTheme.colors.appCoinsColor, progress, appSize)
+    DownloadingProgressBar(AppTheme.colors.appCoinsColor, progress)
+  }
+}
+
+@Composable
+fun DownloadingProgressBar(progressColor: Color, progress: Float) {
+  Row(modifier = Modifier.fillMaxWidth()) {
+    Box(modifier = Modifier.weight(1f)) {
+      AptoideProgressBar(progressColor = progressColor, progress = progress)
+    }
+    Icon(
+      imageVector = Icons.Default.Close,
+      contentDescription = "Cancel download",
+      modifier = Modifier
+        .clickable(onClick = {
+          // TODO:
+        })
+        .padding(start = 12.dp)
+        .size(12.dp)
+    )
+
+  }
+}
+
+@Composable
+fun DownloadingProgressLabel(color: Color, progress: Float, appSize: Long) {
+  Box(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(bottom = 4.dp)
+  ) {
+    Text(
+      text = "Downloading", fontSize = MaterialTheme.typography.caption.fontSize,
+      color = color, modifier = Modifier.align(Alignment.TopStart)
+    )
+    Text(
+      text = "$progress of $appSize", fontSize = MaterialTheme.typography.caption.fontSize,
+      color = color, modifier = Modifier.align(Alignment.TopEnd)
+    )
+  }
+}
+
+
+@Composable
+fun IndeterminateDownloadView(label: String, labelColor: Color, progressColor: Color) {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(start = 16.dp, top = 12.dp, end = 16.dp, bottom = 19.dp)
+  ) {
+    Text(
+      text = label,
+      fontSize = MaterialTheme.typography.caption.fontSize,
+      color = labelColor, modifier = Modifier.padding(bottom = 6.dp)
+    )
+    AptoideIndeterminateProgressBar(progressColor = progressColor)
+  }
+}
+
+@Composable
+fun AptoideIndeterminateProgressBar(progressColor: Color) {
+  LinearProgressIndicator(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(2.dp),
+    backgroundColor = Color(0xFF6F6F6F),
+    color = progressColor
+  )
+}
+
+@Composable
+fun AptoideProgressBar(progressColor: Color, progress: Float) {
+  LinearProgressIndicator(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(8.dp),
+    backgroundColor = Color(0xFF6F6F6F),
+    color = progressColor,
+    progress = progress
+  )
+}
