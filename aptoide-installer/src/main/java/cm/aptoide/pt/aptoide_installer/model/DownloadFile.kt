8ee92a99c7f7c27f4195e164e3db905833f8221a
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-27 22:04:48 +0100

    [APP-884] Added missing attributes to DownloadFile model

--- aptoide-installer/src/main/java/cm/aptoide/pt/aptoide_installer/model/DownloadFile.kt
+++ aptoide-installer/src/main/java/cm/aptoide/pt/aptoide_installer/model/DownloadFile.kt
@@ -4,5 +4,19 @@ data class DownloadFile(
   val md5: String,
   val path: String,
   val altPath: String,
-  val packageName: String
+  val packageName: String,
+  val versionCode: Int,
+  val versionName: String,
+  val fileName: String,
+  val fileType: FileType,
+  val subFileType: SubFileType,
+  val cachePath: String
 )
+
+enum class FileType {
+  APK, OBB, SPLIT
+}
+
+enum class SubFileType {
+  MAIN, PATCH, ASSET, FEATURE, BASE, SUBTYPE_APK
+}
