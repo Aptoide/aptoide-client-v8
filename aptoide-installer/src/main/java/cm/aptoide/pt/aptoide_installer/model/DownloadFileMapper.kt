7bf54da13a9ca636cd6cbd19a5edb8489de48e88
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-27 22:40:45 +0100

    [APP-884] Fixed DownloadFileMapper as DownloadFile constructor changed

--- aptoide-installer/src/main/java/cm/aptoide/pt/aptoide_installer/model/DownloadFileMapper.kt
+++ aptoide-installer/src/main/java/cm/aptoide/pt/aptoide_installer/model/DownloadFileMapper.kt
@@ -17,7 +17,55 @@ class DownloadFileMapper {
       fileToDownload.md5,
       fileToDownload.link,
       fileToDownload.altLink,
-      fileToDownload.packageName
+      fileToDownload.packageName,
+      fileToDownload.versionCode,
+      fileToDownload.versionName,
+      fileToDownload.fileName,
+      mapFileType(fileToDownload.fileType),
+      mapFileSubType(fileToDownload.subFileType), fileToDownload.filePath
     )
+  }
+
+  private fun mapFileSubType(subFileType: Int): SubFileType {
+    return when (subFileType) {
+      FileToDownload.ASSET -> {
+        SubFileType.ASSET
+      }
+      FileToDownload.BASE -> {
+        SubFileType.BASE
+      }
+      FileToDownload.FEATURE -> {
+        SubFileType.FEATURE
+      }
+      FileToDownload.SUBTYPE_APK -> {
+        SubFileType.SUBTYPE_APK
+      }
+      FileToDownload.MAIN -> {
+        SubFileType.MAIN
+      }
+      FileToDownload.PATCH -> {
+        SubFileType.PATCH
+      }
+      else -> {
+        SubFileType.SUBTYPE_APK
+      }
+    }
+  }
+
+  private fun mapFileType(fileType: Int): FileType {
+    return when (fileType) {
+      FileToDownload.APK -> {
+        FileType.APK
+      }
+      FileToDownload.OBB -> {
+        FileType.OBB
+      }
+      FileToDownload.SPLIT -> {
+        FileType.SPLIT
+      }
+      else -> {
+        FileType.APK
+      }
+    }
   }
 }
