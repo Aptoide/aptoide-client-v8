983d750a68ab123d19222a77239221ffa17836c8
phillipsgoncalves <filisgoncalves@gmail.com>
2022-07-27 03:10:12 +0100

    [APP-884] Created repository module to provide all download and install dependencies

---
+++ aptoide-installer/src/main/java/cm/aptoide/pt/aptoide_installer/di/RepositoryModule.kt
@@ -0,0 +1,156 @@
+package cm.aptoide.pt.aptoide_installer.di
+
+import android.os.Environment
+import cm.aptoide.pt.aptoide_installer.AptoideInstallManager
+import cm.aptoide.pt.aptoide_installer.InstallManager
+import cm.aptoide.pt.aptoide_installer.data.AptoideDownloadPersistence
+import cm.aptoide.pt.aptoide_installer.data.download.filedownloader.FileDownloadManagerProvider
+import cm.aptoide.pt.aptoide_installer.data.download.filedownloader.FilePathProvider
+import cm.aptoide.pt.aptoide_installer.data.download.filedownloader.Md5Comparator
+import cm.aptoide.pt.aptoide_installer.model.DownloadFileMapper
+import cm.aptoide.pt.aptoide_installer.model.DownloadStateMapper
+import cm.aptoide.pt.downloadmanager.*
+import cm.aptoide.pt.downloads_database.data.AptoideDownloadRepository
+import cm.aptoide.pt.downloads_database.data.DownloadRepository
+import cm.aptoide.pt.installedapps.data.InstalledAppsRepository
+import cm.aptoide.pt.utils.FileUtils
+import com.liulishuo.filedownloader.FileDownloader
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+  @Singleton
+  @Provides
+  fun provideInstallManager(
+    downloadManager: DownloadManager,
+    downloadStateMapper: DownloadStateMapper,
+    downloadFileMapper: DownloadFileMapper,
+    installedAppsRepository: InstalledAppsRepository
+  ): InstallManager {
+    return AptoideInstallManager(
+      downloadManager,
+      downloadStateMapper,
+      downloadFileMapper,
+      installedAppsRepository
+    )
+  }
+
+  @Singleton
+  @Provides
+  fun provideDownloadStateMapper(): DownloadStateMapper {
+    return DownloadStateMapper()
+  }
+
+  @Singleton
+  @Provides
+  fun provideDownloadFileMapper(): DownloadFileMapper {
+    return DownloadFileMapper()
+  }
+
+
+  @Singleton
+  @Provides
+  fun providesDownloadManager(
+    downloadsRepository: DownloadsRepository,
+    downloadStatusMapper: DownloadStatusMapper,
+    downloadAppMapper: DownloadAppMapper,
+    appDownloaderProvider: AppDownloaderProvider, fileUtils: FileUtils, pathProvider: PathProvider
+  ): DownloadManager {
+    return AptoideDownloadManager(
+      downloadsRepository, downloadStatusMapper,
+      Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/",
+      downloadAppMapper, appDownloaderProvider, fileUtils, pathProvider
+    )
+  }
+
+  @Singleton
+  @Provides
+  fun providesDownloadsRepository(downloadPersistence: DownloadPersistence): DownloadsRepository {
+    return DownloadsRepository(downloadPersistence)
+  }
+
+  @Singleton
+  @Provides
+  fun providesDownloadPersistence(downloadRepository: DownloadRepository): DownloadPersistence {
+    return AptoideDownloadPersistence(downloadRepository)
+  }
+
+  @Singleton
+  @Provides
+  fun providesDownloadStatusMapper(): DownloadStatusMapper {
+    return DownloadStatusMapper()
+  }
+
+  @Singleton
+  @Provides
+  fun providesDownloadsAppMapper(downloadAppFileMapper: DownloadAppFileMapper): DownloadAppMapper {
+    return DownloadAppMapper(downloadAppFileMapper)
+  }
+
+  @Singleton
+  @Provides
+  fun providesDownloadAppFileMapper(): DownloadAppFileMapper {
+    return DownloadAppFileMapper()
+  }
+
+
+  @Singleton
+  @Provides
+  fun providesAppDownloaderProvider(
+    retryFileDownloaderProvider: RetryFileDownloaderProvider
+  ): AppDownloaderProvider {
+    return AppDownloaderProvider(retryFileDownloaderProvider)
+  }
+
+  @Singleton
+  @Provides
+  fun providesRetryFileDownloaderProvider(
+    fileDownloaderProvider: FileDownloaderProvider
+  ): RetryFileDownloaderProvider {
+    return RetryFileDownloadManagerProvider(fileDownloaderProvider)
+  }
+
+  @Singleton
+  @Provides
+  fun providesFileDownloaderProvider(md5Comparator: Md5Comparator): FileDownloaderProvider {
+    return FileDownloadManagerProvider(
+      Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/",
+      FileDownloader.getImpl(), md5Comparator
+    )
+  }
+
+  @Singleton
+  @Provides
+  fun providesMd5Comparator(): Md5Comparator {
+    return Md5Comparator(
+      Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/"
+    )
+  }
+
+  @Singleton
+  @Provides
+  fun providesFileUtils(): FileUtils {
+    return FileUtils()
+  }
+
+  @Singleton
+  @Provides
+  fun providesPathProvider(): PathProvider {
+    return FilePathProvider(
+      Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/apks/",
+      Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/", Environment.getExternalStorageDirectory()
+        .absolutePath + "/.aptoide/obb/"
+    )
+  }
+}
