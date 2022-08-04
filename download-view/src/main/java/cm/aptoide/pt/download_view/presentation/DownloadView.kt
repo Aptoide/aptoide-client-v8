bccba3c71d252e0b67b5b8ee73052aac6a760a8a
phillipsgoncalves <filisgoncalves@gmail.com>
2022-08-04 12:56:16 +0100

    [APP-884] Fixed progress bar rounded corner

--- download-view/src/main/java/cm/aptoide/pt/download_view/presentation/DownloadView.kt
+++ download-view/src/main/java/cm/aptoide/pt/download_view/presentation/DownloadView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -12,6 +13,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
@@ -513,7 +515,10 @@ fun AptoideProgressBar(progressColor: Color, progress: Float) {
   LinearProgressIndicator(
     modifier = Modifier
       .fillMaxWidth()
-      .height(8.dp),
+      .height(8.dp)
+      .clip(
+        RoundedCornerShape(8.dp)
+      ),
     backgroundColor = Color(0xFF6F6F6F),
     color = progressColor,
     progress = progress / 100
