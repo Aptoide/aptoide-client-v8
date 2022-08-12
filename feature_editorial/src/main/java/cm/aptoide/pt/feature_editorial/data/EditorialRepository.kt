6bdd2bf41f035b1dace5409fb2855735863a120f
jdandradex <joao.andrade@aptoide.com>
2022-08-12 09:51:03 +0100

    [APP-873] refactor: rename variable, we are using widget view url instead of article id here.

--- feature_editorial/src/main/java/cm/aptoide/pt/feature_editorial/data/EditorialRepository.kt
+++ feature_editorial/src/main/java/cm/aptoide/pt/feature_editorial/data/EditorialRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface EditorialRepository {
   fun getLatestArticle(): Flow<EditorialResult>
   fun getArticleDetail(articleId: String): Flow<EditorialDetailResult>
-  fun getArticleMeta(articleId: String): Flow<EditorialResult>
+  fun getArticleMeta(editorialWidgetUrl: String): Flow<EditorialResult>
 
   sealed interface EditorialResult {
     data class Success(val data: Article) : EditorialResult
