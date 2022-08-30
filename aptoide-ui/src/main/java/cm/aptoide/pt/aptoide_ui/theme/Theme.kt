73b0898ccc67e53abf6f104a980b0e51ca7aa840
phillipsgoncalves <filisgoncalves@gmail.com>
2022-08-30 23:55:30 +0100

    [APP-1310] Checking variable to decide if should use cobrand theme colors or aptoide theme colors

--- aptoide-ui/src/main/java/cm/aptoide/pt/aptoide_ui/theme/Theme.kt
+++ aptoide-ui/src/main/java/cm/aptoide/pt/aptoide_ui/theme/Theme.kt
@@ -2,48 +2,13 @@ package cm.aptoide.pt.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
+import cm.aptoide.pt.aptoide_ui.theme.AptoideBaseTheme
+import cm.aptoide.pt.aptoide_ui.theme.CobrandTheme
 
-private val darkMaterialColorPalette = darkColors(
-    background = black,
-    onBackground = Color.White,
-    primary = pinkishOrange,
-    primaryVariant = pastelOrange,
-    secondary = pinkishOrange,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    surface = blackDarkMode,
-    onSurface = greyMedium,
-    error = error
-)
-
-private val darkColorPalette = AppColors(
-    unselectedLabelColor = greyMedium,
-    materialColors = darkMaterialColorPalette
-)
-
-private val lightMaterialColorPalette = lightColors(
-    background = Color.White,
-    onBackground = black,
-    primary = pinkishOrange,
-    primaryVariant = pastelOrange,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    surface = Color.White,
-    onSurface = black,
-    error = error
-)
-
-private val lightColorPalette = AppColors(
-    unselectedLabelColor = grey,
-    materialColors = lightMaterialColorPalette
-)
 
 object AppTheme {
   val colors: AppColors
@@ -53,24 +18,40 @@ object AppTheme {
 }
 
 private val LocalAppColors = staticCompositionLocalOf {
-  lightColorPalette //default
+  AptoideBaseTheme.lightColorPalette //default
 }
 
 @Composable
 fun AptoideTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  baseAppTheme: String = getBaseAppTheme(),
+  content: @Composable () -> Unit
 ) {
-  val colors = if (darkTheme) darkColorPalette else lightColorPalette
+  val colors = getThemeColors(baseAppTheme, darkTheme)
   val typography = if (darkTheme) darkTypography else lightTypography
+
   CompositionLocalProvider(
-      LocalAppColors provides colors,
+    LocalAppColors provides colors,
   ) {
     MaterialTheme(
-        colors = colors.materialColors,
-        typography = typography,
-        shapes = shapes,
-        content = content
+      colors = colors.materialColors,
+      typography = typography,
+      shapes = shapes,
+      content = content
     )
   }
 }
+
+fun getBaseAppTheme(): String {
+  //should return buildconfig variable
+  return "cobrand"
+}
+
+private fun getThemeColors(baseTheme: String, isDarkTheme: Boolean): AppColors {
+  return if (baseTheme == "cobrand") {
+    if (isDarkTheme) CobrandTheme.darkColorPalette else CobrandTheme.lightColorPalette
+  } else {
+    if (isDarkTheme) AptoideBaseTheme.darkColorPalette else AptoideBaseTheme.lightColorPalette
+  }
+}
+
