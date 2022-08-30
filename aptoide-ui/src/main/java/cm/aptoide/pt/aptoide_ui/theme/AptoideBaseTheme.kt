440704106e64619a2acda1ae240e6d48a6613a7f
phillipsgoncalves <filisgoncalves@gmail.com>
2022-08-30 23:53:27 +0100

    [APP-1310] Extracted aptoide theme colors to a new object class

---
+++ aptoide-ui/src/main/java/cm/aptoide/pt/aptoide_ui/theme/AptoideBaseTheme.kt
@@ -0,0 +1,44 @@
+package cm.aptoide.pt.aptoide_ui.theme
+
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+import cm.aptoide.pt.theme.*
+
+object AptoideBaseTheme {
+  private val darkMaterialColorPalette = darkColors(
+    background = black,
+    onBackground = Color.White,
+    primary = pinkishOrange,
+    primaryVariant = pastelOrange,
+    secondary = pinkishOrange,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    surface = blackDarkMode,
+    onSurface = greyMedium,
+    error = error
+  )
+
+  val darkColorPalette = AppColors(
+    unselectedLabelColor = greyMedium,
+    materialColors = darkMaterialColorPalette
+  )
+
+  private val lightMaterialColorPalette = lightColors(
+    background = Color.White,
+    onBackground = black,
+    primary = pinkishOrange,
+    primaryVariant = pastelOrange,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    surface = Color.White,
+    onSurface = black,
+    error = error
+  )
+
+  val lightColorPalette = AppColors(
+    unselectedLabelColor = grey,
+    materialColors = lightMaterialColorPalette
+  )
+
+}
