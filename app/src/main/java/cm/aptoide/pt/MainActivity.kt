6a5c8d3615b2f266cecbd339b4ed26cae76959cf
phillipsgoncalves <filisgoncalves@gmail.com>
2022-08-31 00:58:22 +0100

    [APP-1310] Injecting bottom navigation manager in the main view to decide if bottom navigation should be shown or not

--- app/src/main/java/cm/aptoide/pt/MainActivity.kt
+++ app/src/main/java/cm/aptoide/pt/MainActivity.kt
@@ -3,17 +3,23 @@ package cm.aptoide.pt
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import cm.aptoide.pt.home.BottomNavigationManager
 import cm.aptoide.pt.home.MainView
 import cm.aptoide.pt.theme.AptoideTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+  @Inject
+  lateinit var bottomNavigationManager: BottomNavigationManager
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       AptoideTheme {
-        MainView()
+        MainView(bottomNavigationManager.shouldShowBottomNavigation())
       }
     }
   }
