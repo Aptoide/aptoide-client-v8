075b53d860ba4fbdd61113cd64be4bee52f9253e
phillipsgoncalves <filisgoncalves@gmail.com>
2022-09-15 01:41:27 +0100

    [APP-1309] implemented empty state for reviews view

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/ReviewsView.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/ReviewsView.kt
@@ -19,22 +19,47 @@ import coil.transform.RoundedCornersTransformation
 
 @Composable
 fun ReviewsView(app: App) {
-  Row(modifier = Modifier.padding(start = 40.dp, top = 22.dp, end = 32.dp)) {
-    Column(
-      modifier = Modifier.padding(end = 43.dp),
-      horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-      Text(text = "" + app.rating.avgRating, fontSize = MaterialTheme.typography.h3.fontSize)
-      RatingStars(app.rating.avgRating)
-      Text(text = "" + app.rating.totalVotes + " Reviews", modifier = Modifier.padding(top = 12.dp))
-    }
-    Column(modifier = Modifier.padding(top = 12.dp)) {
-      app.rating.votes?.forEach {
-        val progress = (it.count.toDouble() / app.rating.totalVotes).toFloat()
-        VotesRow(ratingNumber = it.value.toString(), progress = progress)
+  if ((app.rating.totalVotes == 0L)) {
+    Row(modifier = Modifier.padding(start = 40.dp, top = 22.dp, end = 32.dp)) {
+      Column(
+        modifier = Modifier.padding(end = 43.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        Text(text = "-", fontSize = MaterialTheme.typography.h3.fontSize)
+        RatingStars(0.0)
+        Text(
+          text = "" + app.rating.totalVotes + " Reviews",
+          modifier = Modifier.padding(top = 12.dp)
+        )
       }
-    }
+      Column(modifier = Modifier.padding(top = 12.dp)) {
+        app.rating.votes?.forEach {
+          VotesRow(ratingNumber = it.value.toString(), progress = 0f)
+        }
+      }
 
+    }
+  } else {
+    Row(modifier = Modifier.padding(start = 40.dp, top = 22.dp, end = 32.dp)) {
+      Column(
+        modifier = Modifier.padding(end = 43.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+        Text(text = "" + app.rating.avgRating, fontSize = MaterialTheme.typography.h3.fontSize)
+        RatingStars(app.rating.avgRating)
+        Text(
+          text = "" + app.rating.totalVotes + " Reviews",
+          modifier = Modifier.padding(top = 12.dp)
+        )
+      }
+      Column(modifier = Modifier.padding(top = 12.dp)) {
+        app.rating.votes?.forEach {
+          val progress = (it.count.toDouble() / app.rating.totalVotes).toFloat()
+          VotesRow(ratingNumber = it.value.toString(), progress = progress)
+        }
+      }
+
+    }
   }
 }
 
