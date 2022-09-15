06d4a9f123d959c0b8f54c6de0cab18e693f5ca6
phillipsgoncalves <filisgoncalves@gmail.com>
2022-09-15 11:35:02 +0100

    [APP-1309] Added message to the reviews emptystate and fixed margins

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/ReviewsView.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/ReviewsView.kt
@@ -20,35 +20,55 @@ import coil.transform.RoundedCornersTransformation
 @Composable
 fun ReviewsView(app: App) {
   if ((app.rating.totalVotes == 0L)) {
-    Row(modifier = Modifier.padding(start = 40.dp, top = 22.dp, end = 32.dp)) {
-      Column(
-        modifier = Modifier.padding(end = 43.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-      ) {
-        Text(text = "-", fontSize = MaterialTheme.typography.h3.fontSize)
-        RatingStars(0.0)
-        Text(
-          text = "" + app.rating.totalVotes + " Reviews",
-          modifier = Modifier.padding(top = 12.dp)
-        )
-      }
-      Column(modifier = Modifier.padding(top = 12.dp)) {
-        app.rating.votes?.forEach {
-          VotesRow(ratingNumber = it.value.toString(), progress = 0f)
+
+    Column(modifier = Modifier.padding(top = 24.dp, start = 32.dp, end = 32.dp)) {
+      Text(
+        text = "There are no reviews or ratings yet. Be the first one!",
+        fontSize = MaterialTheme.typography.caption.fontSize
+      )
+
+      Row(modifier = Modifier.padding(start = 8.dp, top = 34.dp)) {
+        Column(
+          modifier = Modifier.padding(end = 40.dp),
+          horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+          Text(
+            text = "-",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            modifier = Modifier.padding(bottom = 18.dp)
+          )
+          RatingStars(0.0)
+          Text(
+            text = "" + app.rating.totalVotes + " Reviews",
+            modifier = Modifier.padding(top = 12.dp),
+            fontSize = MaterialTheme.typography.caption.fontSize
+          )
         }
+        Column(modifier = Modifier.padding(top = 12.dp)) {
+          app.rating.votes?.forEach {
+            VotesRow(ratingNumber = it.value.toString(), progress = 0f)
+          }
+        }
+
       }
 
     }
+
   } else {
     Row(modifier = Modifier.padding(start = 40.dp, top = 22.dp, end = 32.dp)) {
       Column(
-        modifier = Modifier.padding(end = 43.dp),
+        modifier = Modifier.padding(end = 40.dp),
         horizontalAlignment = Alignment.CenterHorizontally
       ) {
-        Text(text = "" + app.rating.avgRating, fontSize = MaterialTheme.typography.h3.fontSize)
+        Text(
+          text = "" + app.rating.avgRating,
+          fontSize = MaterialTheme.typography.h3.fontSize,
+          modifier = Modifier.padding(bottom = 12.dp)
+        )
         RatingStars(app.rating.avgRating)
         Text(
           text = "" + app.rating.totalVotes + " Reviews",
+          fontSize = MaterialTheme.typography.caption.fontSize,
           modifier = Modifier.padding(top = 12.dp)
         )
       }
