91400166baf68c0a52e3583b5ccc71199ec309ec
phillipsgoncalves <filisgoncalves@gmail.com>
2022-09-20 22:25:33 +0100

    [APP-1309] Added aptoide toolbar to aptoide-ui module

---
+++ aptoide-ui/src/main/java/cm/aptoide/pt/aptoide_ui/toolbar/AptoideToolbar.kt
@@ -0,0 +1,26 @@
+package cm.aptoide.pt.aptoide_ui.toolbar
+
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SportsEsports
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import cm.aptoide.pt.aptoide_ui.BuildConfig
+import cm.aptoide.pt.theme.AppTheme
+import cm.aptoide.pt.theme.AptoideTheme
+
+@Preview
+@Composable
+fun AptoideActionBar() {
+  AptoideTheme {
+    TopAppBar(
+      backgroundColor = AppTheme.colors.background, elevation = Dp(0f)
+    ) {
+      Icon(imageVector = Icons.Outlined.SportsEsports, contentDescription = null)
+      Text(BuildConfig.MARKET_NAME)
+    }
+  }
+}
