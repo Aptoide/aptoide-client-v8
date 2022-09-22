3c23b4483761baf455801c052180cb88a279b5cf
phillipsgoncalves <filisgoncalves@gmail.com>
2022-09-22 14:31:17 +0100

    [APP-1309] Created detailed app object to represent an app that can be downloadable as it has the paths for the files

---
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/DetailedApp.kt
@@ -0,0 +1,39 @@
+package cm.aptoide.pt.feature_apps.data
+
+import cm.aptoide.pt.feature_apps.domain.Rating
+import cm.aptoide.pt.feature_apps.domain.Store
+
+data class DetailedApp(
+  val name: String,
+  val packageName: String,
+  val md5: String,
+  val appSize: Long,
+  val icon: String,
+  val malware: String?,
+  val rating: Rating,
+  val downloads: Int,
+  val versionName: String,
+  val versionCode: Int,
+  val featureGraphic: String,
+  val isAppCoins: Boolean,
+  val screenshots: List<String>?,
+  val description: String?,
+  val store: Store,
+  val releaseDate: String?,
+  val updateDate: String?,
+  val website: String?,
+  val email: String?,
+  val privacyPolicy: String?,
+  val permissions: List<String>?, val file: DetailedFile, val obb: DetailedObb?
+)
+
+data class DetailedFile(
+  var vername: String,
+  var vercode: Int,
+  var md5: String,
+  var filesize: Long,
+  var path: String,
+  var path_alt: String
+)
+
+data class DetailedObb(val main: DetailedFile, val patch: DetailedFile?)
