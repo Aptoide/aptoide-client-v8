7708bafca5a9b015b1e9ab2a0ab564c692893fa9
phillipsgoncalves <filisgoncalves@gmail.com>
2022-09-30 00:52:02 +0100

    [APP-1309] renamed theme typography entity to apptypography and added all necessary variables to follow the design defined possible text styles

---
+++ aptoide-ui/src/main/java/cm/aptoide/pt/aptoide_ui/theme/AppTypography.kt
@@ -0,0 +1,28 @@
+package cm.aptoide.pt.aptoide_ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+
+data class AppTypography(
+  val materialTypography: Typography,
+  val regular_XXL: TextStyle,
+  val regular_XL: TextStyle,
+  val regular_L: TextStyle,
+  val regular_M: TextStyle,
+  val regular_S: TextStyle,
+  val regular_XS: TextStyle,
+  val regular_XXS: TextStyle,
+  val bold_XXL: TextStyle,
+  val bold_XL: TextStyle,
+  val medium_L: TextStyle,
+  val medium_M: TextStyle,
+  val medium_S: TextStyle,
+  val medium_XS: TextStyle,
+  val medium_XXS: TextStyle,
+  val button_L: TextStyle,
+  val button_M: TextStyle,
+  val button_S: TextStyle
+)
+
+
+
