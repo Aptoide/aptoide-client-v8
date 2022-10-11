c3d894757874bc68295f1b4b734c80b146a66144
phillipsgoncalves <filisgoncalves@gmail.com>
2022-10-11 23:00:12 +0100

    [DT-1] Fixed other versions view

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/OtherVersionsView.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/OtherVersionsView.kt
@@ -4,16 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import cm.aptoide.pt.aptoide_ui.textformatter.DateUtils
 import cm.aptoide.pt.aptoide_ui.textformatter.TextFormatter
+import cm.aptoide.pt.aptoide_ui.theme.AppTheme
 import cm.aptoide.pt.feature_apps.R
 import cm.aptoide.pt.feature_apps.data.App
 import coil.compose.rememberImagePainter
@@ -33,26 +36,26 @@ fun OtherVersionRow(app: App) {
     modifier = Modifier
       .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
       .fillMaxWidth()
-      .height(54.dp)
+      .height(57.dp)
   ) {
     Box(
       modifier = Modifier
         .fillMaxWidth()
-        .height(54.dp)
+        .height(57.dp)
     ) {
       Column(
         modifier = Modifier
           .align(Alignment.TopStart)
       ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(verticalAlignment = CenterVertically) {
           Text(
             text = app.versionName,
             modifier = Modifier
-              .padding(end = 6.dp)
-              .width(160.dp),
+              .padding(end = 4.dp)
+              .wrapContentWidth(),
             overflow = TextOverflow.Ellipsis,
             maxLines = 1,
-            fontSize = MaterialTheme.typography.h6.fontSize,
+            style = AppTheme.typography.medium_L,
           )
           Image(
             painter = painterResource(id = cm.aptoide.pt.aptoide_ui.R.drawable.ic_icon_trusted),
@@ -64,15 +67,17 @@ fun OtherVersionRow(app: App) {
         }
         app.updateDate?.let {
           Text(
-            text = it,
-            fontSize = MaterialTheme.typography.overline.fontSize,
+            text = DateUtils.getTimeDiffString(it),
+            color = AppTheme.colors.greyText,
+            style = AppTheme.typography.regular_XXS,
             modifier = Modifier.padding(bottom = 2.dp)
           )
         }
         Text(
           text = TextFormatter.withSuffix(app.downloads.toLong()) + " Downloads",
           overflow = TextOverflow.Ellipsis,
-          fontSize = MaterialTheme.typography.overline.fontSize
+          color = AppTheme.colors.greyText,
+          style = AppTheme.typography.regular_XXS
         )
       }
 
@@ -80,6 +85,8 @@ fun OtherVersionRow(app: App) {
         Text(
           text = "" + app.store.subscribers?.let { TextFormatter.withSuffix(it) } + " followers",
           overflow = TextOverflow.Ellipsis,
+          style = AppTheme.typography.regular_XXS,
+          color = AppTheme.colors.greyText,
           modifier = Modifier
             .padding(end = 8.dp)
             .align(CenterVertically)
@@ -94,6 +101,7 @@ fun OtherVersionRow(app: App) {
           modifier = Modifier
             .width(48.dp)
             .height(48.dp)
+            .clip(RoundedCornerShape(16.dp))
         )
       }
     }
