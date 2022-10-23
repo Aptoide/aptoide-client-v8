2a9d39d048ab4a582d243ca620b7ed4decce4425
phillipsgoncalves <filisgoncalves@gmail.com>
2022-10-23 23:31:58 +0100

    [DT-4] Cache widgets after successfull storeWidgets request

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/AptoideWidgetsRepository.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/AptoideWidgetsRepository.kt
@@ -4,23 +4,36 @@ import cm.aptoide.pt.feature_apps.data.network.model.WidgetsJSON
 import cm.aptoide.pt.feature_apps.data.network.service.WidgetsRemoteService
 import cm.aptoide.pt.feature_apps.domain.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 internal class AptoideWidgetsRepository @Inject constructor(private val widgetsService: WidgetsRemoteService) :
   WidgetsRepository {
 
+  private val cachedGetStoreWidgets: MutableMap<String, Widget> = mutableMapOf()
+
   override fun getStoreWidgets() = flow {
     val widgetsListResponse = widgetsService.getStoreWidgets()
     if (widgetsListResponse.isSuccessful) {
       widgetsListResponse.body()?.datalist?.list?.let {
-        emit(Result.Success(it.map { widgetNetwork -> widgetNetwork.toDomainModel() }))
+        emit(Result.Success(it.map { widgetNetwork ->
+          val widget = widgetNetwork.toDomainModel()
+          cachedGetStoreWidgets[widget.title + widget.tag] = widget
+          return@map widget
+        }))
       }
     } else {
       emit(Result.Error(IllegalStateException()))
     }
   }.flowOn(Dispatchers.IO)
+
+
+  override fun getWidget(widgetIdentifier: String): Flow<Widget?> {
+    return flowOf(cachedGetStoreWidgets[widgetIdentifier])
+  }
 
   private fun WidgetsJSON.WidgetNetwork.toDomainModel(): Widget {
     return Widget(
