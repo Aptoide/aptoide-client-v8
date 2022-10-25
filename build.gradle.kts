b0bd2bf8f65d287cdb63b31abaad492a2741fcd7
jdandradex <joao.andrade@aptoide.com>
2022-10-25 16:14:15 +0100

    [DT-16] feature: add firebase analytics & crashlytics integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ buildscript {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${CoreVersion.KOTLIN}")
     classpath("com.google.dagger:hilt-android-gradle-plugin:${GradlePluginVersion.HILT}")
     classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
+    classpath("com.google.gms:google-services:${GradlePluginVersion.GMS}")
+    classpath("com.google.firebase:firebase-crashlytics-gradle:${GradlePluginVersion.CRASHLYTICS}")
   }
 }
 
