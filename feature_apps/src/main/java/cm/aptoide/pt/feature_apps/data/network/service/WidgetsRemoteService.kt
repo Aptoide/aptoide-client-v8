0c367c7bd772de43581868a908f1a3f323d0c754
phillipsgoncalves <filisgoncalves@gmail.com>
2022-11-08 18:27:27 +0000

    [DT-51] Fixed visibility of widgetsService

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/network/service/WidgetsRemoteService.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/network/service/WidgetsRemoteService.kt
@@ -4,9 +4,8 @@ import cm.aptoide.pt.feature_apps.data.network.model.BaseV7DataListResponse
 import cm.aptoide.pt.feature_apps.data.network.model.WidgetsJSON
 import retrofit2.Response
 
-internal interface WidgetsRemoteService {
+interface WidgetsRemoteService {
 
   suspend fun getStoreWidgets(): Response<BaseV7DataListResponse<WidgetsJSON.WidgetNetwork>>
 
 }
-
