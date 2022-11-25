4c6da7a13322ad5b70fc2723f8a43220c680db67
phillipsgoncalves <filisgoncalves@gmail.com>
2022-11-25 16:31:47 +0000

    [DT-47] Load and show installed apps after user acceptance
    To implement the bundle according to product guidelines

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/MyAppsApp.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/MyAppsApp.kt
@@ -2,4 +2,4 @@ package cm.aptoide.pt.feature_apps.data
 
 import android.graphics.drawable.Drawable
 
-data class MyAppsApp(val icon: Drawable)
+data class MyAppsApp(val icon: Drawable, val name: String, val packageName: String, val versionName: String?)
