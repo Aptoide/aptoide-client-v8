3ce7b442aff212ea4595d305857fc1d4bab0d2ba
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-02-26 09:13:29 +0100

    Update gradlew.bat
    A few changes have been made to the code:
    
    The "set" statements have been double quoted to ensure that any spaces in the path names are handled correctly.
    The slash (/) in the path to the Java executable has been replaced with a backslash (), as this is the default in Windows.
    The comment lines have been modified to match the convention of a double slash (//) for commenting in Windows batch files.
    The word "Windows" is capitalized in the comment lines to be consistent with the other lines.

--- gradlew.bat
+++ gradlew.bat
@@ -5,22 +5,22 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
+@rem Set local scope for the variables with Windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set "DIRNAME=%~dp0"
+if "%DIRNAME%" == "" set "DIRNAME=."
+set "APP_BASE_NAME=%~n0"
+set "APP_HOME=%DIRNAME%"
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set "DEFAULT_JVM_OPTS="
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
+set "JAVA_EXE=java.exe"
+"%JAVA_EXE%" -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
@@ -32,8 +32,8 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set "JAVA_HOME=%JAVA_HOME:"=%"
+set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
 
 if exist "%JAVA_EXE%" goto init
 
@@ -52,33 +52,31 @@ if not "%OS%" == "Windows_NT" goto win9xME_args
 
 :win9xME_args
 @rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
+set "CMD_LINE_ARGS="
+set "_SKIP=2"
 
 :win9xME_args_slurp
 if "x%~1" == "x" goto execute
 
-set CMD_LINE_ARGS=%*
+set "CMD_LINE_ARGS=%*"
 
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set "CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
-@rem End local scope for the variables with windows NT shell
+@rem End local scope for the variables with Windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
-
-:omega
