10bed0c032931b5bb3749ba23ef7b2b2a0397ac8
Pedro Agostinho <pedro.agostinho@aptoide.com>
2023-04-17 16:20:51 +0100

    [DT-354] Move CategoryDetailViewModel and state into module

---
+++ feature_categories/src/main/java/cm/aptoide/pt/feature_categories/presentation/CategoryDetailViewModel.kt
@@ -0,0 +1,80 @@
+package cm.aptoide.pt.feature_categories.presentation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cm.aptoide.pt.feature_apps.data.App
+import cm.aptoide.pt.feature_categories.domain.GetCategoryAppsListUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.io.IOException
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryDetailViewModel @Inject constructor(
+  savedStateHandle: SavedStateHandle,
+  private val getCategoryAppsUseCase: GetCategoryAppsListUseCase
+) : ViewModel() {
+
+  private val categoryName: String? = savedStateHandle["name"]
+  private val viewModelState = MutableStateFlow(CategoryDetailViewModelState())
+
+  val uiState = viewModelState.map { it.toUiState() }
+    .stateIn(
+      viewModelScope,
+      SharingStarted.Eagerly,
+      viewModelState.value.toUiState()
+    )
+
+  init {
+    reload()
+  }
+
+  fun reload() {
+    viewModelScope.launch {
+      viewModelState.update { it.copy(type = CategoryDetailViewUiStateType.LOADING) }
+      categoryName?.let {
+        getCategoryAppsUseCase(categoryName)
+          .catch { e ->
+            Timber.w(e)
+            viewModelState.update {
+              it.copy(
+                type = when (e) {
+                  is IOException -> CategoryDetailViewUiStateType.NO_CONNECTION
+                  else -> CategoryDetailViewUiStateType.ERROR
+                }
+              )
+            }
+          }
+          .collect { categoryApps ->
+            if (categoryApps.isEmpty()) {
+              viewModelState.update { it.copy(type = CategoryDetailViewUiStateType.EMPTY) }
+            } else {
+              viewModelState.update {
+                it.copy(
+                  appList = categoryApps,
+                  categoryName = categoryName,
+                  type = CategoryDetailViewUiStateType.IDLE
+                )
+              }
+            }
+          }
+      }
+    }
+  }
+}
+
+private data class CategoryDetailViewModelState(
+  val appList: List<App> = emptyList(),
+  val categoryName: String = "",
+  val type: CategoryDetailViewUiStateType = CategoryDetailViewUiStateType.IDLE,
+) {
+  fun toUiState(): CategoryDetailViewUiState =
+    CategoryDetailViewUiState(
+      appList = appList,
+      categoryName = categoryName,
+      type = type
+    )
+}
