336b0488bb444d4fc6e999a3e8c4a71b3f8956ad
Pedro Agostinho <pedro.agostinho@aptoide.com>
2023-05-26 16:08:03 +0100

    [DT-325] Rename app videos parameter

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/presentation/AppGridView.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/presentation/AppGridView.kt
@@ -138,7 +138,7 @@ class AppProvider : PreviewParameterProvider<App> {
         isAppCoins = true,
         screenshots = listOf("dasdsa", "dsadas"),
         description = "app description",
-        youtubeVideos = listOf("", ""),
+        videos = listOf("", ""),
         store = Store(
           storeName = "rmota",
           icon = "rmota url",
