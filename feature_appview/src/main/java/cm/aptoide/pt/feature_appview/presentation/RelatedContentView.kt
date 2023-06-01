8b272a4d481b5f9c378049b7302c77c7a4b54946
Sergei Mitrofanov <goretz.m@gmail.com>
2023-06-01 08:52:17 +0100

    [DT-442] Auto apply the code style

--- feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/RelatedContentView.kt
+++ feature_appview/src/main/java/cm/aptoide/pt/feature_appview/presentation/RelatedContentView.kt
@@ -2,7 +2,14 @@ package cm.aptoide.pt.feature_appview.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
