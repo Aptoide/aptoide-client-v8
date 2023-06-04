0222e2348977770a722e094c235e83405588d38d
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-06-04 17:22:17 +0200

    Add 'installed' table to the database schema

--- app/src/androidTest/resources/cm.aptoide.pt/create_scheduled.sql
+++ app/src/androidTest/resources/cm.aptoide.pt/create_scheduled.sql
@@ -1,2 +1,8 @@
-CREATE TABLE installed (id_installed INTEGER PRIMARY KEY AUTOINCREMENT, package_name TEXT UNIQUE ONCONFLICT REPLACE,
-name TEXT, version_code INTEGER DEFAULT 0, version_name TEXT DEFAULT "", signature TEXT DEFAULT "");
+CREATE TABLE installed (
+  id_installed INTEGER PRIMARY KEY AUTOINCREMENT,
+  package_name TEXT UNIQUE ON CONFLICT REPLACE,
+  name TEXT,
+  version_code INTEGER DEFAULT 0,
+  version_name TEXT DEFAULT "",
+  signature TEXT DEFAULT ""
+);
