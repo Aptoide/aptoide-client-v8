b4c017d346f9901a4666d44aa4244549b280d7f1
AngeloMendonca <angelo.mendonca@aptoide.com>
2023-06-09 10:07:50 +0100

    [DT-336] Add analytics usage

--- app/src/main/java/cm/aptoide/pt/AptoideApplication.kt
+++ app/src/main/java/cm/aptoide/pt/AptoideApplication.kt
@@ -2,15 +2,20 @@ package cm.aptoide.pt
 
 import android.app.Application
 import android.content.Context
+import android.content.res.Configuration
 import android.preference.PreferenceManager
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import cm.aptoide.pt.analytics.Analytics
+import cm.aptoide.pt.aptoide_network.di.StoreName
 import cm.aptoide.pt.install_manager.InstallManager
+import cm.aptoide.pt.settings.data.UserPreferencesRepository
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -29,10 +34,32 @@ class AptoideApplication : Application() {
   @Inject
   lateinit var installManager: InstallManager
 
+  @Inject
+  lateinit var analytics: Analytics
+
+  @Inject
+  @StoreName
+  lateinit var storeName: String
+
+  @Inject
+  lateinit var userPreferencesRepository: UserPreferencesRepository
+
   override fun onCreate() {
     super.onCreate()
     initTimber()
     startInstallManager()
+    setUserProperties()
+  }
+
+  private fun setUserProperties() {
+    analytics.setUserProperties(storeName = storeName)
+
+    CoroutineScope(Dispatchers.IO).launch {
+      val isDarkTheme = userPreferencesRepository.isDarkTheme().first()
+        ?: (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+
+      analytics.setUserProperties(isDarkTheme = isDarkTheme)
+    }
   }
 
   private fun startInstallManager() {
