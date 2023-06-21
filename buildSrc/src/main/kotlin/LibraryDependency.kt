c09efec83ec68160d27e5ae4116f6fbf9f81dc5a
AngeloMendonca <angelo.mendonca@aptoide.com>
2023-06-21 10:07:01 +0100

    [DT-338] Add google services dependency

--- buildSrc/src/main/kotlin/LibraryDependency.kt
+++ buildSrc/src/main/kotlin/LibraryDependency.kt
@@ -63,6 +63,7 @@ private object LibraryVersion {
   const val FIREBASE = "32.0.0"
   const val DATASTORE = "1.0.0"
   const val GMS_PLAY_SERVICES_ADS = "18.0.1"
+  const val PLAY_SERVICES_BASEMENT = "18.2.0"
   const val ACCOMPANIST_PAGER = "0.30.1"
   const val ACCOMPANIST_WEBVIEW = "0.30.1"
   const val ACCOMPANIST_PERMISSIONS = "0.30.1"
@@ -142,6 +143,8 @@ object LibraryDependency {
   const val DATASTORE = "androidx.datastore:datastore-preferences:${LibraryVersion.DATASTORE}"
   const val GMS_PLAY_SERVICES_ADS =
     "com.google.android.gms:play-services-ads-identifier:${LibraryVersion.GMS_PLAY_SERVICES_ADS}"
+  const val PLAY_SERVICES_BASEMENT =
+    "com.google.android.gms:play-services-base:${LibraryVersion.PLAY_SERVICES_BASEMENT}"
   const val ACCOMPANIST_PAGER =
     "com.google.accompanist:accompanist-pager:${LibraryVersion.ACCOMPANIST_PAGER}"
   const val ACCOMPANIST_WEBVIEW =
