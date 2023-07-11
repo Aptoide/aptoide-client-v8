6e499de5af6b132c33d52027e2574ab8b1b8852b
Ricardo Frango <ricardo.frango@aptoide.com>
2023-07-11 16:38:28 +0100

    [DT 497] Extract the hasNotificationsPermission extension function

--- extensions/src/main/java/cm/aptoide/pt/extensions/ContextExtensions.kt
+++ extensions/src/main/java/cm/aptoide/pt/extensions/ContextExtensions.kt
@@ -1,9 +1,18 @@
 package cm.aptoide.pt.extensions
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
+import androidx.core.content.ContextCompat
 
 val Context.isActiveNetworkMetered
   get() = (getSystemService(
     Context.CONNECTIVITY_SERVICE
   ) as ConnectivityManager).isActiveNetworkMetered
+
+@SuppressLint("InlinedApi")
+fun Context.hasNotificationsPermission(): Boolean = ContextCompat.checkSelfPermission(
+  this,
+  android.Manifest.permission.POST_NOTIFICATIONS
+) == PackageManager.PERMISSION_GRANTED
