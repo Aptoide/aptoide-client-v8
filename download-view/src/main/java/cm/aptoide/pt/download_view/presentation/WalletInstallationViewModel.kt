0ca09800cf8e938c8b6477820bd69841ed14fd7a
Ricardo Frango <ricardo.frango@aptoide.com>
2023-08-11 12:14:52 +0100

    [DT 483] Make wallet installation depend on feature flag

--- download-view/src/main/java/cm/aptoide/pt/download_view/presentation/WalletInstallationViewModel.kt
+++ download-view/src/main/java/cm/aptoide/pt/download_view/presentation/WalletInstallationViewModel.kt
@@ -7,6 +7,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cm.aptoide.pt.extensions.runPreviewable
+import cm.aptoide.pt.feature_flags.domain.FeatureFlags
 import cm.aptoide.pt.install_manager.InstallManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -17,6 +18,7 @@ import javax.inject.Inject
 @HiltViewModel
 class WalletInstallationViewModel @Inject constructor(
   installManager: InstallManager,
+  featureFlags: FeatureFlags
 ) : ViewModel() {
 
   private val appInstaller = installManager.getApp("com.appcoins.wallet")
@@ -34,19 +36,20 @@ class WalletInstallationViewModel @Inject constructor(
     viewModelScope.launch {
       combine(
         appInstaller.packageInfo,
-        appInstaller.tasks.flatMapConcat { it?.stateAndProgress ?: flowOf(null) }
+        appInstaller.tasks.flatMapConcat { it?.stateAndProgress ?: flowOf(null) },
       ) { packageInfo, task -> Pair(packageInfo, task) }
         .catch { throwable -> throwable.printStackTrace() }
         .collect { (info, task) ->
-          viewModelState.update { info != null || task != null }
+          val enabled = featureFlags.get("enable_wallet_companion_app").equals("true")
+          viewModelState.update { enabled && info == null && task == null }
         }
     }
   }
 }
 
 @Composable
-fun walletInstalled(): Boolean = runPreviewable(
-  preview = { true },
+fun shouldInstallWallet(): Boolean = runPreviewable(
+  preview = { false },
   real = {
     val walletViewModel = hiltViewModel<WalletInstallationViewModel>()
     val walletUiState by walletViewModel.uiState.collectAsState()
