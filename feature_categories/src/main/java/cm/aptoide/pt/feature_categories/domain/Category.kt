215d872b773faed5f302debfb1ea6f598c683eca
Sergei Mitrofanov <goretz.m@gmail.com>
2023-08-24 09:45:38 +0100

    [DT-673] Create random category getter

--- feature_categories/src/main/java/cm/aptoide/pt/feature_categories/domain/Category.kt
+++ feature_categories/src/main/java/cm/aptoide/pt/feature_categories/domain/Category.kt
@@ -1,10 +1,19 @@
 package cm.aptoide.pt.feature_categories.domain
 
+import cm.aptoide.pt.extensions.getRandomString
+
 data class Category(
   val id: Long,
   val name: String,
   val title: String,
   val icon: String? = null,
   val graphic: String? = null,
-  val background: String? = null
+  val background: String? = null,
 )
+
+val randomCategory
+  get() = Category(
+    id = System.currentTimeMillis(),
+    name = getRandomString(range = 1..1),
+    title = getRandomString(range = 1..3, capitalize = true),
+  )
