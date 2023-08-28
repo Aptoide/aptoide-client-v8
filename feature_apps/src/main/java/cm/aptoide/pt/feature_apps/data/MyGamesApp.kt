e65c1ae2b23197a8215c11aa53ad9951184365db
Pedro Agostinho <pedro.agostinho@aptoide.com>
2023-08-28 11:03:47 +0100

    [DT-575] Create random MyGames app getter

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/MyGamesApp.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/data/MyGamesApp.kt
@@ -1,10 +1,25 @@
 package cm.aptoide.pt.feature_apps.data
 
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.VectorDrawable
+import cm.aptoide.pt.extensions.getRandomString
+import kotlin.random.Random
 
 data class MyGamesApp(
   val icon: Drawable,
   val name: String,
   val packageName: String,
-  val versionName: String?
+  val versionName: String?,
 )
+
+val randomMyGamesApp
+  get() = MyGamesApp(
+    icon = VectorDrawable(),
+    name = getRandomString(range = 2..5, capitalize = true),
+    packageName = getRandomString(range = 3..5, separator = "."),
+    versionName = if (Random.nextBoolean()) {
+      "${Random.nextInt(3)}.${Random.nextInt(20)}.${Random.nextInt(100)}"
+    } else {
+      null
+    }
+  )
