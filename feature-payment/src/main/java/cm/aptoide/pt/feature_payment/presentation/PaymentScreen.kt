3b9e1a8b680282fce836b07973a3ffac57afe6ba
Ricardo Frango <ricardo.frango@aptoide.com>
2023-09-21 14:53:51 +0100

    [DT-708] Add initial payment architecture

--- feature-payment/src/main/java/cm/aptoide/pt/feature_payment/presentation/PaymentScreen.kt
+++ feature-payment/src/main/java/cm/aptoide/pt/feature_payment/presentation/PaymentScreen.kt
@@ -2,8 +2,12 @@ package cm.aptoide.pt.feature_payment.presentation
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 import cm.aptoide.pt.extensions.PreviewAll
+import cm.aptoide.pt.extensions.runPreviewable
 
 @PreviewAll
 @Composable
@@ -13,5 +17,17 @@ fun PaymentScreenPreview() {
 
 @Composable
 fun PaymentScreen() {
+  val paymentViewState = paymentViewState()
+
   Text(text = "Hello Payment", color = Color.White)
 }
+
+@Composable
+fun paymentViewState(): Int = runPreviewable(
+  preview = { 0 },
+  real = {
+    val paymentViewModel = hiltViewModel<PaymentViewModel>()
+    val paymentUiState by paymentViewModel.uiState.collectAsState()
+    paymentUiState
+  },
+)
---
+++ feature-payment/src/main/java/cm/aptoide/pt/feature_payment/repository/product/ProductRepository.kt
@@ -0,0 +1,11 @@
+package cm.aptoide.pt.feature_payment.repository.product
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ProductRepositoryImpl @Inject constructor() : ProductRepository {
+
+}
+interface ProductRepository {
+}
