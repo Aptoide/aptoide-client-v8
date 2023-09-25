6363592b5c4071d3a6c8144b0b05b53b5f5bfacc
Ricardo Frango <ricardo.frango@aptoide.com>
2023-09-25 16:14:05 +0100

    [DT-708] Add OSP url parser

---
+++ feature-payment/src/main/java/cm/aptoide/pt/feature_payment/parser/exception/DomainNameMissingException.kt
@@ -0,0 +1,3 @@
+package cm.aptoide.pt.feature_payment.parser.exception
+
+class DomainNameMissingException(message: String?) : Throwable(message = message)
--- feature-payment/src/main/java/cm/aptoide/pt/feature_payment/presentation/PaymentScreen.kt
+++ feature-payment/src/main/java/cm/aptoide/pt/feature_payment/presentation/PaymentScreen.kt
@@ -1,32 +1,36 @@
 package cm.aptoide.pt.feature_payment.presentation
 
+import android.net.Uri
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
-import androidx.hilt.navigation.compose.hiltViewModel
 import cm.aptoide.pt.extensions.PreviewAll
 import cm.aptoide.pt.extensions.runPreviewable
 
 @PreviewAll
 @Composable
 fun PaymentScreenPreview() {
-  PaymentScreen()
+  PaymentScreen(
+    Uri.parse(
+      "https://apichain.dev.catappult.io/transaction/inapp?product=gas&domain=com.appcoins.trivialdrivesample.test&callback_url=https%3A%2F%2Fwww.mygamestudio.com%2FcompletePurchase%3FuserId%3D1234&signature=76a21fc764668b1e31e13c7cb98f2768ab52b3415ba4cd3c6455d223cc3fdaa0"
+    )
+  )
 }
 
 @Composable
-fun PaymentScreen() {
-  val paymentViewState = paymentViewState()
+fun PaymentScreen(uri: Uri?) {
+  val paymentViewState = paymentViewState(uri)
 
-  Text(text = "Hello Payment", color = Color.White)
+  Text(text = paymentViewState, color = Color.Red)
 }
 
 @Composable
-fun paymentViewState(): Int = runPreviewable(
-  preview = { 0 },
+fun paymentViewState(uri: Uri?): String = runPreviewable(
+  preview = { "Preview" },
   real = {
-    val paymentViewModel = hiltViewModel<PaymentViewModel>()
+    val paymentViewModel = paymentViewModel(uri = uri)
     val paymentUiState by paymentViewModel.uiState.collectAsState()
     paymentUiState
   },
