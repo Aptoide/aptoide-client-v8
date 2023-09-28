77541d04f6eb48a2069628b9f331989aa51f2654
Ricardo Frango <ricardo.frango@aptoide.com>
2023-09-28 17:28:50 +0100

    [DT-708] Add unique id

--- guest-wallet/src/main/java/cm/aptoide/pt/guest_wallet/di/PaymentModule.kt
+++ guest-wallet/src/main/java/cm/aptoide/pt/guest_wallet/di/PaymentModule.kt
@@ -1,15 +1,25 @@
 package cm.aptoide.pt.guest_wallet.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import cm.aptoide.pt.feature_payment.wallet.WalletProvider
+import cm.aptoide.pt.guest_wallet.BuildConfig
 import cm.aptoide.pt.guest_wallet.RealWalletProvider
 import cm.aptoide.pt.guest_wallet.repository.wallet.WalletRepository
 import cm.aptoide.pt.guest_wallet.repository.wallet.WalletRepositoryImpl
 import cm.aptoide.pt.guest_wallet.unique_id.UniqueIDProvider
 import cm.aptoide.pt.guest_wallet.unique_id.UniqueIDProviderImpl
+import cm.aptoide.pt.guest_wallet.unique_id.generator.IDGenerator
+import cm.aptoide.pt.guest_wallet.unique_id.generator.IDGeneratorImpl
+import cm.aptoide.pt.guest_wallet.unique_id.repository.UniqueIdRepository
+import cm.aptoide.pt.guest_wallet.unique_id.repository.UniqueIdRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -18,7 +28,15 @@ internal interface PaymentModule {
 
   @Singleton
   @Binds
+  fun bindIDGenerator(idGenerator: IDGeneratorImpl): IDGenerator
+
+  @Singleton
+  @Binds
   fun bindUniqueIDProvider(uniqueIDProvider: UniqueIDProviderImpl): UniqueIDProvider
+
+  @Singleton
+  @Binds
+  fun bindUniqueIdRepositoryImpl(uniqueIdRepositoryImpl: UniqueIdRepositoryImpl): UniqueIdRepository
 
   @Singleton
   @Binds
@@ -27,4 +45,21 @@ internal interface PaymentModule {
   @Singleton
   @Binds
   fun bindWalletProvider(walletProvider: RealWalletProvider): WalletProvider
+
+  companion object {
+
+    @Singleton
+    @Provides
+    @JvmStatic
+    @UniqueIdSharedPreferences
+    fun provideUniqueIdSharedPrefences(@ApplicationContext context: Context): SharedPreferences =
+      context.getSharedPreferences(
+        "${BuildConfig.LIBRARY_PACKAGE_NAME}.unique_id_prefs",
+        Context.MODE_PRIVATE
+      )
+  }
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UniqueIdSharedPreferences
--- guest-wallet/src/main/java/cm/aptoide/pt/guest_wallet/unique_id/UniqueIDProvider.kt
+++ guest-wallet/src/main/java/cm/aptoide/pt/guest_wallet/unique_id/UniqueIDProvider.kt
@@ -1,15 +1,24 @@
 package cm.aptoide.pt.guest_wallet.unique_id
 
+import cm.aptoide.pt.guest_wallet.unique_id.generator.IDGenerator
+import cm.aptoide.pt.guest_wallet.unique_id.repository.UniqueIdRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-internal class UniqueIDProviderImpl @Inject constructor() : UniqueIDProvider {
-  override fun getUniqueId(): String {
-    TODO("Not yet implemented")
+class UniqueIDProviderImpl @Inject constructor(
+  private val generator: IDGenerator,
+  private val uniqueIdRepository: UniqueIdRepository,
+) : UniqueIDProvider {
+  override suspend fun getUniqueId(): String {
+    val uniqueId = uniqueIdRepository.getUniqueId()
+    return uniqueId ?: run {
+      generator.generateUniqueID()
+        .also { uniqueIdRepository.storeUniqueId(it) }
+    }
   }
-
 }
-internal interface UniqueIDProvider {
-  fun getUniqueId(): String
+
+interface UniqueIDProvider {
+  suspend fun getUniqueId(): String
 }
