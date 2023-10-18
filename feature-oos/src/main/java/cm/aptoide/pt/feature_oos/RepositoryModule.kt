6e0bdbce903d8ef69a40f1e5e3f2b0a5ab268e2f
Pedro Agostinho <pedro.agostinho@aptoide.com>
2023-10-18 17:51:57 +0100

    [DT-763] Remove unnecessary uninstall observer annotation

--- feature-oos/src/main/java/cm/aptoide/pt/feature_oos/RepositoryModule.kt
+++ feature-oos/src/main/java/cm/aptoide/pt/feature_oos/RepositoryModule.kt
@@ -5,7 +5,3 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class UninstallPackagesFilter
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class UninstallObserver
