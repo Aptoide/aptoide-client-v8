5d2f8ded13ba2ed66b447694f2adbf320db39375
Ricardo Frango <ricardo.frango@aptoide.com>
2023-11-06 11:27:29 +0000

    [DT-726] Refactor interceptors and fix AcceptLanguageInterceptor

--- aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/AcceptLanguageInterceptor.kt
+++ aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/AcceptLanguageInterceptor.kt
@@ -6,20 +6,18 @@ import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class AcceptLanguageInterceptor @Inject constructor(
-  @ApplicationContext private val context: Context
-) : Interceptor {
+@Singleton
+class AcceptLanguageInterceptor @Inject constructor(@ApplicationContext private val context: Context) : Interceptor {
 
   override fun intercept(chain: Chain): Response {
-    val resources = context.resources
+    val locale = context.resources.configuration.locales[0]
+    val language = "${locale.language}-${locale.country}, ${locale.language};q=0.9, *;q=0.7"
     val originalRequest = chain.request()
     val newRequest = originalRequest.newBuilder()
-      .addHeader(
-        "Accept-Language", resources.configuration.locale.language
-          + "-"
-          + resources.configuration.locale.country
-      ).build()
+      .addHeader("Accept-Language", language)
+      .build()
     return chain.proceed(newRequest)
   }
 }
