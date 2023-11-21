6ea43e78d8ebd7d1bdd86133be318bb7a302c58a
Ricardo Frango <ricardo.frango@aptoide.com>
2023-11-21 15:41:51 +0000

    [DT-741] Refactor payment method

--- payments/base/payment-manager/src/main/java/cm/aptoide/pt/payment_manager/payment/PaymentMethod.kt
+++ payments/base/payment-manager/src/main/java/cm/aptoide/pt/payment_manager/payment/PaymentMethod.kt
@@ -22,44 +22,59 @@ interface PaymentMethod<T> {
   ): Transaction
 }
 
+val emptyProductInfo = ProductInfoData(
+  sku = "Sku",
+  title = "Title",
+  description = "Description",
+  priceValue = "Price Value",
+  priceCurrency = "Price Currency",
+  priceInDollars = "Price In Dollars"
+)
+
+val emptyPurchaseRequest = PurchaseRequest(
+  scheme = "PurchaseRequest scheme",
+  host = "PurchaseRequest host",
+  path = "PurchaseRequest path",
+  product = "PurchaseRequest product",
+  domain = "PurchaseRequest domain",
+  callbackUrl = "PurchaseRequest callback url",
+  orderReference = "PurchaseRequest order reference",
+  signature = "PurchaseRequest signature",
+  value = 1.0,
+  currency = "PurchaseRequest currency",
+  oemId = "PurchaseRequest oemid",
+  oemPackage = "PurchaseRequest oempackage",
+  ospUri = Uri.EMPTY,
+  metadata = "metadata",
+  to = "To",
+  productToken = "Product Token",
+  skills = false
+)
+
+val emptyProductInfoData = ProductInfoData(
+  sku = "ProductInfoData sku",
+  title = "ProductInfoData title",
+  description = "ProductInfoData description",
+  priceValue = "ProductInfoData price value",
+  priceCurrency = "",
+  priceInDollars = "ProductInfoData price value in dollars"
+)
+
+val emptyWalletData = WalletData(
+  address = "wallet address",
+  ewt = "wallet ewt",
+  signature = "wallet signature"
+)
+
 val emptyPaymentMethod = object : PaymentMethod<String> {
   override val id = "PaymentMethodData id"
   override val label = "PaymentMethodData label"
   override val iconUrl = "PaymentMethodData icon url"
   override val available = true
   override val developerWallet = "Developer Wallet"
-  override val wallet = WalletData(
-    address = "wallet address",
-    ewt = "wallet ewt",
-    signature = "wallet signature"
-  )
-  override val productInfo = ProductInfoData(
-    sku = "ProductInfoData sku",
-    title = "ProductInfoData title",
-    description = "ProductInfoData description",
-    priceValue = "ProductInfoData price value",
-    priceCurrency = "",
-    priceInDollars = "ProductInfoData price value in dollars"
-  )
-  override val purchaseRequest = PurchaseRequest(
-    scheme = "PurchaseRequest scheme",
-    host = "PurchaseRequest host",
-    path = "PurchaseRequest path",
-    product = "PurchaseRequest product",
-    domain = "PurchaseRequest domain",
-    callbackUrl = "PurchaseRequest callback url",
-    orderReference = "PurchaseRequest order reference",
-    signature = "PurchaseRequest signature",
-    value = 1.0,
-    currency = "PurchaseRequest currency",
-    oemId = "PurchaseRequest oemid",
-    oemPackage = "PurchaseRequest oempackage",
-    ospUri = Uri.EMPTY,
-    metadata = "metadata",
-    to = "To",
-    productToken = "Product Token",
-    skills = false
-  )
+  override val wallet = emptyWalletData
+  override val productInfo = emptyProductInfoData
+  override val purchaseRequest = emptyPurchaseRequest
 
   override suspend fun createTransaction(
     paymentDetails: String,
