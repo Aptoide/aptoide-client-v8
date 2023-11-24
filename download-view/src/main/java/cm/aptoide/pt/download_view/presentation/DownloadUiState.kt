91086770438f6b5730ccdb130d22acfca5d66af9
Sergei Mitrofanov <goretz.m@gmail.com>
2023-11-24 21:56:50 +0000

    [DT-840] Encapsulate download actions into download ui states

--- download-view/src/main/java/cm/aptoide/pt/download_view/presentation/DownloadUiState.kt
+++ download-view/src/main/java/cm/aptoide/pt/download_view/presentation/DownloadUiState.kt
@@ -1,13 +1,42 @@
 package cm.aptoide.pt.download_view.presentation
 
 sealed class DownloadUiState {
-  object Install : DownloadUiState()
-  object Processing : DownloadUiState()
-  data class Downloading(val downloadProgress: Int = 0) : DownloadUiState()
-  data class Installing(val downloadProgress: Int = 0) : DownloadUiState()
+  data class Install(
+    val install: () -> Unit,
+  ) : DownloadUiState()
+
+  data class Processing(
+    val cancel: (() -> Unit)?,
+  ) : DownloadUiState()
+
+  data class Downloading(
+    val downloadProgress: Int = 0,
+    val cancel: () -> Unit,
+  ) : DownloadUiState()
+
+  data class Installing(
+    val downloadProgress: Int = 0,
+  ) : DownloadUiState()
+
   object Uninstalling : DownloadUiState()
-  object Installed : DownloadUiState()
-  object Outdated : DownloadUiState()
-  object Error : DownloadUiState()
-  object ReadyToInstall : DownloadUiState()
+
+  data class Installed(
+    val open: () -> Unit,
+    val uninstall: () -> Unit,
+  ) : DownloadUiState()
+
+  data class Outdated(
+    val open: () -> Unit,
+    val update: () -> Unit,
+    val uninstall: () -> Unit,
+  ) : DownloadUiState()
+
+  data class Error(
+    val retry: () -> Unit,
+    val clear: () -> Unit,
+  ) : DownloadUiState()
+
+  data class ReadyToInstall(
+    val cancel: () -> Unit,
+  ) : DownloadUiState()
 }
