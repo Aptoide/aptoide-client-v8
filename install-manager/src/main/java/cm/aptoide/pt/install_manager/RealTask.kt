bf636e41aa5ded883ef82b010492ead0c0bc8e69
Sergei Mitrofanov <goretz.m@gmail.com>
2023-12-05 10:47:11 +0000

    [DT-840] Remove catching exceptions outside the workers flows

--- install-manager/src/main/java/cm/aptoide/pt/install_manager/RealTask.kt
+++ install-manager/src/main/java/cm/aptoide/pt/install_manager/RealTask.kt
@@ -6,37 +6,40 @@ import cm.aptoide.pt.install_manager.environment.FreeSpaceChecker
 import cm.aptoide.pt.install_manager.repository.TaskInfoRepository
 import cm.aptoide.pt.install_manager.workers.PackageDownloader
 import cm.aptoide.pt.install_manager.workers.PackageInstaller
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.transformWhile
 import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
 
 internal class RealTask internal constructor(
   private val scope: CoroutineScope,
-  internal val taskInfo: TaskInfo,
+  taskInfo: TaskInfo,
+  private val jobDispatcher: JobDispatcher,
   private val freeSpaceChecker: FreeSpaceChecker,
   private val packageDownloader: PackageDownloader,
   private val packageInstaller: PackageInstaller,
   private val taskInfoRepository: TaskInfoRepository,
 ) : Task {
 
-  override var isFinished = false
-    private set
+  override val isFinished
+    get() = _stateAndProgress.value.first in listOf(
+      Task.State.ABORTED,
+      Task.State.CANCELED,
+      Task.State.COMPLETED,
+      Task.State.FAILED
+    )
 
-  override val packageName: String = taskInfo.packageName
+  private var _taskInfo: TaskInfo = taskInfo
 
-  override val type: Task.Type = taskInfo.type
+  override val packageName: String = _taskInfo.packageName
 
-  private val installPackageInfo: InstallPackageInfo = taskInfo.installPackageInfo
+  override val type: Task.Type = _taskInfo.type
+
+  private val installPackageInfo: InstallPackageInfo = _taskInfo.installPackageInfo
 
   private val _stateAndProgress = MutableStateFlow(Task.State.PENDING to -1)
 
@@ -52,32 +55,31 @@ internal class RealTask internal constructor(
     )
   }
 
+  internal fun enqueue(alreadySaved: Boolean = false): Task {
+    scope.launch {
+      if (!alreadySaved) taskInfoRepository.saveJob(_taskInfo)
+      jobDispatcher.enqueue(this@RealTask)
+    }
+    return this
+  }
+
   internal suspend fun start() {
     if (isFinished) return // Already finished.
-    when (type) {
-      Task.Type.INSTALL -> flow<Pair<Task.State, Int>> {
-        val missingSpace = freeSpaceChecker.missingSpace(downloadSize)
-        if (missingSpace > 0) {
-          throw OutOfSpaceException(missingSpace, "Not enough free space to download and install")
-        }
-      }
-        .onCompletion { if (it == null) emitAll(downloadFlow) }
-        .onCompletion { if (it == null) emitAll(installFlow) }
+    val result = when (type) {
+      Task.Type.UNINSTALL -> packageInstaller.uninstall(packageName)
+        .collectErrorFor(Task.State.UNINSTALLING)
 
-      Task.Type.UNINSTALL -> uninstallFlow
-    }
-      .catch {
-        emit(
-          when (it) {
-            is AbortException -> Task.State.ABORTED to -1
-            is CancellationException -> Task.State.CANCELED to -1
-            else -> Task.State.FAILED to -1
-          }
-        )
-      }
-      .onEach { _stateAndProgress.emit(it) }
-      .onCompletion { finalize() }
-      .collect()
+      else -> freeSpaceChecker.missingSpace(downloadSize)
+        .takeIf { it > 0 }
+        ?.let { Task.State.FAILED }
+        ?: packageDownloader.download(packageName, installPackageInfo)
+          .collectErrorFor(Task.State.DOWNLOADING)
+        ?: _stateAndProgress.emit(Task.State.READY_TO_INSTALL to -1).let { null }
+        ?: packageInstaller.install(packageName, installPackageInfo)
+          .collectErrorFor(Task.State.INSTALLING)
+    } ?: Task.State.COMPLETED
+    _stateAndProgress.emit(result to -1)
+    taskInfoRepository.removeAll(packageName)
   }
 
   override fun cancel() {
@@ -86,29 +88,20 @@ internal class RealTask internal constructor(
       val cancelled = packageDownloader.cancel(packageName) || packageInstaller.cancel(packageName)
       if (!cancelled) {
         _stateAndProgress.tryEmit(Task.State.CANCELED to -1)
-        finalize()
+        taskInfoRepository.removeAll(packageName)
       }
     }
   }
 
-  private val downloadFlow: Flow<Pair<Task.State, Int>>
-    get() = packageDownloader.download(packageName, installPackageInfo)
-      .map { Task.State.DOWNLOADING to it }
-      .onCompletion { if (it == null) emit(Task.State.READY_TO_INSTALL to -1) }
-
-  private val installFlow: Flow<Pair<Task.State, Int>>
-    get() = packageInstaller.install(packageName, installPackageInfo)
-      .map { Task.State.INSTALLING to it }
-      .onCompletion { if (it == null) emit(Task.State.COMPLETED to -1) }
-
-  private val uninstallFlow: Flow<Pair<Task.State, Int>>
-    get() = packageInstaller.uninstall(packageName)
-      .map { Task.State.UNINSTALLING to it }
-      .onCompletion { if (it == null) emit(Task.State.COMPLETED to -1) }
-
-  private suspend fun finalize() {
-    taskInfoRepository.removeAll(packageName)
-    isFinished = true
+  private suspend fun Flow<Int>.collectErrorFor(state: Task.State): Task.State? = try {
+    onEach { _stateAndProgress.emit(state to it) }.collect()
+    null
+  } catch (e: AbortException) {
+    Task.State.ABORTED
+  } catch (e: CancellationException) {
+    Task.State.CANCELED
+  } catch (t: Throwable) {
+    Task.State.FAILED
   }
 }
 
