b33baca003842bc3834649b6dce3674311c86868
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-01-23 16:08:31 +0000

    [DT-1057] Add onTransact to aidl binder

--- payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/AppcoinsBillingBinder.kt
+++ payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/AppcoinsBillingBinder.kt
@@ -1,15 +1,28 @@
 package com.appcoins.billing.sdk
 
+import android.content.pm.PackageManager
+import android.os.Binder
 import android.os.Bundle
+import android.os.Parcel
 import com.appcoins.billing.AppcoinsBilling
+import javax.inject.Inject
 
-class AppcoinsBillingBinder : AppcoinsBilling.Stub() {
+class AppcoinsBillingBinder @Inject constructor(
+  private val packageManager: PackageManager,
+) : AppcoinsBilling.Stub() {
 
   companion object {
     internal const val RESULT_BILLING_UNAVAILABLE =
       3 // this billing API version is not supported for the type requested
     internal const val RESULT_DEVELOPER_ERROR = 5 // invalid arguments provided to the API
     const val RESPONSE_CODE = "RESPONSE_CODE"
+  }
+
+  private var merchantName: String? = null
+
+  override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
+    merchantName = packageManager.getPackagesForUid(Binder.getCallingUid())?.firstOrNull()
+    return super.onTransact(code, data, reply, flags)
   }
 
   override fun isBillingSupported(apiVersion: Int, packageName: String?, type: String?): Int {
