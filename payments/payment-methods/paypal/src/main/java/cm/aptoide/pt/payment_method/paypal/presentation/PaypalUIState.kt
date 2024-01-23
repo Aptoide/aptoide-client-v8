3c26e610667c0e831dafb69af45210eeb3a17947
Sergei Mitrofanov <goretz.m@gmail.com>
2024-01-23 13:51:56 +0000

    [DT-1044] Rename for convenience

--- payments/payment-methods/paypal/src/main/java/cm/aptoide/pt/payment_method/paypal/presentation/PaypalUIState.kt
+++ payments/payment-methods/paypal/src/main/java/cm/aptoide/pt/payment_method/paypal/presentation/PaypalUIState.kt
@@ -2,27 +2,27 @@ package cm.aptoide.pt.payment_method.paypal.presentation
 
 import cm.aptoide.pt.payment_manager.manager.PurchaseRequest
 
-sealed class PaypalScreenUiState {
+sealed class PaypalUIState {
   data class BillingAgreementAvailable(
     val purchaseRequest: PurchaseRequest,
     val paymentMethodName: String,
     val paymentMethodIconUrl: String,
     val onBuyClick: () -> Unit,
     val onRemoveBillingAgreementClick: () -> Unit,
-  ) : PaypalScreenUiState()
+  ) : PaypalUIState()
 
   data class LaunchWebViewActivity(
     val url: String,
     val token: String,
     val onWebViewResult: (String, Boolean) -> Unit,
-  ) : PaypalScreenUiState()
+  ) : PaypalUIState()
 
-  object MakingPurchase : PaypalScreenUiState()
-  object PaypalAgreementRemoved : PaypalScreenUiState()
-  object Loading : PaypalScreenUiState()
-  object Error : PaypalScreenUiState()
+  object MakingPurchase : PaypalUIState()
+  object PaypalAgreementRemoved : PaypalUIState()
+  object Loading : PaypalUIState()
+  object Error : PaypalUIState()
   data class Success(
     val valueInDollars: String,
     val uid: String,
-  ) : PaypalScreenUiState()
+  ) : PaypalUIState()
 }
