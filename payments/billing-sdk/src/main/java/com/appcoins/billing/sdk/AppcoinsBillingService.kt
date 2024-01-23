37cdc371492458088d03abee455caea9a191f42f
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-01-23 16:08:04 +0000

    [DT-1057] Make billing service an entry point and inject binder

--- payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/AppcoinsBillingService.kt
+++ payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/AppcoinsBillingService.kt
@@ -3,10 +3,16 @@ package com.appcoins.billing.sdk
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AppcoinsBillingService : Service() {
 
+  @Inject
+  lateinit var appcoinsBillingBinder: AppcoinsBillingBinder
+
   override fun onBind(intent: Intent): IBinder {
-    return AppcoinsBillingBinder()
+    return appcoinsBillingBinder
   }
 }
