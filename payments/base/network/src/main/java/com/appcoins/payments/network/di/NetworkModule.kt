66c7108838c3b1410200130690afa206caece83a
Sergei Mitrofanov <goretz.m@gmail.com>
2024-02-20 11:57:15 +0000

    [DT-1106] Decouple payments networking from project architecture

--- payments/base/network/src/main/java/com/appcoins/payments/network/di/NetworkModule.kt
+++ payments/base/network/src/main/java/com/appcoins/payments/network/di/NetworkModule.kt
@@ -1,19 +1,15 @@
 package com.appcoins.payments.network.di
 
-import cm.aptoide.pt.aptoide_network.data.network.AcceptLanguageInterceptor
-import cm.aptoide.pt.aptoide_network.data.network.QLogicInterceptor
-import cm.aptoide.pt.aptoide_network.data.network.UserAgentInterceptor
-import cm.aptoide.pt.aptoide_network.data.network.VersionCodeInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit.SECONDS
 import javax.inject.Qualifier
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -23,17 +19,11 @@ object NetworkModule {
   @Provides
   @Singleton
   fun providePaymentsBaseOkHttpClient(
-    userAgentInterceptor: UserAgentInterceptor,
-    qLogicInterceptor: QLogicInterceptor,
-    versionCodeInterceptor: VersionCodeInterceptor,
-    languageInterceptor: AcceptLanguageInterceptor,
+    @UserAgentInterceptor userAgentInterceptor: Interceptor,
     httpLoggingInterceptor: HttpLoggingInterceptor,
   ): OkHttpClient =
     OkHttpClient.Builder()
       .addInterceptor(userAgentInterceptor)
-      .addInterceptor(qLogicInterceptor)
-      .addInterceptor(versionCodeInterceptor)
-      .addInterceptor(languageInterceptor)
       .addInterceptor(httpLoggingInterceptor)
       .build()
 
@@ -41,17 +31,11 @@ object NetworkModule {
   @Provides
   @Singleton
   fun provideBrokerOkHttpClient(
-    userAgentInterceptor: UserAgentInterceptor,
-    qLogicInterceptor: QLogicInterceptor,
-    versionCodeInterceptor: VersionCodeInterceptor,
-    languageInterceptor: AcceptLanguageInterceptor,
+    @UserAgentInterceptor userAgentInterceptor: Interceptor,
     httpLoggingInterceptor: HttpLoggingInterceptor,
   ): OkHttpClient =
     OkHttpClient.Builder()
       .addInterceptor(userAgentInterceptor)
-      .addInterceptor(qLogicInterceptor)
-      .addInterceptor(versionCodeInterceptor)
-      .addInterceptor(languageInterceptor)
       .addInterceptor(httpLoggingInterceptor)
       .readTimeout(30, SECONDS)
       .writeTimeout(30, SECONDS)
@@ -65,3 +49,7 @@ annotation class PaymentsBaseOkHttp
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class BrokerOkHttp
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UserAgentInterceptor
