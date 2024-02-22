8b873930308f21e397514d6eaba5df7966708e33
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-02-22 17:17:31 +0000

    [DT-1110] Remove unneeded compose plugin instances in payments

--- payments/base/oem-extractor/build.gradle.kts
+++ payments/base/oem-extractor/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
   id(GradlePluginId.ANDROID_LIBRARY)
   id(GradlePluginId.ANDROID_MODULE)
-  id(GradlePluginId.COMPOSABLE)
   id(GradlePluginId.HILT)
 }
 
 android {
   namespace = "com.appcoins.oem_extractor"
+
+  buildFeatures {
+    buildConfig = true
+  }
 }
 
 dependencies {
--- payments/base/payment-prefs/build.gradle.kts
+++ payments/base/payment-prefs/build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
   id(GradlePluginId.ANDROID_LIBRARY)
   id(GradlePluginId.ANDROID_MODULE)
-  id(GradlePluginId.COMPOSABLE)
   id(GradlePluginId.HILT)
 }
 
 android {
   namespace = "com.appcoins.payment_prefs"
+
+  buildFeatures {
+    buildConfig = true
+  }
 }
 
 dependencies {
