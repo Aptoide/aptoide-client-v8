c9ec41d63f327a636ce35d4c1d6f0ad6d0ed8d89
Sergei Mitrofanov <goretz.m@gmail.com>
2024-02-23 00:07:50 +0000

    [DT-1107] Pull up the user agent interceptor specific logic
    To simplify the user agent retrieval independent from Retrofit.

--- app/src/main/java/cm/aptoide/pt/network/AptoideGetUserAgent.kt
+++ app/src/main/java/cm/aptoide/pt/network/AptoideGetUserAgent.kt
@@ -2,24 +2,21 @@ package cm.aptoide.pt.network
 
 import android.content.pm.PackageManager
 import cm.aptoide.pt.BuildConfig
-import cm.aptoide.pt.aptoide_network.data.network.UserAgentInterceptor
+import cm.aptoide.pt.aptoide_network.data.network.GetUserAgent
 import cm.aptoide.pt.environment_info.DeviceInfo
 import cm.aptoide.pt.extensions.calculateMD5
 import cm.aptoide.pt.extensions.getPackageInfo
 import cm.aptoide.pt.network.repository.IdsRepository
-import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Response
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AptoideUserAgentInterceptor @Inject constructor(
+class AptoideGetUserAgent @Inject constructor(
   private val packageManager: PackageManager,
   private val idsRepository: IdsRepository,
   private val deviceInfo: DeviceInfo,
-) : UserAgentInterceptor {
+) : GetUserAgent {
 
   private val versionName = BuildConfig.VERSION_NAME
   private val aptoidePackage = BuildConfig.APPLICATION_ID
@@ -34,7 +31,7 @@ class AptoideUserAgentInterceptor @Inject constructor(
       ?: "None"
   }
 
-  override fun buildUserAgent(): String =
+  override operator fun invoke(): String =
     "Aptoide/${versionName} " +
       "(Linux; Android ${deviceInfo.getAndroidRelease()}; " +
       "${deviceInfo.getApiLevel()}; " +
@@ -46,12 +43,4 @@ class AptoideUserAgentInterceptor @Inject constructor(
       "${cachedMd5}; " +
       "${deviceInfo.getScreenDimensions()};" +
       "${idsRepository.aptoideClientUuid})"
-
-  override fun intercept(chain: Interceptor.Chain): Response {
-    val originalRequest = chain.request()
-    val userAgent: String = buildUserAgent()
-    val requestBuilder: Request.Builder = originalRequest.newBuilder()
-    requestBuilder.header("User-Agent", userAgent)
-    return chain.proceed(requestBuilder.build())
-  }
 }
