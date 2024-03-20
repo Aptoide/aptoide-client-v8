3b2813659c91fb1fc09df78b9ad72964298455a6
Ricardo Frango <ricardo.frango@aptoide.com>
2024-03-20 14:29:21 +0000

    [DT-1177] Refactor isBillingSupported sdk method

--- payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/BillingErrorMapper.kt
+++ payments/billing-sdk/src/main/java/com/appcoins/billing/sdk/BillingErrorMapper.kt
@@ -1,24 +1,14 @@
 package com.appcoins.billing.sdk
 
-import com.appcoins.payments.arch.error.ServerErrorException
 import java.net.UnknownHostException
 
 internal interface BillingErrorMapper {
-  fun mapBillingSupportError(exception: Throwable): Int
   fun mapSkuDetailsError(exception: Throwable): Int
   fun mapPurchasesError(exception: Throwable): Int
   fun mapConsumePurchasesError(exception: Throwable): Int
 }
 
 internal class BillingErrorMapperImpl : BillingErrorMapper {
-
-  override fun mapBillingSupportError(exception: Throwable): Int {
-    return when (exception) {
-      is ServerErrorException -> BillingSdkConstants.ResultCode.RESULT_ERROR
-      is UnknownHostException -> BillingSdkConstants.ResultCode.RESULT_SERVICE_UNAVAILABLE
-      else -> BillingSdkConstants.ResultCode.RESULT_BILLING_UNAVAILABLE
-    }
-  }
 
   override fun mapSkuDetailsError(exception: Throwable) = mapException(exception)
 
---
+++ payments/product-inventory/src/main/java/com/appcoins/product_inventory/model/BillingSupport.kt
@@ -0,0 +1,9 @@
+package com.appcoins.product_inventory.model
+
+enum class BillingSupport {
+  SUPPORTED,
+  NOT_SUPPORTED,
+  UNKNOWN_ERROR,
+  NO_INTERNET_CONNECTION,
+  SERVER_ERROR
+}
