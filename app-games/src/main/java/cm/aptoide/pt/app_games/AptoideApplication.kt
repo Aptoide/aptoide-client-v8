83d7d38baeecbaa6d52ba46c1be73c93099ade44
Culurio <claudiohcx@gmail.com>
2024-03-25 10:50:07 +0000

    [APP-2302] Add install manager initialization

--- app-games/src/main/java/cm/aptoide/pt/app_games/AptoideApplication.kt
+++ app-games/src/main/java/cm/aptoide/pt/app_games/AptoideApplication.kt
@@ -7,8 +7,13 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import cm.aptoide.pt.install_manager.InstallManager
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 val Context.userPreferencesDataStore: DataStore<Preferences> by preferencesDataStore(
   name = "userPreferences",
@@ -24,9 +29,24 @@ val Context.themeDataStore: DataStore<Preferences> by preferencesDataStore(name 
 @HiltAndroidApp
 class AptoideApplication : Application() {
 
+  @Inject
+  lateinit var installManager: InstallManager
+
   override fun onCreate() {
     super.onCreate()
     initTimber()
+    startInstallManager()
+  }
+
+  private fun startInstallManager() {
+    CoroutineScope(Dispatchers.IO).launch {
+      try {
+        installManager.restore()
+      } catch (e: Exception) {
+        Timber.e(e)
+        e.printStackTrace()
+      }
+    }
   }
 
   private fun initTimber() {
