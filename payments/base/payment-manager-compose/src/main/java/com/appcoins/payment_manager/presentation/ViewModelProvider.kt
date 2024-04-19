c795c4cb8474d7c96696ccf0a170eac94de9b631
Sergei Mitrofanov <goretz.m@gmail.com>
2024-04-19 08:52:45 +0100

    [DT-1164] Extract preselected payment method logic from payments
    Better keep this logic outside of the payments module responsibility.
    Because it is a specific UI implementation logic that can differ between
    projects that use the payments module.

--- payments/base/payment-manager-compose/src/main/java/com/appcoins/payment_manager/presentation/ViewModelProvider.kt
+++ payments/base/payment-manager-compose/src/main/java/com/appcoins/payment_manager/presentation/ViewModelProvider.kt
@@ -1,27 +1,31 @@
 package com.appcoins.payment_manager.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.Factory
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.appcoins.payment_manager.di.PaymentsModule
-import com.appcoins.payment_prefs.di.PaymentPrefsModule
 import com.appcoins.payments.arch.PaymentsInitializer
 import com.appcoins.payments.arch.PurchaseRequest
 
 @Composable
-fun paymentMethodsViewModel(purchaseRequest: PurchaseRequest): PaymentMethodsViewModel {
-  return viewModel(
+fun paymentMethodsViewModel(purchaseRequest: PurchaseRequest): Pair<PaymentMethodsUiState, () -> Unit> {
+  val vm = viewModel<PaymentMethodsViewModel>(
     factory = object : Factory {
       override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         return PaymentMethodsViewModel(
           purchaseRequest = purchaseRequest,
           paymentManager = PaymentsModule.paymentManager,
-          preSelectedPaymentUseCase = PaymentPrefsModule.preSelectedPaymentUseCase,
           logger = PaymentsInitializer.logger
         ) as T
       }
     }
   )
+
+  val uiState by vm.uiState.collectAsState()
+
+  return uiState to vm::reload
 }
