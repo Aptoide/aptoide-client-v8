0ed62c4964ae0613655776fac8090320ccf59aa1
Sergei Mitrofanov <goretz.m@gmail.com>
2024-04-23 09:43:28 +0100

    [DT-1098] Add custom redirection WebView to redirection activity
    To gain control over user back actions.

--- payments/payment-methods/adyen/src/main/java/com/appcoins/payment_method/adyen/presentation/AdyenRedirectActivity.kt
+++ payments/payment-methods/adyen/src/main/java/com/appcoins/payment_method/adyen/presentation/AdyenRedirectActivity.kt
@@ -4,7 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.LayoutParams
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import com.adyen.checkout.components.model.payments.response.RedirectAction
 import com.adyen.checkout.redirect.RedirectComponent
@@ -20,8 +26,8 @@ class AdyenRedirectActivity : ComponentActivity() {
   private val logger = PaymentsInitializer.logger
 
   @Suppress("DEPRECATION")
-  private val action by lazy {
-    intent.getParcelableExtra<RedirectAction>(AdyenActionResolveContract.REDIRECT_ACTION)!!
+  private val url by lazy {
+    intent.getParcelableExtra<RedirectAction>(AdyenActionResolveContract.REDIRECT_ACTION)!!.url!!
   }
 
   @SuppressLint("SourceLockedOrientationActivity")
@@ -35,21 +41,7 @@ class AdyenRedirectActivity : ComponentActivity() {
     }
 
     try {
-      RedirectComponent.PROVIDER.get(
-        this,
-        application,
-        PaymentsInitializer.redirectConfiguration
-      ).run {
-        removeObservers(this@AdyenRedirectActivity)
-        observe(this@AdyenRedirectActivity) {
-          setResult(
-            RESULT_OK,
-            Intent().putExtra(AdyenActionResolveContract.ACTION_COMPONENT_DATA, it)
-          )
-          finish()
-        }
-        handleAction(this@AdyenRedirectActivity, action)
-      }
+      setContentView(buildAdyenWebView(url))
     } catch (e: Throwable) {
       logger.logError("adyen", e)
       setResult(WRONG_URL)
@@ -64,20 +56,51 @@ class AdyenRedirectActivity : ComponentActivity() {
     super.onBackPressed()
   }
 
-  override fun onNewIntent(intent: Intent?) {
-    super.onNewIntent(intent)
-    val data = intent?.data
-    if (data != null && data.toString()
-        .startsWith(AdyenActionResolveContract.ADYEN_CHECKOUT_SCHEME)
-    ) {
-      RedirectComponent.PROVIDER.get(
-        this,
-        application,
-        PaymentsInitializer.redirectConfiguration
-      ).run {
-        handleIntent(intent)
-        removeObservers(this@AdyenRedirectActivity)
+  private fun handleRedirectResult(uri: Uri) {
+    RedirectComponent.PROVIDER.get(
+      this,
+      application,
+      PaymentsInitializer.redirectConfiguration
+    ).run {
+      removeObservers(this@AdyenRedirectActivity)
+      observe(this@AdyenRedirectActivity) {
+        setResult(
+          RESULT_OK,
+          Intent().putExtra(AdyenActionResolveContract.ACTION_COMPONENT_DATA, it)
+        )
+        finish()
+      }
+      handleIntent(Intent("", uri))
+    }
+  }
+
+  @SuppressLint("SetJavaScriptEnabled")
+  private fun buildAdyenWebView(url: String): View = WebView(this).apply {
+    layoutParams = LayoutParams(
+      LayoutParams.MATCH_PARENT,
+      LayoutParams.MATCH_PARENT
+    )
+
+    webViewClient = object : WebViewClient() {
+      override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?,
+      ): Boolean {
+        return request?.url
+          ?.takeIf { it.scheme == AdyenActionResolveContract.ADYEN_CHECKOUT_SCHEME }
+          ?.let {
+            handleRedirectResult(it)
+            true
+          } ?: super.shouldOverrideUrlLoading(view, request)
       }
     }
+
+    settings.apply {
+      javaScriptEnabled = true
+      domStorageEnabled = true
+      useWideViewPort = true
+    }
+
+    loadUrl(url)
   }
 }
