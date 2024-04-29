92ec6720a71c3e6ef4fb025719997ad8e5c7eaa0
Culurio <claudiohcx@gmail.com>
2024-04-29 09:27:39 +0100

    [APP-2392] Filtering apps Q parameter

--- app-games/src/main/java/cm/aptoide/pt/app_games/network/AptoideQLogicInterceptor.kt
+++ app-games/src/main/java/cm/aptoide/pt/app_games/network/AptoideQLogicInterceptor.kt
@@ -3,28 +3,22 @@ package cm.aptoide.pt.app_games.network
 import android.util.Base64
 import cm.aptoide.pt.aptoide_network.data.network.QLogicInterceptor
 import cm.aptoide.pt.environment_info.DeviceInfo
-import cm.aptoide.pt.settings.repository.UserPreferencesRepository
-import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
 import javax.inject.Inject
 
 class AptoideQLogicInterceptor @Inject constructor(
-  private val userPreferencesRepository: UserPreferencesRepository,
   private val deviceInfo: DeviceInfo
 ) : QLogicInterceptor {
 
   private val cachedFilters: String by lazy { computeFilters() }
 
-  override fun buildQValue(): String? =
-    runBlocking { userPreferencesRepository.isShowCompatibleApps() }
-      .takeIf { it }
-      ?.let { cachedFilters }
+  override fun buildQValue(): String = cachedFilters
 
   override fun intercept(chain: Chain): Response {
     val originalRequest = chain.request()
     val newUrl = originalRequest.url.newBuilder()
-    buildQValue()?.let {
+    buildQValue().let {
       newUrl.addQueryParameter("q", buildQValue())
     }
     val newRequest = originalRequest.newBuilder().url(newUrl.build()).build()
