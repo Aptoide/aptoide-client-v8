53a1de6ebc82a5d343fd30fdf910a0c15e2d7a41
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-05-16 11:35:44 +0100

    [APP-2433] Split preview annotations by UI mode

--- extensions/src/main/java/cm/aptoide/pt/extensions/Composables.kt
+++ extensions/src/main/java/cm/aptoide/pt/extensions/Composables.kt
@@ -31,19 +31,42 @@ fun <T> runPreviewable(
   showSystemUi = true
 )
 @Preview(
+  name = "Normal scale",
+  group = "normal scale",
+  fontScale = 1.0f,
+  device = "id:pixel_5",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale all",
+  group = "max scale all",
+  fontScale = 1.3f,
+  device = "id:3.7 FWVGA slider",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale font",
+  group = "max scale font",
+  fontScale = 1.3f,
+  device = "id:pixel_5",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale screen",
+  group = "max scale screen",
+  fontScale = 1.0f,
+  device = "id:3.7 FWVGA slider",
+  showSystemUi = true
+)
+annotation class PreviewLight
+
+@Preview(
   name = "Min scale all dark",
   group = "min scale all",
   fontScale = 0.85f,
   device = "id:5.4in FWVGA",
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-  name = "Normal scale",
-  group = "normal scale",
-  fontScale = 1.0f,
-  device = "id:pixel_5",
-  showSystemUi = true
 )
 @Preview(
   name = "Normal scale dark",
@@ -54,26 +77,12 @@ fun <T> runPreviewable(
   uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Preview(
-  name = "Max scale all",
-  group = "max scale all",
-  fontScale = 1.3f,
-  device = "id:3.7 FWVGA slider",
-  showSystemUi = true
-)
-@Preview(
   name = "Max scale all dark",
   group = "max scale all",
   fontScale = 1.3f,
   device = "id:3.7 FWVGA slider",
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-  name = "Max scale font",
-  group = "max scale font",
-  fontScale = 1.3f,
-  device = "id:pixel_5",
-  showSystemUi = true
 )
 @Preview(
   name = "Max scale font dark",
@@ -84,13 +93,6 @@ fun <T> runPreviewable(
   uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Preview(
-  name = "Max scale screen",
-  group = "max scale screen",
-  fontScale = 1.0f,
-  device = "id:3.7 FWVGA slider",
-  showSystemUi = true
-)
-@Preview(
   name = "Max scale screen dark",
   group = "max scale screen",
   fontScale = 1.0f,
@@ -98,6 +100,10 @@ fun <T> runPreviewable(
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES
 )
+annotation class PreviewDark
+
+@PreviewLight
+@PreviewDark
 annotation class PreviewAll
 
 @Preview(
@@ -108,19 +114,42 @@ annotation class PreviewAll
   showSystemUi = true
 )
 @Preview(
+  name = "Normal scale",
+  group = "normal scale",
+  fontScale = 1.0f,
+  device = "spec:parent=pixel_5,orientation=landscape",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale all",
+  group = "max scale all",
+  fontScale = 1.3f,
+  device = "spec:parent=3.7 FWVGA slider,orientation=landscape",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale font",
+  group = "max scale font",
+  fontScale = 1.3f,
+  device = "spec:parent=pixel_5,orientation=landscape",
+  showSystemUi = true
+)
+@Preview(
+  name = "Max scale screen",
+  group = "max scale screen",
+  fontScale = 1.0f,
+  device = "spec:parent=3.7 FWVGA slider,orientation=landscape",
+  showSystemUi = true
+)
+annotation class PreviewLandscapeLight
+
+@Preview(
   name = "Min scale all dark",
   group = "min scale all",
   fontScale = 0.85f,
   device = "spec:parent=5.4in FWVGA,orientation=landscape",
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-  name = "Normal scale",
-  group = "normal scale",
-  fontScale = 1.0f,
-  device = "spec:parent=pixel_5,orientation=landscape",
-  showSystemUi = true
 )
 @Preview(
   name = "Normal scale dark",
@@ -131,26 +160,12 @@ annotation class PreviewAll
   uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Preview(
-  name = "Max scale all",
-  group = "max scale all",
-  fontScale = 1.3f,
-  device = "spec:parent=3.7 FWVGA slider,orientation=landscape",
-  showSystemUi = true
-)
-@Preview(
   name = "Max scale all dark",
   group = "max scale all",
   fontScale = 1.3f,
   device = "spec:parent=3.7 FWVGA slider,orientation=landscape",
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-  name = "Max scale font",
-  group = "max scale font",
-  fontScale = 1.3f,
-  device = "spec:parent=pixel_5,orientation=landscape",
-  showSystemUi = true
 )
 @Preview(
   name = "Max scale font dark",
@@ -161,13 +176,6 @@ annotation class PreviewAll
   uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Preview(
-  name = "Max scale screen",
-  group = "max scale screen",
-  fontScale = 1.0f,
-  device = "spec:parent=3.7 FWVGA slider,orientation=landscape",
-  showSystemUi = true
-)
-@Preview(
   name = "Max scale screen dark",
   group = "max scale screen",
   fontScale = 1.0f,
@@ -175,6 +183,10 @@ annotation class PreviewAll
   showSystemUi = true,
   uiMode = Configuration.UI_MODE_NIGHT_YES,
 )
+annotation class PreviewLandscapeDark
+
+@PreviewLandscapeLight
+@PreviewLandscapeDark
 annotation class PreviewAllLandscape
 
 @Composable
