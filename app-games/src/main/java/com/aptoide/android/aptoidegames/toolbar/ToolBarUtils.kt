7f21b25def1508e8ca6cbfde3772438e24540968
Ricardo Frango <ricardo.frango@aptoide.com>
2024-06-07 16:56:51 +0100

    [APP-2556] Add new app games logo

--- app-games/src/main/java/com/aptoide/android/aptoidegames/toolbar/ToolBarUtils.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/toolbar/ToolBarUtils.kt
@@ -8,6 +8,6 @@ import com.aptoide.android.aptoidegames.theme.Palette
 @Composable
 fun String.getToolBarLogo() =
   when (this) {
-    "dev" -> getAptoideGamesToolbarLogoDev(Palette.Black, Palette.Primary)
-    else -> getAptoideGamesToolbarLogo(Palette.Black, Palette.Primary)
+    "dev" -> getAptoideGamesToolbarLogoDev(Palette.Primary)
+    else -> getAptoideGamesToolbarLogo(Palette.Primary)
   }
