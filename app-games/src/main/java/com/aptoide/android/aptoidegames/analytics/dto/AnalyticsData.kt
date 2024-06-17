9d42def5332e3702e639cabcffa2b6a46277134b
Sergei Mitrofanov <goretz.m@gmail.com>
2024-06-17 23:07:09 +0100

    [APP-2585] Add analytics install package info mapper
    To put the current analytics data in the install package info payload.
    To persist it in the TaskInfo DB for later usa in installation events.

---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/analytics/dto/AnalyticsData.kt
@@ -0,0 +1,38 @@
+package com.aptoide.android.aptoidegames.analytics.dto
+
+import androidx.annotation.Keep
+
+@Keep
+data class AnalyticsUIContext(
+  val currentScreen: String,
+  val previousScreen: String?,
+  val bundleMeta: BundleMeta?,
+) {
+  companion object {
+    val Empty = AnalyticsUIContext("", null, null)
+  }
+}
+
+@Keep
+data class AnalyticsPayload(
+  val isApkfy: Boolean,
+  val isAab: Boolean,
+  val aabInstallTime: String,
+  val isAppCoins: Boolean,
+  val isInCatappult: Boolean,
+  val isGame: Boolean,
+  val isMigration: Boolean,
+  val hasObb: Boolean,
+  val versionCode: Int,
+  val context: String,
+  val previousContext: String?,
+  val store: String,
+  val bundleMeta: BundleMeta?,
+  val trustedBadge: String?,
+)
+
+@Keep
+data class BundleMeta(
+  val tag: String,
+  val bundleSource: String,
+)
