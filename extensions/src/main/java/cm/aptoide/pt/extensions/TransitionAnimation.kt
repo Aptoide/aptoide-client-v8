1232a3b70e47890a91b9160ca65a88a2b3f5ae0c
Sergei Mitrofanov <goretz.m@gmail.com>
2024-06-18 10:09:11 +0100

    [APP-2585] Add alternative navigation composables
    For more decoupled and convenient usage.

--- extensions/src/main/java/cm/aptoide/pt/extensions/TransitionAnimation.kt
+++ extensions/src/main/java/cm/aptoide/pt/extensions/TransitionAnimation.kt
@@ -1,5 +1,6 @@
 package cm.aptoide.pt.extensions
 
+import android.os.Bundle
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.core.tween
@@ -9,6 +10,41 @@ import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+
+data class ScreenData(
+  val route: String,
+  val arguments: List<NamedNavArgument> = emptyList(),
+  val deepLinks: List<NavDeepLink> = emptyList(),
+  val content: @Composable (Bundle?, (String) -> Unit, () -> Unit) -> Unit,
+){
+  companion object
+}
+
+fun NavGraphBuilder.staticComposable(
+  navigate: (String) -> Unit,
+  goBack: () -> Unit,
+  screenData: ScreenData,
+) = staticComposable(
+  route = screenData.route,
+  arguments = screenData.arguments,
+  deepLinks = screenData.deepLinks,
+  content = {
+    screenData.content(it.arguments, navigate, goBack)
+  }
+)
+
+fun NavGraphBuilder.animatedComposable(
+  navigate: (String) -> Unit,
+  goBack: () -> Unit,
+  screenData: ScreenData,
+) = animatedComposable(
+  route = screenData.route,
+  arguments = screenData.arguments,
+  deepLinks = screenData.deepLinks,
+  content = {
+    screenData.content(it.arguments, navigate, goBack)
+  }
+)
 
 fun NavGraphBuilder.staticComposable(
   route: String,
