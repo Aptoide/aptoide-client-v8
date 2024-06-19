1754e1a3a1e2ddb3ae3383b4fa2ba7f7564ad55e
Ricardo Frango <ricardo.frango@aptoide.com>
2024-06-19 18:07:34 +0100

    [DT-1293] Add missing accept language to payments requests

--- app-games/src/main/java/com/aptoide/android/aptoidegames/network/AptoideGetHeaders.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/network/AptoideGetHeaders.kt
@@ -1,6 +1,8 @@
 package com.aptoide.android.aptoidegames.network
 
+import android.content.Context
 import android.content.pm.PackageManager
+import cm.aptoide.pt.aptoide_network.data.network.GetAcceptLanguage
 import com.aptoide.android.aptoidegames.BuildConfig
 import com.aptoide.android.aptoidegames.network.repository.IdsRepository
 import cm.aptoide.pt.aptoide_network.data.network.GetUserAgent
@@ -8,16 +10,18 @@ import cm.aptoide.pt.environment_info.DeviceInfo
 import cm.aptoide.pt.extensions.calculateMD5
 import cm.aptoide.pt.extensions.getPackageInfo
 import com.appcoins.payments.network.RestClientInjectParams
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AptoideGetHeaders @Inject constructor(
+  @ApplicationContext private val context: Context,
   private val packageManager: PackageManager,
   private val idsRepository: IdsRepository,
   private val deviceInfo: DeviceInfo,
-) : GetUserAgent, RestClientInjectParams {
+) : GetUserAgent, GetAcceptLanguage, RestClientInjectParams {
 
   private val versionName = BuildConfig.VERSION_NAME
   private val aptoidePackage = BuildConfig.APPLICATION_ID
@@ -45,7 +49,10 @@ class AptoideGetHeaders @Inject constructor(
       "${deviceInfo.getScreenDimensions()};" +
       "${idsRepository.aptoideClientUuid})"
 
-  override fun getUserAgent(): String = invoke()
+  override fun getAcceptLanguage(): String {
+    val locale = context.resources.configuration.locales[0]
+    return "${locale.language}-${locale.country}, ${locale.language};q=0.9, *;q=0.7"
+  }
 
   override val channel: String = "aptoide-games"
 }
--- app/src/main/java/cm/aptoide/pt/network/AptoideGetHeaders.kt
+++ app/src/main/java/cm/aptoide/pt/network/AptoideGetHeaders.kt
@@ -1,22 +1,26 @@
 package cm.aptoide.pt.network
 
+import android.content.Context
 import android.content.pm.PackageManager
 import cm.aptoide.pt.BuildConfig
+import cm.aptoide.pt.aptoide_network.data.network.GetAcceptLanguage
 import cm.aptoide.pt.aptoide_network.data.network.GetUserAgent
 import cm.aptoide.pt.environment_info.DeviceInfo
 import cm.aptoide.pt.extensions.calculateMD5
 import cm.aptoide.pt.extensions.getPackageInfo
 import cm.aptoide.pt.network.repository.IdsRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AptoideGetHeaders @Inject constructor(
+  @ApplicationContext private val context: Context,
   private val packageManager: PackageManager,
   private val idsRepository: IdsRepository,
   private val deviceInfo: DeviceInfo,
-) : GetUserAgent {
+) : GetUserAgent, GetAcceptLanguage {
 
   private val versionName = BuildConfig.VERSION_NAME
   private val aptoidePackage = BuildConfig.APPLICATION_ID
@@ -43,4 +47,9 @@ class AptoideGetHeaders @Inject constructor(
       "${cachedMd5}; " +
       "${deviceInfo.getScreenDimensions()};" +
       "${idsRepository.aptoideClientUuid})"
+
+  override fun getAcceptLanguage(): String {
+    val locale = context.resources.configuration.locales[0]
+    return "${locale.language}-${locale.country}, ${locale.language};q=0.9, *;q=0.7"
+  }
 }
