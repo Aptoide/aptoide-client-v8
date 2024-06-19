ae27bccacf0e98ca83e5be9ce80d949f4277a628
Culurio <claudiohcx@gmail.com>
2024-06-19 10:54:28 +0100

    [APP-2578] Add contact support screen

--- app-games/src/main/java/com/aptoide/android/aptoidegames/SupportActivity.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/SupportActivity.kt
@@ -24,27 +24,47 @@ class SupportActivity : AppCompatActivity() {
   @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    val type = intent.getStringExtra(TYPE)
     setContent {
       val themeViewModel = hiltViewModel<AppThemeViewModel>()
       val isDarkTheme by themeViewModel.uiState.collectAsState()
       AptoideTheme(darkTheme = isDarkTheme ?: isSystemInDarkTheme()) {
         val deviceInfo = rememberDeviceInfo()
         val localContext = LocalContext.current
-        SupportView(
-          title = stringResource(R.string.settings_feedback_title),
-          placeholderText = stringResource(R.string.settings_send_feedback_body),
-          deviceInfo = deviceInfo,
-          email = "aptoide.games@aptoide.zendesk.com",
-          context = localContext,
-          subject = stringResource(R.string.app_info_send_email_subject),
-          navigateBack = ::finish
-        )
+        when (type) {
+          TYPE_SUPPORT -> SupportView(
+            title = "Contact Support", //TODO
+            placeholderText = "Please give us all the details: \n- When did the problem started\n- On which game are you making a purchase\n\nOnce you tap on Send, your mail app will open.\nFeel free to attach a screenshot or video about the issue!", //TODO
+            deviceInfo = deviceInfo,
+            email = "aptoide.games@appcoins.io",
+            context = localContext,
+            subject = "AG Contact Support", //TODO
+            navigateBack = ::finish
+          )
+
+          TYPE_FEEDBACK -> SupportView(
+            title = stringResource(R.string.settings_feedback_title),
+            placeholderText = stringResource(R.string.settings_send_feedback_body),
+            deviceInfo = deviceInfo,
+            email = "aptoide.games@aptoide.zendesk.com",
+            context = localContext,
+            subject = stringResource(R.string.app_info_send_email_subject),
+            navigateBack = ::finish
+          )
+        }
       }
     }
   }
 
   companion object {
-    fun open(
+    const val TYPE = "type"
+    const val TYPE_FEEDBACK = "feedback"
+    const val TYPE_SUPPORT = "support"
+
+    fun openForFeedBack(context: Context) = open(context, TYPE_FEEDBACK)
+
+    fun openForSupport(context: Context) = open(context, TYPE_SUPPORT)
+    private fun open(
       context: Context,
       type: String,
     ) {
