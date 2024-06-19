5be165f1d319b738276513586ab379f92e60c2de
Ricardo Frango <ricardo.frango@aptoide.com>
2024-06-19 17:57:49 +0100

    [DT-1293] Add missing Singleton annotation

--- aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/UserAgentInterceptor.kt
+++ aptoide-network/src/main/java/cm/aptoide/pt/aptoide_network/data/network/UserAgentInterceptor.kt
@@ -3,7 +3,9 @@ package cm.aptoide.pt.aptoide_network.data.network
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class UserAgentInterceptor @Inject constructor(private val provider: GetUserAgent) :
   Interceptor {
 
