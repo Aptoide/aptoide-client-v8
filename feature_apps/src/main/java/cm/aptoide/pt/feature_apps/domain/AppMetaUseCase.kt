d847ceb581e18d87aa8514dee0f1e6153133f184
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-06-26 17:25:58 +0100

    [APP-2543] Make AppMetaUseCase a singleton

--- feature_apps/src/main/java/cm/aptoide/pt/feature_apps/domain/AppMetaUseCase.kt
+++ feature_apps/src/main/java/cm/aptoide/pt/feature_apps/domain/AppMetaUseCase.kt
@@ -3,7 +3,9 @@ package cm.aptoide.pt.feature_apps.domain
 import cm.aptoide.pt.feature_apps.data.App
 import cm.aptoide.pt.feature_apps.data.AppsRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AppMetaUseCase @Inject constructor(private val appsRepository: AppsRepository) {
 
   suspend fun getMetaInfo(packageName: String): App =
