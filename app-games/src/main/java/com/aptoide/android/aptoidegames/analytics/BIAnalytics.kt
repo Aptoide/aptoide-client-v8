cfef209f31a476bca55408dad50ea2be0a380f58
Sergei Mitrofanov <goretz.m@gmail.com>
2024-07-02 12:34:50 +0100

    [APP-2564] Refactor BIAnalytics setup function
    To make BIAnalytics easier to preview and to encapsulate dependencies
    used only once during the setup process.

--- app-games/src/main/java/com/aptoide/android/aptoidegames/analytics/BIAnalytics.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/analytics/BIAnalytics.kt
@@ -3,6 +3,7 @@ package com.aptoide.android.aptoidegames.analytics
 import android.content.Context
 import android.os.Build
 import android.os.Build.VERSION
+import cm.aptoide.pt.feature_categories.analytics.AptoideAnalyticsInfoProvider
 import cm.aptoide.pt.install_manager.InstallManager
 import com.aptoide.android.aptoidegames.BuildConfig
 import com.aptoide.android.aptoidegames.launch.AppLaunchPreferencesManager
@@ -13,82 +14,56 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class BIAnalytics(
-  private val installManager: InstallManager,
-  private val appLaunchPreferencesManager: AppLaunchPreferencesManager,
-  private val context: Context,
-) {
+class BIAnalytics {
 
-  fun init() {
-    Indicative.addProperties(getBaseIndicativeProperties())
-    updateGamesHubInstalled()
-    updateAptoideVanillaInstalled()
-    updateWalletInstalled()
-    handleFirstLaunch()
-    handleGMS()
-    handleLoggedIn()
-    handleTheme()
-  }
-
-  private fun getBaseIndicativeProperties(): Map<String, Any> {
-    return mapOf(
-      "android_api_level" to VERSION.SDK_INT,
-      "aptoide_version_code" to BuildConfig.VERSION_CODE,
-      "android_brand" to Build.MANUFACTURER,
-      "android_model" to Build.MODEL,
-      "aptoide_package" to BuildConfig.APPLICATION_ID
-    )
-  }
-
-  private fun updateGamesHubInstalled() {
-    installManager
-      .getApp("com.dti.folderlauncher")
-      .packageInfoFlow
-      .map { it != null }
-      .onEach { Indicative.addProperty("is_gh_installed", it) }
-      .launchIn(CoroutineScope(Dispatchers.IO))
-  }
-
-  private fun updateAptoideVanillaInstalled() {
-    installManager
-      .getApp("cm.aptoide.pt")
-      .packageInfoFlow
-      .map { it != null }
-      .onEach { Indicative.addProperty("is_vanilla_installed", it) }
-      .launchIn(CoroutineScope(Dispatchers.IO))
-  }
-
-  private fun updateWalletInstalled() {
-    installManager
-      .getApp("com.appcoins.wallet")
-      .packageInfoFlow
-      .map { it != null }
-      .onEach {
-        Indicative.addProperty("is_wallet_app_installed", it)
+  fun setup(
+    context: Context,
+    installManager: InstallManager,
+    analyticsInfoProvider: AptoideAnalyticsInfoProvider,
+    appLaunchPreferencesManager: AppLaunchPreferencesManager,
+  ) {
+    CoroutineScope(Dispatchers.IO).launch {
+      withContext(Dispatchers.Main) {
+        Indicative.launch(context, BuildConfig.INDICATIVE_KEY)
+        Indicative.setUniqueID(analyticsInfoProvider.getAnalyticsId())
       }
-      .launchIn(CoroutineScope(Dispatchers.IO))
-  }
-
-  private fun handleFirstLaunch() {
-    CoroutineScope(Dispatchers.Main).launch {
+      Indicative.addProperties(
+        mapOf(
+          "android_api_level" to VERSION.SDK_INT,
+          "aptoide_version_code" to BuildConfig.VERSION_CODE,
+          "android_brand" to Build.MANUFACTURER,
+          "android_model" to Build.MODEL,
+          "aptoide_package" to BuildConfig.APPLICATION_ID,
+          "theme" to "dark",
+          "logged_in" to "NA",
+          "gms" to context.getGMSValue()
+        )
+      )
+      installManager
+        .getApp("com.dti.folderlauncher")
+        .packageInfoFlow
+        .map { it != null }
+        .onEach { Indicative.addProperty("is_gh_installed", it) }
+        .launchIn(this)
+      installManager
+        .getApp("cm.aptoide.pt")
+        .packageInfoFlow
+        .map { it != null }
+        .onEach { Indicative.addProperty("is_vanilla_installed", it) }
+        .launchIn(this)
+      installManager
+        .getApp("com.appcoins.wallet")
+        .packageInfoFlow
+        .map { it != null }
+        .onEach { Indicative.addProperty("is_wallet_app_installed", it) }
+        .launchIn(this)
       val isFirstLaunch = appLaunchPreferencesManager.isFirstLaunch()
       if (isFirstLaunch) {
         appLaunchPreferencesManager.setIsNotFirstLaunch()
       }
       Indicative.addProperty("first_session", isFirstLaunch)
     }
-  }
-
-  private fun handleTheme() {
-    Indicative.addProperty("theme", "dark")
-  }
-
-  private fun handleLoggedIn() {
-    Indicative.addProperty("logged_in", "NA")
-  }
-
-  private fun handleGMS() {
-    Indicative.addProperty("gms", context.getGMSValue())
   }
 }
