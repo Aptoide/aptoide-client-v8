164b2f809d7ec8f9c72ffa2471b73e5385227ca3
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-07-02 16:07:44 +0100

    [APP-2614] Add apkfy flow to aptoide games

--- app-games/src/main/java/com/aptoide/android/aptoidegames/AptoideGamesBottomSheet.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/AptoideGamesBottomSheet.kt
@@ -1,5 +1,11 @@
 package com.aptoide.android.aptoidegames
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
@@ -11,8 +17,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.aptoide.android.aptoidegames.theme.Palette
 import kotlinx.coroutines.launch
+
+interface BottomSheetContent {
+  @Composable
+  fun Draw(
+    dismiss: () -> Unit,
+    navigate: (String) -> Unit,
+  )
+}
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -45,6 +64,8 @@ fun AptoideGamesBottomSheet(
   ModalBottomSheetLayout(
     sheetState = sheetState,
     scrimColor = Color.Black.copy(alpha = 0.60f),
+    sheetBackgroundColor = Palette.Black,
+    sheetElevation = 0.dp,
     sheetContent = {
       bottomSheetContent?.Draw(
         dismiss = onCloseBottomSheetClick,
@@ -62,10 +83,19 @@ fun AptoideGamesBottomSheet(
   )
 }
 
-interface BottomSheetContent {
-  @Composable
-  fun Draw(
-    dismiss: () -> Unit,
-    navigate: (String) -> Unit,
-  )
+@Composable
+fun BottomSheetHeader() {
+  Box(
+    modifier = Modifier
+      .fillMaxWidth()
+      .height(36.dp),
+    contentAlignment = Alignment.Center
+  ) {
+    Box(
+      modifier = Modifier
+        .size(width = 32.dp, height = 4.dp)
+        .clip(RoundedCornerShape(100.dp))
+        .background(Palette.Grey)
+    )
+  }
 }
---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/ApkfyBottomSheetContent.kt
@@ -0,0 +1,46 @@
+package com.aptoide.android.aptoidegames.apkfy
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import cm.aptoide.pt.feature_apps.data.App
+import com.aptoide.android.aptoidegames.BottomSheetContent
+import com.aptoide.android.aptoidegames.BottomSheetHeader
+import com.aptoide.android.aptoidegames.R
+import com.aptoide.android.aptoidegames.appview.buildAppViewRoute
+import com.aptoide.android.aptoidegames.feature_apps.presentation.AppItem
+import com.aptoide.android.aptoidegames.installer.presentation.InstallViewShort
+import com.aptoide.android.aptoidegames.theme.AGTypography
+import com.aptoide.android.aptoidegames.theme.Palette
+
+class ApkfyBottomSheetContent(private val app: App) : BottomSheetContent {
+  @Composable override fun Draw(
+    dismiss: () -> Unit,
+    navigate: (String) -> Unit,
+  ) {
+    Column(
+      modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 50.dp)
+    ) {
+      BottomSheetHeader()
+      Text(
+        modifier = Modifier.padding(top = 11.dp, bottom = 22.dp),
+        text = stringResource(id = R.string.apkfy_install_title),
+        color = Palette.White,
+        style = AGTypography.Title
+      )
+      AppItem(
+        app = app,
+        onClick = {
+          navigate(buildAppViewRoute(app.packageName))
+          dismiss()
+        }
+      ) {
+        InstallViewShort(app)
+      }
+    }
+  }
+}
