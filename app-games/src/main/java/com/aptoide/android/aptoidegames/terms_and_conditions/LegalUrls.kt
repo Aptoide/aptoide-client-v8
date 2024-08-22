bc14d6e7680fea214d1269c0934d585d797c5ce8
GuiFPires <irespi1@hotmail.com>
2024-08-22 14:32:10 +0100

    [APP-2831] - Implement aptoide.com/legal correct redirect logic

--- app-games/src/main/java/com/aptoide/android/aptoidegames/terms_and_conditions/LegalUrls.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/terms_and_conditions/LegalUrls.kt
@@ -1,22 +1,9 @@
 package com.aptoide.android.aptoidegames.terms_and_conditions
 
-import android.content.Context
-import android.net.Uri
 import com.aptoide.android.aptoidegames.BuildConfig
 
-val Context.tcUrl get() = BuildConfig.TC_URL.addLanguage(this)
+const val tcUrl = BuildConfig.TC_URL
 
-val Context.btUrl get() = BuildConfig.BT_URL.addLanguage(this)
+const val btUrl = BuildConfig.BT_URL
 
-val Context.ppUrl get() = BuildConfig.PP_URL.addLanguage(this)
-
-val paymentsTermsOfUseUrl get() = ""
-
-private fun String.addLanguage(context: Context): String {
-  val resources = context.resources
-  return Uri.parse(this).buildUpon().appendQueryParameter(
-    "lang", resources.configuration.locales.get(0).language
-      + "-"
-      + resources.configuration.locales.get(0).country
-  ).build().toString()
-}
+const val ppUrl = BuildConfig.PP_URL
