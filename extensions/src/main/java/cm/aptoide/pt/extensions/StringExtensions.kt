dd9558e549a235ef8390215fd65dc840d081ea15
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-08-27 16:47:29 +0100

    [APP-2512] Extract text span functions to extensions module

--- extensions/src/main/java/cm/aptoide/pt/extensions/StringExtensions.kt
+++ extensions/src/main/java/cm/aptoide/pt/extensions/StringExtensions.kt
@@ -1,5 +1,10 @@
 package cm.aptoide.pt.extensions
 
+import android.text.Spannable
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
+import androidx.core.text.HtmlCompat
+import androidx.core.text.getSpans
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -15,3 +20,38 @@ fun String.isYoutubeURL(): Boolean {
   val pattern = "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(\\.com)?/.+"
   return matches(Regex(pattern))
 }
+
+fun String.getTextSpans(): Pair<Spanned, List<TextSpan>> {
+  val textSpans = mutableListOf<TextSpan>()
+  val htmlText =
+    HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY, null) { opening, tag, output, _ ->
+      if (tag.matches(Regex("_\\d+"))) {
+        val len: Int = output.length
+        if (opening) {
+          output.setSpan(StrikethroughSpan(), len, len, Spannable.SPAN_MARK_MARK)
+        } else {
+          val obj = output.getSpans<StrikethroughSpan>(0, output.length)
+            .lastOrNull { output.getSpanFlags(it) == Spannable.SPAN_MARK_MARK }
+          val where = output.getSpanStart(obj)
+          textSpans.add(
+            TextSpan(
+              pos = tag.removePrefix("_").toInt(),
+              start = where,
+              end = len,
+              text = output.substring(where, len)
+            )
+          )
+          output.removeSpan(obj)
+        }
+      }
+    }
+
+  return htmlText to textSpans
+}
+
+class TextSpan(
+  val pos: Int,
+  val start: Int,
+  val end: Int,
+  val text: String,
+)
