0fb5144f8f4f80d03cd9614a9456517897eeb4ef
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-10-09 15:04:30 +0100

    [APP-3029] Update version name to 0.9.1

--- app-games/build.gradle.kts
+++ app-games/build.gradle.kts
@@ -18,7 +18,7 @@ android {
   defaultConfig {
     applicationId = "com.aptoide.android.aptoidegames"
     versionCode = Integer.parseInt(project.property("VERSION_CODE_APTOIDEGAMES").toString())
-    versionName = "0.9.0"
+    versionName = "0.9.1"
 
     System.getenv("STORE_NAME")
       .also {
