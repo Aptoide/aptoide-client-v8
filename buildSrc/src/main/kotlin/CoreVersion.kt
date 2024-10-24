549acedb7a6f3c0104bcdf6351d1efedc8c1b520
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-10-24 19:57:52 +0100

    [AND-7] Update core dependencies versions

--- buildSrc/src/main/kotlin/CoreVersion.kt
+++ buildSrc/src/main/kotlin/CoreVersion.kt
@@ -1,5 +1,5 @@
 object CoreVersion {
-  const val KOTLIN = "2.0.20"
-  const val COROUTINES = "1.8.1"
+  const val KOTLIN = "2.0.21"
+  const val COROUTINES = "1.9.0"
   const val NAVIGATION = "2.5.3"
 }
