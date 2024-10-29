6c55b43389ecaee4cbd6d987d6ddfa0cb8c761cd
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-10-29 13:09:26 +0000

    [AND-82] Refactor bottom bar icon creation

--- app-games/src/main/java/com/aptoide/android/aptoidegames/home/BottomBarMenus.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/home/BottomBarMenus.kt
@@ -1,5 +1,7 @@
 package com.aptoide.android.aptoidegames.home
 
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.aptoide.android.aptoidegames.R
 import com.aptoide.android.aptoidegames.analytics.dto.BundleMeta
@@ -14,25 +16,34 @@ import com.aptoide.android.aptoidegames.theme.Palette
 sealed class BottomBarMenus(
   val route: String,
   val titleId: Int,
-  val icon: ImageVector,
 ) {
   object Games : BottomBarMenus(
     route = gamesRoute,
-    titleId = R.string.bottom_navigation_discovery,
-    icon = getDiscovery(Palette.GreyLight)
+    titleId = R.string.bottom_navigation_discovery
   )
 
   object Search : BottomBarMenus(
     route = searchRoute
       .withBundleMeta(BundleMeta("search", "app")),
-    titleId = R.string.search,
-    icon = getSearch(Palette.GreyLight)
+    titleId = R.string.search
   )
 
   object Categories : BottomBarMenus(
     route = buildAllCategoriesRoute()
       .withBundleMeta(BundleMeta("categories-more", "app")),
-    titleId = R.string.categories,
-    icon = getCategories(Palette.GreyLight)
+    titleId = R.string.categories
   )
 }
+
+@Composable
+fun BottomBarMenus.Icon() = when (this) {
+  BottomBarMenus.Games -> getDiscovery(Palette.GreyLight).AsBottomBarIcon()
+  BottomBarMenus.Search -> getSearch(Palette.GreyLight).AsBottomBarIcon()
+  BottomBarMenus.Categories -> getCategories(Palette.GreyLight).AsBottomBarIcon()
+}
+
+@Composable
+private fun ImageVector.AsBottomBarIcon() = Icon(
+  imageVector = this,
+  contentDescription = null,
+)
