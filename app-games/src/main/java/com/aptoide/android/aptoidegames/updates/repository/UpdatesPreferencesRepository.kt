fc31870de9b6d587b541171f1aab30ad9f0b7d1c
phillipsgoncalves <filisgoncalves@gmail.com>
2024-11-14 12:38:43 +0000

    [AND-146] Show Auto Update Games toggle in Settings

---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/updates/repository/UpdatesPreferencesRepository.kt
@@ -0,0 +1,32 @@
+package com.aptoide.android.aptoidegames.updates.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import com.aptoide.android.aptoidegames.di.UpdatesPreferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UpdatesPreferencesRepository @Inject constructor(
+  @UpdatesPreferencesDataStore private val dataStore: DataStore<Preferences>,
+) {
+
+  companion object PreferencesKeys {
+    private val AUTO_UPDATE_GAMES = booleanPreferencesKey("auto_update_games")
+  }
+
+  suspend fun setAutoUpdateGames(shouldAutoUpdateGames: Boolean) {
+    dataStore.edit { bundlePreferences ->
+      bundlePreferences[AUTO_UPDATE_GAMES] = shouldAutoUpdateGames
+    }
+  }
+
+  fun shouldAutoUpdateGames(): Flow<Boolean> =
+    dataStore.data.map { preferences ->
+      preferences[AUTO_UPDATE_GAMES] ?: true
+    }
+}
