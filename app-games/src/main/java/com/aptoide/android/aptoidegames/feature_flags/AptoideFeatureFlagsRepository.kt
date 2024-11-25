b34fb3b9fc126f37219b80f25e3564488dbd71bb
Pedro Agostinho <pedro.agostinho@aptoide.com>
2024-11-25 15:20:25 +0000

    [AND-163] Add firebase remote config

--- app-games/src/main/java/com/aptoide/android/aptoidegames/feature_flags/AptoideFeatureFlagsRepository.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/feature_flags/AptoideFeatureFlagsRepository.kt
@@ -1,8 +1,27 @@
 package com.aptoide.android.aptoidegames.feature_flags
 
+import cm.aptoide.pt.extensions.SuspendValue
 import cm.aptoide.pt.feature_flags.data.FeatureFlagsRepository
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 import org.json.JSONObject
 
 class AptoideFeatureFlagsRepository : FeatureFlagsRepository {
-  override suspend fun getFeatureFlags(): JSONObject = JSONObject()
+
+  // Used to signal about remote config results
+  private var result: SuspendValue<JSONObject> = SuspendValue()
+
+  override suspend fun getFeatureFlags(): JSONObject {
+    Firebase.remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+      val configFlags = JSONObject().apply {
+        Firebase.remoteConfig.all.forEach {
+          put(it.key, it.value.asString())
+        }
+      }
+
+      result.yield(configFlags)
+    }
+
+    return result.await()
+  }
 }
