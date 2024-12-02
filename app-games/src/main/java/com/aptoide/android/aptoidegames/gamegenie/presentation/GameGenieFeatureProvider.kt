bf3e01ca143145e74a169e2a8dedcbe04ed67afa
phillipsgoncalves <filisgoncalves@gmail.com>
2024-12-02 14:54:43 +0000

    [AND-168] Change bottom bar entries based on firebase remote flag

---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/GameGenieFeatureProvider.kt
@@ -0,0 +1,38 @@
+package com.aptoide.android.aptoidegames.gamegenie.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import cm.aptoide.pt.extensions.runPreviewable
+import cm.aptoide.pt.feature_flags.domain.FeatureFlags
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class InjectionsProvider @Inject constructor(
+  val featureFlags: FeatureFlags,
+) : ViewModel()
+
+@Composable
+fun rememberGameGenieVisibility(): Boolean = runPreviewable(
+  preview = { Random.nextBoolean() },
+  real = {
+    val coroutineScope = rememberCoroutineScope()
+    var state by remember { mutableStateOf(false) }
+    val vm = hiltViewModel<InjectionsProvider>()
+    LaunchedEffect(key1 = Unit) {
+      coroutineScope.launch {
+        state = vm.featureFlags.getFlag("show_game_genie", false)
+      }
+    }
+    state
+  }
+)
