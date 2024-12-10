addc4d5727d549c415d7322b6f9da9ecdf3561c4
Sergei Mitrofanov <goretz.m@gmail.com>
2024-12-10 16:37:23 +0000

    [AND-213] Add logging analytics to logcat for debug builds

--- app-games/src/main/java/com/aptoide/android/aptoidegames/analytics/FirebaseAnalyticsSender.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/analytics/FirebaseAnalyticsSender.kt
@@ -2,7 +2,9 @@ package com.aptoide.android.aptoidegames.analytics
 
 import androidx.annotation.Size
 import androidx.core.os.bundleOf
+import com.aptoide.android.aptoidegames.BuildConfig
 import com.google.firebase.analytics.FirebaseAnalytics
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,11 +13,22 @@ class FirebaseAnalyticsSender @Inject constructor(
   private val firebaseAnalytics: FirebaseAnalytics,
 ) : AnalyticsSender {
   override fun setUserProperties(vararg props: Pair<String, Any?>) = props.forEach {
+    if (BuildConfig.DEBUG) {
+      Timber.tag("GA").i("set UP  ${it.first} = ${it.second}")
+    }
     firebaseAnalytics.setUserProperty(it.first, it.second?.toString())
   }
 
   override fun logEvent(
     @Size(min = 1L, max = 40L) name: String,
     params: Map<String, Any?>?,
-  ) = firebaseAnalytics.logEvent(name, params?.let { bundleOf(*it.toList().toTypedArray()) })
+  ) {
+    if (BuildConfig.DEBUG) {
+      Timber.tag("GA").i(name)
+      params?.entries?.forEach {
+        Timber.tag("GA").i("  ${it.key}: ${it.value.toString()}")
+      }
+    }
+    firebaseAnalytics.logEvent(name, params?.let { bundleOf(*it.toList().toTypedArray()) })
+  }
 }
