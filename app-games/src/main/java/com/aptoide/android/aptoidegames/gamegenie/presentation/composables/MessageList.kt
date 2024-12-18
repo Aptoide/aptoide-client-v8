65aa3a59f6d3b57e250b7ef0c150051f20ab4f72
tomascarvalho7 <tomascarvalho7@gmail.com>
2024-12-18 11:08:36 +0000

    [AND-235] Fix last message being cut off on auto scroll

--- app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/composables/MessageList.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/composables/MessageList.kt
@@ -7,7 +7,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import com.aptoide.android.aptoidegames.gamegenie.domain.ChatInteraction
 
@@ -21,6 +27,13 @@ fun MessageList(
   onSuggestionClick: (String) -> Unit,
 ) {
   val listState = rememberLazyListState()
+  var lastItemHeight by remember { mutableStateOf(0) }
+
+  val scrollCallback = @Composable {
+    LaunchedEffect(lastItemHeight) {
+      listState.animateScrollToItem(index = messages.lastIndex, scrollOffset = lastItemHeight)
+    }
+  }
 
   LazyColumn(
     state = listState,
@@ -39,6 +52,10 @@ fun MessageList(
           apps = message.apps.map { app -> app.packageName },
           navigateTo = navigateTo,
           onAllAppsFail = onAllAppsFail,
+          scrollCallback = scrollCallback,
+          modifier = Modifier.onGloballyPositioned { coordinates ->
+            lastItemHeight = coordinates.size.height
+          }
         )
         suggestions.forEach { suggestion ->
           SuggestionBox(suggestion, onSuggestionClick)
@@ -49,6 +66,10 @@ fun MessageList(
           apps = message.apps.map { app -> app.packageName },
           navigateTo = navigateTo,
           onAllAppsFail = onAllAppsFail,
+          scrollCallback = scrollCallback,
+          modifier = Modifier.onGloballyPositioned { coordinates ->
+            lastItemHeight = coordinates.size.height
+          }
         )
       }
 
@@ -58,7 +79,8 @@ fun MessageList(
           isUserMessage = true,
           apps = emptyList(), // No apps for user messages
           navigateTo = navigateTo,
-          onAllAppsFail = onAllAppsFail
+          onAllAppsFail = onAllAppsFail,
+          scrollCallback = scrollCallback
         )
       }
     }
