e58d1836367996f946f67c6ec11fe5c3891ad3d9
Culurio <claudiohcx@gmail.com>
2025-01-07 16:09:24 +0000

    [AND-241] Only auto updates if the switch is on

--- feature_updates/src/main/java/cm/aptoide/pt/feature_updates/repository/UpdatesPreferencesRepository.kt
+++ feature_updates/src/main/java/cm/aptoide/pt/feature_updates/repository/UpdatesPreferencesRepository.kt
@@ -1,13 +1,10 @@
 package cm.aptoide.pt.feature_updates.repository
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
-import cm.aptoide.pt.feature_updates.data.AutoUpdateWorker
 import cm.aptoide.pt.feature_updates.di.UpdatesPreferencesDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -16,7 +13,6 @@ import javax.inject.Singleton
 @Singleton
 class UpdatesPreferencesRepository @Inject constructor(
   @UpdatesPreferencesDataStore private val dataStore: DataStore<Preferences>,
-  @ApplicationContext private val context: Context,
 ) {
 
   companion object PreferencesKeys {
@@ -26,11 +22,6 @@ class UpdatesPreferencesRepository @Inject constructor(
   suspend fun setAutoUpdateGames(shouldAutoUpdateGames: Boolean) {
     dataStore.edit { bundlePreferences ->
       bundlePreferences[AUTO_UPDATE_GAMES] = shouldAutoUpdateGames
-    }
-    if (shouldAutoUpdateGames) {
-      AutoUpdateWorker.enqueue(context)
-    } else {
-      AutoUpdateWorker.cancel(context)
     }
   }
 
