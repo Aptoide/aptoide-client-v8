783bf357347d5f52eb82b9c1f724f3e6fe60fe78
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-01-23 13:31:24 +0000

    [AND-295] Add ag_aptoide_mmp apkfy analytics event

--- app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/analytics/ApkfyManagerProbe.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/analytics/ApkfyManagerProbe.kt
@@ -4,6 +4,8 @@ import cm.aptoide.pt.feature_apkfy.domain.ApkfyManager
 import cm.aptoide.pt.feature_apkfy.domain.ApkfyModel
 import cm.aptoide.pt.feature_campaigns.AptoideMMPCampaign
 import com.aptoide.android.aptoidegames.IdsRepository
+import com.google.gson.Gson
+import retrofit2.HttpException
 
 class ApkfyManagerProbe(
   private val apkfyManager: ApkfyManager,
@@ -16,17 +18,30 @@ class ApkfyManagerProbe(
   }
 
   override suspend fun getApkfy(): ApkfyModel? {
-    if (idsRepository.getId(GUEST_UID_KEY).isEmpty()) {
-      return apkfyManager.getApkfy()
-        ?.also(apkfyAnalytics::setApkfyUTMProperties)
-        ?.also {
-          idsRepository.saveId(GUEST_UID_KEY, it.guestUid)
-        }.also {
-          // TODO: improve this logic
-          AptoideMMPCampaign.guestUID = it?.guestUid ?: idsRepository.getId(GUEST_UID_KEY)
-        }
-    } else {
-      AptoideMMPCampaign.guestUID = idsRepository.getId(GUEST_UID_KEY)
+    try {
+      if (idsRepository.getId(GUEST_UID_KEY).isEmpty()) {
+        return apkfyManager.getApkfy()
+          ?.also(apkfyAnalytics::setApkfyUTMProperties)
+          ?.also {
+            idsRepository.saveId(GUEST_UID_KEY, it.guestUid)
+          }
+          ?.also {
+            apkfyAnalytics.sendApkfySuccessEvent(data = Gson().toJson(it))
+          }
+          .also {
+            // TODO: improve this logic
+            AptoideMMPCampaign.guestUID = it?.guestUid ?: idsRepository.getId(GUEST_UID_KEY)
+          }
+      } else {
+        AptoideMMPCampaign.guestUID = idsRepository.getId(GUEST_UID_KEY)
+        return null
+      }
+    } catch (e: Throwable) {
+      apkfyAnalytics.sendApkfyFailEvent(
+        errorMessage = e.message,
+        errorType = e::class.simpleName,
+        errorCode = (e as? HttpException)?.code()
+      )
       return null
     }
   }
