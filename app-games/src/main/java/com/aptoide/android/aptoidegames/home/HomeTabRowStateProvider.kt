89267c0c766cb21791facb9143bf4288c1d8cc56
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-02-14 20:26:13 +0000

    [AND-299] Fetch home tabs from feature flags

--- app-games/src/main/java/com/aptoide/android/aptoidegames/home/HomeTabRowStateProvider.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/home/HomeTabRowStateProvider.kt
@@ -1,5 +1,6 @@
 package com.aptoide.android.aptoidegames.home
 
+import androidx.annotation.Keep
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,7 +11,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import cm.aptoide.pt.extensions.runPreviewable
 import cm.aptoide.pt.feature_flags.domain.FeatureFlags
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,16 +23,52 @@ class HomeTabRowInjectionsProvider @Inject constructor(
 ) : ViewModel()
 
 @Composable
-fun rememberHomeTabRowState(): Boolean = runPreviewable(
-  preview = { true },
+fun rememberHomeTabRowState(): Pair<Boolean, List<HomeTab>> = runPreviewable(
+  preview = { true to defaultHomeTabs },
   real = {
     val vm = hiltViewModel<HomeTabRowInjectionsProvider>()
     var showHomeTabRow by remember { mutableStateOf(false) }
 
+    var tabs: List<HomeTab>? by remember { mutableStateOf(null) }
+
     LaunchedEffect(Unit) {
       showHomeTabRow = vm.featureFlags.getFlag("show_home_tabs", false)
+
+      try {
+        val tabsJson = vm.featureFlags.getFlagAsString("home_tabs")
+        tabs = Gson().fromJson(tabsJson, FeatureFlagTabRow::class.java).tabs.mapNotNull {
+          when (it.id) {
+            "ForYou" -> HomeTab.ForYou
+
+            "TopCharts" -> runCatching {
+              Gson().fromJson(
+                it.details,
+                HomeTab.TopCharts::class.java
+              )
+            }.getOrDefault(HomeTab.TopCharts())
+
+            "Bonus" -> HomeTab.Bonus
+            "Editorial" -> HomeTab.Editorial
+            "Categories" -> HomeTab.Categories
+            else -> null
+          }
+        }
+      } catch (e: Throwable) {
+        Timber.e(e)
+      }
     }
 
-    showHomeTabRow
+    showHomeTabRow to (tabs.takeIf { !it.isNullOrEmpty() } ?: defaultHomeTabs)
   }
+)
+
+@Keep
+data class FeatureFlagTabRow(
+  val tabs: List<FeatureFlagTab>
+)
+
+@Keep
+data class FeatureFlagTab(
+  val id: String,
+  val details: JsonObject
 )
