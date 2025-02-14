1085e80402cfa1562304e04ac5090807428e3114
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-02-14 18:56:55 +0000

    [AND-299] Simplify AllCategories screen UI state

--- feature_categories/src/main/java/cm/aptoide/pt/feature_categories/presentation/AllCategoriesViewModel.kt
+++ feature_categories/src/main/java/cm/aptoide/pt/feature_categories/presentation/AllCategoriesViewModel.kt
@@ -1,14 +1,18 @@
 package cm.aptoide.pt.feature_categories.presentation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import cm.aptoide.pt.extensions.runPreviewable
 import cm.aptoide.pt.feature_categories.domain.CategoriesUseCase
-import cm.aptoide.pt.feature_categories.domain.Category
+import cm.aptoide.pt.feature_categories.domain.randomCategory
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -23,13 +27,13 @@ class AllCategoriesViewModel @Inject constructor(
 ) : ViewModel() {
 
   private val categoryBundleTag: String? = savedStateHandle.get("tag")
-  private val viewModelState = MutableStateFlow(MoreCategoriesViewModelState())
+  private val viewModelState = MutableStateFlow<AllCategoriesUiState>(AllCategoriesUiState.Loading)
 
-  val uiState = viewModelState.map { it.toUiState() }
+  val uiState = viewModelState
     .stateIn(
       viewModelScope,
       SharingStarted.Eagerly,
-      viewModelState.value.toUiState()
+      viewModelState.value
     )
 
   init {
@@ -38,38 +42,33 @@ class AllCategoriesViewModel @Inject constructor(
 
   fun reload() {
     viewModelScope.launch {
-      viewModelState.update { it.copy(type = AllCategoriesUiStateType.LOADING) }
+      viewModelState.update { AllCategoriesUiState.Loading }
       try {
         val categories = categoriesUseCase.getCategories(categoryBundleTag ?: "")
         viewModelState.update {
-          it.copy(
-            categoryList = categories,
-            type = AllCategoriesUiStateType.IDLE
-          )
+          AllCategoriesUiState.Idle(categories = categories)
         }
       } catch (e: Throwable) {
         Timber.w(e)
         viewModelState.update {
-          it.copy(
-            type = when (e) {
-              is IOException -> AllCategoriesUiStateType.NO_CONNECTION
-              else -> AllCategoriesUiStateType.ERROR
-            }
-          )
+          when (e) {
+            is IOException -> AllCategoriesUiState.NoConnection
+            else -> AllCategoriesUiState.Error
+          }
         }
       }
-
     }
   }
 }
 
-private data class MoreCategoriesViewModelState(
-  val categoryList: List<Category> = emptyList(),
-  val type: AllCategoriesUiStateType = AllCategoriesUiStateType.IDLE,
-) {
-  fun toUiState(): AllCategoriesUiState =
-    AllCategoriesUiState(
-      categoryList = categoryList,
-      type = type
-    )
-}
+@Composable
+fun rememberAllCategories(): Pair<AllCategoriesUiState, () -> Unit> = runPreviewable(
+  preview = {
+    AllCategoriesUiState.Idle(List((0..50).random()) { randomCategory }) to {}
+  }, real = {
+    val vm = hiltViewModel<AllCategoriesViewModel>()
+    val uiState by vm.uiState.collectAsState()
+
+    uiState to vm::reload
+  }
+)
