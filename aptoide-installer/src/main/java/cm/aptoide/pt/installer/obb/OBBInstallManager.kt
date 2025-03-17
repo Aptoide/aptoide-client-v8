3bad379243c9dca8f5a5d04e77c116cc5f973359
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-03-17 11:17:48 +0000

    [AND-291] Extract function to check specific process name

--- aptoide-installer/src/main/java/cm/aptoide/pt/installer/obb/OBBInstallManager.kt
+++ aptoide-installer/src/main/java/cm/aptoide/pt/installer/obb/OBBInstallManager.kt
@@ -1,9 +1,8 @@
 package cm.aptoide.pt.installer.obb
 
-import android.app.ActivityManager
-import android.app.Application.ACTIVITY_SERVICE
 import android.content.Context
 import android.os.Process
+import cm.aptoide.pt.extensions.getProcessName
 import cm.aptoide.pt.install_manager.App
 import cm.aptoide.pt.install_manager.InstallManager
 import cm.aptoide.pt.install_manager.dto.InstallPackageInfo
@@ -28,23 +27,11 @@ class OBBInstallManager(
     installManager.getMissingFreeSpaceFor(installPackageInfo)
 
   override suspend fun restore() {
-    if (getProcessName(
-        context,
+    if (context.getProcessName(
         Process.myPid()
       ) != "${context.packageName}:obbMoverProcess"
     ) {
       installManager.restore()
     }
   }
-}
-
-fun getProcessName(cxt: Context, pid: Int): String? {
-  val am = cxt.getSystemService(ACTIVITY_SERVICE) as ActivityManager
-  val runningApps = am.runningAppProcesses ?: return null
-  for (procInfo in runningApps) {
-    if (procInfo.pid == pid) {
-      return procInfo.processName
-    }
-  }
-  return null
 }
