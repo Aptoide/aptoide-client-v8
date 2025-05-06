ae1c5cb8195b27762c7c34e969fa010fc16d0917
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-05-06 15:27:43 +0100

    [AND-432] Create deticated APKFY screen UI

---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/presentation/ApkfyScreen.kt
@@ -0,0 +1,157 @@
+package com.aptoide.android.aptoidegames.apkfy.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import cm.aptoide.pt.extensions.PreviewDark
+import cm.aptoide.pt.extensions.ScreenData
+import cm.aptoide.pt.feature_apps.data.App
+import cm.aptoide.pt.feature_apps.data.randomApp
+import com.aptoide.android.aptoidegames.R
+import com.aptoide.android.aptoidegames.analytics.presentation.OverrideAnalyticsAPKFY
+import com.aptoide.android.aptoidegames.analytics.presentation.withAnalytics
+import com.aptoide.android.aptoidegames.appview.AppRatingAndDownloads
+import com.aptoide.android.aptoidegames.drawables.backgrounds.myiconpack.getApkfyAppIconBackground
+import com.aptoide.android.aptoidegames.error_views.GenericErrorView
+import com.aptoide.android.aptoidegames.installer.presentation.AppIconWProgress
+import com.aptoide.android.aptoidegames.theme.AGTypography
+import com.aptoide.android.aptoidegames.theme.AptoideTheme
+import com.aptoide.android.aptoidegames.theme.Palette
+import com.aptoide.android.aptoidegames.toolbar.AppGamesTopBar
+
+const val apkfyScreenRoute = "apkfy"
+
+fun apkfyScreen() = ScreenData.withAnalytics(
+  route = apkfyScreenRoute,
+  screenAnalyticsName = "Apkfy",
+) { _, navigate, navigateBack ->
+  val apkfyState = rememberApkfyState()
+
+  Column {
+    AppGamesTopBar(navigateBack = navigateBack, title = "")
+    apkfyState?.app?.let {
+      ApkfyScreen(
+        app = it,
+        navigate = navigate,
+      )
+    } ?: GenericErrorView(navigateBack)
+  }
+}
+
+@Composable
+fun ApkfyScreen(
+  app: App,
+  navigate: (String) -> Unit,
+) {
+  val apkfyAnalytics = rememberApkfyAnalytics()
+
+  LaunchedEffect(Unit) {
+    apkfyAnalytics.sendApkfyShown()
+  }
+
+  OverrideAnalyticsAPKFY(navigate) { navigateTo ->
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+        modifier = Modifier
+          .padding(horizontal = 40.dp)
+          .padding(top = 44.dp),
+        text = stringResource(id = R.string.apkfy_install_title),
+        style = AGTypography.Title,
+        color = Palette.White,
+        textAlign = TextAlign.Center
+      )
+      Spacer(modifier = Modifier.height(55.dp))
+      ApkfyAppInfo(app = app)
+      Spacer(modifier = Modifier.height(17.dp))
+      ApkfyInstallView(
+        modifier = Modifier
+          .padding(horizontal = 16.dp)
+          .padding(bottom = 32.dp),
+        app = app,
+        onInstallStarted = {}
+      )
+    }
+  }
+}
+
+@Composable
+private fun ApkfyAppInfo(
+  modifier: Modifier = Modifier,
+  app: App
+) {
+  Column(
+    modifier = modifier,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    Box(contentAlignment = Alignment.Center) {
+      Image(
+        imageVector = getApkfyAppIconBackground(),
+        contentDescription = null
+      )
+      AppIconWProgress(
+        app = app,
+        modifier = Modifier.size(88.dp),
+        contentDescription = null,
+      )
+    }
+    Column(
+      modifier = Modifier.padding(horizontal = 24.dp),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Text(
+        modifier = Modifier.padding(top = 30.dp),
+        text = app.name,
+        style = AGTypography.TitleGames,
+        color = Palette.White,
+        textAlign = TextAlign.Center,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis,
+      )
+      app.developerName?.let {
+        Text(
+          text = it,
+          style = AGTypography.SmallGames,
+          color = Palette.White,
+          textAlign = TextAlign.Center,
+          maxLines = 2,
+          overflow = TextOverflow.Ellipsis,
+        )
+      }
+      AppRatingAndDownloads(
+        rating = app.pRating,
+        downloads = app.pDownloads
+      )
+    }
+  }
+}
+
+@PreviewDark
+@Composable
+fun ApkfyScreenPreview() {
+  AptoideTheme {
+    ApkfyScreen(
+      app = randomApp,
+      navigate = {},
+    )
+  }
+}
