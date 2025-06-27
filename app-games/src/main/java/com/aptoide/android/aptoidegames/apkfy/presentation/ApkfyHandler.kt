434278ba6aa0528d10227926d2d23bc6da866bca
phillipsgoncalves <filisgoncalves@gmail.com>
2025-06-27 15:26:32 +0100

    [AND-490] Ask unknown sources at the start of apkfy flow

--- app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/presentation/ApkfyHandler.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/presentation/ApkfyHandler.kt
@@ -6,23 +6,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.aptoide.android.aptoidegames.permissions.InstallPermissionsViewModel
 
 @Composable
 fun ApkfyHandler(navigate: (String) -> Unit) {
   val apkfyState = rememberApkfyState()
   var apkfyShown by rememberSaveable { mutableStateOf(false) }
+  val installPermissionsViewModel = hiltViewModel<InstallPermissionsViewModel>()
+
 
   LaunchedEffect(apkfyState, apkfyShown) {
     if (apkfyState != null && !apkfyShown) {
+      installPermissionsViewModel.requestInstallPermissions()
       when (apkfyState) {
         is ApkfyUiState.Default,
-        is ApkfyUiState.Baseline -> navigate(apkfyScreenRoute)
+        is ApkfyUiState.Baseline -> {
+          navigate(apkfyScreenRoute)
+        }
 
-        is ApkfyUiState.VariantA -> navigate(detailedApkfyRoute)
+        is ApkfyUiState.VariantA -> {
+          navigate(detailedApkfyRoute)
+        }
 
-        is ApkfyUiState.RobloxBaseline -> navigate(detailedApkfyRoute)
+        is ApkfyUiState.RobloxBaseline -> {
+          navigate(detailedApkfyRoute)
+        }
 
-        is ApkfyUiState.RobloxVariantA -> navigate(robloxApkfyRoute)
+        is ApkfyUiState.RobloxVariantA -> {
+          navigate(robloxApkfyRoute)
+        }
       }
 
       apkfyShown = true
