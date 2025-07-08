8bd8b86d84b970b65444f7ee7fa69bc4062b3294
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-07-08 19:05:04 +0100

    [AND-482] Add play & earn header in home screen

--- app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
@@ -62,6 +62,7 @@ import com.aptoide.android.aptoidegames.mmp.WithUTM
 import com.aptoide.android.aptoidegames.notifications.NotificationsPermissionRequester
 import com.aptoide.android.aptoidegames.notifications.presentation.rememberNotificationsAnalytics
 import com.aptoide.android.aptoidegames.permissions.notifications.NotificationsPermissionViewModel
+import com.aptoide.android.aptoidegames.play_and_earn.presentation.home.PaEHomeLayout
 import com.aptoide.android.aptoidegames.promo_codes.PromoCodeBottomSheet
 import com.aptoide.android.aptoidegames.promo_codes.rememberPromoCodeApp
 import com.aptoide.android.aptoidegames.promotions.presentation.PromotionDialog
@@ -116,70 +117,71 @@ fun MainView(navController: NavHostController) {
       AptoideGamesBottomSheet(
         navigate = navigate
       ) { showBottomSheet ->
-        Scaffold(
-          snackbarHost = {
-            SnackbarHost(hostState = snackBarHostState) {
-              Popup {
-                Snackbar(
-                  snackbarData = it,
-                  modifier = Modifier
-                    .focusable(false)
-                    .clearAndSetSemantics {},
+        PaEHomeLayout(navController = navController) {
+          Scaffold(
+            snackbarHost = {
+              SnackbarHost(hostState = snackBarHostState) {
+                Popup {
+                  Snackbar(
+                    snackbarData = it,
+                    modifier = Modifier
+                      .focusable(false)
+                      .clearAndSetSemantics {},
+                  )
+                }
+              }
+            },
+            bottomBar = {
+              AppGamesBottomBar(navController = navController)
+            },
+            topBar = {
+              if (showTopBar) {
+                AppGamesToolBar(
+                  navigate = {
+                    if (currentRoute.value?.destination?.route != it) {
+                      navController.navigateTo(it)
+                    }
+                  },
+                  goBackHome
                 )
               }
             }
-          },
-          bottomBar = {
-            AppGamesBottomBar(navController = navController)
-          },
-          topBar = {
-            if (showTopBar) {
-              AppGamesToolBar(
-                navigate = {
-                  if (currentRoute.value?.destination?.route != it) {
-                    navController.navigateTo(it)
+          ) { padding ->
+            NotificationsPermissionWrapper(
+              showDialog = showNotificationsRationaleDialog,
+              onDismiss = { notificationsPermissionViewModel.dismissDialog() },
+              onPermissionResult = {}
+            )
+
+            PromotionDialog(navigate = navController::navigateTo)
+
+            Box(modifier = Modifier.padding(padding)) {
+              NavigationGraph(
+                navController,
+                showSnack = {
+                  coroutineScope.launch {
+                    snackBarHostState.showSnackbar(message = it)
                   }
                 },
-                goBackHome
+                showBottomSheet = showBottomSheet
               )
             }
-          }
-        ) { padding ->
+            ApkfyHandler(navigate = navController::navigateTo)
 
-          NotificationsPermissionWrapper(
-            showDialog = showNotificationsRationaleDialog,
-            onDismiss = { notificationsPermissionViewModel.dismissDialog() },
-            onPermissionResult = {}
-          )
-
-          PromotionDialog(navigate = navController::navigateTo)
-
-          Box(modifier = Modifier.padding(padding)) {
-            NavigationGraph(
-              navController,
-              showSnack = {
-                coroutineScope.launch {
-                  snackBarHostState.showSnackbar(message = it)
-                }
-              },
-              showBottomSheet = showBottomSheet
-            )
-          }
-          ApkfyHandler(navigate = navController::navigateTo)
-
-          LaunchedEffect(promoCodeApp) {
-            if (promoCodeApp != null) {
-              showBottomSheet(
-                PromoCodeBottomSheet(
-                  promoCode = promoCodeApp,
-                  showSnack = {
-                    coroutineScope.launch {
-                      snackBarHostState.showSnackbar(message = it)
+            LaunchedEffect(promoCodeApp) {
+              if (promoCodeApp != null) {
+                showBottomSheet(
+                  PromoCodeBottomSheet(
+                    promoCode = promoCodeApp,
+                    showSnack = {
+                      coroutineScope.launch {
+                        snackBarHostState.showSnackbar(message = it)
+                      }
                     }
-                  }
+                  )
                 )
-              )
-              clearPromoCode()
+                clearPromoCode()
+              }
             }
           }
         }
