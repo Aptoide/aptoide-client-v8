e264c7c9ff168b99e574734c0d68194d8311a93b
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-07-16 14:34:36 +0100

    [AND-468] Add play & earn colors

--- app-games/src/main/java/com/aptoide/android/aptoidegames/theme/Palette.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/theme/Palette.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.graphics.Color
 object Palette {
   val Primary = Color(0xFFC8ED4F)
   val Secondary = Color(0xFF913DD8)
+  val SecondaryLight = Color(0xFFCA8BFF)
   val Error = Color(0xFFFF0000)
 
   val Black = Color(0xFF1E1E26)
@@ -17,4 +18,17 @@ object Palette {
 
   val Trusted = Color(0xFF25B47E)
   val Official = Color(0xFF007AFF)
+
+  //Play and Earn
+  val Yellow50 = Color(0xFFFFEA04)
+  val Yellow100 = Color(0xFFFFC93E)
+  val Yellow150 = Color(0xFFD6A422)
+  val Yellow200 = Color(0xFF876311)
+  val Orange150 = Color(0xFFF58932)
+  val Orange200 = Color(0xFFC04D07)
+  val Blue50 = Color(0xFFE0ECFF)
+  val Blue100 = Color(0xFFBDC3EB)
+  val Blue150 = Color(0xFFB3CFFF)
+  val Blue200 = Color(0xFF676D89)
+  val Blue250 = Color(0xFF495A6D)
 }
