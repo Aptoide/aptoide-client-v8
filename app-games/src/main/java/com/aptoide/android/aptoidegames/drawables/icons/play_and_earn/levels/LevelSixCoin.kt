d9eb96153c4711b5998612ea3689bb8275023717
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-07-22 14:49:16 +0100

    [AND-485] Add level up resources

---
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/drawables/icons/play_and_earn/levels/LevelSixCoin.kt
@@ -0,0 +1,220 @@
+package com.aptoide.android.aptoidegames.drawables.icons.play_and_earn.levels
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.group
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+fun getLevelSixCoinIcon(): ImageVector = ImageVector.Builder(
+  name = "Levelsixcoin",
+  defaultWidth = 99.0.dp,
+  defaultHeight = 100.0.dp,
+  viewportWidth = 99.0f,
+  viewportHeight = 100.0f
+).apply {
+  group {
+    path(
+      fill = SolidColor(Color(0xFFF58932)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(49.5f, 99.28f)
+      curveTo(76.84f, 99.28f, 99.0f, 77.06f, 99.0f, 49.64f)
+      curveTo(99.0f, 22.22f, 76.84f, 0.0f, 49.5f, 0.0f)
+      curveTo(22.16f, 0.0f, 0.0f, 22.22f, 0.0f, 49.64f)
+      curveTo(0.0f, 77.06f, 22.16f, 99.28f, 49.5f, 99.28f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFFEC617)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(49.5f, 99.28f)
+      curveTo(76.84f, 99.28f, 99.0f, 77.06f, 99.0f, 49.64f)
+      curveTo(99.0f, 22.22f, 76.84f, 0.0f, 49.5f, 0.0f)
+      curveTo(22.16f, 0.0f, 0.0f, 22.22f, 0.0f, 49.64f)
+      curveTo(0.0f, 77.06f, 22.16f, 99.28f, 49.5f, 99.28f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFFEDC3E)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(9.3f, 20.69f)
+      lineTo(78.36f, 89.96f)
+      curveTo(84.45f, 85.57f, 89.51f, 79.84f, 93.09f, 73.18f)
+      lineTo(26.03f, 5.93f)
+      curveTo(19.39f, 9.52f, 13.68f, 14.59f, 9.3f, 20.7f)
+      verticalLineTo(20.69f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFFEDC3E)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(3.98f, 30.12f)
+      lineTo(68.97f, 95.29f)
+      curveTo(71.19f, 94.34f, 73.32f, 93.22f, 75.36f, 91.97f)
+      lineTo(7.29f, 23.71f)
+      curveTo(6.03f, 25.76f, 4.93f, 27.9f, 3.98f, 30.12f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFffffff)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(52.82f, 84.71f)
+      curveTo(71.36f, 84.71f, 86.39f, 69.64f, 86.39f, 51.05f)
+      curveTo(86.39f, 32.46f, 71.36f, 17.38f, 52.82f, 17.38f)
+      curveTo(34.28f, 17.38f, 19.25f, 32.46f, 19.25f, 51.05f)
+      curveTo(19.25f, 69.64f, 34.28f, 84.71f, 52.82f, 84.71f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFFEC617)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(50.3f, 82.19f)
+      curveTo(68.84f, 82.19f, 83.87f, 67.12f, 83.87f, 48.53f)
+      curveTo(83.87f, 29.93f, 68.84f, 14.86f, 50.3f, 14.86f)
+      curveTo(31.76f, 14.86f, 16.73f, 29.93f, 16.73f, 48.53f)
+      curveTo(16.73f, 67.12f, 31.76f, 82.19f, 50.3f, 82.19f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFA97F07)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(56.77f, 26.0f)
+      horizontalLineTo(58.0f)
+      verticalLineTo(32.74f)
+      horizontalLineTo(57.5f)
+      curveTo(55.27f, 32.74f, 53.29f, 33.07f, 51.55f, 33.73f)
+      curveTo(49.81f, 34.4f, 48.34f, 35.35f, 47.13f, 36.58f)
+      curveTo(45.94f, 37.79f, 45.02f, 39.25f, 44.38f, 40.97f)
+      curveTo(43.75f, 42.69f, 43.44f, 44.61f, 43.44f, 46.74f)
+      verticalLineTo(53.83f)
+      curveTo(43.44f, 55.36f, 43.58f, 56.7f, 43.88f, 57.88f)
+      curveTo(44.19f, 59.03f, 44.63f, 59.99f, 45.2f, 60.78f)
+      curveTo(45.78f, 61.54f, 46.46f, 62.11f, 47.25f, 62.5f)
+      curveTo(48.03f, 62.88f, 48.91f, 63.06f, 49.88f, 63.06f)
+      curveTo(50.8f, 63.06f, 51.63f, 62.87f, 52.37f, 62.47f)
+      curveTo(53.13f, 62.08f, 53.78f, 61.54f, 54.31f, 60.83f)
+      curveTo(54.85f, 60.11f, 55.26f, 59.27f, 55.54f, 58.31f)
+      curveTo(55.83f, 57.34f, 55.98f, 56.27f, 55.98f, 55.12f)
+      curveTo(55.98f, 53.97f, 55.83f, 52.9f, 55.54f, 51.93f)
+      curveTo(55.26f, 50.95f, 54.85f, 50.11f, 54.31f, 49.41f)
+      curveTo(53.76f, 48.69f, 53.1f, 48.13f, 52.31f, 47.74f)
+      curveTo(51.53f, 47.35f, 50.65f, 47.15f, 49.68f, 47.15f)
+      curveTo(48.35f, 47.15f, 47.18f, 47.46f, 46.16f, 48.09f)
+      curveTo(45.17f, 48.7f, 44.38f, 49.48f, 43.82f, 50.43f)
+      curveTo(43.25f, 51.39f, 42.95f, 52.4f, 42.91f, 53.45f)
+      lineTo(40.36f, 51.81f)
+      curveTo(40.42f, 50.31f, 40.73f, 48.88f, 41.3f, 47.53f)
+      curveTo(41.88f, 46.19f, 42.69f, 44.99f, 43.7f, 43.96f)
+      curveTo(44.74f, 42.9f, 45.98f, 42.08f, 47.42f, 41.5f)
+      curveTo(48.87f, 40.89f, 50.5f, 40.59f, 52.31f, 40.59f)
+      curveTo(54.31f, 40.59f, 56.05f, 40.97f, 57.56f, 41.73f)
+      curveTo(59.08f, 42.49f, 60.35f, 43.54f, 61.37f, 44.87f)
+      curveTo(62.38f, 46.2f, 63.14f, 47.73f, 63.65f, 49.47f)
+      curveTo(64.16f, 51.21f, 64.41f, 53.06f, 64.41f, 55.03f)
+      curveTo(64.41f, 57.1f, 64.07f, 59.03f, 63.39f, 60.8f)
+      curveTo(62.72f, 62.58f, 61.76f, 64.13f, 60.49f, 65.46f)
+      curveTo(59.24f, 66.79f, 57.73f, 67.83f, 55.98f, 68.57f)
+      curveTo(54.22f, 69.31f, 52.25f, 69.68f, 50.06f, 69.68f)
+      curveTo(47.79f, 69.68f, 45.73f, 69.27f, 43.88f, 68.45f)
+      curveTo(42.04f, 67.61f, 40.46f, 66.44f, 39.13f, 64.94f)
+      curveTo(37.8f, 63.43f, 36.78f, 61.64f, 36.05f, 59.57f)
+      curveTo(35.35f, 57.5f, 35.0f, 55.24f, 35.0f, 52.78f)
+      verticalLineTo(49.5f)
+      curveTo(35.0f, 46.06f, 35.5f, 42.91f, 36.49f, 40.06f)
+      curveTo(37.51f, 37.19f, 38.96f, 34.71f, 40.86f, 32.62f)
+      curveTo(42.75f, 30.51f, 45.04f, 28.88f, 47.71f, 27.73f)
+      curveTo(50.39f, 26.58f, 53.41f, 26.0f, 56.77f, 26.0f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFA97F07)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(82.0f, 48.53f)
+      curveTo(82.0f, 30.97f, 67.81f, 16.74f, 50.31f, 16.74f)
+      curveTo(32.8f, 16.74f, 18.61f, 30.97f, 18.61f, 48.53f)
+      curveTo(18.61f, 66.08f, 32.8f, 80.31f, 50.31f, 80.31f)
+      curveTo(67.81f, 80.31f, 82.0f, 66.08f, 82.0f, 48.53f)
+      close()
+      moveTo(85.75f, 48.53f)
+      curveTo(85.75f, 68.16f, 69.88f, 84.07f, 50.31f, 84.07f)
+      curveTo(30.73f, 84.07f, 14.86f, 68.16f, 14.86f, 48.53f)
+      curveTo(14.86f, 28.9f, 30.73f, 12.98f, 50.31f, 12.98f)
+      curveTo(69.88f, 12.98f, 85.75f, 28.9f, 85.75f, 48.53f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFffffff)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(78.15f, 68.76f)
+      lineTo(79.75f, 71.51f)
+      lineTo(82.49f, 73.12f)
+      curveTo(83.22f, 73.55f, 83.22f, 74.63f, 82.49f, 75.06f)
+      lineTo(79.75f, 76.67f)
+      lineTo(78.15f, 79.41f)
+      curveTo(77.72f, 80.15f, 76.64f, 80.15f, 76.21f, 79.41f)
+      lineTo(74.61f, 76.67f)
+      lineTo(71.87f, 75.06f)
+      curveTo(71.13f, 74.63f, 71.13f, 73.55f, 71.87f, 73.12f)
+      lineTo(74.61f, 71.51f)
+      lineTo(76.21f, 68.76f)
+      curveTo(76.64f, 68.03f, 77.72f, 68.03f, 78.15f, 68.76f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFffffff)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(34.78f, 18.55f)
+      lineTo(35.26f, 19.37f)
+      lineTo(36.08f, 19.86f)
+      curveTo(36.82f, 20.29f, 36.82f, 21.36f, 36.08f, 21.8f)
+      lineTo(35.26f, 22.28f)
+      lineTo(34.78f, 23.1f)
+      curveTo(34.35f, 23.84f, 33.28f, 23.84f, 32.85f, 23.1f)
+      lineTo(32.37f, 22.28f)
+      lineTo(31.55f, 21.8f)
+      curveTo(30.82f, 21.36f, 30.82f, 20.29f, 31.55f, 19.86f)
+      lineTo(32.37f, 19.37f)
+      lineTo(32.85f, 18.55f)
+      curveTo(33.28f, 17.82f, 34.35f, 17.82f, 34.78f, 18.55f)
+      close()
+    }
+    path(
+      fill = SolidColor(Color(0xFFffffff)), stroke = null, strokeLineWidth = 0.0f,
+      strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+      pathFillType = NonZero
+    ) {
+      moveTo(2.76f, 52.41f)
+      curveTo(2.76f, 24.99f, 24.92f, 2.77f, 52.26f, 2.77f)
+      curveTo(65.22f, 2.77f, 77.02f, 7.77f, 85.85f, 15.96f)
+      curveTo(76.81f, 6.14f, 63.87f, 0.0f, 49.5f, 0.0f)
+      curveTo(22.16f, 0.0f, 0.0f, 22.23f, 0.0f, 49.64f)
+      curveTo(0.0f, 64.05f, 6.13f, 77.02f, 15.91f, 86.09f)
+      curveTo(7.75f, 77.24f, 2.76f, 65.41f, 2.76f, 52.41f)
+      close()
+    }
+  }
+}.build()
