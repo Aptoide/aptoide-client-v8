f8fd413b68a6e3ce95c39842335996d5c8f397eb
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-07-22 15:05:36 +0100

    [AND-485] Add level up screen

--- app-games/build.gradle.kts
+++ app-games/build.gradle.kts
@@ -309,6 +309,7 @@ dependencies {
   implementation(projects.featureWallet.authorization)
   implementation(projects.featureWallet.walletInfo)
   implementation(projects.featureWallet.gamification)
+  implementation(projects.featureWallet.datastore)
 
   //payments
   implementation(projects.payments.sdk)
@@ -361,6 +362,8 @@ dependencies {
   implementation(libs.credentials.playServices)
   implementation(libs.googleId)
   implementation(libs.play.services.auth)
+
+  implementation(libs.constraintLayout.compose)
 }
 
 fun BaseFlavor.buildConfigFieldFromGradleProperty(gradlePropertyName: String) {
