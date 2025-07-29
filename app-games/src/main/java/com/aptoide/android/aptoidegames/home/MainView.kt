bbd62883b3150b8080b0eded6775ae2bb5277226
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-07-29 23:04:48 +0100

    [AND-485] Add play and earn badge to AG topbar

--- app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
@@ -127,7 +127,7 @@ fun MainView(navController: NavHostController) {
             if (showTopBar) {
               AppGamesToolBar(
                 navigate = {
-                  if (currentRoute.value?.destination?.route != it) {
+                  if (currentRoute.value?.destination?.route?.substringBefore("?") != it) {
                     navController.navigateTo(it)
                   }
                 },
