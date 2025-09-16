b5e406297f07374275e18c9fb96417bf456599cc
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-09-16 18:23:10 +0100

    [AND-532] Add play and earn campaigns data and business logic

---
+++ play-and-earn/campaigns/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.android.module)
+  alias(libs.plugins.composable)
+  alias(libs.plugins.hilt)
+}
+
+android {
+  namespace = "cm.aptoide.pt.play_and_earn.campaigns"
+}
+
+dependencies {
+  implementation(projects.aptoideNetwork)
+  implementation(projects.extension)
+  implementation(projects.featureApps)
+}
