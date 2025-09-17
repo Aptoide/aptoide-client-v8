f38d8c3529646297699b2dd4cfe9e55126f63b4e
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-09-17 11:16:30 +0100

    [AND-534] Add play and earn missions data and business logic

--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/DefaultPaECampaignsRepository.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/DefaultPaECampaignsRepository.kt
@@ -2,9 +2,19 @@ package cm.aptoide.pt.campaigns.data
 
 import cm.aptoide.pt.campaigns.data.model.PaEAppJson
 import cm.aptoide.pt.campaigns.data.model.PaECampaignJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionProgressJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionStatusJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionTypeJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionsJson
 import cm.aptoide.pt.campaigns.data.model.PaEProgressJson
 import cm.aptoide.pt.campaigns.domain.PaEBundle
 import cm.aptoide.pt.campaigns.domain.PaEBundles
+import cm.aptoide.pt.campaigns.domain.PaEMission
+import cm.aptoide.pt.campaigns.domain.PaEMissionProgress
+import cm.aptoide.pt.campaigns.domain.PaEMissionStatus
+import cm.aptoide.pt.campaigns.domain.PaEMissionType
+import cm.aptoide.pt.campaigns.domain.PaEMissions
 import cm.aptoide.pt.campaigns.domain.PaEProgress
 import cm.aptoide.pt.campaigns.domain.PaEApp
 import kotlinx.coroutines.CoroutineDispatcher
@@ -24,6 +34,15 @@ internal class DefaultPaECampaignsRepository @Inject constructor(
       Result.failure(e)
     }
   }
+
+  override suspend fun getCampaignMissions(packageName: String): Result<PaEMissions> =
+    withContext(dispatcher) {
+      try {
+        Result.success(paeCampaignsApi.getCampaignMissions(packageName).toDomainModel())
+      } catch (e: Throwable) {
+        Result.failure(e)
+      }
+    }
 }
 
 private fun PaECampaignJson.toDomainModel(): PaEBundles = PaEBundles(
@@ -56,3 +75,38 @@ private fun PaEProgressJson.toDomainModel() = PaEProgress(
   type = type,
   status = status
 )
+
+private fun PaEMissionsJson.toDomainModel() = PaEMissions(
+  checkpoints = checkpoints.map(PaEMissionJson::toDomainModel),
+  missions = missions.map(PaEMissionJson::toDomainModel)
+)
+
+private fun PaEMissionJson.toDomainModel() = PaEMission(
+  title = title,
+  description = description,
+  icon = icon,
+  type = this.getType(),
+  arguments = arguments,
+  units = units,
+  progress = progress?.toDomainModel()
+)
+
+private fun PaEMissionProgressJson.toDomainModel() = PaEMissionProgress(
+  current = current,
+  target = target,
+  type = type,
+  status = this.getType()
+)
+
+private fun PaEMissionJson.getType() = when (type) {
+  PaEMissionTypeJson.PLAY_TIME -> PaEMissionType.PLAY_TIME
+  PaEMissionTypeJson.STREAK -> PaEMissionType.STREAK
+  PaEMissionTypeJson.CHECKPOINT -> PaEMissionType.CHECKPOINT
+}
+
+private fun PaEMissionProgressJson.getType() = when (status) {
+  PaEMissionStatusJson.PENDING -> PaEMissionStatus.PENDING
+  PaEMissionStatusJson.ONGOING -> PaEMissionStatus.ONGOING
+  PaEMissionStatusJson.COMPLETED -> PaEMissionStatus.COMPLETED
+  null -> null
+}
--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/FakePaECampaignsRepository.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/FakePaECampaignsRepository.kt
@@ -1,8 +1,13 @@
 package cm.aptoide.pt.campaigns.data
 
+import cm.aptoide.pt.campaigns.domain.PaEMissions
 import cm.aptoide.pt.campaigns.domain.PaEBundles
 
+@Suppress("unused")
 internal class FakePaECampaignsRepository : PaECampaignsRepository {
   override suspend fun getCampaigns(): Result<PaEBundles> =
     Result.success(paeCampaigns)
+
+  override suspend fun getCampaignMissions(packageName: String): Result<PaEMissions> =
+    Result.success(paeMissions)
 }
