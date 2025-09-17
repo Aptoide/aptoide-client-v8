109c0ec229c5172da0728749a3d2d38a87a692e5
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-09-17 11:16:30 +0100

    [AND-534] Add play and earn missions data and business logic

--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsApi.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsApi.kt
@@ -1,7 +1,9 @@
 package cm.aptoide.pt.campaigns.data
 
 import cm.aptoide.pt.campaigns.data.model.PaECampaignJson
+import cm.aptoide.pt.campaigns.data.model.PaEMissionsJson
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 internal interface PaECampaignsApi {
@@ -10,4 +12,9 @@ internal interface PaECampaignsApi {
   suspend fun getCampaigns(
     @Query("sections") sections: List<String> = listOf("trending")
   ): PaECampaignJson
+
+  @GET("/play-and-earn/campaigns/{package_name}/missions")
+  suspend fun getCampaignMissions(
+    @Path("package_name") packageName: String
+  ): PaEMissionsJson
 }
