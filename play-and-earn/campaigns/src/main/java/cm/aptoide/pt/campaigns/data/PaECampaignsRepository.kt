1fe18c94ada4353d52fc525c6f1e110e733c96b3
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-09-17 11:16:30 +0100

    [AND-534] Add play and earn missions data and business logic

--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
@@ -1,8 +1,11 @@
 package cm.aptoide.pt.campaigns.data
 
+import cm.aptoide.pt.campaigns.domain.PaEMissions
 import cm.aptoide.pt.campaigns.domain.PaEBundles
 
 interface PaECampaignsRepository {
 
   suspend fun getCampaigns(): Result<PaEBundles>
+
+  suspend fun getCampaignMissions(packageName: String): Result<PaEMissions>
 }
---
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/domain/PaEMissions.kt
@@ -0,0 +1,37 @@
+package cm.aptoide.pt.campaigns.domain
+
+import com.google.gson.JsonObject
+
+data class PaEMissions(
+  val checkpoints: List<PaEMission>,
+  val missions: List<PaEMission>
+)
+
+data class PaEMission(
+  val title: String,
+  val description: String?,
+  val icon: String,
+  val type: PaEMissionType,
+  val arguments: JsonObject,
+  val units: Int,
+  val progress: PaEMissionProgress?
+)
+
+data class PaEMissionProgress(
+  val current: Int?,
+  val target: Int,
+  val type: String,
+  val status: PaEMissionStatus?
+)
+
+enum class PaEMissionType {
+  PLAY_TIME,
+  STREAK,
+  CHECKPOINT
+}
+
+enum class PaEMissionStatus {
+  PENDING,
+  ONGOING,
+  COMPLETED,
+}
