e83afe5ca383bb8d9138ad9c3ae86cc3c8401dcd
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-09-17 11:16:30 +0100

    [AND-534] Add play and earn missions data and business logic

---
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/model/PaEMissionsJson.kt
@@ -0,0 +1,43 @@
+package cm.aptoide.pt.campaigns.data.model
+
+import androidx.annotation.Keep
+import com.google.gson.JsonObject
+
+@Keep
+internal data class PaEMissionsJson(
+  val checkpoints: List<PaEMissionJson>,
+  val missions: List<PaEMissionJson>
+)
+
+@Keep
+internal data class PaEMissionJson(
+  val title: String,
+  val description: String?,
+  val icon: String,
+  val type: PaEMissionTypeJson,
+  val arguments: JsonObject,
+  val units: Int,
+  val progress: PaEMissionProgressJson?
+)
+
+@Keep
+internal data class PaEMissionProgressJson(
+  val current: Int?,
+  val target: Int,
+  val type: String,
+  val status: PaEMissionStatusJson?
+)
+
+@Keep
+internal enum class PaEMissionTypeJson {
+  PLAY_TIME,
+  STREAK,
+  CHECKPOINT
+}
+
+@Keep
+internal enum class PaEMissionStatusJson {
+  PENDING,
+  ONGOING,
+  COMPLETED,
+}
