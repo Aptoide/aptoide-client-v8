77ce125d65798a708e07719420bfa7e96a156635
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-10-10 03:27:34 +0100

    [AND-530] Add wallet refresh api

--- feature-wallet/authorization/src/main/java/cm/aptoide/pt/wallet/authorization/di/RepositoryModule.kt
+++ feature-wallet/authorization/src/main/java/cm/aptoide/pt/wallet/authorization/di/RepositoryModule.kt
@@ -1,12 +1,14 @@
 package cm.aptoide.pt.wallet.authorization.di
 
 import android.content.Context
+import cm.aptoide.pt.aptoide_network.di.ApiChainCatappultDomain
 import cm.aptoide.pt.aptoide_network.di.RawOkHttp
 import cm.aptoide.pt.aptoide_network.di.WebServicesDomain
 import cm.aptoide.pt.wallet.authorization.data.UserAuthApi
 import cm.aptoide.pt.wallet.authorization.data.UserWalletAuthDataStore
 import cm.aptoide.pt.wallet.authorization.data.WalletAuthRepository
 import cm.aptoide.pt.wallet.authorization.data.WalletAuthRepositoryImpl
+import cm.aptoide.pt.wallet.authorization.data.WalletRefreshApi
 import cm.aptoide.pt.wallet.datastore.WalletCoreDataSource
 import dagger.Module
 import dagger.Provides
@@ -37,17 +39,33 @@ internal class RepositoryModule {
       .create(UserAuthApi::class.java)
   }
 
+  @Provides
+  @Singleton
+  fun provideWalletRefreshApi(
+    @RawOkHttp okHttpClient: OkHttpClient,
+    @ApiChainCatappultDomain domain: String,
+  ): WalletRefreshApi {
+    return Retrofit.Builder()
+      .client(okHttpClient)
+      .baseUrl(domain)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+      .create(WalletRefreshApi::class.java)
+  }
+
   @Singleton
   @Provides
   fun provideWalletAuthRepository(
     @ApplicationContext context: Context,
     userAuthApi: UserAuthApi,
+    walletRefreshApi: WalletRefreshApi,
     walletCoreDataSource: WalletCoreDataSource,
     userWalletAuthDataStore: UserWalletAuthDataStore
   ): WalletAuthRepository {
     return WalletAuthRepositoryImpl(
       context = context,
       userAuthApi = userAuthApi,
+      walletRefreshApi = walletRefreshApi,
       dispatcher = Dispatchers.IO,
       walletCoreDataSource = walletCoreDataSource,
       userWalletAuthDataStore = userWalletAuthDataStore
