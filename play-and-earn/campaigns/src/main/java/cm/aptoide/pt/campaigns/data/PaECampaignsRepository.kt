85775078dacfa198a0a3889827fdbb325eabb136
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-11-04 22:37:44 +0000

    [AND-590] Add cache then network strategy to app rewards

--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
@@ -1,11 +1,14 @@
 package cm.aptoide.pt.campaigns.data
 
-import cm.aptoide.pt.campaigns.domain.PaEMissions
 import cm.aptoide.pt.campaigns.domain.PaEBundles
+import cm.aptoide.pt.campaigns.domain.PaEMissions
+import kotlinx.coroutines.flow.Flow
 
 interface PaECampaignsRepository {
 
   suspend fun getCampaigns(): Result<PaEBundles>
 
   suspend fun getCampaignMissions(packageName: String): Result<PaEMissions>
+
+  fun observeCampaignMissions(packageName: String): Flow<Result<PaEMissions>>
 }
