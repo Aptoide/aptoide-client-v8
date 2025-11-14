9b53155de860b0224c652a285df6fd7707b1ac8b
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-11-14 12:31:51 +0000

    [AND-540] Add session sync logic to PaE foreground service

--- app-games/build.gradle.kts
+++ app-games/build.gradle.kts
@@ -319,11 +319,13 @@ dependencies {
   implementation(projects.featureBonus)
   implementation(projects.feautureAppComingSoon)
   implementation(projects.playAndEarn.campaigns)
+  implementation(projects.playAndEarn.sessions)
   implementation(projects.playAndEarn.exchange)
   implementation(projects.featureWallet.authorization)
   implementation(projects.featureWallet.walletInfo)
   implementation(projects.featureWallet.gamification)
   implementation(projects.featureWallet.datastore)
+  implementation(projects.featureUsageStats)
 
   //payments
   implementation(projects.payments.sdk)
