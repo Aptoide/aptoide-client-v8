8e975d5536b21fdf60657a2137b40c2ef0f52de6
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-11-14 00:04:24 +0000

    [AND-584] Add reload on resume on AppView rewards tab

--- app-games/src/main/java/com/aptoide/android/aptoidegames/play_and_earn/presentation/app_view/AppRewardsView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/play_and_earn/presentation/app_view/AppRewardsView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -26,6 +27,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import cm.aptoide.pt.campaigns.domain.PaEMission
 import cm.aptoide.pt.campaigns.domain.PaEMissionStatus
 import cm.aptoide.pt.campaigns.presentation.PaEMissionsUiState
@@ -44,7 +48,22 @@ import com.aptoide.android.aptoidegames.theme.Palette
 fun AppRewardsView(
   packageName: String,
 ) {
-  val missionsState = rememberPaEMissions(packageName)
+  val (missionsState, reload) = rememberPaEMissions(packageName)
+  val lifecycleOwner = LocalLifecycleOwner.current
+
+  DisposableEffect(lifecycleOwner) {
+    val observer = LifecycleEventObserver { _, event ->
+      if (event == Lifecycle.Event.ON_RESUME) {
+        if (missionsState != PaEMissionsUiState.Loading) {
+          reload()
+        }
+      }
+    }
+    lifecycleOwner.lifecycle.addObserver(observer)
+    onDispose {
+      lifecycleOwner.lifecycle.removeObserver(observer)
+    }
+  }
 
   when (missionsState) {
     is PaEMissionsUiState.Idle -> {
