bfe38aa6d1daebe358df9ef25f53c39d8f690386
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-11-14 17:51:50 +0000

    [AND-540] Remove active session in case of expired status

--- play-and-earn/sessions/src/main/java/cm/aptoide/pt/play_and_earn/sessions/data/SessionExceptions.kt
+++ play-and-earn/sessions/src/main/java/cm/aptoide/pt/play_and_earn/sessions/data/SessionExceptions.kt
@@ -2,4 +2,6 @@ package cm.aptoide.pt.play_and_earn.sessions.data
 
 class DuplicateOrOutOfOrderException(message: String) : Exception(message)
 
+class SessionExpiredException(message: String) : Exception(message)
+
 class AlreadyEndedException(message: String) : Exception(message)
