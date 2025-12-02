ffc91e9b5b7a7ab5342971b249181370d6afb387
Pedro Agostinho <pedro.agostinho@aptoide.com>
2025-12-02 19:54:02 +0000

    [AND-539] Extract PaE missions into separate repository

---
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaEMissionsRepository.kt
@@ -0,0 +1,14 @@
+package cm.aptoide.pt.campaigns.data
+
+import cm.aptoide.pt.campaigns.domain.PaEMissions
+import kotlinx.coroutines.flow.Flow
+
+interface PaEMissionsRepository {
+
+  suspend fun getCampaignMissions(
+    packageName: String,
+    forceRefresh: Boolean = false
+  ): Result<PaEMissions>
+
+  fun observeCampaignMissions(packageName: String): Flow<Result<PaEMissions>>
+}
