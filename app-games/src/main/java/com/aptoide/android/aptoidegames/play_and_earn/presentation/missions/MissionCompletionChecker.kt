2c1d1cbea7822f59cb9ae76e77c13de1ac658c58
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-01-08 14:59:08 +0000

    [AND-620] Move PaESession and SessionContext to domain layer

--- app-games/src/main/java/com/aptoide/android/aptoidegames/play_and_earn/presentation/missions/MissionCompletionChecker.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/play_and_earn/presentation/missions/MissionCompletionChecker.kt
@@ -1,7 +1,7 @@
 package com.aptoide.android.aptoidegames.play_and_earn.presentation.missions
 
 import cm.aptoide.pt.campaigns.domain.PaEMission
-import com.aptoide.android.aptoidegames.play_and_earn.presentation.sessions.SessionContext
+import com.aptoide.android.aptoidegames.play_and_earn.domain.sessions.SessionContext
 
 interface MissionCompletionChecker {
 
