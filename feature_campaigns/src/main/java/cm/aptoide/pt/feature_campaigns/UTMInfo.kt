c1ffa61a78695ca41550b13630250721e58b4d05
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-01-09 16:04:31 +0000

    [AND-343] Make UTM values nullable

--- feature_campaigns/src/main/java/cm/aptoide/pt/feature_campaigns/UTMInfo.kt
+++ feature_campaigns/src/main/java/cm/aptoide/pt/feature_campaigns/UTMInfo.kt
@@ -1,9 +1,9 @@
 package cm.aptoide.pt.feature_campaigns
 
 data class UTMInfo(
-  val utmSource: String = "aptoide",
-  val utmMedium: String,
-  val utmCampaign: String,
-  val utmContent: String,
+  val utmSource: String? = "aptoide",
+  val utmMedium: String? = null,
+  val utmCampaign: String? = null,
+  val utmContent: String? = null,
   val utmTerm: String? = null
 )
