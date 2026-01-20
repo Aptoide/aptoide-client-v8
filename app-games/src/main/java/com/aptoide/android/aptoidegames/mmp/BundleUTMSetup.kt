4d67420673cfdfbeac7164c15ea45d54ada2b266
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-01-20 12:26:51 +0000

    [AND-343] Add utm values for other apps group bundles

--- app-games/src/main/java/com/aptoide/android/aptoidegames/mmp/BundleUTMSetup.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/mmp/BundleUTMSetup.kt
@@ -2,10 +2,11 @@ package com.aptoide.android.aptoidegames.mmp
 
 data class UTMConfig(
   val homeContent: String,
-  val seeAllContent: String
+  val seeAllContent: String?
 )
 
 private const val MORE_SUFFIX = "-more"
+private const val APPS_GROUP_PREFIX = "apps-group-"
 
 fun getUTMConfig(bundleTag: String): UTMConfig? {
   val baseTag = bundleTag.removeSuffix(MORE_SUFFIX)
@@ -14,6 +15,14 @@ fun getUTMConfig(bundleTag: String): UTMConfig? {
     "apps-group-trending" -> UTMConfig("home-trending", "trending-seeall")
     "apps-group-editors-choice" -> UTMConfig("home-editors-choice", "editors-choice-seeall")
     "apps-group-featured-appcoins" -> UTMConfig("home-get-rewarded", "get-rewarded-seeall")
-    else -> null
+    "new-app" -> UTMConfig("home-new-app", null)
+    else -> {
+      if (baseTag.startsWith(APPS_GROUP_PREFIX)) {
+        val suffix = baseTag.removePrefix(APPS_GROUP_PREFIX)
+        UTMConfig("home-$suffix", "$suffix-seeall")
+      } else {
+        null
+      }
+    }
   }
 }
