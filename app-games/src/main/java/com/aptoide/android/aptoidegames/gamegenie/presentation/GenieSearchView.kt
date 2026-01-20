ce8c9c59e3f03398740c55e3f697cf1c43dd7a9e
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-01-20 17:32:31 +0000

    [AND-343] Add utm values to gamegenie

--- app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/GenieSearchView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/GenieSearchView.kt
@@ -7,6 +7,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import cm.aptoide.pt.extensions.ScreenData
 import com.aptoide.android.aptoidegames.analytics.presentation.withAnalytics
 import com.aptoide.android.aptoidegames.gamegenie.analytics.rememberGameGenieAnalytics
+import com.aptoide.android.aptoidegames.mmp.WithUTM
 
 const val genieSearchRoute = "genieSearch"
 
@@ -21,26 +22,33 @@ fun gameGenieSearchScreen() = ScreenData.withAnalytics(
   val analytics = rememberGameGenieAnalytics()
   val firstLoad by viewModel.firstLoad.collectAsState(true)
 
-  ConversationsDrawer(
-    mainScreen = {
-      ChatbotView(
-        firstLoad = firstLoad,
-        uiState = uiState,
-        navigateTo = navigate,
-        onError = viewModel::reload,
-        onMessageSend = { message,_ ->
-          viewModel.sendMessage(message)
-          analytics.sendGameGenieMessageSent()
-        },
-        setFirstLoadDone = viewModel::setFirstLoadDone,
-        onSuggestionSend = { message, index ->
-          viewModel.sendMessage(message)
-          analytics.sendGameGenieSuggestionClick(index)
-        }
-      )
-    },
-    loadConversationFn = viewModel::loadConversation,
-    currentChatId = uiState.chat.id,
-    newChatFn = viewModel::emptyChat,
-  )
+  WithUTM(
+    medium = "gamegenie",
+    campaign = "gamegenie",
+    content = "gamegenie-results",
+    navigate = navigate
+  ) { navigate ->
+    ConversationsDrawer(
+      mainScreen = {
+        ChatbotView(
+          firstLoad = firstLoad,
+          uiState = uiState,
+          navigateTo = navigate,
+          onError = viewModel::reload,
+          onMessageSend = { message, _ ->
+            viewModel.sendMessage(message)
+            analytics.sendGameGenieMessageSent()
+          },
+          setFirstLoadDone = viewModel::setFirstLoadDone,
+          onSuggestionSend = { message, index ->
+            viewModel.sendMessage(message)
+            analytics.sendGameGenieSuggestionClick(index)
+          }
+        )
+      },
+      loadConversationFn = viewModel::loadConversation,
+      currentChatId = uiState.chat.id,
+      newChatFn = viewModel::emptyChat,
+    )
+  }
 }
