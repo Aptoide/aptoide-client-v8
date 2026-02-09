c1bef6538864e4244d66f9a0d764a34d90ca99c0
phillipsgoncalves <filisgoncalves@gmail.com>
2026-02-09 19:41:11 +0000

    [AND-681] Implement experiment 8.2

--- app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/presentation/ApkfyUiState.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/apkfy/presentation/ApkfyUiState.kt
@@ -5,5 +5,5 @@ import cm.aptoide.pt.feature_apkfy.presentation.ApkfyData
 sealed class ApkfyUiState(val data: ApkfyData) {
   class Default(data: ApkfyData) : ApkfyUiState(data)
   class Baseline(data: ApkfyData) : ApkfyUiState(data)
-  class RobloxCompanionAppsVariant(data: ApkfyData) : ApkfyUiState(data)
+  class RobloxCompanionAppsVariant(data: ApkfyData, val autoOpenDefault: Boolean) : ApkfyUiState(data)
 }
