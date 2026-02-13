4c9054bd776a3d0c5c89e6f77dc2a6f7bf9cbe42
tomascarvalho7 <tomascarvalho7@gmail.com>
2026-02-13 15:33:18 +0000

    [AND-634] Add Keep annotations to fix conversations reloading in release

--- app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/data/database/model/UserMessageEntity.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/data/database/model/UserMessageEntity.kt
@@ -1,5 +1,8 @@
 package com.aptoide.android.aptoidegames.gamegenie.data.database.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class UserMessageEntity(
   val text: String,
   val image: String? = null,
