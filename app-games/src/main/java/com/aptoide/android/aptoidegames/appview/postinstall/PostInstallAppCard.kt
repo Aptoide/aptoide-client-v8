9bdfec8f543e3a70a4dd151d12c9a2edede063c8
phillipsgoncalves <filisgoncalves@gmail.com>
2026-02-20 10:23:36 +0000

    [AND-703] Fix click on install button starting an installation for the Post install bundle apps

--- app-games/src/main/java/com/aptoide/android/aptoidegames/appview/postinstall/PostInstallAppCard.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/appview/postinstall/PostInstallAppCard.kt
@@ -6,25 +6,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cm.aptoide.pt.extensions.PreviewDark
 import cm.aptoide.pt.feature_apps.data.App
 import cm.aptoide.pt.feature_apps.data.randomApp
+import com.aptoide.android.aptoidegames.R
+import com.aptoide.android.aptoidegames.design_system.PrimarySmallButton
 import com.aptoide.android.aptoidegames.drawables.icons.getBonusIconRight
 import com.aptoide.android.aptoidegames.installer.presentation.AppIconWProgress
-import com.aptoide.android.aptoidegames.installer.presentation.InstallViewShort
 import com.aptoide.android.aptoidegames.theme.AGTypography
 import com.aptoide.android.aptoidegames.theme.AptoideTheme
 import com.aptoide.android.aptoidegames.theme.Palette
@@ -35,50 +37,53 @@ fun PostInstallAppCard(
   onClick: () -> Unit,
   modifier: Modifier = Modifier,
 ) {
-  Column(
+  Box(
     modifier = modifier
       .semantics(mergeDescendants = true) { }
-      .clickable(onClick = onClick)
       .width(88.dp)
       .wrapContentSize(Alignment.TopCenter)
   ) {
-    Box(contentAlignment = Alignment.TopEnd) {
-      AppIconWProgress(
-        app = app,
-        contentDescription = null,
-        modifier = Modifier.size(88.dp),
-      )
-      if (app.isAppCoins) {
-        Image(
-          imageVector = getBonusIconRight(
-            iconColor = Palette.Primary,
-            outlineColor = Palette.Black,
-            backgroundColor = Palette.Secondary
-          ),
+    Column {
+      Box(contentAlignment = Alignment.TopEnd) {
+        AppIconWProgress(
+          app = app,
           contentDescription = null,
-          modifier = Modifier.size(32.dp),
+          modifier = Modifier.size(88.dp),
+        )
+        if (app.isAppCoins) {
+          Image(
+            imageVector = getBonusIconRight(
+              iconColor = Palette.Primary,
+              outlineColor = Palette.Black,
+              backgroundColor = Palette.Secondary
+            ),
+            contentDescription = null,
+            modifier = Modifier.size(32.dp),
+          )
+        }
+      }
+      Text(
+        text = app.name,
+        color = Palette.White,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis,
+        modifier = Modifier
+          .padding(top = 8.dp)
+          .defaultMinSize(minHeight = 36.dp),
+        style = AGTypography.DescriptionGames
+      )
+      Spacer(modifier = Modifier.height(8.dp))
+      Box(
+        modifier = Modifier.fillMaxWidth(),
+        propagateMinConstraints = true
+      ) {
+        PrimarySmallButton(
+          onClick = {},
+          title = stringResource(R.string.search_sponsored_install),
         )
       }
     }
-    Text(
-      text = app.name,
-      color = Palette.White,
-      maxLines = 2,
-      overflow = TextOverflow.Ellipsis,
-      modifier = Modifier
-        .padding(top = 8.dp)
-        .defaultMinSize(minHeight = 36.dp),
-      style = AGTypography.DescriptionGames
-    )
-    Spacer(modifier = Modifier.height(8.dp))
-    Box(
-      modifier = Modifier.fillMaxWidth(),
-      propagateMinConstraints = true
-    ) {
-      InstallViewShort(
-        app = app,
-      )
-    }
+    Box(modifier = Modifier.matchParentSize().clickable(onClick = onClick))
   }
 }
 
