2b5679951bc0134a3254bed19ab880ff561a86ad
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-04-01 14:04:10 +0100

    [AND-739] Remove payments related properties from Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,3 @@
-import org.gradle.kotlin.dsl.get
-
-val compileSdkVersion by extra { libs.versions.compileSdk.get() }
-val minSdkVersion by extra { libs.versions.minSdk.get() }
-
-// Billing SDK
-val supportedSdkVersion by extra { "3" }
-
-// Kotlin
-val coreKtxVersion by rootProject.extra { libs.versions.coreKtx.get() }
-val kotlinxCoroutinesAndroidVersion by rootProject.extra { libs.versions.coroutines.get() }
-
-// KSP
-val kspVersion by extra { libs.versions.kspPlugin.get() }
-
-// Hilt
-val hiltAndroidVersion by rootProject.extra { libs.versions.hilt.get() }
-val daggerHiltCompilerVersion by rootProject.extra { libs.versions.hilt.get() }
-
-// Apache
-val apacheCommonsTextVersion by rootProject.extra { libs.versions.apacheCommonsText.get() }
-
-// Android Material
-val androidxActivityVersion by rootProject.extra { libs.versions.activityKtx.get() }
-
-// Adyen
-val adyenVersion by rootProject.extra { libs.versions.adyenVersion.get() }
-
-// Compose
-val navigationComposeVersion by rootProject.extra { libs.versions.navigationCompose.get() }
-val hiltNavigationComposeVersion by rootProject.extra { libs.versions.hiltNavCompose.get() }
-val composeMaterialVersion by rootProject.extra { libs.versions.compose.get() }
-val lifecycleViewModelComposeVersion by rootProject.extra { libs.versions.viewmodelCompose.get() }
-
 buildscript {
   repositories {
     google()
