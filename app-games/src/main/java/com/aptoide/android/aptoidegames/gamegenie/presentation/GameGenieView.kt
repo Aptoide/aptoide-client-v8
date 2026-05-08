1b6691ab25ac0d683598631d23f3d9042a6c45c7
tomascarvalho7 <tomascarvalho7@gmail.com>
2026-05-08 12:17:16 +0100

    [AND-828] Fix infinite loading bug on GG New chat when on companion screen

--- app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/GameGenieView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/gamegenie/presentation/GameGenieView.kt
@@ -221,7 +221,10 @@ fun gameGenieScreen(
         viewModel.loadConversation(id)
       },
       currentChatId = uiState.chat.id,
-      newChatFn = viewModel::emptyChat,
+      newChatFn = {
+        chatMode = ChatMode.General
+        viewModel.emptyChat()
+      },
     )
   }
 }
