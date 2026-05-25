0be1982491284c35082f6c21b343a37aec92db9b
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-05-25 16:20:58 +0100

    [AND-840] Add Play and Earn sign-in only screen and route

--- app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
+++ app-games/src/main/java/com/aptoide/android/aptoidegames/home/MainView.kt
@@ -60,6 +60,7 @@ import com.aptoide.android.aptoidegames.play_and_earn.presentation.home.PaEHomeL
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.level_up.levelUpScreen
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.permissions.playAndEarnPermissionsScreen
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.rewards.playAndEarnRewardsScreen
+import com.aptoide.android.aptoidegames.play_and_earn.presentation.sign_in.playAndEarnSignInOnlyScreen
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.sign_in.playAndEarnSignInRoute
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.sign_in.playAndEarnSignInScreen
 import com.aptoide.android.aptoidegames.play_and_earn.presentation.unit_exchange.exchangeUnitsScreen
@@ -360,6 +361,12 @@ private fun NavigationGraph(
       },
       goBack = navController::navigateUp,
       screenData = playAndEarnSignInScreen()
+    )
+
+    animatedComposable(
+      navigate = navController::navigateTo,
+      goBack = navController::navigateUp,
+      screenData = playAndEarnSignInOnlyScreen()
     )
 
     animatedComposable(
