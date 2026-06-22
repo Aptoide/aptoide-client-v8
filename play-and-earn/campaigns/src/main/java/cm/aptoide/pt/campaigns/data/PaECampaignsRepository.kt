f701b3a8be979d095d64d8f8323f6fe45ae4ef59
Pedro Agostinho <pedro.agostinho@aptoide.com>
2026-06-22 13:05:54 +0100

    [AND-842] Save full PaE apps in cache

--- play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
+++ play-and-earn/campaigns/src/main/java/cm/aptoide/pt/campaigns/data/PaECampaignsRepository.kt
@@ -1,5 +1,6 @@
 package cm.aptoide.pt.campaigns.data
 
+import cm.aptoide.pt.campaigns.domain.PaEApp
 import cm.aptoide.pt.campaigns.domain.PaEBundles
 
 interface PaECampaignsRepository {
@@ -7,4 +8,6 @@ interface PaECampaignsRepository {
   suspend fun getCampaigns(): Result<PaEBundles>
 
   suspend fun getAvailablePackages(): Result<Set<String>>
+
+  suspend fun getCachedApp(packageName: String): Result<PaEApp?>
 }
